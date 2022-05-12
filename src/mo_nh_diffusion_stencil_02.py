5f46dfc89908aee7f73a3368f8dec41e2ba77320
samkellerhals <samuel.kellerhals@c2sm.ethz.ch>
2022-05-12 15:19:49 +0200

    Add mo_nh_diffusion_stencil_kh_c_gt4py

--- src/mo_nh_diffusion_stencil_02.py
+++ src/mo_nh_diffusion_stencil_02.py
@@ -14,7 +14,9 @@ from functional.iterator.embedded import (
 
 
 def mo_nh_diffusion_stencil_div_numpy(
-    c2e: np.array, vn: np.array, geofac_div: np.array
+    c2e: np.array,
+    vn: np.array,
+    geofac_div: np.array,
 ) -> np.array:
     """Numpy implementation of mo_nh_diffusion_stencil_02.py dusk stencil.
 
@@ -32,6 +34,29 @@ def mo_nh_diffusion_stencil_div_numpy(
     return div
 
 
+def mo_nh_diffusion_stencil_kh_c_numpy(
+    c2e: np.array,
+    kh_smag_ec: np.array,
+    e_bln_c_s: np.array,
+    diff_multfac_smag: np.array,
+):
+    """
+
+    Note:
+        Dusk implementation is the following
+
+        @stencil
+        def mo_nh_diffusion_stencil_02(kh_smag_ec: Field[Edge, K], e_bln_c_s: Field[Cell > Edge], diff_multfac_smag: Field[K], kh_c: Field[Cell, K]):
+        with domain.upward.across[nudging:halo]:
+            kh_c = sum_over(Cell > Edge, kh_smag_ec*e_bln_c_s)/diff_multfac_smag
+
+    """
+    mul = kh_smag_ec[c2e] * e_bln_c_s
+    summed = np.sum(mul, axis=-1)
+    kh_c = summed / diff_multfac_smag
+    return kh_c
+
+
 def mo_nh_diffusion_stencil_div_gt4py(
     c2e: np.array, vn: np.array, geofac_div: np.array
 ) -> np.array:
@@ -45,7 +70,7 @@ def mo_nh_diffusion_stencil_div_gt4py(
 
     vn_field = np_as_located_field(EdgeDim)(vn)
     geofac_div_field = np_as_located_field(CellDim, C2EDim)(geofac_div)
-    out = np_as_located_field(CellDim)(np.zeros(shape=(18,)))
+    out_field = np_as_located_field(CellDim)(np.zeros(shape=(18,)))
 
     @field_operator
     def sum_cell_edge_neighbors(
@@ -54,14 +79,58 @@ def mo_nh_diffusion_stencil_div_gt4py(
         return neighbor_sum(vn(C2E) * geofac_div, axis=C2EDim)
 
     @program
-    def sum_cell_edge_neighbors_program(
+    def exec_stencil(
         vn: Field[[EdgeDim], float32],
         geofac_div: Field[[CellDim, C2EDim], float32],
         out: Field[[CellDim], float32],
     ):
         sum_cell_edge_neighbors(vn, geofac_div, out=out)
 
-    sum_cell_edge_neighbors_program(
-        vn_field, geofac_div_field, out, offset_provider={"C2E": C2E_offset_provider}
+    exec_stencil(
+        vn_field, geofac_div_field, out_field, offset_provider={"C2E": C2E_offset_provider}
     )
-    return out
+    return out_field
+
+
+def mo_nh_diffusion_stencil_kh_c_gt4py(
+    c2e: np.array,
+    kh_smag_ec: np.array,
+    e_bln_c_s: np.array,
+    diff_multfac_smag: np.array,
+):
+    """GT4PY implementation of mo_nh_diffusion_stencil_02.py dusk stencil."""
+    KDim = Dimension("K")
+    EdgeDim = Dimension("Edge")
+    CellDim = Dimension("Cell")
+    C2EDim = Dimension("C2E", True)  # special local dim
+
+    C2E = FieldOffset("C2E", source=EdgeDim, target=(CellDim, C2EDim))
+    C2E_offset_provider = NeighborTableOffsetProvider(c2e, CellDim, EdgeDim, 3)
+
+    kh_smag_ec_field = np_as_located_field(EdgeDim)(kh_smag_ec)
+    e_bln_c_s_field = np_as_located_field(CellDim, C2EDim)(e_bln_c_s)
+    diff_multfac_smag_field = np_as_located_field(CellDim)(diff_multfac_smag)
+    out_field = np_as_located_field(CellDim)(np.zeros(shape=(18,)))
+
+    @field_operator
+    def sum_edge_neighbors_and_divide(
+        kh_smag_ec: Field[[EdgeDim], float32],
+        e_bln_c_s: Field[[CellDim, C2EDim], float32],
+        diff_multfac_smag: Field[[CellDim], float32]
+    ) -> Field[[CellDim], float32]:
+        summed = neighbor_sum(kh_smag_ec(C2E) * e_bln_c_s, axis=C2EDim)
+        divided = summed / diff_multfac_smag
+        return divided
+
+    @program
+    def exec_stencil(
+            kh_smag_ec: Field[[EdgeDim], float32],
+            e_bln_c_s: Field[[CellDim, C2EDim], float32],
+            diff_multfac_smag: Field[[CellDim], float32],
+            out: Field[[CellDim], float32],
+    ):
+        sum_edge_neighbors_and_divide(kh_smag_ec, e_bln_c_s, diff_multfac_smag, out=out)
+
+    exec_stencil(kh_smag_ec_field, e_bln_c_s_field, diff_multfac_smag_field, out_field, offset_provider={"C2E": C2E_offset_provider})
+
+    return out_field
--- tests/mo_nh_diffusion_stencil_02_test.py
+++ tests/mo_nh_diffusion_stencil_02_test.py
@@ -3,8 +3,10 @@ import numpy as np
 from src.mo_nh_diffusion_stencil_02 import (
     mo_nh_diffusion_stencil_div_numpy,
     mo_nh_diffusion_stencil_div_gt4py,
+    mo_nh_diffusion_stencil_kh_c_numpy,
+    mo_nh_diffusion_stencil_kh_c_gt4py
 )
-from .simple_mesh import cell_to_edge_table, n_edges
+from .simple_mesh import cell_to_edge_table, n_edges, n_cells
 
 
 def test_mo_nh_diffusion_stencil_div_equality():
@@ -12,10 +14,17 @@ def test_mo_nh_diffusion_stencil_div_equality():
     geofac_div = np.random.randn(*cell_to_edge_table.shape)
 
     ref = mo_nh_diffusion_stencil_div_numpy(cell_to_edge_table, vn, geofac_div)
-    out = mo_nh_diffusion_stencil_div_gt4py(cell_to_edge_table, vn, geofac_div)
-    assert all(np.isclose(np.asarray(out), ref))
+    out = np.asarray(mo_nh_diffusion_stencil_div_gt4py(cell_to_edge_table, vn, geofac_div))
+    assert all(np.isclose(out, ref))
 
 
-def test_mo_nh_diffusion_stencil_div_equality():
-    # TODO
-    pass
+def test_mo_nh_diffusion_stencil_kh_c_equality():
+    scalar_value = 2
+    c2e_shape = cell_to_edge_table.shape
+    kh_smag_ec = np.random.randn(n_edges)
+    e_bln_c_s = np.random.randn(*c2e_shape)
+    diff_multfac_smag = np.asarray([scalar_value]*c2e_shape[0])
+
+    ref = mo_nh_diffusion_stencil_kh_c_numpy(cell_to_edge_table, kh_smag_ec, e_bln_c_s, diff_multfac_smag)
+    out = np.asarray(mo_nh_diffusion_stencil_kh_c_gt4py(cell_to_edge_table, kh_smag_ec, e_bln_c_s, diff_multfac_smag))
+    assert all(np.isclose(out, ref))
