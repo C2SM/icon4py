c40f2f3dac26ca0779b572a296bf12988fb87b56
samkellerhals <samuel.kellerhals@c2sm.ethz.ch>
2022-05-17 10:13:26 +0200

    Remove add_kdim as it is unused

--- tests/utils.py
+++ tests/utils.py
@@ -14,19 +14,6 @@ def zero_field(mesh, *dims):
     )
 
 
-def add_kdim(arr: np.array, k: int) -> np.array:
-    """Adds a k dimension to a numpy array.
-
-    Args:
-        arr: numpy array.
-        k: number of k levels.
-
-    Returns:
-        Array with added k dimension.
-    """
-    return np.repeat(arr[None, :], k, axis=0).T
-
-
 def get_cell_to_k_table(k_arr, k):
     """Creates cell to k table based on an input array and k value.
 
