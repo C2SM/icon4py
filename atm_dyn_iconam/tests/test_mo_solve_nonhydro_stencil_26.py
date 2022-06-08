6e29676ae72cca33ec6895ac503df0f7d8c9ee5d
nfarabullini <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>
2022-06-08 10:37:42 +0200

    mo_solve_nonhydro_stencil_26

---
+++ atm_dyn_iconam/tests/test_mo_solve_nonhydro_stencil_26.py
@@ -0,0 +1,47 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import numpy as np
+
+from icon4py.atm_dyn_iconam.mo_solve_nonhydro_stencil_26 import (
+    mo_solve_nonhydro_stencil_26,
+)
+from icon4py.common.dimension import EdgeDim, KDim
+from icon4py.testutils.simple_mesh import SimpleMesh
+from icon4py.testutils.utils import random_field
+
+
+def mo_solve_nonhydro_stencil_26_numpy(
+    z_graddiv_vn: np.array, vn: np.array, scal_divdamp_o2
+) -> np.array:
+    vn = vn + (scal_divdamp_o2 * z_graddiv_vn)
+    return vn
+
+
+def test_mo_solve_nonhydro_stencil_26():
+    mesh = SimpleMesh()
+
+    z_graddiv_vn = random_field(mesh, EdgeDim, KDim)
+    vn = random_field(mesh, EdgeDim, KDim)
+    scal_divdamp_o2 = np.float64(5.0)
+
+    ref = mo_solve_nonhydro_stencil_26_numpy(
+        np.asarray(z_graddiv_vn), np.asarray(vn), scal_divdamp_o2
+    )
+    mo_solve_nonhydro_stencil_26(
+        z_graddiv_vn,
+        vn,
+        scal_divdamp_o2,
+        offset_provider={},
+    )
+    assert np.allclose(vn, ref)
