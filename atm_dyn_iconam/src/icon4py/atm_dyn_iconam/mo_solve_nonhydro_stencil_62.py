dfeb30e24bdd3aa8d672135c5718a43f6363ce91
samkellerhals <kellerhalssamuel@gmail.com>
2022-06-08 14:00:39 +0200

    Add mo_solve_nonhydro_stencil_62

---
+++ atm_dyn_iconam/src/icon4py/atm_dyn_iconam/mo_solve_nonhydro_stencil_62.py
@@ -0,0 +1,37 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from functional.ffront.decorator import field_operator, program
+from functional.ffront.fbuiltins import Field
+
+from icon4py.common.dimension import CellDim, KDim
+
+
+@field_operator
+def _mo_solve_nonhydro_stencil_62(
+    w_now: Field[[CellDim, KDim], float],
+    grf_tend_w: Field[[CellDim, KDim], float],
+    dtime: float,
+) -> Field[[CellDim, KDim], float]:
+    w_new = w_now + dtime * grf_tend_w
+    return w_new
+
+
+@program
+def mo_solve_nonhydro_stencil_62(
+    w_now: Field[[CellDim, KDim], float],
+    grf_tend_w: Field[[CellDim, KDim], float],
+    dtime: float,
+    w_new: Field[[CellDim, KDim], float],
+):
+    _mo_solve_nonhydro_stencil_62(w_now, grf_tend_w, dtime, out=w_new)
