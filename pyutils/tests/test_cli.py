dab6af303edb800f9129d0b14f23a5937e0b274c
samkellerhals <kellerhalssamuel@gmail.com>
2022-06-15 17:42:50 +0200

    use isolated filesystem for tests

--- pyutils/tests/test_cli.py
+++ pyutils/tests/test_cli.py
@@ -11,6 +11,8 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import os
+
 import pytest
 from click.testing import CliRunner
 
@@ -42,6 +44,7 @@ def test_codegen_single_fo(cli):
     assert CPP_HEADERS in result.output
 
 
+# TODO: mark parametrise with different stencils.
 def test_codegen_multiple_fo(cli):
     stencil_name = "mo_velocity_advection_stencil_05"
     module_path = get_module_path("atm_dyn_iconam", stencil_name)
@@ -50,17 +53,25 @@ def test_codegen_multiple_fo(cli):
     assert CPP_HEADERS in result.output
 
 
+# TODO: mark parametrise with different stencils.
 def test_metadatagen_multiple_fo(cli):
     stencil_name = "mo_velocity_advection_stencil_05"
+    fname = f"{stencil_name}.dat"
     module_path = get_module_path("atm_dyn_iconam", stencil_name)
-    # TODO: use isolated filesystem and check for correct metadata
-    result = cli.invoke(main, [module_path, "--output-metadata", f"{stencil_name}.dat"])
-    assert result.exit_code == 0
+
+    with cli.isolated_filesystem():
+        result = cli.invoke(
+            main, [module_path, "--output-metadata", f"{stencil_name}.dat"]
+        )
+        assert result.exit_code == 0
+        assert fname in os.listdir(os.getcwd())
 
 
 def test_metadatagen_single_fo(cli):
     stencil_name = "mo_nh_diffusion_stencil_06"
+    fname = f"{stencil_name}.dat"
     module_path = get_module_path("atm_dyn_iconam", stencil_name)
-    # TODO: use isolated filesystem and check for correct metadata
-    result = cli.invoke(main, [module_path, "--output-metadata", f"{stencil_name}.dat"])
-    assert result.exit_code == 0
+    with cli.isolated_filesystem():
+        result = cli.invoke(main, [module_path, "--output-metadata", fname])
+        assert result.exit_code == 0
+        assert fname in os.listdir(os.getcwd())
