28d75971504f285562b7c2f05d966ef8bd824499
Abishek Gopal <abishekg@guests-hpx-1-173.intern.ethz.ch>
2022-06-16 15:12:38 +0200

    mo_solve_nonhydro_stencil_03 after pre-commit

--- atm_dyn_iconam/tests/test_mo_solve_nonhydro_stencil_03.py
+++ atm_dyn_iconam/tests/test_mo_solve_nonhydro_stencil_03.py
@@ -1,3 +1,16 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 import numpy as np
 
 from icon4py.atm_dyn_iconam.mo_solve_nonhydro_stencil_03 import (
@@ -9,7 +22,7 @@ from icon4py.testutils.utils import random_field
 
 
 def mo_solve_nonhydro_stencil_03_numpy(
-  z_exner_ex_pr: np.array,
+    z_exner_ex_pr: np.array,
 ):
     z_exner_ex_pr = np.zeros_like(z_exner_ex_pr)
     return z_exner_ex_pr
@@ -20,7 +33,6 @@ def test_mo_solve_nonhydro_stencil_03():
     z_exner_ex_pr = random_field(mesh, CellDim, KDim)
 
     ref = mo_solve_nonhydro_stencil_03_numpy(
-
         np.asarray(z_exner_ex_pr),
     )
 
