87e2ebd8c6365cb748b2f0df91615ba921b0e41b
nfarabullini <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>
2022-07-04 11:53:09 +0200

    mo_nh_diffusion_stencil_16, mo_nh_diffusion_stencil_08, mo_intp_rbf_rbf_vec_interpol_vertex_dom_b

---
+++ atm_dyn_iconam/tests/test_mo_nh_diffusion_stencil_16.py
@@ -0,0 +1,62 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import numpy as np
+
+from icon4py.atm_dyn_iconam.mo_nh_diffusion_stencil_16 import (
+    mo_nh_diffusion_stencil_16,
+)
+from icon4py.common.dimension import CellDim, KDim
+from icon4py.testutils.simple_mesh import SimpleMesh
+from icon4py.testutils.utils import random_field
+
+
+def mo_nh_diffusion_stencil_16_numpy(
+    z_temp: np.array,
+    area: np.array,
+    theta_v: np.array,
+    exner: np.array,
+    rd_o_cvd,
+) -> np.array:
+    area = np.expand_dims(area, axis=0)
+    z_theta = theta_v
+    theta_v = theta_v + (area * z_temp)
+    exner = exner * (1.0 + rd_o_cvd * (theta_v / z_theta - 1.0))
+    return exner
+
+
+def test_mo_nh_diffusion_stencil_16():
+    mesh = SimpleMesh()
+
+    z_temp = random_field(mesh, CellDim, KDim)
+    area = random_field(mesh, KDim)
+    theta_v = random_field(mesh, CellDim, KDim)
+    exner = random_field(mesh, CellDim, KDim)
+    rd_o_cvd = np.float64(5.0)
+
+    ref = mo_nh_diffusion_stencil_16_numpy(
+        np.asarray(z_temp),
+        np.asarray(area),
+        np.asarray(theta_v),
+        np.asarray(exner),
+        rd_o_cvd,
+    )
+    mo_nh_diffusion_stencil_16(
+        z_temp,
+        area,
+        theta_v,
+        exner,
+        rd_o_cvd,
+        offset_provider={},
+    )
+    assert np.allclose(exner, ref)
