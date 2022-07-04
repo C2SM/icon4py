84bb74bde761cea6d7d51431c7e348af42751704
nfarabullini <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>
2022-07-04 13:37:47 +0200

    mo_solve_nonhydro_stencil_32, mo_solve_nonhydro_stencil_33

---
+++ atm_dyn_iconam/tests/test_mo_solve_nonhydro_stencil_32.py
@@ -0,0 +1,81 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import numpy as np
+
+from icon4py.atm_dyn_iconam.mo_solve_nonhydro_stencil_32 import (
+    mo_solve_nonhydro_stencil_32,
+)
+from icon4py.common.dimension import EdgeDim, KDim
+from icon4py.testutils.simple_mesh import SimpleMesh
+from icon4py.testutils.utils import random_field, zero_field
+
+
+def mo_solve_nonhydro_stencil_32_mass_fl_e_numpy(
+    z_rho_e: np.array, z_vn_avg: np.array, ddqz_z_full_e: np.array
+) -> np.array:
+    mass_fl_e = z_rho_e * z_vn_avg * ddqz_z_full_e
+    return mass_fl_e
+
+
+def mo_solve_nonhydro_stencil_32_z_theta_v_fl_e_numpy(
+    mass_fl_e: np.array, z_theta_v_e: np.array
+) -> np.array:
+    z_theta_v_fl_e = mass_fl_e * z_theta_v_e
+    return z_theta_v_fl_e
+
+
+def mo_solve_nonhydro_stencil_32_numpy(
+    z_rho_e: np.array,
+    z_vn_avg: np.array,
+    ddqz_z_full_e: np.array,
+    z_theta_v_e: np.array,
+):
+    mass_fl_e = mo_solve_nonhydro_stencil_32_mass_fl_e_numpy(
+        z_rho_e, z_vn_avg, ddqz_z_full_e
+    )
+    z_theta_v_fl_e = mo_solve_nonhydro_stencil_32_z_theta_v_fl_e_numpy(
+        mass_fl_e, z_theta_v_e
+    )
+    return mass_fl_e, z_theta_v_fl_e
+
+
+def test_mo_solve_nonhydro_stencil_32():
+    mesh = SimpleMesh()
+
+    z_rho_e = random_field(mesh, EdgeDim, KDim)
+    z_vn_avg = random_field(mesh, EdgeDim, KDim)
+    ddqz_z_full_e = random_field(mesh, EdgeDim, KDim)
+    mass_fl_e = random_field(mesh, EdgeDim, KDim)
+    z_theta_v_e = random_field(mesh, EdgeDim, KDim)
+    z_theta_v_fl_e = zero_field(mesh, EdgeDim, KDim)
+
+    mass_fl_e_ref, z_theta_v_fl_e_ref = mo_solve_nonhydro_stencil_32_numpy(
+        np.asarray(z_rho_e),
+        np.asarray(z_vn_avg),
+        np.asarray(ddqz_z_full_e),
+        np.asarray(z_theta_v_e),
+    )
+
+    mo_solve_nonhydro_stencil_32(
+        z_rho_e,
+        z_vn_avg,
+        ddqz_z_full_e,
+        z_theta_v_e,
+        mass_fl_e,
+        z_theta_v_fl_e,
+        offset_provider={},
+    )
+
+    assert np.allclose(mass_fl_e, mass_fl_e_ref)
+    assert np.allclose(z_theta_v_fl_e, z_theta_v_fl_e_ref)
---
+++ atm_dyn_iconam/tests/test_mo_solve_nonhydro_stencil_33.py
@@ -0,0 +1,56 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import numpy as np
+
+from icon4py.atm_dyn_iconam.mo_solve_nonhydro_stencil_33 import (
+    mo_solve_nonhydro_stencil_33,
+)
+from icon4py.common.dimension import EdgeDim, KDim
+from icon4py.testutils.simple_mesh import SimpleMesh
+from icon4py.testutils.utils import zero_field
+
+
+def mo_solve_nonhydro_stencil_33_vn_traj_numpy(vn_traj: np.array) -> np.array:
+    vn_traj = np.zeros_like(vn_traj)
+    return vn_traj
+
+
+def mo_solve_nonhydro_stencil_33_mass_flx_me_numpy(mass_flx_me: np.array) -> np.array:
+    mass_flx_me = np.zeros_like(mass_flx_me)
+    return mass_flx_me
+
+
+def mo_solve_nonhydro_stencil_33_numpy(vn_traj: np.array, mass_flx_me: np.array):
+    vn_traj = mo_solve_nonhydro_stencil_33_vn_traj_numpy(vn_traj)
+    mass_flx_me = mo_solve_nonhydro_stencil_33_mass_flx_me_numpy(mass_flx_me)
+    return vn_traj, mass_flx_me
+
+
+def test_mo_solve_nonhydro_stencil_33():
+    mesh = SimpleMesh()
+
+    vn_traj = zero_field(mesh, EdgeDim, KDim)
+    mass_flx_me = zero_field(mesh, EdgeDim, KDim)
+
+    vn_traj_ref, mass_flx_me_ref = mo_solve_nonhydro_stencil_33_numpy(
+        np.asarray(vn_traj), np.asarray(mass_flx_me)
+    )
+    mo_solve_nonhydro_stencil_33(
+        vn_traj,
+        mass_flx_me,
+        offset_provider={},
+    )
+
+    assert np.allclose(vn_traj, vn_traj_ref)
+    assert np.allclose(mass_flx_me, mass_flx_me_ref)
