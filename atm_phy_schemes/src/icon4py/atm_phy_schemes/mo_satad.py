7ad531f31b5eebb00516b89af05163e958786ff9
nfarabullini <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>
2022-07-20 13:58:37 +0200

    Added TODOs

--- atm_phy_schemes/src/icon4py/atm_phy_schemes/mo_satad.py
+++ atm_phy_schemes/src/icon4py/atm_phy_schemes/mo_satad.py
@@ -11,8 +11,9 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-""" 
-Changes:
+"""
+Changes.
+
 - Only implemented Tetens (ipsat = 1). Dropped Murphy-Koop.
 - Harmonized name of constants
 - Only implementend gpu version. Maybe further optimizations possible for CPU (check original code)
@@ -35,14 +36,27 @@ Nice to have:
 from functional.ffront.decorator import field_operator, program
 from functional.ffront.fbuiltins import Field
 
+from icon4py.atm_phy_schemes.mo_convect_tables import (
+    c1es,
+    c3les,
+    c4les,
+    c5les,
+)
+from icon4py.common.dimension import CellDim, KDim
+from icon4py.shared.mo_physical_constants import (
+    alv,
+    clw,
+    cvd,
+    rv,
+    tmelt,
+)
+
+
 # from functional.ffront.fbuiltins import exp #TODO: Requires math builtins
 
-from icon4py.common.dimension import CellDim, KDim
-
-from icon4py.shared.mo_physical_constants import rv, cvd, clw, alv, tmelt
-from icon4py.atm_phy_schemes.mo_convect_tables import c1es, c3les, c4les, c5les
-
+# TODO: flake8 has specific requirements for docstrings, I changed the format so that I could commit but the phrasing/grammar needs to be adapted
 # TODO: Local constants What to do with these?
+# TODO: as of right now, constants need to be imported in field_operator(s) and program(s)
 cp_v = 1850.0  # specific heat of water vapor at constant pressure (Landolt-Bornstein)
 ci = 2108.0  # specific heat of ice
 
@@ -50,14 +64,16 @@ tol = 1e-3
 maxiter = 10
 zqwmin = 1e-20
 
+# TODO: field operators need a return type
+
 
 @field_operator
 def _latent_heat_vaporization(t: Field[[CellDim, KDim], float]):
     """
-    Description:
+    Provide description.
+
     Latent heat of vaporization as internal energy and taking into account Kirchoff's relations
     """
-
     return alv + (cp_v - clw) * (t - tmelt) - rv * t
 
 
@@ -69,16 +85,17 @@ def _sat_pres_water(t: Field[[CellDim, KDim], float]):
     )  # DL: TODO swicth back once math-builtins available in GT4Py
 
 
-#   return c1es * exp(c3les * (t - tmelt) / (t - c4les))
+# comment: return c1es * exp(c3les * (t - tmelt) / (t - c4les))
 
 
 @field_operator
 def _qsat_rho(t: Field[[CellDim, KDim], float], rho: Field[[CellDim, KDim], float]):
     """
-    Specific humidity at water saturation (with respect to flat surface)
+    Provide description.
+
+    Specific humidity at water saturation (with respect to flat surface).
     depending on the temperature "t" and the total density "rhotot")
     """
-
     return _sat_pres_water(t) / (rho * rv * t)
 
 
@@ -87,25 +104,29 @@ def _dqsatdT_rho(
     t: Field[[CellDim, KDim], float], zqsat: Field[[CellDim, KDim], float]
 ):
     """
+    Provide description.
+
     Partial derivative of the specific humidity at water saturation with
     respect to the temperature at constant total density. Depends on
     temperature "t" and the saturation specific humidity "zqsat".
     """
-
     beta = c5les / (t - c4les) ** 2 - 1.0 / t
     return beta * zqsat
 
 
+# TODO: in case that multiple fields are returned by a field_operator, the return type needs to be a tuple
 @field_operator
 def _satad(
     qv: Field[[CellDim, KDim], float],
     qc: Field[[CellDim, KDim], float],
     t: Field[[CellDim, KDim], float],
     rho: Field[[CellDim, KDim], float],
+    # TODO: qv_new is not used here, perhaps it should be removed (?)
     qv_new: Field[[CellDim, KDim], float],
 ) -> Field[[CellDim, KDim], float]:
     """
-    Description:
+    Provide description.
+
     Saturation adjustment condenses/evaporates specific humidity (qv) into/from
     cloud water content (qc) such that a gridpoint is just saturated. Temperature (t)
     is adapted accordingly and pressure adapts itself in ICON.
@@ -118,12 +139,12 @@ def _satad(
 
     Originally inspirered from satad_v_3D_gpu of ICON release 2.6.4.
     """
-
     # TODO: Inline below??
     lwdocvd = _latent_heat_vaporization(t) / cvd
 
     # check, which points will still be subsaturated even after evaporating
     # all cloud water.For these gridpoints Newton iteration is not necessary.
+    # TODO: to implement conditionals in gt4py, the 'where' builtin has to be used
     if qv + qc <= _qsat_rho(t, rho):
         # All the cloud water is evaporated, but (sub)saturation remains .
         # No iteration of temperature needed.
@@ -146,6 +167,7 @@ def _satad(
 
             # Helpers (TODO refactor and remove?)
             qwd = _qsat_rho(t, rho)
+            # TODO: _dqsatdT_rho has two input fields in its definition, however 3 are given here
             dqwd = _dqsatdT_rho(qwd, twork, tmelt)
 
             # Newton
@@ -164,6 +186,7 @@ def _satad(
     return qv, qc, t
 
 
+# TODO: for now, programs cannot return multiple fields as a tuple, however it is possible to call multiple field_operators from the same program to return such fields
 @program
 def satad(
     qv: Field[[CellDim, KDim], float],
