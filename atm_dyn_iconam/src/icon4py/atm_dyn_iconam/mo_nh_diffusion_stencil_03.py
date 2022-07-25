dcbdcb99fae15d986a3332f799b9773e3c1cb61b
mroethlin <Matthias.Roethlin@MeteoSwiss.ch>
2022-07-25 16:09:19 +0200

    attempt at doing k offsets

---
+++ atm_dyn_iconam/src/icon4py/atm_dyn_iconam/mo_nh_diffusion_stencil_03.py
@@ -0,0 +1,62 @@
+# from dusk.script import *
+
+# lb, nudging, interior, halo, end = HorizontalDomains(0, 1000, 2000, 3000, 4000)
+
+# @stencil
+# def mo_nh_diffusion_stencil_03(div: Field[Cell, K], kh_c: Field[Cell, K], wgtfac_c: Field[Cell, K],
+#         div_ic: Field[Cell, K], hdef_ic: Field[Cell, K]):
+#     with domain.upward[1:].across[nudging:halo] as k:
+#         div_ic  =  wgtfac_c*div  + (1. - wgtfac_c)*div[k-1]
+#         hdef_ic = (wgtfac_c*kh_c + (1. - wgtfac_c)*kh_c[k-1])**2
+
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from functional.ffront.decorator import field_operator, program
+from functional.ffront.fbuiltins import Field, FieldOffset
+
+from icon4py.common.dimension import CellDim, KDim
+
+Koff = FieldOffset("Koff", source=KDim, target=(KDim,))
+
+
+@field_operator
+def _mo_nh_diffusion_stencil_03_div_ic(
+    wgtfac_c: Field[[CellDim, KDim], float],
+    div: Field[[CellDim, KDim], float],
+) -> Field[[CellDim, KDim], float]:
+    div_ic = wgtfac_c * div + (1.0 - wgtfac_c) * div(Koff[-1])
+    # div_ic = wgtfac_c * div + (1.0 - wgtfac_c) * div
+    return div_ic
+
+
+@field_operator
+def _mo_nh_diffusion_stencil_03_hdef_ic(
+    wgtfac_c: Field[[CellDim, KDim], float],
+    kh_c: Field[[CellDim, KDim], float],
+) -> Field[[CellDim, KDim], float]:
+    hdef_ic = wgtfac_c * kh_c + (1.0 - wgtfac_c) * kh_c(Koff[-1])
+    # hdef_ic = wgtfac_c * kh_c + (1.0 - wgtfac_c) * kh_c
+    return hdef_ic
+
+
+@program
+def mo_nh_diffusion_stencil_03(
+    wgtfac_c: Field[[CellDim, KDim], float],
+    kh_c: Field[[CellDim, KDim], float],
+    div: Field[[CellDim, KDim], float],
+    div_ic: Field[[CellDim, KDim], float],
+    hdef_ic: Field[[CellDim, KDim], float],
+):
+    _mo_nh_diffusion_stencil_03_div_ic(wgtfac_c, div, out=div_ic)
+    _mo_nh_diffusion_stencil_03_hdef_ic(wgtfac_c, kh_c, out=hdef_ic)
---
+++ atm_dyn_iconam/tests/test_mo_nh_diffusion_stencil_03.py
@@ -0,0 +1,86 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import numpy as np
+
+from icon4py.atm_dyn_iconam.mo_nh_diffusion_stencil_03 import (
+    mo_nh_diffusion_stencil_03,
+)
+from icon4py.common.dimension import CellDim, KDim
+from icon4py.testutils.simple_mesh import SimpleMesh
+from icon4py.testutils.utils import random_field, zero_field
+
+# if "Koff[-1]" is removed from `mo_nh_diffusion_stencil_03` the lower two versions
+# verify -> problem is indeed with offset
+def mo_nh_diffusion_stencil_03_div_ic_numpy(
+    wgtfac_c: np.array,
+    div: np.array,
+) -> np.array:
+    div_ic = wgtfac_c[:, 1:] * div[:, 1:] + (1.0 - wgtfac_c[:, 1:]) * div[:, :-1]
+    # div_ic = wgtfac_c[:, 1:] * div[:, 1:] + (1.0 - wgtfac_c[:, 1:]) * div[:, 1:]
+    # div_ic = wgtfac_c * div + (1.0 - wgtfac_c) * div
+    return div_ic
+
+
+# if "Koff[-1]" is removed from `mo_nh_diffusion_stencil_03` the lower two versions
+# verify -> problem is indeed with offset
+def mo_nh_diffusion_stencil_03_hdef_ic_numpy(
+    wgtfac_c: np.array,
+    k_hc: np.array,
+) -> np.array:
+    hdef_ic = wgtfac_c[:, 1:] * k_hc[:, 1:] + (1.0 - wgtfac_c[:, 1:]) * k_hc[:, :-1]
+    # hdef_ic = wgtfac_c[:, 1:] * k_hc[:, 1:] + (1.0 - wgtfac_c[:, 1:]) * k_hc[:, 1:]
+    # hdef_ic = wgtfac_c * k_hc + (1.0 - wgtfac_c) * k_hc
+    return hdef_ic
+
+
+def mo_nh_diffusion_stencil_03_numpy(
+    wgtfac_c: np.array,
+    div: np.array,
+    k_hc: np.array,
+):
+    div_ic = mo_nh_diffusion_stencil_03_div_ic_numpy(wgtfac_c, div)
+    hdef_ic = mo_nh_diffusion_stencil_03_hdef_ic_numpy(wgtfac_c, k_hc)
+    return div_ic, hdef_ic
+
+
+def test_mo_nh_diffusion_stencil_03():
+    mesh = SimpleMesh()
+
+    wgtfac_c = random_field(mesh, CellDim, KDim)
+    div = random_field(mesh, CellDim, KDim)
+    k_hc = random_field(mesh, CellDim, KDim)
+
+    div_ic = zero_field(mesh, CellDim, KDim)
+    hdef_ic = zero_field(mesh, CellDim, KDim)
+
+    div_ref, kh_c_ref = mo_nh_diffusion_stencil_03_numpy(
+        np.asarray(wgtfac_c),
+        np.asarray(div),
+        np.asarray(k_hc),
+    )
+
+    mo_nh_diffusion_stencil_03(
+        wgtfac_c,
+        k_hc,
+        div,
+        div_ic,
+        hdef_ic,
+        offset_provider={"Koff": KDim},
+    )
+
+    print(k_hc[:, 5])  # completely different
+    print(kh_c_ref[:, 5])
+
+    assert np.allclose(k_hc[:, 1:], kh_c_ref)
+    assert np.allclose(div[:, 1:], div_ref)
