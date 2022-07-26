2b075693e2b387f71533247b9b129349bd569d7e
abishekg7 <56273301+abishekg7@users.noreply.github.com>
2022-07-26 09:23:22 +0200

    Two new stencils (builtins) + new random number generator (#46)
    * "Adding mo_solve_nonhydro_stencils 66 and 67. 67 required postive field values, hence a new random field based on a uniform distribution with min, max bounds is proposed here."
    
    Not using latest multiple field operators syntax as math builtins aren't merged with it yet. 
    
    * Adding type hints and docstring for random_field utility. Modifying return type hints of two stencils to Tuple[np.array]
    
    * Replacing Tuple from typing with inbuilt tuple. Found/fixed a bug in test_mo_solve_nonhydro_stencil_66, after using random_mask
    
    * Removing np.float64 typecasts for constants.

---
+++ atm_dyn_iconam/tests/test_mo_solve_nonhydro_stencil_66.py
@@ -0,0 +1,96 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import numpy as np
+
+from icon4py.atm_dyn_iconam.mo_solve_nonhydro_stencil_66 import (
+    mo_solve_nonhydro_stencil_66,
+)
+from icon4py.common.dimension import CellDim, KDim
+from icon4py.testutils.simple_mesh import SimpleMesh
+from icon4py.testutils.utils import random_field, random_mask
+
+
+def mo_solve_nonhydro_stencil_66_theta_v_numpy(
+    bdy_halo_c: np.array,
+    theta_v: np.array,
+    exner: np.array,
+) -> np.array:
+
+    theta_v = np.where(bdy_halo_c == 1, exner, theta_v)
+    return theta_v
+
+
+def mo_solve_nonhydro_stencil_66_exner_numpy(
+    bdy_halo_c: np.array,
+    rho: np.array,
+    exner: np.array,
+    rd_o_cvd: float,
+    rd_o_p0ref: float,
+) -> np.array:
+
+    exner = np.where(
+        bdy_halo_c == 1, np.exp(rd_o_cvd * np.log(rd_o_p0ref * rho * exner)), exner
+    )
+    return exner
+
+
+def mo_solve_nonhydro_stencil_66_numpy(
+    bdy_halo_c: np.array,
+    rho: np.array,
+    theta_v: np.array,
+    exner: np.array,
+    rd_o_cvd: float,
+    rd_o_p0ref: float,
+) -> tuple[np.array]:
+    bdy_halo_c = np.expand_dims(bdy_halo_c, axis=-1)
+
+    theta_v = mo_solve_nonhydro_stencil_66_theta_v_numpy(bdy_halo_c, theta_v, exner)
+    exner = mo_solve_nonhydro_stencil_66_exner_numpy(
+        bdy_halo_c, rho, exner, rd_o_cvd, rd_o_p0ref
+    )
+
+    return theta_v, exner
+
+
+def test_mo_solve_nonhydro_stencil_66():
+    mesh = SimpleMesh()
+
+    rd_o_cvd = 10.0
+    rd_o_p0ref = 20.0
+    bdy_halo_c = random_mask(mesh, CellDim, numeric=True)
+    exner = random_field(mesh, CellDim, KDim, low=1, high=2)
+    rho = random_field(mesh, CellDim, KDim, low=1, high=2)
+    theta_v = random_field(mesh, CellDim, KDim, low=1, high=2)
+
+    theta_v_ref, exner_ref = mo_solve_nonhydro_stencil_66_numpy(
+        np.asarray(bdy_halo_c),
+        np.asarray(rho),
+        np.asarray(theta_v),
+        np.asarray(exner),
+        rd_o_cvd,
+        rd_o_p0ref,
+    )
+
+    mo_solve_nonhydro_stencil_66(
+        bdy_halo_c,
+        rho,
+        theta_v,
+        exner,
+        rd_o_cvd,
+        rd_o_p0ref,
+        offset_provider={},
+    )
+
+    assert np.allclose(theta_v, theta_v_ref)
+    assert np.allclose(exner, exner_ref)
--- testutils/src/icon4py/testutils/utils.py
+++ testutils/src/icon4py/testutils/utils.py
@@ -26,9 +26,25 @@ def random_mask(mesh, *dims, numeric=False):
     return np_as_located_field(*dims)(arr)
 
 
-def random_field(mesh, *dims):
+def random_field(mesh, *dims, low: float = -1.0, high: float = 1.0):
+    """Generate a random field between specified bounds.
+
+    Initialize a LocatedField with random values between a lower and
+    higher bound, using a uniform random distribution.
+
+    Args:
+        mesh: SimpleMesh object
+        dims: Iterable of mesh dimensions
+        low: lower bound of random values
+        high: higher bound of random values
+
+    Returns:
+        LocatedField with random values
+    """
     return np_as_located_field(*dims)(
-        np.random.randn(*map(lambda x: mesh.size[x], dims))
+        np.random.default_rng().uniform(
+            low=low, high=high, size=tuple(map(lambda x: mesh.size[x], dims))
+        )
     )
 
 
