2ed50d82b7cfce581472aa3c97cf84a7bf93804a
David Leutwyler <david.leutwyler@env.ethz.ch>
2022-08-02 19:31:48 +0200

    make parsable for IDEs

--- shared/src/icon4py/shared/mo_math_constants.py
+++ shared/src/icon4py/shared/mo_math_constants.py
@@ -19,43 +19,59 @@ These constants are provided with more significant digits than necessary
 for a 64-bit double precision number; they may be used for other purposes where
 the extra precision is necessary or useful.
 
-TODO:
-- Implement docsting mechnism such that description appears when hovering over
-symbol in IDE's. Problem There is no __doc__ for variables.
-
 """
 # Mathematical constants (in brackets original C names)
-euler = 2.71828182845904523536028747135266250  # e
-log2e = 1.44269504088896340735992468100189214  # log2(e)
-log10e = 0.434294481903251827651128918916605082  # log10(e)
-ln2 = 0.693147180559945309417232121458176568  # ln(2)
-ln10 = 2.30258509299404568401799145468436421  # ln(10)
-pi = 3.14159265358979323846264338327950288  # pi
-pi_2 = 1.57079632679489661923132169163975144  # pi/2
-pi_4 = 0.785398163397448309615660845819875721  # pi/4
+euler = 2.71828182845904523536028747135266250
+"""e"""
+log2e = 1.44269504088896340735992468100189214
+"""log2(e)"""
+log10e = 0.434294481903251827651128918916605082
+"""log10(e)"""
+ln2 = 0.693147180559945309417232121458176568
+"""ln(2)"""
+ln10 = 2.30258509299404568401799145468436421
+"""ln(10)"""
+pi = 3.14159265358979323846264338327950288
+"""Ratio of a circle's circumference to its diameter"""
+pi2 = pi * 2.0
+"""2*pi"""
+pi_2 = 1.57079632679489661923132169163975144
+"""pi/2"""
+pi_4 = 0.785398163397448309615660845819875721
+"""pi/4"""
 rpi = 0.318309886183790671537767526745028724  # 1/pi
-rpi_2 = 0.636619772367581343075535053490057448  # 2/pi
-rsqrtpi_2 = 1.12837916709551257389615890312154517  # 2/(sqrt(pi)
-sqrt2 = 1.41421356237309504880168872420969808  # sqrt(2)
-sqrt1_2 = 0.707106781186547524400844362104849039  # 1/sqrt(2)
-sqrt3 = 1.7320508075688772935274463415058723  # sqrt(3)
-sqrt1_3 = 0.5773502691896257645091487805019575  # 1/sqrt(3)
+"""1/pi"""
+rpi_2 = 0.636619772367581343075535053490057448
+"""2/pi"""
+rsqrtpi_2 = 1.12837916709551257389615890312154517
+"""2/(sqrt(pi)"""
+sqrt2 = 1.41421356237309504880168872420969808
+"""Square root of 2"""
+sqrt1_2 = 0.707106781186547524400844362104849039
+"""1/sqrt(2)"""
+sqrt3 = 1.7320508075688772935274463415058723
+"""Square root of 3"""
+sqrt1_3 = 0.5773502691896257645091487805019575
+"""1/sqrt(3)"""
 cos45 = sqrt1_2
+"""Cosine of 45°"""
 one_third = 1.0 / 3.0
 
 
 # some more useful constants
-pi_5 = pi * 0.2  # half angle of pentagon
-pi2 = pi * 2.0
-rad2deg = 180.0 / pi  # conversion factor from radians to degree
-deg2rad = pi / 180.0  # conversion factor from degree to radians
-eps = 1.0e-8  # residual bound for solvers DL: Actually 1.19209e-07, no?
+pi_5 = pi * 0.2
+"""Half angle of pentagon"""
 
-# Yields the machine epsilon in single and double precision
+rad2deg = 180.0 / pi
+"""Conversion factor from radians to degree"""
+deg2rad = pi / 180.0
+"""Conversion factor from degree to radians"""
+eps = 1.0e-8  # DL: Actually 1.19209e-07, no?
+"""Residual bound for solvers """
+
 dbl_eps = abs(7.0 / 3 - 4.0 / 3 - 1)
+"""Machine epsilon in single and double precision"""
 pi_180 = pi / 180.0
 
-# phi0 is  the latitude of the lowest major triangle corner
-# and the latitude of the major hexagonal faces centers
-# phi0 = 0.5*pi - 2.*acos(1.0/(2.*sin(pi/5.)))
-phi0 = 0.46364760900080614903
+phi0 = 0.46364760900080614903  # phi0 = 0.5*pi - 2.*acos(1.0/(2.*sin(pi/5.)))
+"""latitude of the lowest major triangle corner and the latitude of the major hexagonal faces centers"""
--- shared/src/icon4py/shared/mo_physical_constants.py
+++ shared/src/icon4py/shared/mo_physical_constants.py
@@ -12,9 +12,9 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 """
-This module provides physical constants for the ICON general circulation models.
+Physical constants for the ICON general circulation models.
 
-Physical constants are grouped as follows:
+They are grouped as follows:
 - Natural constants
 - Molar weights
 - Earth and Earth orbit constants
@@ -24,35 +24,49 @@ mixing ratio over liquid water (*c_les*) or ice(*c_ies*)
 """
 
 
-# Natural constants
-# -----------------
-#
+# Natural constants (WMO/SI values)
+# ---------------------------------
 
-# WMO/SI values
-avo = 6.02214179e23  # [1/mo]    Avogadro constant
-ak = 1.3806504e-23  # [J/K]     Boltzmann constant
-argas = 8.314472  # [J/K/mol] molar/universal/ideal gas constant
-stbo = 5.6704e-8  # [W/m2/K4] Stephan-Boltzmann constant
-
+avo = 6.02214179e23
+""" Avogadro constant [1/mo] """
+ak = 1.3806504e-23
+""" Boltzmann constant [J/K] """
+argas = 8.314472
+""" molar/universal/ideal gas constant [J/K/mol] """
+stbo = 5.6704e-8
+""" Stephan-Boltzmann constant [W/m2/K4] """
 
 # Molar weights
 # -------------
 
 # Pure species
-amco2 = 44.011  # [g/mol] CO2
-amch4 = 16.043  # [g/mol] CH4
-amo3 = 47.9982  # [g/mol] O3
-amo2 = 31.9988  # [g/mol] O2
-amn2o = 44.013  # [g/mol] N2O
-amc11 = 137.3686  # [g/mol] CFC11
-amc12 = 120.9140  # [g/mol] CFC12
-amw = 18.0154  # [g/mol] H2O
-amo = 15.9994  # [g/mol] O
-amno = 30.0061398  # [g/mol] NO
-amn2 = 28.0134  # [g/mol] N2
+amco2 = 44.011
+""" Molar weight of CO2 [g/mol] """
+amch4 = 16.043
+""" Molar weight of CH4 [g/mol] """
+amo3 = 47.9982
+""" Molar weight of O3 [g/mol] """
+amo2 = 31.9988
+""" Molar weight of O2 [g/mol] """
+amn2o = 44.013
+""" Molar weight of N2O [g/mol] """
+amc11 = 137.3686
+""" Molar weight of CFC11 [g/mol] """
+amc12 = 120.9140
+""" Molar weight of CFC12 [g/mol] """
+amw = 18.0154
+""" Molar weight of H2O [g/mol] """
+amo = 15.9994
+""" Molar weight of O [g/mol] """
+amno = 30.0061398
+""" Molar weight of NO [g/mol] """
+amn2 = 28.0134
+""" Molar weight of N2 [g/mol] """
 
 # Mixed species
 amd = 28.970  # [g/mol] dry air
+""" Molar weight of dry air [g/mol] """
+
 
 # Auxiliary constants
 # ppmv2gg converts ozone from volume mixing ratio in ppmv
@@ -63,136 +77,207 @@ o3mr2gg = amo3 / amd
 # Earth and Earth orbit constants
 # -------------------------------
 
-earth_radius = 6.371229e6  # [m]    average radius
-inverse_earth_radius = 1.0 / earth_radius  # [1/m]
-earth_angular_velocity = 7.29212e-5  # [rad/s]  angular velocity
+earth_radius = 6.371229e6
+"""Average Radius of Earth [m]"""
+inverse_earth_radius = 1.0 / earth_radius
+"""Inverse Radius of Earth [1/m]"""
+earth_angular_velocity = 7.29212e-5
+"""Angular velocity Earth[rad/s] """
 
+rae = 0.1277e-2
+""" Ratio of atm. scale height to Earth radius [m/m]"""
 
-# WMO/SI value
-grav = 9.80665  # [m/s2] av. gravitational acceleration
-rgrav = 1.0 / grav  # [s2/m]
-
-rae = 0.1277e-2  # [m/m]  ratio of atm. scale height to Earth radius
+# WMO/SI values
+grav = 9.80665
+"""Average gravitational acceleration of Earth [m/s2]"""
+rgrav = 1.0 / grav
+"""Inverse gravitational acceleration of Earth [s2/m]"""
 
 
 # Thermodynamic constants for the dry and moist atmosphere
 # --------------------------------------------------------
 
 # Dry air
-rd = 287.04  # [J/K/kg] gas constant
-cpd = 1004.64  # [J/K/kg] specific heat at constant pressure
-cvd = cpd - rd  # [J/K/kg] specific heat at constant volume
-con_m = 1.50e-5  # [m^2/s]  kinematic viscosity of dry air
-con_h = 2.20e-5  # [m^2/s]  scalar conductivity of dry air
-con0_h = 2.40e-2  # [J/m/s/K]thermal conductivity of dry air
-eta0d = 1.717e-5  # [N*s/m2] dyn viscosity of dry air at tmelt
+rd = 287.04
+"""Gas constant of dry air [J/K/kg]"""
+cpd = 1004.64
+"""Specific heat of dry air at constant pressure [J/K/kg]"""
+cvd = cpd - rd
+""""Specific heat of dry air at constant volume [J/K/kg]"""
+con_m = 1.50e-5
+"""Kinematic viscosity of dry air [m^2/s]"""
+con_h = 2.20e-5
+"""calar conductivity of dry air [m^2/s]"""
+con0_h = 2.40e-2
+"""Thermal conductivity of dry air [J/m/s/K]"""
+eta0d = 1.717e-5
+"""Dynamic viscosity of dry air at tmelt [N*s/m2] """
 
 # H2O
 # - gas
-rv = 461.51  # [J/K/kg] gas constant for water vapor
-cpv = 1869.46  # [J/K/kg] specific heat at constant pressure
-cvv = cpv - rv  # [J/K/kg] specific heat at constant volume
-dv0 = 2.22e-5  # [m^2/s]  diff coeff of H2O vapor in dry air at tmelt
+rv = 461.51
+"""Gas constant of water vapor [J/K/kg] """
+cpv = 1869.46
+"""Specific heat of water vapour at constant pressure [J/K/kg] """
+cvv = cpv - rv
+"""Specific heat of water vapour at constant volume [J/K/kg] """
+dv0 = 2.22e-5
+"""Diffusion coefficient of water vapor in dry air at tmelt [m^2/s]"""
 # - liquid / water
-rhoh2o = 1000.0  # [kg/m3]  density of liquid water
+rhoh2o = 1000.0
+"""Density of liquid water [kg/m3]  """
 # - solid / ice
-rhoice = 916.7  # [kg/m3]  density of pure ice
+rhoice = 916.7
+"""Density of pure ice [kg/m3]"""
 cv_i = 2000.0
+
 # - phase changes
-alv = 2.5008e6  # [J/kg]   latent heat for vaporisation
-als = 2.8345e6  # [J/kg]   latent heat for sublimation
-alf = als - alv  # [J/kg]   latent heat for fusion
-tmelt = 273.15  # [K]      melting temperature of ice/snow
-t3 = 273.16  # [K]      Triple point of water at 611hPa
+alv = 2.5008e6
+"""Latent heat of vaporisation for water [J/kg]"""
+als = 2.8345e6
+"""Latent heat of sublimation for water [J/kg]"""
+alf = als - alv
+"""latent heat of fusion for water [J/kg] """
+tmelt = 273.15
+"""Melting temperature of ice/snow [K]"""
+t3 = 273.16
+"""Triple point of water at 611hPa [K]"""
 
 # Auxiliary constants
-rdv = rd / rv  # [ ]
-vtmpc1 = rv / rd - 1.0  # [ ]
-vtmpc2 = cpv / cpd - 1.0  # [ ]
-rcpv = cpd / cpv - 1.0  # [ ]
-alvdcp = alv / cpd  # [K]
-alsdcp = als / cpd  # [K]
-rcpd = 1.0 / cpd  # [K*kg/J]
-rcvd = 1.0 / cvd  # [K*kg/J]
-rcpl = 3.1733  # cp_d / cp_l - 1
+rdv = rd / rv
+"""[ ]"""
+vtmpc1 = rv / rd - 1.0
+"""[ ]"""
+vtmpc2 = cpv / cpd - 1.0
+"""[ ]"""
+rcpv = cpd / cpv - 1.0
+"""[ ]"""
+alvdcp = alv / cpd
+"""[K]"""
+alsdcp = als / cpd
+"""[K]"""
+rcpd = 1.0 / cpd
+"""[K*kg/J]"""
+rcvd = 1.0 / cvd
+"""[K*kg/J]"""
+rcpl = 3.1733
+"""cp_d / cp_l - 1"""
 
-clw = (rcpl + 1.0) * cpd  # specific heat capacity of liquid water
+clw = (rcpl + 1.0) * cpd
+"""Specific heat capacity of liquid water"""
 cv_v = (rcpv + 1.0) * cpd - rv
 
-o_m_rdv = 1.0 - rd / rv  # [ ]
-rd_o_cpd = rd / cpd  # [ ]
-cvd_o_rd = cvd / rd  # [ ]
+o_m_rdv = 1.0 - rd / rv
+"""[ ]"""
+rd_o_cpd = rd / cpd
+"""[ ]"""
+cvd_o_rd = cvd / rd
+"""[ ]"""
 
-p0ref = 100000.0  # [Pa]  reference pressure for Exner function
+
+p0ref = 100000.0
+"""Reference pressure for Exner function [Pa]"""
 
 # Variables for computing cloud cover in RH scheme
 uc1 = 0.8
 ucl = 1.00
 
-# U.S. standard atmosphere vertical tropospheric temperature gradient
-dtdz_standardatm = -6.5e-3  # [ K/m ]
+dtdz_standardatm = -6.5e-3
+"""vertical tropospheric temperature gradient of U.S. standard atmosphere [K/m]"""
 
-# constants for radiation module
-zemiss_def = 0.996  # lw sfc default emissivity factor
+# Constants for radiation module
+# ------------------------------
+zemiss_def = 0.996
+"""Longwave surface default emissivity factor"""
 
-# salinity factor for reduced saturation vapor pressure over oceans
+#
 salinity_fac = 0.981
+"""Salinity factor for reduced saturation vapor pressure over oceans"""
 
-# dielectric constants at reference temperature 0°C for radar reflectivity calculation:
+# For radar reflectivity calculation
 K_w_0 = 0.93
+"""dielectric constant at reference temperature 0°C"""
 K_i_0 = 0.176
+"""dielectric constant at reference temperature 0°C"""
 
-# ------------below are parameters for ocean model---------------
-# coefficients in linear EOS
-a_T = 2.55e-04  # thermal expansion coefficient (kg/m3/K)
-b_S = 7.64e-01  # haline contraction coefficient (kg/m3/psu)
+# Parameters for ocean model
+# --------------------------
 
-# density reference values, to be constant in Boussinesq ocean models
-rho_ref = 1025.022  # reference density [kg/m^3]
-rho_inv = 0.0009755881663  # inverse reference density [m^3/kg]
-sal_ref = 35.0  # reference salinity [psu]
+# Coefficients in linear EOS
+a_T = 2.55e-04
+"""Thermal expansion coefficient of liquid water [kg/m3/K]"""
+b_S = 7.64e-01
+"""Haline contraction coefficient of liquid water [kg/m3/psu]"""
 
-SItodBar = 1.0e-4  # Conversion from pressure [p] to pressure [bar]
+# Density reference values, to be constant in Boussinesq ocean models
+rho_ref = 1025.022
+"""Reference density of liquid water [kg/m^3]"""
+rho_inv = 0.0009755881663
+"""Inverse reference density of liquid water [m^3/kg]"""
+sal_ref = 35.0
+"""reference salinity of liquid water [psu] """
+
+SItodBar = 1.0e-4
+"""Conversion from pressure [p] to pressure [bar]"""
 # used in ocean thermodynamics
 sfc_press_pascal = 101300.0
+""" Surface pressure [Pa] """
 sfc_press_bar = 101300.0 * SItodBar
+""" Surface pressure [bar] """
 
-p0sl_bg = 101325.0  # [Pa]     sea level pressure
+p0sl_bg = 101325.0
+"""Sea level pressure [Pa]"""
 
-# ----------below are parameters for sea-ice and lake model---------------
+# Parameters for sea-ice and lake model
+# -------------------------------------
 
-ks = 0.31  # heat conductivity snow     [J  / (m s K)]
-ki = 2.1656  # heat conductivity ice      [J  / (m s K)]
-rhoi = 917.0  # density of sea ice         [kg / m**3]
-rhos = 300.0  # density of snow            [kg / m**3]
-ci = 2106.0  # Heat capacity of ice       [J / (kg K)]
-cs = 2090.0  # Heat capacity of snow      [J / (kg K)]
+ks = 0.31
+"""Heat conductivity snow [J/m/s/K]"""
+ki = 2.1656
+"""Heat conductivity ice [J/m/s/K]"""
+rhoi = 917.0
+"""Density of sea ice [kg/m3]"""
+rhos = 300.0
+"""Density of snow [kg/m3]"""
+ci = 2106.0
+"""Heat capacity of ice [J/kg/K]"""
+cs = 2090.0
+"""Heat capacity of snow [J/kg/K]"""
+Tf = -1.80
+"""Temperature ice bottom [C]"""
+mu = 0.054
+"""Constant in linear freezing"""
+albedoW = 0.07
+"""Albedo ocean used in atmosphere"""
 
-Tf = -1.80  # Temperature ice bottom     [C]
-mu = 0.054  # Constant in linear freezing-
-albedoW = 0.07  # albedo of the ocean used in atmosphere
 
-
-fr_fac = 1.1925  # Frank Roeske energy budget closing factor for OMIP
+fr_fac = 1.1925
+"""Frank Roeske energy budget closing factor for OMIP"""
 
 # CCSM3 albedo scheme - not used for coupling
-alb_ice_vis = 0.73  # Albedo of dry ice  (visible)
-alb_ice_nir = 0.33  # Albedo of dry ice  (near-infrared)
-alb_sno_vis = 0.96  # Albedo of dry snow (visible)
-alb_sno_nir = 0.68  # Albedo of dry snow (near-infrared)
-I_0 = 0.17  # Ice-surface penetrating shortwave fraction
-Cd_ia = 1.2e-3  # Ice-atmosphere drag coefficient
-Cd_io = 3.0e-3  # Ice-ocean drag coefficient
-Ch_io = 12.0e-3  # Ice-ocean heat transfer coefficient
+alb_ice_vis = 0.73
+"""Albedo of dry ice (visible)"""
+alb_ice_nir = 0.33
+"""Albedo of dry ice (near-infrared)"""
+alb_sno_vis = 0.96
+"""Albedo of dry snow (visible)"""
+alb_sno_nir = 0.68
+"""Albedo of dry snow (near-infrared)"""
+I_0 = 0.17
+"""Ice-surface penetrating shortwave fraction"""
+Cd_ia = 1.2e-3
+"""Ice-atmosphere drag coefficient"""
+Cd_io = 3.0e-3
+"""Ice-ocean drag coefficient"""
+Ch_io = 12.0e-3
+"""Ice-ocean heat transfer coefficient"""
 
-# --------- parameters for NWP sea-ice model (we should agree on a single value)-----
-# _cdm>
-# The value of the salt-water freezing point is the same as in GME and COSMO (-1.7 dgr C).
-# Note that a different value (Tf=-1.8 dgr C) is defined in "mo_physical_constants".
-# _cdm<
+# Parameters for NWP sea-ice model
+# --------------------------------
+tf_salt = 271.45  # (Note: Differs from Tf)
+"""salt-water freezing point [K] """
 
-tf_salt = 271.45  # salt-water freezing point [K] (note that it differs from Tf)
-
-# Length of day in seconds, as integer and real
-rdaylen = 86400.0  # [s]
-idaylen = int(rdaylen)  # [s]
+rdaylen = 86400.0
+"""Length of day [s] as float [s]"""
+idaylen = int(rdaylen)
+"""Length of day [s] as integer [s]"""
