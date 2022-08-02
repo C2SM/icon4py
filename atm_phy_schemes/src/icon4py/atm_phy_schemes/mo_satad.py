67afa55d8d58ca8941def28d993d9b1d33fd600c
David Leutwyler <david.leutwyler@env.ethz.ch>
2022-08-02 22:18:28 +0200

    compiles

--- atm_phy_schemes/src/icon4py/atm_phy_schemes/mo_satad.py
+++ atm_phy_schemes/src/icon4py/atm_phy_schemes/mo_satad.py
@@ -19,36 +19,55 @@ Changes.
 - Only implementend gpu version. Maybe further optimizations possible for CPU (check original code)
 
 TODO:
-- Can we call field_operator from a field_operator? -> Workinprogress
-- Is where statement the nicest possible syntax for the IF-ELSE below? Also, where currently can only return 1 Field
+- Is where statement the nicest possible syntax for the IF-ELSE below? Also, where currently can only return 1 Field(
+(a) Do np.where(fun1, func2) implementation
+(b) How would algorithm look in local view? In current localview frontend.
+(c) ...
 - Implement Newtonian iteration! -> Needs fixted-size for loop feature in GT4Py
+(0) For loop outside (maybe low priority)
+(a) Unroll by hand
+(b) Naive unroll of compile time FOR, maybe optimize
+(c) Tracing
 - Should pre-computations be removed? E.g, lwdocvd and qwa ???
-- Assignment operators: Suppress/blacklist with (linters?) until supported?
+(a) Currently only partly optimized. Should be fine if in same @program
+- Assignment operators: --> WONTFIX for now, since we want to behave like numpy.
 - Support for docstings inside field_operators. For now: Place all docstrings outside of field_operators as they are not supported inside.
-- What to do with local constants?
+--> Trivial to implement
+- What to do with
+
 
 Nice to have:
 - Document constants such that description appears when hovering over symbol in IDE's -> mo_physical_constants.py et al
+-> DL:adapt code
 
 Comment from FORTRAN version:
 - Suggested by U. Blahak: Replace pres_sat_water, pres_sat_ice and spec_humi by
 lookup tables in mo_convect_tables. Bit incompatible change!
+
+
+# Results: Big ticket features
+0. Docstings in field_operator (Nikki)
+1. start planning for loop or recursion (Linus)
+2. start thinking about multiple returns form where (Nikki)
+3. local, global, compile-time constants (Peter)
+
+
 """
 from functional.ffront.decorator import field_operator, program
-from functional.ffront.fbuiltins import Field, exp
+from functional.ffront.fbuiltins import Field, exp, where, abs, maximum
 
-from icon4py.atm_phy_schemes.mo_convect_tables import c1es, c3les, c4les, c5les
+# from icon4py.atm_phy_schemes.mo_convect_tables import c1es, c3les, c4les, c5les
 from icon4py.common.dimension import CellDim, KDim
-from icon4py.shared.mo_physical_constants import alv, clw, cvd, rv, tmelt
 
+# from icon4py.shared.mo_physical_constants import alv, clw, cvd, rv, tmelt
 
-# TODO: Local constants What to do with these?
-cp_v = 1850.0  # specific heat of water vapor at constant pressure (Landolt-Bornstein)
-ci = 2108.0  # specific heat of ice
+# # TODO: Local constants What to do with these?
+# cp_v = 1850.0  # specific heat of water vapor at constant pressure (Landolt-Bornstein)
+# ci = 2108.0  # specific heat of ice
 
-tol = 1e-3
-maxiter = 10
-zqwmin = 1e-20
+# tol = 1e-3
+# maxiter = 10  #
+# zqwmin = 1e-20
 
 # TODO: Docstrings will crash field_Operators
 """
@@ -63,6 +82,15 @@ def _latent_heat_vaporization(
     t: Field[[CellDim, KDim], float]
 ) -> Field[[CellDim, KDim], float]:
 
+    # TODO: Remove later
+    alv = 2.5008e6
+    tmelt = 273.15
+    rv = 461.51
+    cpd = 1004.64
+    rcpl = 3.1733
+    clw = (rcpl + 1.0) * cpd
+    cp_v = 1850.0
+
     return alv + (cp_v - clw) * (t - tmelt) - rv * t
 
 
@@ -71,6 +99,12 @@ def _latent_heat_vaporization(
 
 @field_operator
 def _sat_pres_water(t: Field[[CellDim, KDim], float]) -> Field[[CellDim, KDim], float]:
+    # TODO: Remove later
+    tmelt = 273.15
+    c1es = 610.78
+    c3les = 17.269
+    c4les = 35.86
+
     return c1es * exp(c3les * (t - tmelt) / (t - c4les))
 
 
@@ -81,6 +115,9 @@ def _sat_pres_water(t: Field[[CellDim, KDim], float]) -> Field[[CellDim, KDim], 
 def _qsat_rho(
     t: Field[[CellDim, KDim], float], rho: Field[[CellDim, KDim], float]
 ) -> Field[[CellDim, KDim], float]:
+
+    rv = 461.51  # TODO: Remove
+
     return _sat_pres_water(t) / (rho * rv * t)
 
 
@@ -96,8 +133,79 @@ def _dqsatdT_rho(
     t: Field[[CellDim, KDim], float], zqsat: Field[[CellDim, KDim], float]
 ) -> Field[[CellDim, KDim], float]:
 
+    # TODO: Remove later
+    tmelt = 273.15
+    c3les = 17.269
+    c4les = 35.86
+    c5les = c3les * (tmelt - c4les)
+
     beta = c5les / (t - c4les) ** 2 - 1.0 / t
     return beta * zqsat
+
+
+@field_operator
+def _newtonian_for_body(
+    t: Field[[CellDim, KDim], float],
+    qv: Field[[CellDim, KDim], float],
+    rho: Field[[CellDim, KDim], float],
+    lwdocvd: Field[[CellDim, KDim], float],
+) -> Field[[CellDim, KDim], float]:
+    # TODO: RENAME and test if identical. Double check with Hannes
+    # Save the starting value of temperature t
+    twork = t
+
+    fT = twork - t + lwdocvd * (_qsat_rho(twork, rho) - qv)
+    dfT = 1.0 + lwdocvd * _dqsatdT_rho(twork, _qsat_rho(twork, rho))
+
+    return twork - fT / dfT
+
+
+@field_operator
+def _conditional_newtonian_for_body(
+    t: Field[[CellDim, KDim], float],
+    qv: Field[[CellDim, KDim], float],
+    rho: Field[[CellDim, KDim], float],
+    lwdocvd: Field[[CellDim, KDim], float],
+) -> Field[[CellDim, KDim], float]:
+    # TODO: RENAME and test if identical. Double check with Hannes
+    # Save the starting value of temperature t
+    twork = t
+
+    # Here we still have to iterate ...
+    tworkold = twork
+
+    fT = twork - t + lwdocvd * (_qsat_rho(twork, rho) - qv)
+    dfT = 1.0 + lwdocvd * _dqsatdT_rho(twork, _qsat_rho(twork, rho))
+
+    tol = 1e-3  # TODO: Remove
+    return where(abs(twork - tworkold) > tol, twork - fT / dfT, tworkold)
+
+
+@field_operator
+def _newtonian_iteration_t(
+    t: Field[[CellDim, KDim], float],
+    qv: Field[[CellDim, KDim], float],
+    rho: Field[[CellDim, KDim], float],
+) -> Field[[CellDim, KDim], float]:
+
+    # TODO: Remove
+    rd = 287.04
+    cpd = 1004.64
+    cvd = cpd - rd
+
+    lwdocvd = _latent_heat_vaporization(t) / cvd
+
+    # for _ in range(1, maxiter):
+    twork = _newtonian_for_body(t, qv, rho, lwdocvd)
+    twork = _conditional_newtonian_for_body(twork, qv, rho, lwdocvd)
+    twork = _conditional_newtonian_for_body(twork, qv, rho, lwdocvd)
+    twork = _conditional_newtonian_for_body(twork, qv, rho, lwdocvd)
+    twork = _conditional_newtonian_for_body(twork, qv, rho, lwdocvd)
+    twork = _conditional_newtonian_for_body(twork, qv, rho, lwdocvd)
+    twork = _conditional_newtonian_for_body(twork, qv, rho, lwdocvd)
+    twork = _conditional_newtonian_for_body(twork, qv, rho, lwdocvd)
+    twork = _conditional_newtonian_for_body(twork, qv, rho, lwdocvd)
+    return _conditional_newtonian_for_body(twork, qv, rho, lwdocvd)
 
 
 """
@@ -130,53 +238,26 @@ def _satad(
     Field[[CellDim, KDim], float],
 ]:
 
-    # TODO: DL: Inline below??
-    lwdocvd = _latent_heat_vaporization(t) / cvd
+    # TODO: Remove
+    rd = 287.04
+    cpd = 1004.64
+    cvd = cpd - rd
+    zqwmin = 1e-20
+
+    tTest = t - _latent_heat_vaporization(t) / cvd * qc
 
     # check, which points will still be subsaturated even after evaporating
     # all cloud water.For these gridpoints Newton iteration is not necessary.
-    # TODO: to implement conditionals in gt4py, the 'where' builtin has to be used
+    subsaturated = qv + qc <= _qsat_rho(t, rho)
 
-    if qv + qc <= _qsat_rho(t, rho):
-        # All the cloud water is evaporated, but (sub)saturation remains .
-        # No iteration of temperature needed.
-        qv = qv + qc
-        qc = 0.0
-        t = t - lwdocvd * qc
-    else:
-        # Save the starting value of temperature t
-        twork = t
+    qv = where(subsaturated, qv + qc, _qsat_rho(t, rho))
+    qc = where(subsaturated, 0.0, maximum(qv + qc - _qsat_rho(t, rho), zqwmin))
+    t = where(subsaturated, tTest, _newtonian_iteration_t(t, qv, rho))
 
-        # Storage variable for the "old" values in the below iteration.
-        # Add nonesense increment to trigger the iteration.
-        # TODO: DL: Make nicer with iteration functionality
-        tworkold = twork + 10.0
-
-        for _ in 1, maxiter:
-            if abs(twork - tworkold) > tol:
-                # Here we still have to iterate ...
-                tworkold = twork
-
-                # Helpers (TODO refactor and remove?)
-                qwd = _qsat_rho(t, rho)
-                dqwd = _dqsatdT_rho(twork, qwd)
-
-                # Newton
-                fT = twork - t + lwdocvd * (qwd - qv)
-                dfT = 1.0 + lwdocvd * dqwd
-                twork = twork - fT / dfT
-
-        t = twork
-
-        # The extrapolation of qsat from the second-last iteration step
-        # is disregarded. This is typically done to exactly preserve the
-        # internal energy. Omitting that introduces only a small error.
-        qwa = _qsat_rho(t, rho)
-        qc = max(qc + qv - qwa, zqwmin)
-        qv = qwa
     return t, qv, qc
 
 
+# DL: Do we actually need the program?
 @program
 def satad(
     qv: Field[[CellDim, KDim], float],
@@ -184,4 +265,4 @@ def satad(
     t: Field[[CellDim, KDim], float],
     rho: Field[[CellDim, KDim], float],
 ):
-    _satad(qv, qc, t, rho, out=(qv, qc, t))
+    _satad(qv, qc, t, rho, out=(t, qv, qc))
