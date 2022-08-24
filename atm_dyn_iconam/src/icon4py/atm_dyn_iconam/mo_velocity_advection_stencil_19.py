05328f0c8c0f307e39f8d42fa932ff4b52ce65c8
nfarabullini <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>
2022-08-24 17:15:46 +0200

    Draft for mo_velocity_advection_stencil_19

--- atm_dyn_iconam/src/icon4py/atm_dyn_iconam/mo_velocity_advection_stencil_19.py
+++ atm_dyn_iconam/src/icon4py/atm_dyn_iconam/mo_velocity_advection_stencil_19.py
@@ -10,3 +10,76 @@
 # distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+from functional.ffront.decorator import field_operator, program
+from functional.ffront.fbuiltins import Field, neighbor_sum
+
+from icon4py.common.dimension import (
+    E2C,
+    E2EC,
+    E2V,
+    CellDim,
+    E2CDim,
+    E2VDim,
+    ECDim,
+    EdgeDim,
+    KDim,
+    Koff,
+    VertexDim,
+)
+
+
+@field_operator
+def _mo_velocity_advection_stencil_19(
+    z_kin_hor_e: Field[[EdgeDim, KDim], float],
+    coeff_gradekin: Field[[ECDim], float],
+    z_ekinh: Field[[CellDim, KDim], float],
+    zeta: Field[[VertexDim, KDim], float],
+    vt: Field[[EdgeDim, KDim], float],
+    f_e: Field[[EdgeDim], float],
+    c_lin_e: Field[[EdgeDim, E2CDim], float],
+    z_w_con_c_full: Field[[CellDim, KDim], float],
+    vn_ie: Field[[EdgeDim, KDim], float],
+    ddqz_z_full_e: Field[[EdgeDim, KDim], float],
+):
+    ddt_vn_adv = -(
+        (coeff_gradekin(E2EC[0]) - coeff_gradekin(E2EC[1])) * z_kin_hor_e
+        + (
+            -coeff_gradekin(E2EC[0]) * z_ekinh(E2C[0])
+            + coeff_gradekin(E2EC[1]) * z_ekinh(E2C[1])
+        )
+        + vt * (f_e + 0.5 * neighbor_sum(zeta(E2V), axis=E2VDim))
+        + neighbor_sum(z_w_con_c_full(E2C) * c_lin_e, axis=E2CDim)
+        * (vn_ie - vn_ie(Koff[1]))
+        / ddqz_z_full_e
+    )
+    return ddt_vn_adv
+
+
+@program
+def mo_velocity_advection_stencil_19(
+    z_kin_hor_e: Field[[EdgeDim, KDim], float],
+    coeff_gradekin: Field[[ECDim], float],
+    z_ekinh: Field[[CellDim, KDim], float],
+    zeta: Field[[VertexDim, KDim], float],
+    vt: Field[[EdgeDim, KDim], float],
+    f_e: Field[[EdgeDim], float],
+    c_lin_e: Field[[EdgeDim, E2CDim], float],
+    z_w_con_c_full: Field[[CellDim, KDim], float],
+    vn_ie: Field[[EdgeDim, KDim], float],
+    ddqz_z_full_e: Field[[EdgeDim, KDim], float],
+    ddt_vn_adv: Field[[EdgeDim, KDim], float],
+):
+    _mo_velocity_advection_stencil_19(
+        z_kin_hor_e,
+        coeff_gradekin,
+        z_ekinh,
+        zeta,
+        vt,
+        f_e,
+        c_lin_e,
+        z_w_con_c_full,
+        vn_ie,
+        ddqz_z_full_e,
+        out=ddt_vn_adv,
+    )
