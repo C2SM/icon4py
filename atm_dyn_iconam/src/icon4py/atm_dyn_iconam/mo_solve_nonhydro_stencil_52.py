1c0714fe8de21f4c8320dab4abbc6e3497633c80
nfarabullini <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>
2022-09-01 16:24:22 +0200

    Partial solution for mo_solve_nonhydro_stencil_52

--- atm_dyn_iconam/src/icon4py/atm_dyn_iconam/mo_solve_nonhydro_stencil_52.py
+++ atm_dyn_iconam/src/icon4py/atm_dyn_iconam/mo_solve_nonhydro_stencil_52.py
@@ -17,13 +17,6 @@ from functional.ffront.fbuiltins import Field
 from icon4py.common.dimension import CellDim, KDim, Koff
 
 
-@scan_operator(axis=KDim, forward=False, init=0.0)
-def _mo_solve_nonhydro_stencil_52_scan(
-    w_state: float, z_a: float, z_g: float, w: float
-) -> float:
-    return (w - z_a * w_state) * z_g
-
-
 @field_operator
 def _mo_solve_nonhydro_stencil_52_z_q(
     vwind_impl_wgt: Field[[CellDim], float],
@@ -36,12 +29,46 @@ def _mo_solve_nonhydro_stencil_52_z_q(
     cpd: float,
 ) -> Field[[CellDim, KDim], float]:
     z_gamma = dtime * cpd * vwind_impl_wgt * theta_v_ic / ddqz_z_half
-    z_a = -z_gamma * z_beta(Koff[-1]) * z_alpha(Koff[1])
+    z_a = -z_gamma * z_beta(Koff[-1]) * z_alpha(Koff[-1])
     z_c = -z_gamma * z_beta * z_alpha(Koff[1])
     z_b = 1.0 + z_gamma * z_alpha * (z_beta(Koff[-1]) + z_beta)
     z_g = 1.0 / (z_b + z_a * z_q(Koff[-1]))
     z_q = -z_c * z_g
     return z_q
+
+
+@program
+def mo_solve_nonhydro_stencil_52_z_q(
+    vwind_impl_wgt: Field[[CellDim], float],
+    theta_v_ic: Field[[CellDim, KDim], float],
+    ddqz_z_half: Field[[CellDim, KDim], float],
+    z_alpha: Field[[CellDim, KDim], float],
+    z_beta: Field[[CellDim, KDim], float],
+    z_q: Field[[CellDim, KDim], float],
+    dtime: float,
+    cpd: float,
+):
+    _mo_solve_nonhydro_stencil_52_z_q(
+        vwind_impl_wgt,
+        theta_v_ic,
+        ddqz_z_half,
+        z_alpha,
+        z_beta,
+        z_q,
+        dtime,
+        cpd,
+        out=z_q,
+    )
+
+
+@scan_operator(axis=KDim, forward=True, init=0.0)
+def _mo_solve_nonhydro_stencil_52_w_scan(
+    w_state: float,
+    z_a: float,
+    z_g: float,
+    w: float,
+) -> float:
+    return (w - z_a * w_state) * z_g
 
 
 @field_operator
@@ -58,11 +85,11 @@ def _mo_solve_nonhydro_stencil_52_w(
     cpd: float,
 ) -> Field[[CellDim, KDim], float]:
     z_gamma = dtime * cpd * vwind_impl_wgt * theta_v_ic / ddqz_z_half
-    z_a = -z_gamma * z_beta(Koff[-1]) * z_alpha(Koff[1])
+    z_a = -z_gamma * z_beta(Koff[-1]) * z_alpha(Koff[-1])
     z_b = 1.0 + z_gamma * z_alpha * (z_beta(Koff[-1]) + z_beta)
     z_g = 1.0 / (z_b + z_a * z_q(Koff[-1]))
     w_before = z_w_expl - z_gamma * (z_exner_expl(Koff[-1]) - z_exner_expl)
-    w = _mo_solve_nonhydro_stencil_52_scan(z_a, z_g, w_before)
+    w = _mo_solve_nonhydro_stencil_52_w_scan(z_a, z_g, w_before)
     return w
 
 
@@ -80,17 +107,7 @@ def mo_solve_nonhydro_stencil_52(
     dtime: float,
     cpd: float,
 ):
-    _mo_solve_nonhydro_stencil_52_z_q(
-        vwind_impl_wgt,
-        theta_v_ic,
-        ddqz_z_half,
-        z_alpha,
-        z_beta,
-        z_q,
-        dtime,
-        cpd,
-        out=z_q[:, 2:],
-    )
+
     _mo_solve_nonhydro_stencil_52_w(
         vwind_impl_wgt,
         theta_v_ic,
@@ -102,5 +119,5 @@ def mo_solve_nonhydro_stencil_52(
         z_q,
         dtime,
         cpd,
-        out=w[:, 2:],
+        out=w,
     )
--- atm_dyn_iconam/tests/test_mo_solve_nonhydro_stencil_52.py
+++ atm_dyn_iconam/tests/test_mo_solve_nonhydro_stencil_52.py
@@ -15,10 +15,11 @@ import numpy as np
 
 from icon4py.atm_dyn_iconam.mo_solve_nonhydro_stencil_52 import (
     mo_solve_nonhydro_stencil_52,
+    mo_solve_nonhydro_stencil_52_z_q,
 )
 from icon4py.common.dimension import CellDim, KDim
 from icon4py.testutils.simple_mesh import SimpleMesh
-from icon4py.testutils.utils import random_field, zero_field
+from icon4py.testutils.utils import random_field
 
 
 def mo_solve_nonhydro_stencil_52_numpy(
@@ -30,29 +31,34 @@ def mo_solve_nonhydro_stencil_52_numpy(
     z_exner_expl: np.array,
     z_w_expl: np.array,
     z_q: np.array,
+    w: np.array,
     dtime,
     cpd,
 ) -> tuple[np.array]:
     vwind_impl_wgt = np.expand_dims(vwind_impl_wgt, axis=-1)
+
     z_gamma = dtime * cpd * vwind_impl_wgt * theta_v_ic / ddqz_z_half
-    last_k_level = z_alpha.shape[1]
-    z_a = np.zeros_like(z_alpha)
-    z_b = np.zeros_like(z_beta)
-    z_g = np.zeros_like(z_beta)
-    w = np.zeros_like(z_w_expl)
-    for k in range(2, last_k_level):
-        z_a[:, k] = -z_gamma[:, k] * z_beta[:, k - 1] * z_alpha[:, k - 1]
+    last_k_level = z_gamma.shape[1] - 1
+    z_a = np.zeros_like(z_gamma)
+    z_b = np.zeros_like(z_gamma)
+    z_c = np.zeros_like(z_gamma)
+    z_g = np.zeros_like(z_gamma)
+
+    for k in range(last_k_level):
+        z_a[:, k] = -z_gamma[:, k] * z_beta[:, k + 1] * z_alpha[:, k + 1]
+        z_c[:, k] = -z_gamma[:, k] * z_beta[:, k] * z_alpha[:, k - 1]
         z_b[:, k] = 1.0 + z_gamma[:, k] * z_alpha[:, k] * (
-            z_beta[:, k - 1] + z_beta[:, k]
+            z_beta[:, k + 1] + z_beta[:, k]
         )
-        z_g[:, k] = 1.0 / (z_b[:, k] + z_a[:, k] * z_q[:, k - 1])
-    z_c = -z_gamma * z_beta[k] * z_alpha[k + 1]
-    z_q = -z_c * z_g
-    for k in range(2, last_k_level):
+        z_g[:, k] = 1.0 / (z_b[:, k] + z_a[:, k] * z_q[:, k + 1])
+        z_q[:, k] = -z_c[:, k] * z_g[:, k]
+
+    for k in range(last_k_level):
         w[:, k] = z_w_expl[:, k] - z_gamma[:, k] * (
-            z_exner_expl[:, k - 1] - z_exner_expl[:, k]
+            z_exner_expl[:, k + 1] - z_exner_expl[:, k]
         )
-        w[:, k] = (w[:, k] - z_a[:, k] * w[:, k - 1]) * z_g[:, k]
+    for k in range(last_k_level):
+        w[:, k] = (w[:, k] - z_a[:, k] * w[:, k + 1]) * z_g[:, k]
     return z_q, w
 
 
@@ -68,8 +74,8 @@ def test_mo_solve_nonhydro_stencil_52():
     dtime = 8.0
     cpd = 7.0
 
-    z_q = zero_field(mesh, CellDim, KDim)
-    w = zero_field(mesh, CellDim, KDim)
+    z_q = random_field(mesh, CellDim, KDim)
+    w = random_field(mesh, CellDim, KDim)
 
     z_q_ref, w_ref = mo_solve_nonhydro_stencil_52_numpy(
         np.asarray(vwind_impl_wgt),
@@ -80,6 +86,7 @@ def test_mo_solve_nonhydro_stencil_52():
         np.asarray(z_exner_expl),
         np.asarray(z_w_expl),
         np.asarray(z_q),
+        np.asarray(w),
         dtime,
         cpd,
     )
@@ -99,5 +106,17 @@ def test_mo_solve_nonhydro_stencil_52():
         offset_provider={"Koff": KDim},
     )
 
-    assert np.allclose(z_q_ref, z_q)
+    mo_solve_nonhydro_stencil_52_z_q(
+        vwind_impl_wgt,
+        theta_v_ic,
+        ddqz_z_half,
+        z_alpha,
+        z_beta,
+        z_q,
+        dtime,
+        cpd,
+        offset_provider={"Koff": KDim},
+    )
+
+    assert np.allclose(z_q_ref[:, :-1], z_q[:, :-1])
     assert np.allclose(w_ref, w)
