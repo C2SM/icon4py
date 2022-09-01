6627b561345d2403cd098d8f6ffe1d9d30cf8c4a
Hannes Vogt <hannes@havogt.de>
2022-09-01 12:29:38 +0000

    hacks in itir

---
+++ atm_dyn_iconam/src/icon4py/atm_dyn_iconam/mo_nh_diffusion_stencil_15.py
@@ -0,0 +1,85 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from functional.iterator.builtins import (
+    deref,
+    if_,
+    named_range,
+    shift,
+    unstructured_domain,
+)
+from functional.iterator.runtime import closure, fendef, fundef
+
+from icon4py.common.dimension import C2E2C, CellDim, KDim, Koff
+
+
+@fundef
+def step(i, geofac_n2s_nbh, vcoef, theta_v, zd_vertidx):
+    d_vcoef = deref(shift(i)(vcoef))
+    s_theta_v = shift(C2E2C, i, Koff, deref(shift(i)(zd_vertidx)))(theta_v)
+    return deref(shift(i)(geofac_n2s_nbh)) * (
+        d_vcoef * deref(s_theta_v) + (1.0 - d_vcoef) * deref(shift(Koff, 1)(s_theta_v))
+    )
+
+
+@fundef
+def _mo_nh_diffusion_stencil_15(
+    mask, zd_vertidx, zd_diffcoef, geofac_n2s_c, geofac_n2s_nbh, vcoef, theta_v, z_temp
+):
+    summed = (
+        step(0, geofac_n2s_nbh, vcoef, theta_v, zd_vertidx)
+        + step(1, geofac_n2s_nbh, vcoef, theta_v, zd_vertidx)
+        + step(2, geofac_n2s_nbh, vcoef, theta_v, zd_vertidx)
+    )
+    update = deref(z_temp) + deref(zd_diffcoef) * (
+        deref(theta_v) * deref(geofac_n2s_c) + summed
+    )
+
+    return if_(deref(mask), update, deref(z_temp))
+
+
+@fendef
+def mo_nh_diffusion_stencil_15(
+    mask,
+    zd_vertidx,
+    zd_diffcoef,
+    geofac_n2s_c,
+    geofac_n2s_nbh,
+    vcoef,
+    theta_v,
+    z_temp,
+    hstart,
+    hend,
+    kstart,
+    kend,
+):
+    closure(
+        unstructured_domain(
+            named_range(CellDim, hstart, hend), named_range(KDim, kstart, kend)
+        ),
+        _mo_nh_diffusion_stencil_15,
+        z_temp,
+        [
+            mask,
+            zd_vertidx,
+            zd_diffcoef,
+            geofac_n2s_c,
+            geofac_n2s_nbh,
+            vcoef,
+            theta_v,
+            z_temp,
+        ],
+    )
+
+
+print(mo_nh_diffusion_stencil_15.itir(*([None] * 12)))
