565986b250c05082eeb46cf87354a4b1e98cffd5
David Leutwyler <david.leutwyler@env.ethz.ch>
2022-09-07 13:45:31 +0000

    draft version of graupel toy as scan

--- atm_phy_schemes/tests/test_graupel_toy_scan.py
+++ atm_phy_schemes/tests/test_graupel_toy_scan.py
@@ -13,16 +13,16 @@
 
 import numpy as np
 from functional.ffront.decorator import program, scan_operator
+from functional.ffront.fbuiltins import Field
 from hypothesis import given, settings
 
-from icon4py.common.dimension import CellDim, KDim, Koff
+from icon4py.common.dimension import CellDim, KDim
 from icon4py.testutils.simple_mesh import SimpleMesh
 from icon4py.testutils.utils import random_field_strategy, zero_field
 
 
-"""Implicit sedimentaion"""
+"""Do Implicit sedimentaion
 
-"""
 
 TODO GT4Py team:
 - ternary in scan_operator (Niki, fast track)
@@ -33,10 +33,7 @@ TODO GT4Py team:
 
 
 To discuss:
-- Fucntional model in, out, overwrite
-- Multiple returns from scan_operator?
-- Return 3D field?
-
+- Returns from scan. Carry, carry + return, 2D Fields in Return
 
 TODO DL:
 - Make compile
@@ -52,10 +49,13 @@ Questions for field_operator version
 """
 
 
-def graupel_toy_numpy(qc, qr, precipitaion, a=0.1, b=0.2):
-    """Current goal is ot match this routine."""
+def graupel_toy_numpy(qc, qr):
+    """Match this routine."""
     sedimentation = 0.0
 
+    a = 0.1
+    b = 0.2
+    precipitaion = np.zeros(np.shape(qc)[1])
     for cell, k in np.ndindex(np.shape(qc)):
 
         # Autoconversion: Cloud Drops -> Rain Drops
@@ -66,101 +66,74 @@ def graupel_toy_numpy(qc, qr, precipitaion, a=0.1, b=0.2):
         qr[cell, k] += sedimentation
 
         # Compute new sedimentation rate (made up formula).
-        sedimentation = b * qr[cell, k - 1] ** 3 if qr[cell, k] <= 0.1 else 0.0
+        sedimentation = b * qr[cell, k - 1] ** 3  # if qr[cell, k] <= 0.1 else 0.0
 
         # Precipitation is qr arriving at the ground
         if k is not np.shape(qc)[1]:
             qr[cell, k] -= sedimentation
         else:
-            precipitaion[cell, k] = qr[cell, k]
+            precipitaion[cell] = sedimentation
 
     return precipitaion
 
 
-@scan_operator(axis=KDim, forward=True, init=0.0)
-def _graupel_scan_operator(state: float, qc: float, qr: float) -> float:
+@scan_operator(axis=KDim, forward=True, init=(0.0, 0.0, 0.0))
+def _graupel_toy_scan(
+    carry: tuple[float, float, float], qc_in: float, qr_in: float
+) -> tuple[float, float, float]:
 
     a = 0.1
-    return state + a
+    b = 0.2
+
+    # unpack carry
+    # TODO Discussion: Split current carry return into carry + return ? Here we dont touch qc_kMinus1
+    # Also: Discuss syntax
+    sedimentation_kMinus1, qc_kMinus1, qr_kMinus1 = carry
+
+    # Autoconversion: Cloud Drops -> Rain Drops
+    qc = qc_in - qc_in * a
+    qr = qc_in + qc_in * a
+
+    # Add sedimentation from gridpoint above
+    qr = qr + sedimentation_kMinus1
+
+    # Compute new sedimentation rate (made up formula).
+    sedimentation = b * qr_kMinus1**3  # if qr[cell, k] <= 0.1 else 0.0
+
+    # if k is not np.shape(qc)[1]: #TODO: Need if on index field
+    qr = qr_in - sedimentation
+
+    return (sedimentation, qc, qr)
 
 
-# @given(
-#     random_field_strategy(SimpleMesh(), CellDim, KDim, min_value=1e-7, max_value=1),
-#     random_field_strategy(SimpleMesh(), CellDim, KDim, min_value=1e-7, max_value=1),
-# )
-# @settings(deadline=None, max_examples=10)
-# def test_graupel(qc, qr):
+@program
+def graupel_toy_scan(
+    qc: Field[[CellDim, KDim], float],
+    qr: Field[[CellDim, KDim], float],
+    sedimentation: Field[[CellDim, KDim], float],
+):
 
-#     mesh = SimpleMesh()
-#     qc_out = zero_field(mesh, CellDim, KDim)
-#     qr_out = zero_field(mesh, CellDim, KDim)
-
-#     graupel(qc, qr, qc_out, qr_out, offset_provider={"Koff": KDim})
-
-#     precipitation_out_numpy = np.asarray(zero_field(mesh, CellDim))
-#     qc_out_numpy = np.asarray(qc_out).copy()
-#     qr_out_numpy = np.asarray(qr_out).copy()
-
-#     qc_out_numpy, qr_out_numpy, precipitation_out_numpy = graupel_numpy(
-#         np.asarray(qc), np.asarray(qr)
-#     )  # Changes qc, qr
-#     maximizeTendency(precipitation_out_numpy, 0.0, "precipitation")
+    _graupel_toy_scan(qc, qr, out=(sedimentation, qc, qr))
 
 
-# @scan_operator(axis=KDim, forward=True, init=(0.0, 0.0, 0.0))
-# def _graupel_stub(
-#     state: tuple[float, float, float],
-#     # Prognostic 3D Fields -> These are INOUT!
-#     qc: float,  # Field
-#     qr: float,  # Fieldmade up if
-#     precipitation: float,
-#     # Scalars
-#     a: float,
-#     b: float,
-# ) -> tuple[float, float, float]:
+@given(
+    random_field_strategy(SimpleMesh(), CellDim, KDim, min_value=1e-7, max_value=1),
+    random_field_strategy(SimpleMesh(), CellDim, KDim, min_value=1e-7, max_value=1),
+)
+@settings(deadline=None, max_examples=10)
+def test_graupel_scan(qc, qr):
 
-#     # Unwrap state
-#     qc_up, qr_up, precipitation_up = state
+    mesh = SimpleMesh()
+    sedimentation = zero_field(mesh, CellDim, KDim)
 
-#     # # Autoconversion: Cloud Drops -> Rain Drops
-#     # qc = qc - qc * a
-#     # qr = qr + qc * a
+    qc_numpy = np.asarray(qc).copy()
+    qr_numpy = np.asarray(qc).copy()
 
-#     # # Rain sedimentation (contribution from gridbox above)
-#     # qr = qr + precipitation_up
+    graupel_toy_scan(qc, qr, sedimentation, offset_provider={})
 
-#     # # Compute new sedimentation rate. Depends on qc value below
-#     # # in F90 there is an (if k /= ke) here
-#     # # We need to replace this with two compute regions.
-#     # # The second region doesnt have the feature blow
-#     # precipitation = 1e-6  # IF(qr_up <= 0.1, 0.0, b * qr_up ** 3)
+    # qc_numpy and qr_numpy are modified in place
+    precipitation_numpy = graupel_toy_numpy(qc_numpy, qr_numpy)
 
-#     # qr = qr - precipitation
-
-#     return (qc, qr, precipitation)
-
-
-# @program
-# def graupel_stub(
-#     qc: Field[CellDim, KDim, float],
-#     qr: Field[CellDim, KDim, float],
-#     rain_sedimentation: Field[CellDim, float],
-# ):
-#     a = 1
-#     b = 2
-#     _graupel_stub(
-#         qc,
-#         qr,
-#         a,
-#         b,
-#         out=(
-#             qc,
-#             qr,
-#             rain_sedimentation,
-#         ),
-#     )
-
-#     # precipitation = rain_sedimentation[last]
-
-
-
+    assert np.allclose(np.asarray(qc), qc_numpy)
+    assert np.allclose(np.asarray(qr), qr_numpy)
+    assert np.allclose(np.asarray(sedimentation)[:, -1], precipitation_numpy)
