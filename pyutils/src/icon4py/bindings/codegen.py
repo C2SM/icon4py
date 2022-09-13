51b3fe2c1d42cdc4b47f65f3c7a0c799b7aab123
samkellerhals <kellerhalssamuel@gmail.com>
2022-09-13 17:59:05 +0200

    Generate verify func declaration

--- pyutils/src/icon4py/bindings/codegen.py
+++ pyutils/src/icon4py/bindings/codegen.py
@@ -11,41 +11,78 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from dataclasses import dataclass
 from pathlib import Path
 from typing import Sequence
 
 import numpy as np
 from eve import Node
 from eve.codegen import JinjaTemplate as as_jinja
-from eve.codegen import TemplatedGenerator, format_source
+from eve.codegen import TemplatedGenerator
 
 from icon4py.bindings.cppgen import render_python_type
+from icon4py.pyutils.metadata import get_field_infos
 from icon4py.pyutils.stencil_info import StencilInfo
 
 
-@dataclass
 class CppHeader:
-    stencil_info: StencilInfo
+    def __init__(self, stencil_info: StencilInfo):
+        self.stencil_info = stencil_info
+        self.generator = HeaderGenerator
+        self.fields = get_field_infos(stencil_info.fvprog)
 
     def write(self, outpath: Path):
         header = self._generate_header()
-        source = format_source("cpp", HeaderGenerator.apply(header), style="LLVM")
+        source = self.generator.apply(header)
         self._source_to_file(outpath, source)
 
     def _generate_header(self):
+        stencil_name = self.stencil_info.fvprog.past_node.id
+        dsl_params = self._make_verify_params("_dsl")
+        rel_params = self._make_verify_params("_rel_tol")
+        abs_params = self._make_verify_params("_abs_tol")
+        out_params = self._make_verify_params()
+
         header = HeaderFile(
             runFunc=StencilFuncDeclaration(
-                funcname=self.stencil_info.fvprog.past_node.id,
+                funcname=stencil_name,
                 parameters=[
                     FunctionParameter(
-                        name=param.id, dtype=np.dtype(param.type.dtype.__str__())
+                        name=field_name,
+                        dtype=np.dtype(field_info.field.type.dtype.__str__()),
+                        inp=field_info.inp,
+                        out=field_info.out,
                     )
-                    for param in self.stencil_info.fvprog.past_node.params
+                    for field_name, field_info in self.fields.items()
                 ],
-            )
+            ),
+            # TODO: return correct parameters (dsl fields, output fields, rel/abs fields)
+            verifyFunc=VerifyFuncDeclaration(
+                funcname=stencil_name,
+                dsl_params=dsl_params,
+                rel_params=rel_params,
+                abs_params=abs_params,
+                out_params=out_params,
+            ),
         )
         return header
+
+    def _make_verify_params(self, pstring=""):
+
+        names = [
+            str(f"{f}{pstring}") for f, f_info in self.fields.items() if f_info.out
+        ]
+        types = [
+            np.dtype(f_info.field.type.dtype.__str__())
+            for f, f_info in self.fields.items()
+            if f_info.out
+        ]
+
+        field_dict = dict(zip(names, types))
+
+        return [
+            FunctionParameter(name=name, dtype=dtype, inp=False, out=True)
+            for name, dtype in field_dict.items()
+        ]
 
     def _source_to_file(self, outpath: Path, src: str):
         # write even if dir does not exist
@@ -57,6 +94,8 @@ class CppHeader:
 class FunctionParameter(Node):
     name: str
     dtype: np.dtype
+    inp: bool
+    out: bool
 
 
 class StencilFuncDeclaration(Node):
@@ -64,8 +103,18 @@ class StencilFuncDeclaration(Node):
     parameters: Sequence[FunctionParameter]
 
 
+class VerifyFuncDeclaration(Node):
+    # TODO: add (dsl fields, output fields, rel/abs fields)
+    funcname: str
+    dsl_params: Sequence[FunctionParameter]
+    out_params: Sequence[FunctionParameter]
+    rel_params: Sequence[FunctionParameter]
+    abs_params: Sequence[FunctionParameter]
+
+
 class HeaderFile(Node):
     runFunc: StencilFuncDeclaration
+    verifyFunc: VerifyFuncDeclaration
 
 
 class HeaderGenerator(TemplatedGenerator):
@@ -76,6 +125,7 @@ class HeaderGenerator(TemplatedGenerator):
         #include "driver-includes/cuda_utils.hpp"
         extern "C" {
         {{runFunc}}
+        {{verifyFunc}}
         }
         """
     )
@@ -83,6 +133,12 @@ class HeaderGenerator(TemplatedGenerator):
     StencilFuncDeclaration = as_jinja(
         """\
         void run_{{funcname}}({{",".join(parameters)}}, const int verticalStart, const int verticalEnd, const int horizontalStart, const int horizontalEnd) ;
+        """
+    )
+
+    VerifyFuncDeclaration = as_jinja(
+        """\
+        bool verify_{{funcname}}({{",".join(dsl_params)}}, {{",".join(out_params)}}, {{",".join(abs_params)}}, {{",".join(rel_params)}}, const int iteration) ;
         """
     )
 
