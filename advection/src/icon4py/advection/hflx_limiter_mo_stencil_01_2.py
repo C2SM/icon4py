24912528635eef66b317944bf1e653804fc345e9
abishekg7 <abishek.gopal@c2sm.ethz.ch>
2022-09-21 16:17:02 +0200

    edits to hflx_limiter_mo_stencil_01_2

--- advection/src/icon4py/advection/hflx_limiter_mo_stencil_01_2.py
+++ advection/src/icon4py/advection/hflx_limiter_mo_stencil_01_2.py
@@ -30,24 +30,21 @@ def _hflx_limiter_mo_stencil_01_2(
 
 ) -> tuple[Field[[CellDim, KDim], float],Field[[CellDim, KDim], float],Field[[CellDim, KDim], float],Field[[CellDim, KDim], float],Field[[CellDim, KDim], float],Field[[CellDim, KDim], float]]:
 
-    #z_mflx_anti = p_dtime * geofac_div / p_rhodz_new * z_anti
-    #z_mflx_anti = p_dtime * geofac_div / (p_rhodz_new * z_anti(C2E))
+    z_mflx_anti = p_dtime * geofac_div / p_rhodz_new * z_anti(C2E)
 
-    #min_z_flx_anti = minimum(0.0, p_dtime * geofac_div / (p_rhodz_new * z_anti(C2E)))
-    #max_z_flx_anti = maximum(0.0, p_dtime * geofac_div / (p_rhodz_new * z_anti(C2E)))
+    min_z_flx_anti = minimum(0.0, z_mflx_anti)
+    max_z_flx_anti = maximum(0.0, z_mflx_anti)
 
-    z_mflx_anti_in = -neighbor_sum(minimum(0.0, p_dtime * geofac_div / (p_rhodz_new * z_anti(C2E))), axis=C2EDim)
-    z_mflx_anti_out = neighbor_sum(maximum(0.0, p_dtime * geofac_div / (p_rhodz_new * z_anti(C2E))), axis=C2EDim)
-    #z_fluxdiv_c = neighbor_sum(z_mflx_low(C2E) * geofac_div, axis=C2EDim)
-    z_fluxdiv_c = broadcast(0.0, (CellDim, KDim))
+    z_mflx_anti_in = -neighbor_sum(min_z_flx_anti, axis=C2EDim)
+    z_mflx_anti_out = -neighbor_sum(max_z_flx_anti, axis=C2EDim)
+    #z_mflx_anti_in = -neighbor_sum(maximum(0.0, p_dtime * geofac_div / p_rhodz_new * z_anti(C2E)), axis=C2EDim)
+    #z_mflx_anti_out = neighbor_sum(maximum(0.0, p_dtime * geofac_div / p_rhodz_new * z_anti(C2E)), axis=C2EDim)
+    z_fluxdiv_c = neighbor_sum(z_mflx_low(C2E) * geofac_div, axis=C2EDim)
 
+    z_tracer_new_low = (p_cc * p_rhodz_now - p_dtime * z_fluxdiv_c) / p_rhodz_new
+    z_tracer_max = maximum(p_cc, z_tracer_new_low)
+    z_tracer_min = minimum(p_cc, z_tracer_new_low)
 
-    #z_tracer_new_low = (p_cc * p_rhodz_now - p_dtime * z_fluxdiv_c) / p_rhodz_new
-    #z_tracer_max = maximum(p_cc, z_tracer_new_low)
-    #z_tracer_min = minimum(p_cc, z_tracer_new_low)
-    z_tracer_new_low = broadcast(0.0, (CellDim, KDim))
-    z_tracer_max = broadcast(0.0, (CellDim, KDim))
-    z_tracer_min = broadcast(0.0, (CellDim, KDim))
 
     return z_mflx_anti_in, z_mflx_anti_out, z_fluxdiv_c, z_tracer_new_low, z_tracer_max, z_tracer_min
 
