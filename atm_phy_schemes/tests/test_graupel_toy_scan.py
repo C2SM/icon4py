e4f5ae30dcc25851523776e35fa6c9aaee8b5647
David Leutwyler <david.leutwyler@env.ethz.ch>
2022-10-03 13:00:20 +0000

    enable ternary

--- atm_phy_schemes/tests/test_graupel_toy_scan.py
+++ atm_phy_schemes/tests/test_graupel_toy_scan.py
@@ -12,10 +12,9 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import numpy as np
-from hypothesis import given, settings
-
-from functional.ffront.decorator import program, field_operator, scan_operator
+from functional.ffront.decorator import field_operator, program, scan_operator
 from functional.ffront.fbuiltins import Field
+from hypothesis import given, settings
 
 from icon4py.common.dimension import CellDim, KDim
 from icon4py.testutils.simple_mesh import SimpleMesh
@@ -26,27 +25,16 @@ from icon4py.testutils.utils import random_field_strategy, zero_field
 
 
 TODO GT4Py team:
-- ternary in scan_operator (Niki, fast track)
 - local IF statement in scan_operator --> big ticket item (Till)
 - think about optimized returning CellDim field (hotfix Till)
 - Pass scalars to scan()
 - Need index field for field_operator version
-
+- Need to pre-allocate everything we return
 
 To discuss:
 - Returns from scan. Carry, carry + return, 2D Fields in Return
 
-TODO DL:
-- Make compile
-- Recode in Python -> test scan operator
-- send code snippet to Till
 
-Questions:
-1. How and when to switch qc in and qc_out? -> Hannes nees to explain me functional programming
-
-
-Questions for field_operator version
-1.  Cant return both 2D and 3D tuples in field operator. "Incompatible fields in tuple: all fields must have the same dimensions." Is this intentional?
 """
 
 MESH = SimpleMesh()
@@ -68,14 +56,13 @@ def graupel_toy_numpy(qc, qr):
         qr[cell, k] += sedimentation
 
         # Compute new sedimentation rate (made up formula).
-        sedimentation = b * qr[cell, k - 1] ** 3  # if qr[cell, k] <= 0.1 else 0.0
+        sedimentation = b * qr[cell, k - 1] ** 3 if qr[cell, k] <= 0.1 else 0.0
 
         # Precipitation is qr arriving at the ground
         if k != qc.shape[1]:
             qr[cell, k] -= sedimentation
         else:
             precipitation[cell] = sedimentation
-
 
     return precipitation
 
@@ -89,8 +76,6 @@ def _graupel_toy_scan(
     b = 0.2
 
     # unpack carry
-    # TODO Discussion: Split current carry return into carry + return ? Here we dont touch qc_kMinus1
-    # Also: Discuss syntax
     sedimentation_kMinus1, qc_kMinus1, qr_kMinus1 = carry
 
     # Autoconversion: Cloud Drops -> Rain Drops
@@ -101,7 +86,7 @@ def _graupel_toy_scan(
     qr = qr + sedimentation_kMinus1
 
     # Compute new sedimentation rate (made up formula).
-    sedimentation = b * qr_kMinus1**3  # if qr[cell, k] <= 0.1 else 0.0
+    sedimentation = b * qr_kMinus1**3 if qr <= 0.1 else 0.0
 
     # if k is not np.shape(qc)[1]: #TODO: Need if on index field
     qr = qr_in - sedimentation
@@ -134,20 +119,18 @@ def graupel_toy_scan(
 )
 @settings(deadline=None, max_examples=10)
 def test_graupel_scan(qc, qr):
-    mesh = MESH
-    sedimentation = zero_field(mesh, CellDim, KDim)
 
-    qc_numpy = np.asarray(qc).copy()
-    qr_numpy = np.asarray(qc).copy()
+    mesh = MESH
+
+    sedimentation = zero_field(mesh, CellDim, KDim)
+    qc_numpy = np.asarray(qc)
+    qr_numpy = np.asarray(qr)
 
     graupel_toy_scan(qc, qr, sedimentation, offset_provider={})
 
     # qc_numpy and qr_numpy are modified in place
     precipitation_numpy = graupel_toy_numpy(qc_numpy, qr_numpy)
 
-    # Uncomment the following assert once gt4py fixes the bug
-    # when returning multiple fields
-    # assert np.allclose(np.asarray(qc), qc_numpy)
-    # assert np.allclose(np.asarray(qr), qr_numpy)
-    # TODO: Understand why the k order is the opposite of the expected order
+    assert np.allclose(np.asarray(qc), qc_numpy)
+    assert np.allclose(np.asarray(qr), qr_numpy)
     assert np.allclose(np.asarray(sedimentation)[:, 0], precipitation_numpy)
