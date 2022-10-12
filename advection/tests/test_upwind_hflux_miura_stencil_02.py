8b324a3bec04f3154d183e81d6f6e92133380133
Magdalena <luzm@ethz.ch>
2022-10-12 14:00:12 +0200

    Tracer advection stencil porting ml -> tracer advection stencils (#98)
    * adding hflx_limiter_mo_stencil_02
    
    * changes order of args and return a tuple
    
    * Revert "changes order of args and return a tuple"
    
    This reverts commit 99638df7024fcbcccf1ad37cda2b5e7651efb918.
    
    * porting step_advection_stencil_01
    
    * porting step_advection_stencil_02
    
    * porting upwind_hflux_miura_stencil_02
    
    * hflx_limiter_mo_stencil_04
    
    * fix step_advection_stencil_01
    
    * hflx_limiter_mo_stencil_03
    
    * clean up: move upwind_hflux_miura_stencil_02.py
    
    * add test for conditional branch in hflx_limiter_mo_stencil_04
    
    * switch to using min_over in hflx_limiter_mo_stencil_03
    
    * refactor hflx_limiter_mo_stencil_04, fix tests for Koffset
    
    * add upwind_vflux_ppm_stencil_01
    
    * upwind_hflux_miura_stencil_02 - fix sparse field issue, split into separate sums instead of using neighbor_sum
    
    * remove commented out code
    
    * fix: upwind_vflux_ppm_stencil_01
    
    * fix: hflx_limiter_mo_stencil_01: return tuple, use int32
    
    * hflx_limiter_pd_stencil_02.py: use int fields

---
+++ advection/tests/test_upwind_hflux_miura_stencil_02.py
@@ -0,0 +1,72 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import numpy as np
+from functional.iterator.embedded import StridedNeighborOffsetProvider
+
+from icon4py.advection.upwind_hflux_miura_stencil_02 import (
+    upwind_hflux_miura_stencil_02,
+)
+from icon4py.common.dimension import C2E2CDim, CECDim, CellDim, KDim
+from icon4py.testutils.simple_mesh import SimpleMesh
+from icon4py.testutils.utils import as_1D_sparse_field, random_field, zero_field
+
+
+def upwind_hflux_miura_stencil_02_numpy(
+    c2e2c: np.ndarray,
+    p_cc: np.ndarray,
+    lsq_pseudoinv_1: np.ndarray,
+    lsq_pseudoinv_2: np.ndarray,
+) -> tuple[np.ndarray]:
+    p_cc_e = np.expand_dims(p_cc, axis=1)
+    n_diff = p_cc[c2e2c] - p_cc_e
+    lsq_pseudoinv_2 = np.expand_dims(lsq_pseudoinv_2, axis=-1)
+    lsq_pseudoinv_1 = np.expand_dims(lsq_pseudoinv_1, axis=-1)
+    p_coeff_1 = np.sum(lsq_pseudoinv_1 * n_diff, axis=1)
+    p_coeff_2 = np.sum(lsq_pseudoinv_2 * n_diff, axis=1)
+    return p_coeff_1, p_coeff_2
+
+
+def test_upwind_hflux_miura_stencil_02():
+    mesh = SimpleMesh()
+    p_cc = random_field(mesh, CellDim, KDim)
+    lsq_pseudoinv_1 = random_field(mesh, CellDim, C2E2CDim)
+    lsq_pseudoinv_1_field = as_1D_sparse_field(lsq_pseudoinv_1, CECDim)
+
+    lsq_pseudoinv_2 = random_field(mesh, CellDim, C2E2CDim)
+    lsq_pseudoinv_2_field = as_1D_sparse_field(lsq_pseudoinv_2, CECDim)
+    p_coeff_1 = zero_field(mesh, CellDim, KDim)
+    p_coeff_2 = zero_field(mesh, CellDim, KDim)
+
+    ref_1, ref_2 = upwind_hflux_miura_stencil_02_numpy(
+        mesh.c2e2c,
+        np.asarray(p_cc),
+        np.asarray(lsq_pseudoinv_1),
+        np.asarray(lsq_pseudoinv_2),
+    )
+
+    upwind_hflux_miura_stencil_02(
+        p_cc,
+        lsq_pseudoinv_1_field,
+        lsq_pseudoinv_2_field,
+        p_coeff_1,
+        p_coeff_2,
+        offset_provider={
+            "C2E2C": mesh.get_c2e2c_offset_provider(),
+            "C2CEC": StridedNeighborOffsetProvider(CellDim, CECDim, mesh.n_c2e2c),
+        },
+    )
+    co1 = np.asarray(p_coeff_1)
+    co2 = np.asarray(p_coeff_2)
+    assert np.allclose(ref_1, co1)
+    assert np.allclose(ref_2, co2)
