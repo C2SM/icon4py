a379eb9150119fe9cdf2b119257f00a9f012b638
Magdalena Luz <luzm@ethz.ch>
2022-10-14 08:35:19 +0200

    rename set_zero field operators

--- advection/src/icon4py/advection/set_zero_c_k.py
+++ advection/src/icon4py/advection/set_zero_c_k.py
@@ -19,10 +19,10 @@ from icon4py.common.dimension import CellDim, KDim
 
 
 @field_operator
-def _set_zero() -> Field[[CellDim, KDim], float]:
+def _set_zero_c_k() -> Field[[CellDim, KDim], float]:
     return broadcast(0.0, (CellDim, KDim))
 
 
 @program
 def set_zero_c_k(field: Field[[CellDim, KDim], float]):
-    _set_zero(out=field)
+    _set_zero_c_k(out=field)
