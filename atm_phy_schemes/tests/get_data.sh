be5cb9a0d13601d674ad52e22d641466bd7955ae
David Leutwyler <david.leutwyler@env.ethz.ch>
2022-11-04 11:39:00 +0100

    fix

--- atm_phy_schemes/tests/get_data.sh
+++ atm_phy_schemes/tests/get_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #Download serialized data from IAC FTP
-wget -r --no-parent -nH -nc --cut-dirs=3 -q -ftp://iacftp.ethz.ch/pub_read/davidle/ser_data_icon_graupel/ser_data/
+wget -r --no-parent -nH -nc --cut-dirs=3 -q ftp://iacftp.ethz.ch/pub_read/davidle/ser_data_icon_graupel/ser_data/
