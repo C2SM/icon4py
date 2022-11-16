3686b7fe6e53844f0b300a21172415ec7a6629e0
Samuel <kellerhalssamuel@gmail.com>
2022-11-16 09:15:28 +0100

    Port Bindings Generator from c++ to Python (#90)
    Introduces the rewritten bindings generator, implemented in Python, to icon4py. It replaces the c++ bindings generator from icon-exclaim.
    
    Co-authored-by: mroethlin <Matthias.Roethlin@MeteoSwiss.ch>
    Co-authored-by: mroethlin <53755555+mroethlin@users.noreply.github.com>

---
+++ pyutils/src/icon4py/pyutils/metadata.py
@@ -0,0 +1,184 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+from __future__ import annotations
+
+import dataclasses
+import importlib
+import types
+from typing import Any, TypeGuard
+
+import eve
+from functional.common import Dimension, DimensionKind
+from functional.ffront import common_types as ct
+from functional.ffront import program_ast as past
+from functional.ffront.decorator import FieldOperator, Program, program
+from functional.iterator import ir as itir
+
+from icon4py.common.dimension import CellDim, EdgeDim, Koff, VertexDim
+from icon4py.pyutils.exceptions import (
+    InvalidConnectivityException,
+    MultipleFieldOperatorException,
+)
+from icon4py.pyutils.icochainsize import IcoChainSize
+
+
+@dataclasses.dataclass(frozen=True)
+class StencilInfo:
+    fvprog: Program
+    connectivity_chains: list[eve.concepts.SymbolRef]
+    offset_provider: dict
+
+
+@dataclasses.dataclass(frozen=True)
+class FieldInfo:
+    field: past.DataSymbol
+    inp: bool
+    out: bool
+
+
+def is_list_of_names(obj: Any) -> TypeGuard[list[past.Name]]:
+    return isinstance(obj, list) and all(isinstance(i, past.Name) for i in obj)
+
+
+def get_field_infos(fvprog: Program) -> dict[str, FieldInfo]:
+    """Extract and format the in/out fields from a Program."""
+    assert is_list_of_names(
+        fvprog.past_node.body[0].args
+    ), "Found unsupported expression in input arguments."
+    input_arg_ids = set(arg.id for arg in fvprog.past_node.body[0].args)
+
+    out_arg = fvprog.past_node.body[0].kwargs["out"]
+    assert isinstance(out_arg, (past.Name, past.TupleExpr))
+    output_fields = out_arg.elts if isinstance(out_arg, past.TupleExpr) else [out_arg]
+    output_arg_ids = set(arg.id for arg in output_fields)  # type: ignore
+
+    fields: dict[str, FieldInfo] = {
+        field_node.id: FieldInfo(
+            field=field_node,
+            inp=(field_node.id in input_arg_ids),
+            out=(field_node.id in output_arg_ids),
+        )
+        for field_node in fvprog.past_node.params
+    }
+
+    return fields
+
+
+def import_definition(name: str) -> Program | FieldOperator | types.FunctionType:
+    """Import a stencil from a given module.
+
+    Note:
+        The stencil program and module are assumed to have the same name.
+    """
+    module_name, member_name = name.split(":")
+    fencil = getattr(importlib.import_module(module_name), member_name)
+    return fencil
+
+
+def get_fvprog(fencil_def: Program | Any) -> Program:
+    match fencil_def:
+        case Program():
+            fvprog = fencil_def
+        case _:
+            fvprog = program(fencil_def)
+
+    if len(fvprog.past_node.body) > 1:
+        raise MultipleFieldOperatorException()
+
+    return fvprog
+
+
+def provide_offset(offset: str) -> types.SimpleNamespace | Dimension:
+    if offset == Koff.value:
+        assert len(Koff.target) == 1
+        assert Koff.source == Koff.target[0]
+        return Koff.source
+    else:
+        return provide_neighbor_table(offset)
+
+
+def provide_neighbor_table(chain: str) -> types.SimpleNamespace:
+    """Build an offset provider based on connectivity chain string.
+
+    Connectivity strings must contain one of the following connectivity type identifiers:
+    C (cell), E (Edge), V (Vertex) and be separated by a '2' e.g. 'E2V'. If the origin is to
+    be included, the string should terminate with O (uppercase o), e.g. 'C2E2CO`.
+
+    Handling of "new" sparse dimensions
+
+    A new sparse dimension may look like C2CE or V2CVEC. In this case, we need to strip the 2
+    and pass the tokens after to the algorithm below
+    """
+    # note: this seems really brittle. maybe agree on a keyword to indicate new sparse fields?
+    new_sparse_field = any(
+        len(token) > 1 for token in chain.split("2")
+    ) and not chain.endswith("O")
+    if new_sparse_field:
+        chain = chain.split("2")[1]
+
+    location_chain = []
+    include_center = False
+    for letter in chain:
+        if letter == "C":
+            location_chain.append(CellDim)
+        elif letter == "E":
+            location_chain.append(EdgeDim)
+        elif letter == "V":
+            location_chain.append(VertexDim)
+        elif letter == "O":
+            include_center = True
+        elif letter == "2":
+            pass
+        else:
+            raise InvalidConnectivityException(location_chain)
+    return types.SimpleNamespace(
+        max_neighbors=IcoChainSize.get(location_chain) + include_center,
+        has_skip_values=False,
+    )
+
+
+def scan_for_offsets(fvprog: Program) -> list[eve.concepts.SymbolRef]:
+    """Scan PAST node for offsets and return a set of all offsets."""
+    all_types = (
+        fvprog.past_node.pre_walk_values().if_isinstance(past.Symbol).getattr("type")
+    )
+    all_field_types = [
+        symbol_type
+        for symbol_type in all_types
+        if isinstance(symbol_type, ct.FieldType)
+    ]
+    all_dims = set(i for j in all_field_types for i in j.dims)
+    all_offset_labels = (
+        fvprog.itir.pre_walk_values()
+        .if_isinstance(itir.OffsetLiteral)
+        .getattr("value")
+        .to_list()
+    )
+    all_dim_labels = [dim.value for dim in all_dims if dim.kind == DimensionKind.LOCAL]
+
+    # we want to preserve order in the offsets for code generation reproducibility
+    sorted_dims = sorted(set(all_offset_labels + all_dim_labels))
+    return sorted_dims
+
+
+def get_stencil_info(
+    fencil_def: Program | FieldOperator | types.FunctionType,
+) -> StencilInfo:
+    """Generate StencilInfo dataclass from a fencil definition."""
+    fvprog = get_fvprog(fencil_def)
+    offsets = scan_for_offsets(fvprog)
+    offset_provider = {}
+    for offset in offsets:
+        offset_provider[offset] = provide_offset(offset)
+    connectivity_chains = [offset for offset in offsets if offset != Koff.value]
+    return StencilInfo(fvprog, connectivity_chains, offset_provider)
---
+++ pyutils/tests/test_exceptions.py
@@ -0,0 +1,154 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import pytest
+from functional.ffront.decorator import field_operator, program
+from functional.ffront.fbuiltins import Dimension, Field, neighbor_sum
+
+from icon4py.bindings.codegen.render.location import LocationRenderer
+from icon4py.bindings.entities import Offset, chain_from_str
+from icon4py.bindings.exceptions import (
+    BindingsRenderingException,
+    BindingsTypeConsistencyException,
+)
+from icon4py.bindings.workflow import PyBindGen
+from icon4py.common.dimension import E2CDim, EdgeDim, KDim
+from icon4py.pyutils.metadata import get_stencil_info
+
+
+def test_invalid_offset():
+    with pytest.raises(BindingsTypeConsistencyException):
+        Offset(chain="E2X")
+
+
+def test_invalid_field_in_program():
+    ZDim = Dimension("Z")
+
+    @field_operator
+    def bad_stencil(
+        z: Field[[ZDim], float],
+    ) -> Field[[ZDim], float]:
+        return z
+
+    @program
+    def bad_program(
+        z: Field[[ZDim], float],
+    ):
+        bad_stencil(z, out=z)
+
+    with pytest.raises(BindingsTypeConsistencyException):
+        stencil_info = get_stencil_info(bad_program)
+        PyBindGen(stencil_info, 128, 1)
+
+
+def test_chain_from_str():
+    with pytest.raises(BindingsTypeConsistencyException):
+        chain_from_str("E2Z")
+
+
+def test_non_sparse_field_neighbors():
+    @field_operator
+    def bad_stencil(
+        a: Field[[EdgeDim], float],
+    ) -> Field[[EdgeDim], float]:
+        return a
+
+    @program
+    def bad_program(
+        a: Field[[EdgeDim], float],
+    ):
+        bad_stencil(a, out=a)
+
+    with pytest.raises(BindingsTypeConsistencyException):
+        stencil_info = get_stencil_info(bad_program)
+        bindgen = PyBindGen(stencil_info, 128, 1)
+        [field.get_num_neighbors() for field in bindgen.fields]
+
+
+def test_ctype_rendering_exception():
+    @field_operator
+    def bad_stencil(
+        a: Field[[EdgeDim], float],
+    ) -> Field[[EdgeDim], float]:
+        return a
+
+    @program
+    def bad_program(
+        a: Field[[EdgeDim], float],
+    ):
+        bad_stencil(a, out=a)
+
+    with pytest.raises(BindingsRenderingException):
+        stencil_info = get_stencil_info(bad_program)
+        bindgen = PyBindGen(stencil_info, 128, 1)
+        [field.renderer.render_ctype("py") for field in bindgen.fields]
+
+
+def test_sparse_field_sid_rendering_exception():
+    @field_operator
+    def reduction(nb_field: Field[[EdgeDim, E2CDim], float]) -> Field[[EdgeDim], float]:
+        return neighbor_sum(nb_field, axis=E2CDim)
+
+    @program
+    def reduction_prog(
+        nb_field: Field[[EdgeDim, E2CDim], float], out: Field[[EdgeDim], float]
+    ):
+        reduction(nb_field, out=out)
+
+    with pytest.raises(BindingsRenderingException):
+        stencil_info = get_stencil_info(reduction_prog)
+        bindgen = PyBindGen(stencil_info, 128, 1)
+        [field.renderer.render_sid() for field in bindgen.fields]
+
+
+def test_scalar_sid_rendering_exception():
+    @field_operator
+    def bad_stencil(a: Field[[EdgeDim], float], b: float) -> Field[[EdgeDim], float]:
+        return a + b
+
+    @program
+    def bad_program(a: Field[[EdgeDim], float], b: float):
+        bad_stencil(a, b, out=a)
+
+    stencil_info = get_stencil_info(bad_program)
+    bindgen = PyBindGen(stencil_info, 128, 1)
+
+    with pytest.raises(BindingsRenderingException):
+        [field.renderer.render_sid() for field in bindgen.fields]
+
+    with pytest.raises(BindingsRenderingException):
+        [field.renderer.render_stride_type() for field in bindgen.fields]
+
+
+def test_serialise_func_rendering_exception():
+    @field_operator
+    def bad_stencil(
+        a: Field[[KDim], float],
+    ) -> Field[[KDim], float]:
+        return a
+
+    @program
+    def bad_program(
+        a: Field[[KDim], float],
+    ):
+        bad_stencil(a, out=a)
+
+    with pytest.raises(BindingsRenderingException):
+        stencil_info = get_stencil_info(bad_program)
+        bindgen = PyBindGen(stencil_info, 128, 1)
+        [field.renderer.render_serialise_func() for field in bindgen.fields]
+
+
+def test_invalid_location_type():
+    with pytest.raises(BindingsRenderingException):
+        LocationRenderer.location_type("Klevel")
