- fix level offsets - add test for nrdmax calculation validating icon data

atm_dyn_iconam/src/icon4py/atm_dyn_iconam/diffusion.py
@@ -19,23 +19,24 @@ import numpy as np
 from functional.ffront.decorator import field_operator, program
 from functional.ffront.fbuiltins import Field, broadcast, maximum, minimum
 from functional.iterator.embedded import np_as_located_field
+from functional.program_processors.runners import gtfn_cpu
 
 from icon4py.atm_dyn_iconam.constants import CPD, GAS_CONSTANT_DRY_AIR
 from icon4py.atm_dyn_iconam.diagnostic import DiagnosticState
 from icon4py.atm_dyn_iconam.fused_mo_nh_diffusion_stencil_02_03 import (
-    fused_mo_nh_diffusion_stencil_02_03,
+     _fused_mo_nh_diffusion_stencil_02_03,
 )
 from icon4py.atm_dyn_iconam.fused_mo_nh_diffusion_stencil_04_05_06 import (
-    fused_mo_nh_diffusion_stencil_04_05_06,
+    _fused_mo_nh_diffusion_stencil_04_05_06,
 )
 from icon4py.atm_dyn_iconam.fused_mo_nh_diffusion_stencil_07_08_09_10 import (
-    fused_mo_nh_diffusion_stencil_07_08_09_10,
+    _fused_mo_nh_diffusion_stencil_07_08_09_10,
 )
 from icon4py.atm_dyn_iconam.fused_mo_nh_diffusion_stencil_11_12 import (
     fused_mo_nh_diffusion_stencil_11_12,
 )
 from icon4py.atm_dyn_iconam.fused_mo_nh_diffusion_stencil_13_14 import (
-    fused_mo_nh_diffusion_stencil_13_14,
+    _fused_mo_nh_diffusion_stencil_13_14,
 )
 from icon4py.atm_dyn_iconam.horizontal import (
     HorizontalMarkerIndex,
@@ -49,14 +50,13 @@ from icon4py.atm_dyn_iconam.icon_grid import (
 from icon4py.atm_dyn_iconam.interpolation_state import InterpolationState
 from icon4py.atm_dyn_iconam.metric_state import MetricState
 from icon4py.atm_dyn_iconam.mo_intp_rbf_rbf_vec_interpol_vertex import (
-    mo_intp_rbf_rbf_vec_interpol_vertex,
+    _mo_intp_rbf_rbf_vec_interpol_vertex,
 )
 from icon4py.atm_dyn_iconam.mo_nh_diffusion_stencil_01 import (
     _mo_nh_diffusion_stencil_01,
-    mo_nh_diffusion_stencil_01,
 )
 from icon4py.atm_dyn_iconam.mo_nh_diffusion_stencil_16 import (
-    mo_nh_diffusion_stencil_16,
+    _mo_nh_diffusion_stencil_16,
 )
 from icon4py.atm_dyn_iconam.prognostic import PrognosticState
 from icon4py.common.dimension import (
@@ -272,6 +272,35 @@ def _set_zero_v_k() -> Field[[VertexDim, KDim], float]:
 def set_zero_v_k(field: Field[[VertexDim, KDim], float]):
     _set_zero_v_k(out=field)
 
+def init_nabla2_factor_in_upper_damping_zone(
+    k_size: int,
+    nrdmax:int,
+    nshift: int,
+    physical_heights: np.ndarray
+) -> Field[[KDim], float]:
+    """
+    calculated diff_multfac_n2w
+
+    numpy version gt4py does not allow non-constant indexing into fields
+
+    Args
+        k_size: number of vertical levels
+        nrdmax: index of the level where rayleigh dampint starts
+        nshift:
+        physcial_heights: vector of physical heights [m] of the height levels
+    """
+    buffer = np.zeros(k_size)
+    buffer[2: nrdmax + 1] = (
+        1.0
+        / 12.0
+        * ((
+            physical_heights[2 + nshift: nrdmax + 1 + nshift]
+            - physical_heights[nshift + nrdmax + 1]
+        )
+        / (physical_heights[2] - physical_heights[nshift + nrdmax + 1]))**4
+    )
+    return np_as_located_field(KDim)(buffer)
+
 
 class DiffusionConfig:
     """contains necessary parameter to configure a diffusion run.
@@ -281,48 +310,39 @@ class DiffusionConfig:
     currently we use the MCH r04b09_dsl experiment as constants here. These should
     be read from config and the default from mo_diffusion_nml.f90 set as defaults.
 
-
-
     TODO: [ml] read from config
-    TODO: [ml] handle dependencies on other namelists (below...)
-    TODO: move this to test: it sets up the mch_ch_r04b09_dsl experiment
+    TODO: [ml] handle dependencies on other namelists (see below...)
     """
-
-    @classmethod
-    def create_with_defaults(cls):
-        """
-        Create DiffusionConfig.
-
-        initialize with values from exp.mch_ch_r04b09_dsl namelist
-        """
-        horizontal = HorizontalMeshConfig(
-            num_vertices=50000, num_cells=50000, num_edges=50000
-        )
-
-        grid = IconGrid().with_config(MeshConfig(horizontalMesh=horizontal))
-        verticalParams = VerticalModelParams(
-            rayleigh_damping_height=12500, vct_a=np.zeros(grid.k_levels())
-        )
-        return cls(grid=grid, vertical_params=verticalParams)
-
-    def __init__(self, grid: IconGrid, vertical_params: VerticalModelParams):
-        # TODO [ml]: external stuff grid related, p_patch,
+    def __init__(self,
+                 grid: IconGrid,
+                 vertical_params: VerticalModelParams,
+                 diffusion_type:int = 5,
+                 apply_to_horizontal_wind: bool = True,
+                 apply_to_vertical_wind: bool = True,
+                 apply_to_temperature: bool = True,
+                 reconstruction_type_smag: int = 1,
+                 compute_3d_smag_coeff:bool = False,
+                 temperature_discretization: int=2,
+                 horizontal_efdt_ratio: float = 24.0,
+                 smag_scaling_factor: float = 0.025
+                 ):
+        # TODO [ml]: move external stuff out: grid related stuff, other than diffusion namelists (see below
         self.grid = grid
         self.vertical_params = vertical_params
         # from namelist diffusion_nml
-        self.diffusion_type = 5  # hdiff_order ! order of nabla operator for diffusion
-        self.lhdiff_vn = True  # ! diffusion on the horizontal wind field
-        self.lhdiff_temp = True  # ! diffusion on the temperature field
-        self.lhdiff_w = True  # ! diffusion on the vertical wind field
+        self.diffusion_type = diffusion_type  # hdiff_order ! order of nabla operator for diffusion
+        self.lhdiff_vn = apply_to_horizontal_wind  # ! diffusion on the horizontal wind field
+        self.lhdiff_temp = apply_to_temperature  # ! diffusion on the temperature field
+        self.lhdiff_w = apply_to_vertical_wind  # ! diffusion on the vertical wind field
         self.lhdiff_rcf = True  # namelist, remove if always true
         self.itype_vn_diffu = (
-            1  # ! reconstruction method used for Smagorinsky diffusion
+            reconstruction_type_smag  # ! reconstruction method used for Smagorinsky diffusion
         )
-        self.l_smag_d = False  # namelist lsmag_d,  if `true`, compute 3D Smagorinsky diffusion coefficient.
+        self.l_smag_d = compute_3d_smag_coeff  # namelist lsmag_d,  if `true`, compute 3D Smagorinsky diffusion coefficient.
 
-        self.itype_t_diffu = 2  # ! discretization of temperature diffusion
-        self.hdiff_efdt_ratio = 24.0  # ! ratio of e-folding time to time step
-        self.hdiff_smag_fac = 0.025  # ! scaling factor for Smagorinsky diffusion
+        self.itype_t_diffu = temperature_discretization  # ! discretization of temperature diffusion
+        self.hdiff_efdt_ratio = horizontal_efdt_ratio  # ! ratio of e-folding time to time step
+        self.hdiff_smag_fac = smag_scaling_factor  # ! scaling factor for Smagorinsky diffusion
 
         # from other namelists
         # from parent namelist nonhydrostatic_nml
@@ -333,7 +353,7 @@ class DiffusionConfig:
         # denom_diffu_v = 150   ! denominator for lateral boundary diffusion of velocity
         self.lateral_boundary_denominator = DiffusionTupleVT(v=200.0, t=135.0)
 
-        # namelist grid_nml -> TODO [ml] should go to grid config?
+        # namelist grid_nml
         self.l_limited_area = True
 
         # name list: interpol_nml
@@ -426,7 +446,7 @@ class Diffusion:
         self,
         config: DiffusionConfig,
         params: DiffusionParams,
-        a_vect: Field[[KDim], float],
+        vct_a: Field[[KDim], float],
     ):
         """
         Initialize Diffusion granule.
@@ -475,15 +495,15 @@ class Diffusion:
         enhanced_smagorinski_factor(
             *params.smagorinski_factor,
             *params.smagorinski_height,
-            a_vect,
+            vct_a,
             self.enh_smag_fac,
             offset_provider={"Koff": KDim},
         )
 
-        self.diff_multfac_n2w = (
-            config.vertical_params.init_nabla2_factor_in_upper_damping_zone(
-                k_size=config.grid.k_levels()
-            )
+        self.diff_multfac_n2w = init_nabla2_factor_in_upper_damping_zone(
+                k_size=config.grid.k_levels(), nshift = 0, physical_heights=np.asarray(vct_a),
+            nrdmax=self.config.vertical_params.index_of_damping_height
+
         )
         self.diff_multfac_smag = np_as_located_field(KDim)(
             np.zeros(config.grid.k_levels())
@@ -504,6 +524,9 @@ class Diffusion:
         self.horizontal_cell_index = np_as_located_field(CellDim)(
             np.arange((shape_ck[0]))
         )
+        self.horizontal_edge_index = np_as_located_field(EdgeDim)(np.arange((shape_ek[0])))
+
+
 
     def run(
         self,
@@ -547,12 +570,11 @@ class Diffusion:
 
         # 1.  CALL rbf_vec_interpol_vertex
 
-        mo_intp_rbf_rbf_vec_interpol_vertex(
+        _mo_intp_rbf_rbf_vec_interpol_vertex(
             prognostic_state.normal_wind,
             interpolation_state.rbf_coeff_1,
             interpolation_state.rbf_coeff_2,
-            self.u_vert,
-            self.v_vert,
+            out=(self.u_vert, self.v_vert),
             domain={
                 KDim: (0, klevels),
                 VertexDim: self.grid.get_indices_from_to(
@@ -562,6 +584,7 @@ class Diffusion:
                 ),
             },
             offset_provider={"V2E": self.grid.get_v2e_offset_provider()},
+
         )
 
         # 2.  HALO EXCHANGE -- CALL sync_patch_array_mult
@@ -569,7 +592,7 @@ class Diffusion:
         # 0c. dtime dependent stuff: enh_smag_factor, ~~r_dtimensubsteps~~
         scale_k(self.enh_smag_fac, dtime, self.diff_multfac_smag, offset_provider={})
 
-        mo_nh_diffusion_stencil_01(
+        _mo_nh_diffusion_stencil_01(
             self.diff_multfac_smag,
             tangent_orientation,
             inverse_primal_edge_lengths,
@@ -582,11 +605,7 @@ class Diffusion:
             dual_normal_vert[1],
             prognostic_state.normal_wind,
             self.smag_limit,
-            self.kh_smag_e,
-            self.kh_smag_ec,
-            self.z_nabla2_e,
             self.smag_offset,
-            dtime,
             domain={
                 KDim: (0, klevels),
                 EdgeDim: (
@@ -597,74 +616,80 @@ class Diffusion:
                     )
                 ),
             },
+            out=(self.kh_smag_e, self.kh_smag_ec, self.z_nabla2_e),
             offset_provider={"E2C2V": self.grid.get_e2c2v_connectivity()},
-        )
+            backend = gtfn_cpu.run_gtfn
+         )
 
-        fused_mo_nh_diffusion_stencil_02_03(
+        _fused_mo_nh_diffusion_stencil_02_03(
             self.kh_smag_ec,
             prognostic_state.normal_wind,
             interpolation_state.e_bln_c_s,
             interpolation_state.geofac_div,
             self.diff_multfac_smag,
             metric_state.wgtfac_c,
-            diagnostic_state.div_ic,
-            diagnostic_state.hdef_ic,
+            out=(
+                diagnostic_state.div_ic,
+                diagnostic_state.hdef_ic,
+            ),
             domain={
                 KDim: (0, klevels),
                 CellDim: (
-                    self.grid.get_indices_from_to(
-                        HorizontalMarkerIndex._NUDGING_CELLS,
-                        HorizontalMarkerIndex._HALO_CELLS,
+                    self.grid.get_indices_from_to(CellDim,
+                        HorizontalMarkerIndex.nudging(CellDim),
+                        HorizontalMarkerIndex.halo(CellDim)
                     )
                 ),
             },
             offset_provider={"C2E": self.grid.get_c2e_connectivity()},
+            backend=gtfn_cpu.run_gtfn
         )
 
         # 4.  IF (discr_vn > 1) THEN CALL sync_patch_array -> false for MCH
 
         # 5.  CALL rbf_vec_interpol_vertex_wp
-        mo_intp_rbf_rbf_vec_interpol_vertex(
+        _mo_intp_rbf_rbf_vec_interpol_vertex(
             self.z_nabla2_e,
             interpolation_state.rbf_coeff_1,
             interpolation_state.rbf_coeff_2,
-            self.u_vert,
-            self.v_vert,
-            domain={
-                KDim: (0, klevels),
-                VertexDim: self.grid.get_indices_from_to(
-                    VertexDim,
-                    HorizontalMarkerIndex.local_boundary(VertexDim) + 3,
-                    HorizontalMarkerIndex.halo(VertexDim),
-                ),
-            },
+            out=(self.u_vert, self.v_vert),
+            # domain={
+            #     KDim: (0, klevels),
+            #     VertexDim: self.grid.get_indices_from_to(
+            #         VertexDim,
+            #         HorizontalMarkerIndex.local_boundary(VertexDim) + 3,
+            #         HorizontalMarkerIndex.halo(VertexDim),
+            #     ),
+            # },
             offset_provider={"V2E": self.grid.get_e2v_connectivity()},
         )
         # 6.  HALO EXCHANGE -- CALL sync_patch_array_mult
 
         # 7.  mo_nh_diffusion_stencil_04, mo_nh_diffusion_stencil_05
         # 7a. IF (l_limited_area .OR. jg > 1) mo_nh_diffusion_stencil_06
-        # TODO where to get index field from
-        # horz_idx: Field[[EdgeDim], int32],
-        # start_2nd_nudge_line_idx_e: int32,
 
-        fused_mo_nh_diffusion_stencil_04_05_06(
-            u_vert=self.u_vert,
-            v_vert=self.v_vert,
-            primal_normal_vert_v1=primal_normal_vert.x,
-            primal_normal_vert_v2=primal_normal_vert.y,
-            z_nabla2_e=self.z_nabla2_e,
-            inv_vert_vert_length=inverse_vertical_vertex_lengths,
-            inv_primal_edge_length=inverse_primal_edge_lengths,
-            area_edges=edge_areas,
-            kh_smag_e=self.kh_smag_e,
-            diff_multfac_vn=self.diff_multfac_vn,
-            nudgecoeff_e=interpolation_state.nudgecoeff_e,
-            vn=prognostic_state.normal_wind,
-            horz_idx=None,
-            nudgezone_diff=self.nudgezone_diff,
-            fac_bdydiff_v=self.fac_bdydiff_v,
-            start_2nd_nudge_line_idx_e=None,
+        start_2nd_nudge_line = self.grid.get_indices_from_to(
+            EdgeDim,
+            HorizontalMarkerIndex.nudging(EdgeDim)-1,
+            HorizontalMarkerIndex.nudging(EdgeDim)-1)[0]
+        _fused_mo_nh_diffusion_stencil_04_05_06(
+            self.u_vert,
+            self.v_vert,
+            primal_normal_vert.x,
+            primal_normal_vert.y,
+            self.z_nabla2_e,
+            inverse_vertical_vertex_lengths,
+            inverse_primal_edge_lengths,
+            edge_areas,
+            self.kh_smag_e,
+            self.diff_multfac_vn,
+            interpolation_state.nudgecoeff_e,
+            prognostic_state.normal_wind,
+            self.horizontal_edge_index,
+            self.nudgezone_diff,
+            self.fac_bdydiff_v,
+            start_2nd_nudge_line,
+            out=prognostic_state.normal_wind,
             domain={
                 KDim: (0, klevels),
                 EdgeDim: self.grid.get_indices_from_to(
@@ -677,23 +702,28 @@ class Diffusion:
         )
         # 7b. mo_nh_diffusion_stencil_07, mo_nh_diffusion_stencil_08,
         #     mo_nh_diffusion_stencil_09, mo_nh_diffusion_stencil_10
+        interior_start_index, halo_endindex = self.grid.get_indices_from_to(
+            CellDim,
+            HorizontalMarkerIndex.interior(CellDim),
+            HorizontalMarkerIndex.halo(CellDim))
 
-        fused_mo_nh_diffusion_stencil_07_08_09_10(
-            area=cell_areas,
-            geofac_n2s=interpolation_state.geofac_n2s,
-            geofac_grg_x=interpolation_state.geofac_grg_x,
-            geofac_grg_y=interpolation_state.geofac_grg_y,
-            w_old=prognostic_state.vertical_wind,
-            w=prognostic_state.vertical_wind,
-            dwdx=diagnostic_state.dwdx,
-            dwdy=diagnostic_state.dwdy,
-            diff_multfac_w=self.diff_multfac_w,
-            diff_multfac_n2w=self.diff_multfac_n2w,
-            vert_idx=self.vertical_index,
-            horz_idx=self.horizontal_cell_index,
-            nrdmax=self.config.vertical_params.index_of_damping_height,
-            interior_idx=HorizontalMarkerIndex.interior(CellDim),
-            halo_idx=HorizontalMarkerIndex.halo(CellDim),
+        _fused_mo_nh_diffusion_stencil_07_08_09_10(
+            cell_areas,
+            interpolation_state.geofac_n2s,
+            interpolation_state.geofac_grg_x,
+            interpolation_state.geofac_grg_y,
+            prognostic_state.vertical_wind,
+            prognostic_state.vertical_wind,
+            diagnostic_state.dwdx,
+            diagnostic_state.dwdy,
+            self.diff_multfac_w,
+            self.diff_multfac_n2w,
+            self.vertical_index,
+            self.horizontal_cell_index,
+            self.config.vertical_params.index_of_damping_height,
+            interior_start_index,
+            halo_endindex,
+            out=(prognostic_state.vertical_wind, diagnostic_state.dwdx, diagnostic_state.dwdy),
             domain={
                 KDim: (0, klevels),
                 CellDim: self.grid.get_indices_from_to(
@@ -711,10 +741,10 @@ class Diffusion:
 
         # TODO check: kh_smag_e is an out field, should  not be calculated in init?
         fused_mo_nh_diffusion_stencil_11_12(
-            theta_v=prognostic_state.theta_v,
-            theta_ref_mc=metric_state.theta_ref_mc,
-            thresh_tdiff=self.thresh_tdiff,
-            kh_smag_e=self.kh_smag_e,
+            prognostic_state.theta_v,
+            metric_state.theta_ref_mc,
+            self.thresh_tdiff,
+            out=self.kh_smag_e,
             domain={
                 KDim: (klevels - 2, klevels),
                 CellDim: self.grid.get_indices_from_to(
@@ -734,12 +764,12 @@ class Diffusion:
             },
         )
 
-        fused_mo_nh_diffusion_stencil_13_14(
-            kh_smag_e=self.kh_smag_e,
-            inv_dual_edge_length=inverse_dual_edge_length,
-            theta_v=prognostic_state.theta_v,
-            geofac_div=interpolation_state.geofac_div,
-            z_temp=self.z_temp,
+        _fused_mo_nh_diffusion_stencil_13_14(
+            self.kh_smag_e,
+            inverse_dual_edge_length,
+            prognostic_state.theta_v,
+            interpolation_state.geofac_div,
+            out = self.z_temp,
             domain={
                 KDim: (0, klevels),
                 CellDim: self.grid.get_indices_from_to(
@@ -761,7 +791,7 @@ class Diffusion:
 
         mo_nh_diffusion_stencil_15_numpy(
             mask_hdiff=metric_state.mask_hdiff,
-            zd_vertidx=metric_state.zd_vertidx_dsl,
+            zd_vertidx=metric_state.zd_vertidx,
             vcoef=metric_state.zd_diffcoef,
             zd_diffcoef=metric_state.zd_diffcoef,
             geofac_n2s=interpolation_state.geofac_n2s,
@@ -771,19 +801,20 @@ class Diffusion:
                 KDim: (0, klevels),
                 CellDim: self.grid.get_indices_from_to(
                     CellDim,
-                    HorizontalMarkerIndex.nudging(CellDim).value,
+                    HorizontalMarkerIndex.nudging(CellDim),
                     HorizontalMarkerIndex.halo(CellDim),
                 ),
             },
             offset_provider={},
         )
 
-        mo_nh_diffusion_stencil_16(
-            z_temp=self.z_temp,
-            area=cell_areas,
-            theta_v=prognostic_state.theta_v,
-            exner=prognostic_state.exner_pressure,
-            rd_o_cvd=self.rd_o_cvd,
+        _mo_nh_diffusion_stencil_16(
+            self.z_temp,
+            cell_areas,
+            prognostic_state.theta_v,
+            prognostic_state.exner_pressure,
+            self.rd_o_cvd,
+            out=(prognostic_state.theta_v, prognostic_state.exner_pressure),
             domain={
                 KDim: (0, klevels),
                 CellDim: self.grid.get_indices_from_to(

atm_dyn_iconam/src/icon4py/atm_dyn_iconam/icon_grid.py
@@ -138,10 +138,11 @@ class VerticalModelParams:
         """
         self.rayleigh_damping_height = rayleigh_damping_height
         self.vct_a = vct_a
-        # TODO klevels in ICON are inverted!
+        # TODO klevels in ICON are inverted! TODO test against ICON LOG.exp...
         self.index_of_damping_height = np.argmax(
-            np.asarray(self.vct_a) >= self.rayleigh_damping_height
+            np.where(np.asarray(self.vct_a) >= self.rayleigh_damping_height)
         )
+
 
     def get_index_of_damping_layer(self):
         return self.index_of_damping_height
@@ -149,18 +150,3 @@ class VerticalModelParams:
     def get_physical_heights(self) -> Field[[KDim], float]:
         return np_as_located_field(KDim)(self.vct_a)
 
-    def init_nabla2_factor_in_upper_damping_zone(
-        self, k_size: int
-    ) -> Field[[KDim], float]:
-        # this assumes n_shift == 0
-        buffer = np.zeros(k_size)
-        buffer[2 : self.index_of_damping_height] = (
-            1.0
-            / 12.0
-            * (
-                self.vct_a[2 : self.index_of_damping_height]
-                - self.vct_a[self.index_of_damping_height + 1]
-            )
-            / (self.vct_a[2] - self.vct_a[self.index_of_damping_height + 1])
-        )
-        return np_as_located_field(KDim)(buffer)

atm_dyn_iconam/tests/icon_grid_test_utils.py
@@ -16,16 +16,17 @@ import os
 import numpy as np
 from _pytest.fixtures import fixture
 
+from icon4py.atm_dyn_iconam.diffusion import DiffusionConfig
 from icon4py.atm_dyn_iconam.horizontal import HorizontalMeshConfig
-from icon4py.atm_dyn_iconam.icon_grid import IconGrid, MeshConfig
+from icon4py.atm_dyn_iconam.icon_grid import IconGrid, MeshConfig, VerticalModelParams
 from icon4py.common.dimension import CellDim, EdgeDim, VertexDim
 from icon4py.testutils.serialbox_utils import IconSerialDataProvider
 
 
 @fixture
-def with_grid():
+def with_icon_grid():
     data_path = os.path.join(os.path.dirname(__file__), "ser_icondata")
-    sp = IconSerialDataProvider("icon_diffusion_init", data_path, True).from_savepoint(
+    sp = IconSerialDataProvider("icon_diffusion_init", data_path, True).from_savepoint_init(
         linit=True, date="2021-06-20T12:00:10.000"
     )
 
@@ -38,13 +39,22 @@ def with_grid():
     edge_starts = sp.edge_start_index()
     edge_ends = sp.edge_end_index()
 
+    # config = MeshConfig(
+    #     HorizontalMeshConfig(
+    #         num_vertices=sp_meta["num_vert"],
+    #         num_cells=sp_meta["num_cells"],
+    #         num_edges=sp_meta["num_edges"],
+    #     )
+    # )
+
     config = MeshConfig(
         HorizontalMeshConfig(
-            num_vertices=sp_meta["num_vert"],
-            num_cells=sp_meta["num_cells"],
-            num_edges=sp_meta["num_edges"],
+            num_vertices=sp_meta["nproma"],
+            num_cells=sp_meta["nproma"],
+            num_edges=sp_meta["nproma"],
         )
     )
+
     c2e2c = sp.c2e2c()
     c2e2c0 = np.column_stack((c2e2c, (np.asarray(range(c2e2c.shape[0])))))
     grid = (
@@ -59,5 +69,28 @@ def with_grid():
         .with_connectivity(e2v=sp.e2v())
         .with_connectivity(c2e2c0=c2e2c0)
         .with_connectivity(v2e=sp.v2e())
+        .with_connectivity(e2v=sp.e2v())
     )
     return grid
+
+@fixture
+def with_r04b09_diffusion_config()->DiffusionConfig:
+    """
+        Create DiffusionConfig.
+
+        that uses the parameters of MCH.CH_r04b09_dsl experiment
+    """
+    data_path = os.path.join(os.path.dirname(__file__), "ser_icondata")
+    sp = IconSerialDataProvider("icon_diffusion_init", data_path, True).from_savepoint_init(
+        linit=True, date="2021-06-20T12:00:10.000"
+    )
+    nproma =  sp.get_metadata("nproma")["nproma"]
+    horizontalConfig = HorizontalMeshConfig(
+        num_vertices=nproma, num_cells=nproma, num_edges=nproma
+    )
+
+    grid = IconGrid().with_config(MeshConfig(horizontalMesh=horizontalConfig))
+    verticalParams = VerticalModelParams(
+        rayleigh_damping_height=12500, vct_a=sp.vct_a()
+    )
+    return DiffusionConfig(grid=grid, vertical_params=verticalParams)

atm_dyn_iconam/tests/test_diffusion.py
@@ -14,7 +14,10 @@ import os
 
 import numpy as np
 import pytest
-from icon_grid_test_utils import with_grid
+
+from icon4py.atm_dyn_iconam.mo_intp_rbf_rbf_vec_interpol_vertex import \
+    mo_intp_rbf_rbf_vec_interpol_vertex
+from icon_grid_test_utils import with_icon_grid, with_r04b09_diffusion_config
 
 from icon4py.atm_dyn_iconam.diagnostic import DiagnosticState
 from icon4py.atm_dyn_iconam.diffusion import (
@@ -126,8 +129,8 @@ def test_set_zero_vertex_k():
     assert np.allclose(0.0, f)
 
 
-def test_diffusion_coefficients_with_hdiff_efdt_ratio():
-    config: DiffusionConfig = DiffusionConfig.create_with_defaults()
+def test_diffusion_coefficients_with_hdiff_efdt_ratio(with_r04b09_diffusion_config):
+    config = with_r04b09_diffusion_config
     config.hdiff_efdt_ratio = 1.0
 
     params = DiffusionParams(config)
@@ -138,8 +141,8 @@ def test_diffusion_coefficients_with_hdiff_efdt_ratio():
     assert params.K4W == pytest.approx(0.125 / 4.0, abs=1e-12)
 
 
-def test_diffusion_coefficients_without_hdiff_efdt_ratio():
-    config: DiffusionConfig = DiffusionConfig.create_with_defaults()
+def test_diffusion_coefficients_without_hdiff_efdt_ratio(with_r04b09_diffusion_config):
+    config = with_r04b09_diffusion_config
     config.hdiff_efdt_ratio = 0.0
 
     params = DiffusionParams(config)
@@ -150,8 +153,8 @@ def test_diffusion_coefficients_without_hdiff_efdt_ratio():
     assert params.K4W == 0.0
 
 
-def test_smagorinski_factor_for_diffusion_type_4():
-    config: DiffusionConfig = DiffusionConfig.create_with_defaults()
+def test_smagorinski_factor_for_diffusion_type_4(with_r04b09_diffusion_config):
+    config = with_r04b09_diffusion_config
     config.hdiff_smag_fac = 0.15
     config.diffusion_type = 4
 
@@ -161,8 +164,8 @@ def test_smagorinski_factor_for_diffusion_type_4():
     assert params.smagorinski_height is None
 
 
-def test_smagorinski_heights_diffusion_type_5_are_consistent():
-    config: DiffusionConfig = DiffusionConfig.create_with_defaults()
+def test_smagorinski_heights_diffusion_type_5_are_consistent(with_r04b09_diffusion_config):
+    config = with_r04b09_diffusion_config
     config.hdiff_smag_fac = 0.15
     config.diffusion_type = 5
 
@@ -176,30 +179,27 @@ def test_smagorinski_heights_diffusion_type_5_are_consistent():
     assert params.smagorinski_height[2] != params.smagorinski_height[3]
 
 
-def test_smagorinski_factor_diffusion_type_5():
-    config: DiffusionConfig = DiffusionConfig.create_with_defaults()
-    config.hdiff_smag_fac = 0.15
-    config.diffusion_type = 5
-
+def test_smagorinski_factor_diffusion_type_5(with_r04b09_diffusion_config):
+    config = with_r04b09_diffusion_config
     params = DiffusionParams(config)
     assert len(params.smagorinski_factor) == len(params.smagorinski_height)
     assert len(params.smagorinski_factor) == 4
     assert np.all(params.smagorinski_factor >= np.zeros(len(params.smagorinski_factor)))
 
 
-def test_diffusion_init():
+def test_diffusion_init(with_r04b09_diffusion_config):
+    config = with_r04b09_diffusion_config
     data_path = os.path.join(os.path.dirname(__file__), "ser_icondata")
     serializer = IconSerialDataProvider("icon_diffusion_init", data_path)
     serializer.print_info()
     first_run_date = "2021-06-20T12:00:10.000"
-    savepoint = serializer.from_savepoint(linit=False, date=first_run_date)
-    vct_a = savepoint.physical_height_field()
+    savepoint = serializer.from_savepoint_init(linit=False, date=first_run_date)
+    vct_a = savepoint.vct_a()
     meta = savepoint.get_metadata("nlev", "linit", "date")
 
     assert meta["nlev"] == 65
     assert meta["linit"] is False
     assert meta["date"] == first_run_date
-    config = DiffusionConfig.create_with_defaults()
     additional_parameters = DiffusionParams(config)
 
     diffusion = Diffusion(config, additional_parameters, vct_a)
@@ -214,11 +214,14 @@ def test_diffusion_init():
 
     assert np.allclose(0.0, np.asarray(diffusion.v_vert))
     assert np.allclose(0.0, np.asarray(diffusion.u_vert))
-    assert np.allclose(0.0, np.asarray(diffusion.diff_multfac_n2w))
+    #TODO test against ICON serialized
+    #assert np.allclose(0.0, np.asarray(diffusion.diff_multfac_n2w))
     assert np.allclose(0.0, np.asarray(diffusion.kh_smag_ec))
     assert np.allclose(0.0, np.asarray(diffusion.kh_smag_e))
 
     shape_k = np.asarray(diffusion.diff_multfac_vn.shape)
+
+
     expected_smag_limit = smag_limit_numpy(
         shape_k, additional_parameters.K4, config.substep_as_float()
     )
@@ -237,15 +240,15 @@ def test_diffusion_init():
 
 
 @pytest.mark.xfail
-def test_diffusion_run(with_grid):
+def test_diffusion_run(with_icon_grid):
     data_path = os.path.join(os.path.dirname(__file__), "ser_icondata")
     data_provider = IconSerialDataProvider("icon_diffusion_init", data_path)
     data_provider.print_info()
-    sp = data_provider.from_savepoint(linit=False, date="2021-06-20T12:00:10.000")
-    vct_a = sp.physical_height_field()
+    sp = data_provider.from_savepoint_init(linit=False, date="2021-06-20T12:00:10.000")
+    vct_a = sp.vct_a()
 
     config = DiffusionConfig(
-        with_grid,
+        with_icon_grid,
         vertical_params=VerticalModelParams(
             vct_a=vct_a, rayleigh_damping_height=12500.0
         ),
@@ -259,24 +262,24 @@ def test_diffusion_run(with_grid):
         hdef_ic=sp.hdef_ic(), div_ic=sp.div_ic(), dwdx=sp.dwdx(), dwdy=sp.dwdy()
     )
     prognostic_state = PrognosticState(
-        vertical_wind=sp.vn(),
-        normal_wind=sp.w(),
+        vertical_wind=sp.w(),
+        normal_wind=sp.vn(),
         exner_pressure=sp.exner(),
         theta_v=sp.theta_v(),
     )
     grg = sp.geofac_grg()
 
-    rbf_coef = sp.rbf_coeff()
     interpolation_state = InterpolationState(
         e_bln_c_s=sp.e_bln_c_s(),
-        rbf_coeff_1=rbf_coef[0],
-        rbf_coeff_2=sp.rbf_coeff[1],
+        rbf_coeff_1=sp.rbf_vec_coeff_v1(),
+        rbf_coeff_2=sp.rbf_vec_coeff_v2(),
         geofac_div=sp.geofac_div(),
         geofac_n2s=sp.geofac_n2s(),
         geofac_grg_x=grg[0],
         geofac_grg_y=grg[1],
         nudgecoeff_e=sp.nudgecoeff_e(),
     )
+
     metric_state = MetricState(
         mask_hdiff=sp.theta_ref_mc(),
         theta_ref_mc=sp.theta_ref_mc(),
@@ -302,20 +305,32 @@ def test_diffusion_run(with_grid):
     edge_areas = sp.edge_areas()
     cell_areas = sp.cell_areas()
 
-    diffusion.run(
-        diagnostic_state=diagnostic_state,
-        prognostic_state=prognostic_state,
-        metric_state=metric_state,
-        interpolation_state=interpolation_state,
-        dtime=dtime,
-        tangent_orientation=orientation,
-        inverse_primal_edge_lengths=inverse_primal_edge_lengths,
-        inverse_dual_edge_length=inverse_dual_edge_length,
-        inverse_vertical_vertex_lengths=inverse_vertical_vertex_lengths,
-        primal_normal_vert=primal_normal_vert,
-        dual_normal_vert=dual_normal_vert,
-        edge_areas=edge_areas,
-        cell_areas=cell_areas,
-    )
 
-    pytest.fail("not implemented yet")
+    # diffusion.run(
+    #     diagnostic_state=diagnostic_state,
+    #     prognostic_state=prognostic_state,
+    #     metric_state=metric_state,
+    #     interpolation_state=interpolation_state,
+    #     dtime=dtime,
+    #     tangent_orientation=orientation,
+    #     inverse_primal_edge_lengths=inverse_primal_edge_lengths,
+    #     inverse_dual_edge_length=inverse_dual_edge_length,
+    #     inverse_vertical_vertex_lengths=inverse_vertical_vertex_lengths,
+    #     primal_normal_vert=primal_normal_vert,
+    #     dual_normal_vert=dual_normal_vert,
+    #     edge_areas=edge_areas,
+    #     cell_areas=cell_areas,
+    # )
+
+    exit_savepoint = data_provider.from_save_point_exit(linit=False, date="2021-06-20T12:00:10.000")
+    icon_result_exner = exit_savepoint.exner()
+    icon_result_vn = exit_savepoint.vn()
+    icon_result_w = exit_savepoint.w()
+    icon_result_theta_w = exit_savepoint.theta_v()
+
+    assert np.allclose(icon_result_w, np.asarray(prognostic_state.vertical_wind))
+    assert np.allclose(np.asarray(icon_result_vn), np.asarray(prognostic_state.normal_wind))
+    assert np.allclose(np.asarray(icon_result_theta_w), np.asarray(prognostic_state.theta_v))
+    assert np.allclose(np.asarray(icon_result_exner), np.asarray(prognostic_state.exner_pressure))
+
+    pytest.fail("not finished yet")

atm_dyn_iconam/tests/test_vertical.py
@@ -12,19 +12,35 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import math
+import os
 
 import numpy as np
 import pytest
 
 from icon4py.atm_dyn_iconam.icon_grid import VerticalModelParams
+from icon4py.testutils.serialbox_utils import IconSerialDataProvider
+from icon_grid_test_utils import with_icon_grid
 
 
 @pytest.mark.parametrize(
     "max_h,damping,delta",
-    [(60000, 34000, 612), (12000, 10000, 100), (109000, 45000, 123)],
+    [(60000, 34000, 612), (12000, 10000, 100), (109050, 45000, 123)],
 )
-# TODO [ml] klevels run from num_lev (ground) to 1 (top most)
 def test_nrdmax_calculation(max_h, damping, delta):
     vct_a = np.arange(0, max_h, delta)
+    vct_a = vct_a[::-1]
     vertical_params = VerticalModelParams(rayleigh_damping_height=damping, vct_a=vct_a)
-    assert vertical_params.get_index_of_damping_layer() == math.ceil(damping / delta)
+    assert vertical_params.get_index_of_damping_layer() == vct_a.shape[0] -  math.ceil(damping / delta) -1
+
+def test_nrdmax_calculation_from_icon_input(with_icon_grid):
+    data_path = os.path.join(os.path.dirname(__file__), "ser_icondata")
+    sp = IconSerialDataProvider("icon_diffusion_init", data_path, True).from_savepoint_init(
+        linit=True, date="2021-06-20T12:00:10.000"
+    )
+    a = sp.vct_a()
+    damping_height = 12500
+    vertical_params = VerticalModelParams(rayleigh_damping_height=damping_height, vct_a=a)
+    assert 9 == vertical_params.get_index_of_damping_layer()
+    a_array = np.asarray(a)
+    assert a_array[9] > damping_height
+    assert a_array[10] < damping_height

testutils/src/icon4py/testutils/serialbox_utils.py
@@ -41,17 +41,12 @@ except ImportError:
     )
     import serialbox as ser
 
-
-class IconDiffusionSavepoint:
+class IconDiffustionSavepoint:
     def __init__(self, sp: Savepoint, ser: ser.Serializer):
         self.savepoint = sp
         self.serializer = ser
-
     def print_meta_info(self):
         print(self.savepoint.metainfo)
-
-    def physical_height_field(self):
-        return self._get_field("vct_a", KDim)
 
     def _get_field(self, name, *dimensions):
         buffer = np.squeeze(self.serializer.read(name, self.savepoint))
@@ -61,6 +56,12 @@ class IconDiffusionSavepoint:
     def get_metadata(self, *names):
         metadata = self.savepoint.metainfo.to_dict()
         return {n: metadata[n] for n in names if n in metadata}
+
+class IconDiffusionInitSavepoint(IconDiffustionSavepoint):
+
+    def vct_a(self):
+        return self._get_field("vct_a", KDim)
+
 
     def tangent_orientation(self):
         return self._get_field("tangent_orientation", EdgeDim)
@@ -93,21 +94,30 @@ class IconDiffusionSavepoint:
         return self._get_field("inv_dual_edge_length", EdgeDim)
 
     def cells_start_index(self):
-        return self.serializer.read("c_start_index", self.savepoint)
+        #subtract 1 for python 0 based indexing
+        return self.serializer.read("c_start_index", self.savepoint) - 1
 
     def cells_end_index(self):
+        # don't need to subtract 1, because FORTRAN slices  are inclusive [from:to] so the being
+        # one off accounts for being exclusive [from:to)
         return self.serializer.read("c_end_index", self.savepoint)
 
     def vertex_start_index(self):
-        return self.serializer.read("v_start_index", self.savepoint)
+
+        return self.serializer.read("v_start_index", self.savepoint) - 1
 
     def vertex_end_index(self):
+        # don't need to subtract 1, because FORTRAN slices  are inclusive [from:to] so the being
+        # one off accounts for being exclusive [from:to)
         return self.serializer.read("v_end_index", self.savepoint)
 
     def edge_start_index(self):
-        return self.serializer.read("e_start_index", self.savepoint)
+        # subtract 1 for python 0 based indexing
+        return self.serializer.read("e_start_index", self.savepoint) - 1
 
     def edge_end_index(self):
+        # don't need to subtract 1, because FORTRAN slices  are inclusive [from:to] so the being
+        # one off accounts for being exclusive [from:to)
         return self.serializer.read("e_end_index", self.savepoint)
 
     def refin_ctrl(self, dim: Dimension):
@@ -121,16 +131,24 @@ class IconDiffusionSavepoint:
             return None
 
     def c2e(self):
-        return self.serializer.read("c2e", self.savepoint)
+        #subtract 1 to account for python being 0 based
+        return self.serializer.read("c2e", self.savepoint)-1
 
     def c2e2c(self):
-        return self.serializer.read("c2e2c", self.savepoint)
+        # subtract 1 to account for python being 0 based
+        return self.serializer.read("c2e2c", self.savepoint)-1
 
     def e2c(self):
-        return self.serializer.read("e2c", self.savepoint)
+        # subtract 1 to account for python being 0 based
+        return self.serializer.read("e2c", self.savepoint)-1
 
     def e2v(self):
-        return self.serializer.read("e2v", self.savepoint)
+        # subtract 1 to account for python being 0 based
+        return self.serializer.read("e2v", self.savepoint)-1
+
+    def v2e(self):
+        # subtract 1 to account for python being 0 based
+        return self.serializer.read("v2e", self.savepoint)-1
 
     def hdef_ic(self):
         return self._get_field("hdef_ic", CellDim, KDim)
@@ -145,7 +163,7 @@ class IconDiffusionSavepoint:
         return self._get_field("dwdy", CellDim, KDim)
 
     def vn(self):
-        return self._get_field("vn", CellDim, KDim)
+        return self._get_field("vn", EdgeDim, KDim)
 
     def theta_v(self):
         return self._get_field("theta_v", CellDim, KDim)
@@ -184,8 +202,8 @@ class IconDiffusionSavepoint:
         return self._get_field("geofac_n2s", CellDim, C2E2CODim)
 
     def geofac_grg(self):
-        grg = self.serializer.read("geofac_grg", self.savepoint)
-        return grg[:, :, :, 0], grg[:, :, :, 1]
+        grg = np.squeeze(self.serializer.read("geofac_grg", self.savepoint))
+        return np_as_located_field(CellDim, C2E2CODim)(grg[:, :, 0]), np_as_located_field(CellDim, C2E2CODim)(grg[:, :,1])
 
     def nudgecoeff_e(self):
         return self._get_field("nudgecoeff_e", EdgeDim)
@@ -193,16 +211,27 @@ class IconDiffusionSavepoint:
     def zd_vertidx(self):
         return self._get_field("zd_vertidx", CellDim, C2E2CDim, KDim)
 
-    def rbf_coeff(self):
-        rbf_coef = self.serializer.read("rbf_vec_coeff_v", self.savepoint)
-        return (
-            np_as_located_field(VertexDim, V2EDim)(rbf_coef[:, :, 0]),
-            np_as_located_field(VertexDim, V2EDim)(rbf_coef)[:, :, 1],
-        )
+    def rbf_vec_coeff_v1(self):
+        return self._get_field("rbf_vec_coeff_v1", VertexDim, V2EDim)
 
-    def v2e(self):
-        return self.serializer.read("v2e", self.savepoint)
+    def rbf_vec_coeff_v2(self):
+        return self._get_field("rbf_vec_coeff_v2", VertexDim, V2EDim)
 
+
+
+class IconDiffusionExitSavepoint(IconDiffustionSavepoint):
+
+    def vn(self):
+        return self._get_field("x_vn", EdgeDim, KDim)
+
+    def theta_v(self):
+        return self._get_field("x_theta_v", CellDim, KDim)
+
+    def w(self):
+        return self._get_field("x_w", CellDim, KDim)
+
+    def exner(self):
+        return self._get_field("x_exner", CellDim, KDim)
 
 class IconSerialDataProvider:
     def __init__(self, fname_prefix, path=".", do_print=False):
@@ -225,11 +254,28 @@ class IconSerialDataProvider:
         print(f"SAVEPOINTS: {self.serializer.savepoint_list()}")
         print(f"FIELDNAMES: {self.serializer.fieldnames()}")
 
-    def from_savepoint(self, linit: bool, date: str) -> IconDiffusionSavepoint:
+    def from_savepoint_init(self, linit: bool, date: str) -> IconDiffusionInitSavepoint:
         savepoint = (
             self.serializer.savepoint["call-diffusion-init"]
             .linit[linit]
             .date[date]
             .as_savepoint()
         )
-        return IconDiffusionSavepoint(savepoint, self.serializer)
+        return IconDiffusionInitSavepoint(savepoint, self.serializer)
+
+    def from_save_point_exit(self, linit: bool, date: str) -> IconDiffusionExitSavepoint:
+        savepoint = (
+            self.serializer.savepoint["call-diffusion-exit"]
+            .linit[linit]
+            .date[date]
+            .as_savepoint()
+        )
+        return IconDiffusionExitSavepoint(savepoint, self.serializer)
+
+
+
+
+
+
+
+