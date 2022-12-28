054f0abba87411d1f9fa737ac1c0753b8cb1df21
mroethlin <Matthias.Roethlin@MeteoSwiss.ch>
2022-12-28 16:23:25 +0100

    remove more debug stuff

--- pyutils/src/icon4py/pyutils/icon4pygen.py
+++ pyutils/src/icon4py/pyutils/icon4pygen.py
@@ -59,7 +59,3 @@ def main(
     stencil_info = get_stencil_info(fencil_def)
     GTHeader(stencil_info)(outpath, imperative)
     PyBindGen(stencil_info, levels_per_thread, block_size)(outpath)
-
-
-if __name__ == "__main__":
-    main()
