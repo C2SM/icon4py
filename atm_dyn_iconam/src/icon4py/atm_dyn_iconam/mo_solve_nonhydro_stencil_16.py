2d279c0dcfc55e2f752feeb54756500e8a320b85
mroethlin <Matthias.Roethlin@MeteoSwiss.ch>
2023-02-13 15:37:10 +0100

    try to reintegrate sten 16

--- atm_dyn_iconam/src/icon4py/atm_dyn_iconam/mo_solve_nonhydro_stencil_16.py
+++ atm_dyn_iconam/src/icon4py/atm_dyn_iconam/mo_solve_nonhydro_stencil_16.py
@@ -12,13 +12,66 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from gt4py.next.ffront.decorator import field_operator, program
-from gt4py.next.ffront.fbuiltins import Field, broadcast, where
+from gt4py.next.ffront.fbuiltins import Field, broadcast, where, int32
 
-from icon4py.common.dimension import E2C, CellDim, EdgeDim, KDim
+from icon4py.common.dimension import E2C, E2EC, ECDim, CellDim, EdgeDim, KDim
 
 
 @field_operator
-def _mo_solve_nonhydro_stencil_16(
+def _compute_btraj(
+    p_vn: Field[[EdgeDim, KDim], float],
+    p_vt: Field[[EdgeDim, KDim], float],
+    cell_idx: Field[[ECDim], int32],
+    cell_blk: Field[[ECDim], int32],
+    pos_on_tplane_e_1: Field[[ECDim], float],
+    pos_on_tplane_e_2: Field[[ECDim], float],
+    primal_normal_cell_1: Field[[ECDim], float],
+    dual_normal_cell_1: Field[[ECDim], float],
+    primal_normal_cell_2: Field[[ECDim], float],
+    dual_normal_cell_2: Field[[ECDim], float],
+    p_dthalf: float,
+) -> tuple[
+    Field[[EdgeDim, KDim], int32],
+    Field[[EdgeDim, KDim], int32],
+    Field[[EdgeDim, KDim], float],
+    Field[[EdgeDim, KDim], float],
+]:
+    lvn_pos = where(p_vn > 0.0, True, False)
+
+    p_cell_idx = where(lvn_pos, cell_idx(E2EC[0]), cell_idx(E2EC[1]))
+    p_cell_blk = where(lvn_pos, cell_blk(E2EC[0]), cell_blk(E2EC[1]))
+
+    z_ntdistv_bary_1 = -(
+        p_vn * p_dthalf
+        + where(lvn_pos, pos_on_tplane_e_1(E2EC[0]), pos_on_tplane_e_1(E2EC[1]))
+    )
+
+    z_ntdistv_bary_2 = -(
+        p_vt * p_dthalf
+        + where(lvn_pos, pos_on_tplane_e_2(E2EC[0]), pos_on_tplane_e_2(E2EC[1]))
+    )
+
+    p_distv_bary_1 = where(
+        lvn_pos,
+        z_ntdistv_bary_1 * primal_normal_cell_1(E2EC[0])
+        + z_ntdistv_bary_2 * dual_normal_cell_1(E2EC[0]),
+        z_ntdistv_bary_1 * primal_normal_cell_1(E2EC[1])
+        + z_ntdistv_bary_2 * dual_normal_cell_1(E2EC[1]),
+    )
+
+    p_distv_bary_2 = where(
+        lvn_pos,
+        z_ntdistv_bary_1 * primal_normal_cell_2(E2EC[0])
+        + z_ntdistv_bary_2 * dual_normal_cell_2(E2EC[0]),
+        z_ntdistv_bary_1 * primal_normal_cell_2(E2EC[1])
+        + z_ntdistv_bary_2 * dual_normal_cell_2(E2EC[1]),
+    )
+
+    return p_cell_idx, p_cell_blk, p_distv_bary_1, p_distv_bary_2
+
+
+@field_operator
+def _sten_16(
     p_vn: Field[[EdgeDim, KDim], float],
     rho_ref_me: Field[[EdgeDim, KDim], float],
     theta_ref_me: Field[[EdgeDim, KDim], float],
@@ -61,9 +114,19 @@ def _mo_solve_nonhydro_stencil_16(
     return z_rho_e, z_theta_v_e
 
 
-@program
-def mo_solve_nonhydro_stencil_16(
+@field_operator
+def _mo_solve_nonhydro_btraj_and_stencil_16(
     p_vn: Field[[EdgeDim, KDim], float],
+    p_vt: Field[[EdgeDim, KDim], float],
+    cell_idx: Field[[ECDim], int32],
+    cell_blk: Field[[ECDim], int32],
+    pos_on_tplane_e_1: Field[[ECDim], float],
+    pos_on_tplane_e_2: Field[[ECDim], float],
+    primal_normal_cell_1: Field[[ECDim], float],
+    dual_normal_cell_1: Field[[ECDim], float],
+    primal_normal_cell_2: Field[[ECDim], float],
+    dual_normal_cell_2: Field[[ECDim], float],
+    p_dthalf: float,
     rho_ref_me: Field[[EdgeDim, KDim], float],
     theta_ref_me: Field[[EdgeDim, KDim], float],
     p_distv_bary_1: Field[[EdgeDim, KDim], float],
@@ -74,10 +137,27 @@ def mo_solve_nonhydro_stencil_16(
     z_grad_rth_4: Field[[CellDim, KDim], float],
     z_rth_pr_1: Field[[CellDim, KDim], float],
     z_rth_pr_2: Field[[CellDim, KDim], float],
-    z_rho_e: Field[[EdgeDim, KDim], float],
-    z_theta_v_e: Field[[EdgeDim, KDim], float],
-):
-    _mo_solve_nonhydro_stencil_16(
+) -> tuple[
+    Field[[EdgeDim, KDim], int32],
+    Field[[EdgeDim, KDim], int32],
+    Field[[EdgeDim, KDim], float],
+    Field[[EdgeDim, KDim], float],
+]:
+    (p_cell_idx, p_cell_blk, p_distv_bary_1, p_distv_bary_2,) = _compute_btraj(
+        p_vn,
+        p_vt,
+        cell_idx,
+        cell_blk,
+        pos_on_tplane_e_1,
+        pos_on_tplane_e_2,
+        primal_normal_cell_1,
+        dual_normal_cell_1,
+        primal_normal_cell_2,
+        dual_normal_cell_2,
+        p_dthalf,
+    )
+
+    z_rho_e, z_theta_v_e = _sten_16(
         p_vn,
         rho_ref_me,
         theta_ref_me,
@@ -89,5 +169,60 @@ def mo_solve_nonhydro_stencil_16(
         z_grad_rth_4,
         z_rth_pr_1,
         z_rth_pr_2,
-        out=(z_rho_e, z_theta_v_e),
+    )
+
+    return p_cell_idx, p_cell_blk, z_rho_e, z_theta_v_e
+
+
+@program
+def mo_solve_nonhydro_stencil_16(
+    p_vn: Field[[EdgeDim, KDim], float],
+    p_vt: Field[[EdgeDim, KDim], float],
+    cell_idx: Field[[ECDim], int32],
+    cell_blk: Field[[ECDim], int32],
+    pos_on_tplane_e_1: Field[[ECDim], float],
+    pos_on_tplane_e_2: Field[[ECDim], float],
+    primal_normal_cell_1: Field[[ECDim], float],
+    dual_normal_cell_1: Field[[ECDim], float],
+    primal_normal_cell_2: Field[[ECDim], float],
+    dual_normal_cell_2: Field[[ECDim], float],
+    p_dthalf: float,
+    rho_ref_me: Field[[EdgeDim, KDim], float],
+    theta_ref_me: Field[[EdgeDim, KDim], float],
+    p_distv_bary_1: Field[[EdgeDim, KDim], float],
+    p_distv_bary_2: Field[[EdgeDim, KDim], float],
+    z_grad_rth_1: Field[[CellDim, KDim], float],
+    z_grad_rth_2: Field[[CellDim, KDim], float],
+    z_grad_rth_3: Field[[CellDim, KDim], float],
+    z_grad_rth_4: Field[[CellDim, KDim], float],
+    z_rth_pr_1: Field[[CellDim, KDim], float],
+    z_rth_pr_2: Field[[CellDim, KDim], float],
+    p_cell_idx: Field[[EdgeDim, KDim], int32],
+    p_cell_blk: Field[[EdgeDim, KDim], int32],
+    z_rho_e: Field[[EdgeDim, KDim], float],
+    z_theta_v_e: Field[[EdgeDim, KDim], float],
+):
+    _mo_solve_nonhydro_btraj_and_stencil_16(
+        p_vn,
+        p_vt,
+        cell_idx,
+        cell_blk,
+        pos_on_tplane_e_1,
+        pos_on_tplane_e_2,
+        primal_normal_cell_1,
+        dual_normal_cell_1,
+        primal_normal_cell_2,
+        dual_normal_cell_2,
+        p_dthalf,
+        rho_ref_me,
+        theta_ref_me,
+        p_distv_bary_1,
+        p_distv_bary_2,
+        z_grad_rth_1,
+        z_grad_rth_2,
+        z_grad_rth_3,
+        z_grad_rth_4,
+        z_rth_pr_1,
+        z_rth_pr_2,
+        out=(p_cell_idx, p_cell_blk, z_rho_e, z_theta_v_e),
     )
