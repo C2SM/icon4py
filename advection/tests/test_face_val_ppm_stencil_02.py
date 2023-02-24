9a7c47545cc2bab673531330e9a50874eda361fb
Daniel Hupp <daniel.hupp@meteoswiss.ch>
2023-02-24 12:20:04 +0100

    add second test

---
+++ advection/tests/test_face_val_ppm_stencil_02.py
@@ -0,0 +1,93 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import numpy as np
+
+from icon4py.advection.face_val_ppm_stencil_02 import face_val_ppm_stencil_02
+from icon4py.common.dimension import CellDim, KDim
+from icon4py.testutils.simple_mesh import SimpleMesh
+from icon4py.testutils.utils import random_field, zero_field
+from gt4py.next.ffront.fbuiltins import int32
+
+
+def face_val_ppm_stencil_02_numpy(
+    p_cc: np.array,
+    p_cellhgt_mc_now: np.array,
+    p_face_in: np.array,
+    vert_idx: np.array,
+    slev: int32,
+    elev: int32,
+    slevp1: int32,
+    elevp1: int32,
+):
+
+    p_face_a = p_face_in
+
+    p_face_a[:,1:] = p_cc[:,1:]*(1. - (p_cellhgt_mc_now[:,1:] / p_cellhgt_mc_now[:,:-1])) + (p_cellhgt_mc_now[:,1:]/(p_cellhgt_mc_now[:,:-1] + p_cellhgt_mc_now[:,1:])) * ((p_cellhgt_mc_now[:,1:] / p_cellhgt_mc_now[:,:-1])* p_cc[:,1:] + p_cc[:,:-1])
+
+    p_face = np.where( (vert_idx==slevp1) | (vert_idx==elev), p_face_a, p_face_in)
+    p_face = np.where( (vert_idx==slev), p_cc, p_face)
+    p_face[:,1:] = np.where( (vert_idx[1:]==elevp1), p_cc[:,:-1], p_face[:,1:])
+
+    return p_face
+
+
+
+def test_face_val_ppm_stencil_02():
+    mesh = SimpleMesh()
+    p_cc = random_field(mesh, CellDim, KDim)
+    p_cellhgt_mc_now = random_field(mesh, CellDim, KDim)
+    p_face_in = random_field(mesh, CellDim, KDim)
+    p_face = random_field(mesh, CellDim, KDim)
+    vert_idx = zero_field(mesh, KDim, dtype=int32)
+
+    for i in range(len(vert_idx.__array__())):
+      vert_idx[i] = i
+
+    
+    slev = int32(1)
+    slevp1 = slev + int32(1)
+    elev = vert_idx[-3]
+    elevp1 = elev + int32(1)
+
+    # print(vert_idx.__array__()) 
+    # print(elev)
+
+    ref = face_val_ppm_stencil_02_numpy(
+        np.asarray(p_cc),
+        np.asarray(p_cellhgt_mc_now),
+        np.asarray(p_face_in),
+        np.asarray(vert_idx),
+        slev,
+        elev,
+        slevp1,
+        elevp1,
+    )
+
+    face_val_ppm_stencil_02(
+        p_cc,
+        p_cellhgt_mc_now,
+        p_face_in,
+        vert_idx,
+        slev,
+        elev,
+        slevp1,
+        elevp1,
+        p_face,
+        offset_provider={"Koff": KDim},
+    )
+
+    # assert np.allclose(ref[:,1:], p_face)
+    # assert np.allclose(ref, p_face[:,1:])
+    # assert np.allclose(ref, p_face[:,1:])
+    assert np.allclose(ref, p_face[:,:])
