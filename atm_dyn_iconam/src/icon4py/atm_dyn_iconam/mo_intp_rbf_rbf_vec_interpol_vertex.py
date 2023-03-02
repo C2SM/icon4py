dba1b283f9a128f23ffab5027608cfe4f48df8bb
Magdalena Luz <luzm@ethz.ch>
2023-03-02 16:02:07 +0100

    remove fixed TODO

--- atm_dyn_iconam/src/icon4py/atm_dyn_iconam/mo_intp_rbf_rbf_vec_interpol_vertex.py
+++ atm_dyn_iconam/src/icon4py/atm_dyn_iconam/mo_intp_rbf_rbf_vec_interpol_vertex.py
@@ -28,8 +28,6 @@ def _mo_intp_rbf_rbf_vec_interpol_vertex(
     p_v_out = neighbor_sum(p_e_in(V2E) * ptr_coeff_2, axis=V2EDim)
     return p_u_out, p_v_out
 
-
-# TODO unit test does not work on gtfn backend, getting "ValueError: buffer has incorrect itemsize: 8; expected 4"
 @program(backend=gtfn_cpu.run_gtfn)
 def mo_intp_rbf_rbf_vec_interpol_vertex(
     p_e_in: Field[[EdgeDim, KDim], float],
