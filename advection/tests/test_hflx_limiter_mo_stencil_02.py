c6c66d4c3305af5c07635e57758cc1db17214e34
Daniel Hupp <daniel.hupp@meteoswiss.ch>
2023-03-03 15:55:19 +0100

    fix test

--- advection/tests/test_hflx_limiter_mo_stencil_02.py
+++ advection/tests/test_hflx_limiter_mo_stencil_02.py
@@ -26,6 +26,8 @@ def hflx_limiter_mo_stencil_02_numpy(
     refin_ctrl: np.ndarray,
     p_cc: np.ndarray,
     z_tracer_new_low: np.ndarray,
+    z_tracer_max: np.ndarray,
+    z_tracer_min: np.ndarray,
     lo_bound: float,
     hi_bound: float,
 ):
@@ -34,34 +36,45 @@ def hflx_limiter_mo_stencil_02_numpy(
         np.equal(refin_ctrl, lo_bound * np.ones(refin_ctrl.shape, dtype=int32)),
         np.equal(refin_ctrl, hi_bound * np.ones(refin_ctrl.shape, dtype=int32)),
     )
-    z_tracer_new_tmp = np.where(
+    z_tracer_new_out = np.where(
         condition,
         np.minimum(1.1 * p_cc, np.maximum(0.9 * p_cc, z_tracer_new_low)),
         z_tracer_new_low,
     )
-    z_tracer_max = np.where(
-        condition, np.maximum(p_cc, z_tracer_new_tmp), z_tracer_new_low
+    z_tracer_max_out = np.where(
+        condition, np.maximum(p_cc, z_tracer_new_out), z_tracer_max
     )
-    z_tracer_min = np.where(
-        condition, np.minimum(p_cc, z_tracer_new_tmp), z_tracer_new_low
+    z_tracer_min_out = np.where(
+        condition, np.minimum(p_cc, z_tracer_new_out), z_tracer_min
     )
-    return z_tracer_max, z_tracer_min
+    return z_tracer_new_out, z_tracer_max_out, z_tracer_min_out
 
 
 def test_hflx_limiter_mo_stencil_02_some_matching_condition():
     mesh = SimpleMesh()
+
     hi_bound = np.int32(1)
     lo_bound = np.int32(5)
+
     refin_ctrl = constant_field(mesh, hi_bound, CellDim, dtype=int32)
+
     refin_ctrl[0:2] = np.int32(3)
+
     p_cc = random_field(mesh, CellDim, KDim)
-    z_tracer_new_low = random_field(mesh, CellDim, KDim)
+    z_tracer_new_low_in = random_field(mesh, CellDim, KDim)
+    z_tracer_max_in = random_field(mesh, CellDim, KDim)
+    z_tracer_min_in = random_field(mesh, CellDim, KDim)
+
+    z_tracer_new_low = zero_field(mesh, CellDim, KDim)
     z_tracer_max = zero_field(mesh, CellDim, KDim)
     z_tracer_min = zero_field(mesh, CellDim, KDim)
-    ref_max, ref_min = hflx_limiter_mo_stencil_02_numpy(
+
+    ref_new_low, ref_max, ref_min = hflx_limiter_mo_stencil_02_numpy(
         np.asarray(refin_ctrl),
         np.asarray(p_cc),
-        np.asarray(z_tracer_new_low),
+        np.asarray(z_tracer_new_low_in),
+        np.asarray(z_tracer_max_in),
+        np.asarray(z_tracer_min_in),
         lo_bound,
         hi_bound,
     )
@@ -69,35 +82,54 @@ def test_hflx_limiter_mo_stencil_02_some_matching_condition():
     hflx_limiter_mo_stencil_02(
         refin_ctrl,
         p_cc,
-        z_tracer_new_low,
+        z_tracer_new_low_in,
+        z_tracer_max_in,
+        z_tracer_min_in,
         lo_bound,
         hi_bound,
+        z_tracer_new_low,
         z_tracer_max,
         z_tracer_min,
         offset_provider={},
     )
+
+    assert np.allclose(z_tracer_new_low, ref_new_low)
     assert np.allclose(z_tracer_max, ref_max)
     assert np.allclose(z_tracer_min, ref_min)
 
 
 def test_hflx_limiter_mo_stencil_02_none_matching_condition():
     mesh = SimpleMesh()
+
     hi_bound = np.int32(3)
     lo_bound = np.int32(1)
+
     refin_ctrl = constant_field(mesh, 2, CellDim, dtype=int32)
+
     p_cc = random_field(mesh, CellDim, KDim)
-    z_tracer_new_low = random_field(mesh, CellDim, KDim)
+
+    z_tracer_new_low_in = random_field(mesh, CellDim, KDim)
+    z_tracer_max_in = random_field(mesh, CellDim, KDim)
+    z_tracer_min_in = random_field(mesh, CellDim, KDim)
+
+    z_tracer_new_low = zero_field(mesh, CellDim, KDim)
     z_tracer_max = zero_field(mesh, CellDim, KDim)
     z_tracer_min = zero_field(mesh, CellDim, KDim)
+
     hflx_limiter_mo_stencil_02(
         refin_ctrl,
         p_cc,
-        z_tracer_new_low,
+        z_tracer_new_low_in,
+        z_tracer_max_in,
+        z_tracer_min_in,
         lo_bound,
         hi_bound,
+        z_tracer_new_low,
         z_tracer_max,
         z_tracer_min,
         offset_provider={},
     )
-    assert np.allclose(z_tracer_min, z_tracer_new_low)
-    assert np.allclose(z_tracer_max, z_tracer_new_low)
+
+    assert np.allclose(z_tracer_new_low_in, z_tracer_new_low)
+    assert np.allclose(z_tracer_min_in, z_tracer_min)
+    assert np.allclose(z_tracer_max_in, z_tracer_max)
