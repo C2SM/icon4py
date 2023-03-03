38520aa814314b337ae6b7131eb8d2265f53a05a
Daniel Hupp <daniel.hupp@meteoswiss.ch>
2023-03-03 15:12:57 +0100

    fix stencil

--- advection/src/icon4py/advection/hflx_limiter_mo_stencil_02.py
+++ advection/src/icon4py/advection/hflx_limiter_mo_stencil_02.py
@@ -23,20 +23,22 @@ def _hflx_limiter_mo_stencil_02(
     refin_ctrl: Field[[CellDim], int32],
     p_cc: Field[[CellDim, KDim], float],
     z_tracer_new_low: Field[[CellDim, KDim], float],
+    z_tracer_max: Field[[CellDim, KDim], float],
+    z_tracer_min: Field[[CellDim, KDim], float],
     lo_bound: int32,
     hi_bound: int32,
-) -> tuple[Field[[CellDim, KDim], float], Field[[CellDim, KDim], float]]:
+) -> tuple[Field[[CellDim, KDim], float], Field[[CellDim, KDim], float], Field[[CellDim, KDim], float]]:
     condition = (refin_ctrl == lo_bound) | (refin_ctrl == hi_bound)
-    z_tracer_new_tmp = where(
+    z_tracer_new_out = where(
         condition,
         minimum(1.1 * p_cc, maximum(0.9 * p_cc, z_tracer_new_low)),
         z_tracer_new_low,
     )
-    return where(
-        condition,
-        (maximum(p_cc, z_tracer_new_tmp), minimum(p_cc, z_tracer_new_tmp)),
-        (z_tracer_new_low, z_tracer_new_low),
-    )
+
+    z_tracer_max_out = where(condition, maximum(p_cc, z_tracer_new_out), z_tracer_max)
+    z_tracer_min_out = where(condition, minimum(p_cc, z_tracer_new_out), z_tracer_min)
+
+    return(z_tracer_new_out, z_tracer_max_out, z_tracer_min_out)
 
 
 @program
@@ -44,16 +46,21 @@ def hflx_limiter_mo_stencil_02(
     refin_ctrl: Field[[CellDim], int32],
     p_cc: Field[[CellDim, KDim], float],
     z_tracer_new_low: Field[[CellDim, KDim], float],
-    lo_bound: int32,
-    hi_bound: int32,
     z_tracer_max: Field[[CellDim, KDim], float],
     z_tracer_min: Field[[CellDim, KDim], float],
+    lo_bound: int32,
+    hi_bound: int32,
+    z_tracer_new_low_out: Field[[CellDim, KDim], float],
+    z_tracer_max_out: Field[[CellDim, KDim], float],
+    z_tracer_min_out: Field[[CellDim, KDim], float],
 ):
     _hflx_limiter_mo_stencil_02(
         refin_ctrl,
         p_cc,
         z_tracer_new_low,
+        z_tracer_max,
+        z_tracer_min,
         lo_bound,
         hi_bound,
-        out=(z_tracer_max, z_tracer_min),
+        out=(z_tracer_new_low_out, z_tracer_max_out, z_tracer_min_out),
     )
