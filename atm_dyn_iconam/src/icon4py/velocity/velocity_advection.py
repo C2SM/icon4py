7b678e67102320fa6517651c116773122037f4f2
nfarabullini <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>
2023-03-07 13:09:06 +0100

    removed commented code

--- atm_dyn_iconam/src/icon4py/velocity/velocity_advection.py
+++ atm_dyn_iconam/src/icon4py/velocity/velocity_advection.py
@@ -572,24 +572,24 @@ class VelocityAdvection:
             offset_provider={"C2E": self.grid.get_c2e_connectivity(), "C2EDim": C2EDim},
         )
 
-        # mo_velocity_advection_stencil_18(
-        #     levelmask=self.levelmask,
-        #     cfl_clipping=self.cfl_clipping,
-        #     owner_mask=owner_mask,
-        #     z_w_con_c=self.z_w_con_c,
-        #     ddqz_z_half=self.metric_state.ddqz_z_half,
-        #     area=cell_areas,
-        #     geofac_n2s=self.interpolation_state.geofac_n2s,
-        #     w=prognostic_state.w,
-        #     ddt_w_adv=diagnostic_state.ddt_w_adv_pc,
-        #     scalfac_exdiff=scalfac_exdiff,
-        #     cfl_w_limit=cfl_w_limit,
-        #     dtime=dtime,
-        #     offset_provider={
-        #         "C2E2CO": self.grid.get_c2e2co_connectivity(),
-        #         "C2E2CODim": C2E2CODim,
-        #     },
-        # )
+        mo_velocity_advection_stencil_18(
+            levelmask=self.levelmask,
+            cfl_clipping=self.cfl_clipping,
+            owner_mask=owner_mask,
+            z_w_con_c=self.z_w_con_c,
+            ddqz_z_half=self.metric_state.ddqz_z_half,
+            area=cell_areas,
+            geofac_n2s=self.interpolation_state.geofac_n2s,
+            w=prognostic_state.w,
+            ddt_w_adv=diagnostic_state.ddt_w_adv_pc,
+            scalfac_exdiff=scalfac_exdiff,
+            cfl_w_limit=cfl_w_limit,
+            dtime=dtime,
+            offset_provider={
+                "C2E2CO": self.grid.get_c2e2co_connectivity(),
+                "C2E2CODim": C2E2CODim,
+            },
+        )
 
         mo_velocity_advection_stencil_19(
             z_kin_hor_e=z_fields.z_kin_hor_e,
