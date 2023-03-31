918b508ccbd69137b1c24fb5dc29f20bb0f7e576
Christopher Bignamini <christopher.bignamini@gmail.com>
2023-03-31 18:07:25 +0200

    CLI created

--- pyutils/src/icon4py/f2ser/cli.py
+++ pyutils/src/icon4py/f2ser/cli.py
@@ -10,3 +10,51 @@
 # distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+import pathlib
+
+import click
+
+from icon4py.f2ser.logger import setup_logger
+from icon4py.f2ser.deserialise import ParsedGranuleDeserialiser
+from icon4py.f2ser.parse import GranuleParser
+
+logger = setup_logger(__name__)
+
+@click.command("icon_f2ser")
+@click.argument(
+    "input_filepath",
+    type=click.Path(
+        exists=True, dir_okay=False, resolve_path=True, path_type=pathlib.Path
+    ),
+)
+@click.argument(
+    "output_filepath",
+    type=click.Path(dir_okay=False, resolve_path=True, path_type=pathlib.Path),
+)
+def main(
+    input_filepath: pathlib.Path,
+    dep_filepath: pathlib.Path, # TODO: to be removed
+    output_filepath: pathlib.Path,
+) -> None:
+    """Command line interface for interacting with the ICON-f2ser serialization Preprocessor.
+
+    Usage:
+        icon_f2ser <input_filepath> <output_filepath>
+
+    Options:
+
+    Arguments:
+        input_filepath Path to the input file to process.
+        dep_filepath Path to the input file dependencies.
+        output_filepath Path to the output file to generate.
+    """
+
+    parser = GranuleParser(input_filepath, dep_filepath)
+    parsed = parser.parse()
+    deserialiser = ParsedGranuleDeserialiser(parsed, directory=".")
+    interface = deserialiser.deserialise()
+    #run_code_generation(interface, output_filepath)
+
+if __name__ == "__main__":
+    main()
