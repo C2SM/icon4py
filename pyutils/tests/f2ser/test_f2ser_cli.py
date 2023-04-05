2afb29b941444c73d8b42b1e8d71e283f1192ba0
samkellerhals <kellerhalssamuel@gmail.com>
2023-04-05 09:58:45 +0200

    Add f2ser as a console script

---
+++ pyutils/tests/f2ser/test_f2ser_cli.py
@@ -0,0 +1,35 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import pytest
+from click.testing import CliRunner
+
+from icon4py.f2ser.cli import main
+
+
+@pytest.fixture
+def outfile(tmp_path):
+    return str(tmp_path / "gen.f90")
+
+
+@pytest.fixture
+def cli():
+    return CliRunner()
+
+
+def test_cli(diffusion_granule, diffusion_granule_deps, outfile, cli):
+    inp = str(diffusion_granule)
+    deps = [str(p) for p in diffusion_granule_deps]
+    args = [inp, outfile, "-d", ",".join(deps)]
+    result = cli.invoke(main, args)
+    assert result.exit_code == 0
