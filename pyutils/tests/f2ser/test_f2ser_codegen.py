173f2de5e1ba23de51230d4a21df78c131cf896a
Christopher Bignamini <christopher.bignamini@gmail.com>
2023-04-28 16:12:12 +0200

    Serialization directives generation test created

--- pyutils/tests/f2ser/test_f2ser_codegen.py
+++ pyutils/tests/f2ser/test_f2ser_codegen.py
@@ -11,10 +11,12 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import pytest
+
 from icon4py.f2ser.deserialise import ParsedGranuleDeserialiser
 from icon4py.f2ser.parse import GranuleParser
 from icon4py.liskov.codegen.serialisation.generate import SerialisationGenerator
-
+from icon4py.liskov.codegen.types import GeneratedCode
 
 def test_deserialiser_diffusion_codegen(diffusion_granule, diffusion_granule_deps):
     parser = GranuleParser(diffusion_granule, diffusion_granule_deps)
@@ -24,3 +26,26 @@ def test_deserialiser_diffusion_codegen(diffusion_granule, diffusion_granule_dep
     generator = SerialisationGenerator(interface)
     generated = generator()
     assert len(generated) == 3
+
+@pytest.fixture
+def expected_no_deps_serialization_directives():
+    serialization_directives = [
+        GeneratedCode(startln=12,
+                      source='\n!$ser init directory="." prefix="test"\n\n!$ser savepoint no_deps_init_in\n\n!$ser data a=a\n\n!$ser data b=b'),
+        GeneratedCode(startln=14,
+                      source='\n!$ser savepoint no_deps_init_out\n\n!$ser data c=c\n\n!$ser data b=b'),
+        GeneratedCode(startln=20,
+                      source='\n!$ser savepoint no_deps_run_in\n\n!$ser data a=a\n\n!$ser data b=b'),
+        GeneratedCode(startln=22,
+                      source='\n!$ser savepoint no_deps_run_out\n\n!$ser data c=c\n\n!$ser data b=b')]
+    return serialization_directives
+
+def test_deserialiser_no_deps_codegen(no_deps_source_file, diffusion_granule_deps,
+                                      expected_no_deps_serialization_directives):
+    parser = GranuleParser(no_deps_source_file)
+    parsed = parser.parse()
+    deserialiser = ParsedGranuleDeserialiser(parsed, directory=".", prefix="test")
+    interface = deserialiser.deserialise()
+    generator = SerialisationGenerator(interface)
+    generated = generator()
+    assert(generated == expected_no_deps_serialization_directives())
