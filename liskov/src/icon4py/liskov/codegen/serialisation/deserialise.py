d431da13cc16455db59ca71d01491aabe0f4aa6f
samkellerhals <kellerhalssamuel@gmail.com>
2023-05-05 12:02:34 +0200

    Add special case fixes

--- liskov/src/icon4py/liskov/codegen/serialisation/deserialise.py
+++ liskov/src/icon4py/liskov/codegen/serialisation/deserialise.py
@@ -31,6 +31,19 @@ from icon4py.liskov.parsing.utils import extract_directive
 
 logger = setup_logger(__name__)
 
+KEYS_TO_REMOVE = [
+    "accpresent",
+    "mergecopy",
+    "copies",
+    "horizontal_lower",
+    "horizontal_upper",
+    "vertical_lower",
+    "vertical_upper",
+    "name",
+]
+
+SKIP_VARS = ["ikoffset", "pos_on_tplane_e_1", "pos_on_tplane_e_2"]
+
 
 class InitDataFactory:
     dtype = InitData
@@ -41,6 +54,7 @@ class InitDataFactory:
 
 class SavepointDataFactory:
     def __call__(self, parsed: ts.ParsedDict) -> list[SavepointData]:
+        """Create a list of Start and End Savepoints for each Start and End Stencil directive."""
         start_stencil = extract_directive(
             parsed["directives"], icon4py.liskov.parsing.parse.StartStencil
         )
@@ -50,116 +64,107 @@ class SavepointDataFactory:
 
         deserialised = []
 
-        # todo: remove rel and abs tol values
-        # todo: handle merge copies
-        # todo: allow insertion of additional metadata?
-
         for i, (start, end) in enumerate(zip(start_stencil, end_stencil)):
             named_args = parsed["content"]["StartStencil"][i]
             stencil_name = _extract_stencil_name(named_args, start)
-            to_remove = [
-                "accpresent",
-                "mergecopy",
-                "copies",
-                "horizontal_lower",
-                "horizontal_upper",
-                "vertical_lower",
-                "vertical_upper",
-                "name",
-            ]
-            [pop_item_from_dict(named_args, k, None) for k in to_remove]
 
-            for tol in TOLERANCE_ARGS:
-                for k in named_args.copy().keys():
-                    if k.endswith(tol):
-                        pop_item_from_dict(named_args, k, None)
+            field_names = self._remove_unnecessary_keys(named_args)
 
-            fields = [
-                FieldSerialisationData(
-                    variable=variable,
-                    association=association,
-                    decomposed=False,
-                    dimension=None,
-                    typespec=None,
-                    typename=None,
-                    ptr_var=None,
-                )
-                for variable, association in named_args.items()
+            metadata = [
+                Metadata(key=k, value=v)
+                for k, v in self._get_timestep_variables(stencil_name).items()
             ]
 
-            # todo: make this into a function
-            timestep_variables = {}
+            fields = self._make_fields(field_names)
 
-            if "mo_velocity_advection" in stencil_name:
-                timestep_variables["jstep"] = "jstep_ptr"
-                timestep_variables["nstep"] = "nstep_ptr"
-                timestep_variables["istep"] = "istep"
-
-            diffusion_stencil_names = [
-                "apply",
-                "calculate",
-                "enhance",
-                "update",
-                "temporary",
-                "diffusion",
-            ]
-
-            if any([name in stencil_name for name in diffusion_stencil_names]):
-                timestep_variables["jstep"] = "jstep_ptr"
-                timestep_variables["diffctr"] = "diffctr"
-
-            if "mo_intp_rbf" in stencil_name:
-                timestep_variables["jstep"] = "jstep_ptr"
-                timestep_variables["mo_intp_rbf_ctr"] = "mo_intp_rbf_ctr"
-
-            if "mo_math_divrot" in stencil_name:
-                timestep_variables["jstep"] = "jstep_ptr"
-                timestep_variables["mo_math_divrot_ctr"] = "mo_math_divrot_ctr"
-
-            if "grad_green_gauss" in stencil_name:
-                timestep_variables["jstep"] = "jstep_ptr"
-                timestep_variables["grad_green_gauss_ctr"] = "grad_green_gauss_ctr"
-
-            if "mo_icon_interpolation_scalar" in stencil_name:
-                timestep_variables["jstep"] = "jstep_ptr"
-                timestep_variables[
-                    "mo_icon_interpolation_ctr"
-                ] = "mo_icon_interpolation_ctr"
-
-            if "mo_advection_traj" in stencil_name:
-                timestep_variables["jstep"] = "jstep_ptr"
-                timestep_variables["mo_advection_traj_ctr"] = "mo_advection_traj_ctr"
-
-            if "mo_solve_nonhydro" in stencil_name:
-                timestep_variables["jstep"] = "jstep_ptr"
-                timestep_variables["nstep"] = "nstep_ptr"
-                timestep_variables["mo_solve_nonhydro_ctr"] = "mo_solve_nonhydro_ctr"
-                timestep_variables["istep"] = "istep"
-
-            timestep_metadata = [
-                Metadata(key=k, value=v) for k, v in timestep_variables.items()
-            ]
-
-            deserialised.append(
-                SavepointData(
+            for intent, ln in [("start", start.startln), ("end", end.startln)]:
+                savepoint = SavepointData(
                     subroutine=f"{stencil_name}",
-                    intent="start",
-                    startln=start.startln,
+                    intent=intent,
+                    startln=ln,
                     fields=fields,
-                    metadata=timestep_metadata,
+                    metadata=metadata,
                 )
-            )
+                deserialised.append(savepoint)
 
-            deserialised.append(
-                SavepointData(
-                    subroutine=f"{stencil_name}",
-                    intent="end",
-                    startln=end.startln,
-                    fields=fields,
-                    metadata=timestep_metadata,
-                )
-            )
         return deserialised
+
+    @staticmethod
+    def _remove_unnecessary_keys(named_args: dict) -> dict:
+        """Remove unnecessary keys from named_args, and only return field names."""
+        copy = named_args.copy()
+        [pop_item_from_dict(copy, k, None) for k in KEYS_TO_REMOVE]
+        for tol in TOLERANCE_ARGS:
+            for k in named_args.copy().keys():
+                if k.endswith(tol):
+                    pop_item_from_dict(named_args, k, None)
+        return copy
+
+    @staticmethod
+    def _make_fields(named_args: dict) -> list[FieldSerialisationData]:
+        """Create a list of FieldSerialisationData objects based on named arguments."""
+        fields = [
+            FieldSerialisationData(
+                variable=variable,
+                association="z_hydro_corr(:,:,1)"
+                if association == "z_hydro_corr(:,nlev,1)"
+                else association,
+                decomposed=False,
+                dimension=None,
+                typespec=None,
+                typename=None,
+                ptr_var=None,
+            )
+            for variable, association in named_args.items()
+            if variable not in SKIP_VARS
+        ]
+        return fields
+
+    @staticmethod
+    def _get_timestep_variables(stencil_name: str) -> dict:
+        """Get the corresponding timestep metadata variables for the stencil."""
+        timestep_variables = {
+            "jstep": "jstep_ptr"
+        }  # jstep is always included as it is the main timestep counter
+
+        diffusion_stencil_names = [
+            "apply",
+            "calculate",
+            "enhance",
+            "update",
+            "temporary",
+            "diffusion",
+        ]
+        if any(name in stencil_name for name in diffusion_stencil_names):
+            timestep_variables["diffctr"] = "diffctr"
+
+        if "mo_velocity_advection" in stencil_name:
+            timestep_variables["nstep"] = "nstep_ptr"
+            timestep_variables["istep"] = "istep"
+
+        if "mo_intp_rbf" in stencil_name:
+            timestep_variables["mo_intp_rbf_ctr"] = "mo_intp_rbf_ctr"
+
+        if "mo_math_divrot" in stencil_name:
+            timestep_variables["mo_math_divrot_ctr"] = "mo_math_divrot_ctr"
+
+        if "grad_green_gauss" in stencil_name:
+            timestep_variables["grad_green_gauss_ctr"] = "grad_green_gauss_ctr"
+
+        if "mo_icon_interpolation_scalar" in stencil_name:
+            timestep_variables[
+                "mo_icon_interpolation_ctr"
+            ] = "mo_icon_interpolation_ctr"
+
+        if "mo_advection_traj" in stencil_name:
+            timestep_variables["mo_advection_traj_ctr"] = "mo_advection_traj_ctr"
+
+        if "mo_solve_nonhydro" in stencil_name:
+            timestep_variables["nstep"] = "nstep_ptr"
+            timestep_variables["istep"] = "istep"
+            timestep_variables["mo_solve_nonhydro_ctr"] = "mo_solve_nonhydro_ctr"
+
+        return timestep_variables
 
 
 class SerialisationCodeDeserialiser(Deserialiser):
