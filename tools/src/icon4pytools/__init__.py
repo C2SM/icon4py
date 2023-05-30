a70305394ce69448d7ae5e73d904efba479667de
samkellerhals <kellerhalssamuel@gmail.com>
2023-05-30 16:16:17 +0200

    Add bumpversion

--- tools/src/icon4pytools/__init__.py
+++ tools/src/icon4pytools/__init__.py
@@ -11,11 +11,26 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+"""Package metadata: version, authors, license and copyright."""
+
 from typing import Final
+
+from packaging import version as pkg_version
 
 
 __all__ = [
+    "__author__",
+    "__copyright__",
+    "__license__",
     "__version__",
+    "__version_info__",
 ]
 
-__version__: Final = "0.0.3"
+
+__author__: Final = "ETH Zurich and individual contributors"
+__copyright__: Final = "Copyright (c) 2014-2022 ETH Zurich"
+__license__: Final = "GPL-3.0-or-later"
+
+
+__version__: Final = "0.0.4"
+__version_info__: Final = pkg_version.parse(__version__)
