228570545586a49a6b1972ac6ce39d27adf4abfa
samkellerhals <kellerhalssamuel@gmail.com>
2023-05-30 14:57:04 +0200

    Add icon4pytools.data namespace package and resources

--- tools/tests/f2ser/conftest.py
+++ tools/tests/f2ser/conftest.py
@@ -12,30 +12,31 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from pathlib import Path
+from importlib.resources import files
 
 import pytest
 
 
 @pytest.fixture
 def samples_path():
-    return Path(__file__).parent / "fortran_samples"
+    return files("icon4pytools.data")
 
 
 @pytest.fixture
 def diffusion_granule(samples_path):
-    return samples_path / "diffusion_granule.f90"
+    return samples_path.joinpath("diffusion_granule.f90")
 
 
 @pytest.fixture
 def diffusion_granule_deps(samples_path):
-    return [samples_path / "derived_types_example.f90"]
+    return [samples_path.joinpath("derived_types_example.f90")]
 
 
 @pytest.fixture
 def no_deps_source_file(samples_path):
-    return samples_path / "no_deps_subroutine_example.f90"
+    return samples_path.joinpath("no_deps_subroutine_example.f90")
 
 
 @pytest.fixture
 def not_existing_diffusion_granule(samples_path):
-    return samples_path / "not_existing_file.f90"
+    return samples_path.joinpath("not_existing_file.f90")
