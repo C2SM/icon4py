c9eae2185fe24bf5cdd29bf54e949aef5cab59b0
Samuel <kellerhalssamuel@gmail.com>
2023-05-31 15:58:08 +0200

    Add icon4pytools (#206)
    Introduces the icon4pytools package.
    
    This package contains all icon4py (blue line) related tooling and CLIs such as:
    
        - liskov
        - icon4pygen
        - f2ser
    
    
    
    Co-authored-by: Enrique G. Paredes <18477+egparedes@users.noreply.github.com>

--- atm_dyn_iconam/tests/test_calculate_nabla2_and_smag_coefficients_for_vn.py
+++ atm_dyn_iconam/tests/test_calculate_nabla2_and_smag_coefficients_for_vn.py
@@ -18,8 +18,9 @@ from icon4py.atm_dyn_iconam.calculate_nabla2_and_smag_coefficients_for_vn import
     calculate_nabla2_and_smag_coefficients_for_vn,
 )
 from icon4py.common.dimension import E2C2VDim, ECVDim, EdgeDim, KDim, VertexDim
-from icon4py.testutils.simple_mesh import SimpleMesh
-from icon4py.testutils.utils import as_1D_sparse_field, random_field, zero_field
+
+from .test_utils.helpers import as_1D_sparse_field, random_field, zero_field
+from .test_utils.simple_mesh import SimpleMesh
 
 
 def calculate_nabla2_and_smag_coefficients_for_vn_numpy(
--- atm_dyn_iconam/tests/test_mo_velocity_advection_stencil_12.py
+++ atm_dyn_iconam/tests/test_mo_velocity_advection_stencil_12.py
@@ -17,8 +17,9 @@ from icon4py.atm_dyn_iconam.mo_velocity_advection_stencil_12 import (
     mo_velocity_advection_stencil_12,
 )
 from icon4py.common.dimension import CellDim, KDim
-from icon4py.testutils.simple_mesh import SimpleMesh
-from icon4py.testutils.utils import random_field
+
+from .test_utils.helpers import random_field
+from .test_utils.simple_mesh import SimpleMesh
 
 
 def mo_velocity_advection_stencil_12_numpy(z_w_con_c: np.array) -> np.array:
