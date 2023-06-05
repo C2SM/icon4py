72439756a73f8b13c2abfa55edb4adad59529076
samkellerhals <kellerhalssamuel@gmail.com>
2023-06-05 12:54:15 +0200

    Remove pytest marker

--- atm_dyn_iconam/tests/conftest.py
+++ atm_dyn_iconam/tests/conftest.py
@@ -10,30 +10,3 @@
 # distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-
-import pytest
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        "--benchmark", action="store_true", default=False, help="run benchmarks"
-    )
-
-
-def pytest_configure(config):
-    config.addinivalue_line("markers", "benchmark: mark test as a benchmark")
-
-
-def pytest_collection_modifyitems(config, items):
-    if config.getoption("--benchmark"):
-        # --benchmark given in cli: do not skip benchmarks
-        return
-    skip_benchmark = pytest.mark.skip(reason="need --benchmark option to run")
-    for item in items:
-        if "benchmark" in item.keywords:
-            item.add_marker(skip_benchmark)
-
-
-@pytest.fixture
-def benchmark_rounds():
-    return 3
