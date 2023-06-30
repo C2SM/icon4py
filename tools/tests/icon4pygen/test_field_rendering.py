6b8bcb6399d6b4e7e7eaff12cbb28df79d97d409
Magdalena Luz <luzm@ethz.ch>
2023-06-30 18:13:21 +0200

    (fix) remove erroneous changes in tools.

--- tools/tests/icon4pygen/test_field_rendering.py
+++ tools/tests/icon4pygen/test_field_rendering.py
@@ -10,6 +10,7 @@
 # distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
+
 from gt4py.next.ffront.decorator import field_operator, program
 from gt4py.next.ffront.fbuiltins import Field, neighbor_sum
 from icon4py.common.dimension import E2CDim, EdgeDim, KDim
