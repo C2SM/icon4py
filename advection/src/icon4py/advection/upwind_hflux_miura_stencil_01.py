0ec6e1eb8d30e8d0e9857d077b5f5d295a684396
Nina Burgdorfer <Nina.Burgdorfer@meteoswiss.ch>
2023-07-20 09:16:26 +0200

    adapt stencil upwind_hflux_miura_stencil_01.py

--- advection/src/icon4py/advection/upwind_hflux_miura_stencil_01.py
+++ advection/src/icon4py/advection/upwind_hflux_miura_stencil_01.py
@@ -11,60 +11,49 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from gt4py.next.common import Field
 from gt4py.next.ffront.decorator import field_operator, program
-from gt4py.next.ffront.fbuiltins import Field, where
+from gt4py.next.ffront.fbuiltins import int32, where
 
-from icon4py.common.dimension import E2C, CellDim, EdgeDim, KDim
+from icon4py.common.dimension import CellDim, KDim, EdgeDim, E2C
 
 
 @field_operator
 def _upwind_hflux_miura_stencil_01(
-    p_vn: Field[[EdgeDim, KDim], float],
-    p_cc: Field[[CellDim, KDim], float],
+    z_lsq_coeff_1: Field[[CellDim, KDim], float],
+    z_lsq_coeff_2: Field[[CellDim, KDim], float],
+    z_lsq_coeff_3: Field[[CellDim, KDim], float],
     distv_bary_1: Field[[EdgeDim, KDim], float],
     distv_bary_2: Field[[EdgeDim, KDim], float],
-    z_grad_1: Field[[CellDim, KDim], float],
-    z_grad_2: Field[[CellDim, KDim], float],
     p_mass_flx_e: Field[[EdgeDim, KDim], float],
+    cell_rel_idx_dsl: Field[[EdgeDim, KDim], int32],
 ) -> Field[[EdgeDim, KDim], float]:
 
-    p_out_e = where(
-        p_vn > 0.0,
-        (
-            p_cc(E2C[0])
-            + distv_bary_1 * z_grad_1(E2C[0])
-            + distv_bary_2 * z_grad_2(E2C[0])
-        )
-        * p_mass_flx_e,
-        (
-            p_cc(E2C[1])
-            + distv_bary_1 * z_grad_1(E2C[1])
-            + distv_bary_2 * z_grad_2(E2C[1])
-        )
-        * p_mass_flx_e,
-    )
+	p_out_e = (  where(cell_rel_idx_dsl == int32(1), z_lsq_coeff_1(E2C[1]), z_lsq_coeff_1(E2C[0])) 
+             + distv_bary_1 * where(cell_rel_idx_dsl == int32(1), z_lsq_coeff_2(E2C[1]), z_lsq_coeff_2(E2C[0]))
+             + distv_bary_2 * where(cell_rel_idx_dsl == int32(1), z_lsq_coeff_3(E2C[1]), z_lsq_coeff_3(E2C[0]))
+             ) * p_mass_flx_e
 
-    return p_out_e
-
+	return p_out_e
 
 @program
 def upwind_hflux_miura_stencil_01(
-    p_vn: Field[[EdgeDim, KDim], float],
-    p_cc: Field[[CellDim, KDim], float],
+    z_lsq_coeff_1: Field[[CellDim, KDim], float],
+    z_lsq_coeff_2: Field[[CellDim, KDim], float],
+    z_lsq_coeff_3: Field[[CellDim, KDim], float],
     distv_bary_1: Field[[EdgeDim, KDim], float],
     distv_bary_2: Field[[EdgeDim, KDim], float],
-    z_grad_1: Field[[CellDim, KDim], float],
-    z_grad_2: Field[[CellDim, KDim], float],
     p_mass_flx_e: Field[[EdgeDim, KDim], float],
+    cell_rel_idx_dsl: Field[[EdgeDim, KDim], int32],
     p_out_e: Field[[EdgeDim, KDim], float],
 ):
     _upwind_hflux_miura_stencil_01(
-        p_vn,
-        p_cc,
+        z_lsq_coeff_1,
+        z_lsq_coeff_2,
+        z_lsq_coeff_3,
         distv_bary_1,
         distv_bary_2,
-        z_grad_1,
-        z_grad_2,
         p_mass_flx_e,
-        out=p_out_e,
+        cell_rel_idx_dsl,
+        out=(p_out_e)
     )
