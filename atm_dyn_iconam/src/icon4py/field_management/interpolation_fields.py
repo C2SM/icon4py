f74a1b9135235177a48e56ea0085ba598814f495
Magdalena Luz <luzm@ethz.ch>
2023-08-08 11:39:11 +0200

    add doc string

--- atm_dyn_iconam/src/icon4py/field_management/interpolation_fields.py
+++ atm_dyn_iconam/src/icon4py/field_management/interpolation_fields.py
@@ -30,13 +30,24 @@ def compute_c_lin_e(
     edge_cell_length: np.array,
     inv_dual_edge_length: np.array,
     owner_mask: np.array,
-    start_index: np.int32,
+    second_boundary_layer_start_index: np.int32,
 ) -> np.array:
+    """
+    Compute E2C average inverse distance.
+
+    Args:
+        edge_cell_length: numpy array, representing a Field[[EdgeDim, E2CDim], float]
+        inv_dual_edge_length: inverse dual edge length, numpy array representing a Field[[EdgeDim], float]
+        owner_mask: numpy array, representing a Field[[EdgeDim], bool]boolean field, True for all edges owned by this compute node
+        second_boundary_layer_start_index: start index of the 2nd boundary line: c_lin_e is not calculated for the first boundary layer
+
+    Returns: c_lin_e: numpy array  representing Field[[EdgeDim, E2CDim], float]
+
+    """
 
     c_lin_e_ = edge_cell_length[:, 1] * inv_dual_edge_length[:]
     c_lin_e = np.transpose([c_lin_e_, (1.0 - c_lin_e_)])
-    c_lin_e[0:start_index, :] = 0.0
+    c_lin_e[0:second_boundary_layer_start_index, :] = 0.0
     c_lin_e[:, 0] = np.where(owner_mask, c_lin_e[:, 0], 0.0)
     c_lin_e[:, 1] = np.where(owner_mask, c_lin_e[:, 1], 0.0)
-
     return c_lin_e
