6f8e7666ddd6d87dacdf039f171c2ef90d98b627
Magdalena Luz <luzm@ethz.ch>
2023-08-18 09:14:16 +0200

    diffusion_wrapper.py: use int32 type in index fields diffusion_wrapper.py: start end index fields

--- py2f/src/icon4py/py2f/wrappers/diffusion_wrapper.py
+++ py2f/src/icon4py/py2f/wrappers/diffusion_wrapper.py
@@ -14,6 +14,7 @@ import numpy as np
 
 # flake8: noqa
 from gt4py.next.common import Field
+from gt4py.next.ffront.fbuiltins import int32
 
 from icon4py.common.dimension import (
     C2E2CDim,
@@ -26,12 +27,12 @@ from icon4py.common.dimension import (
     EdgeDim,
     KDim,
     V2EDim,
-    VertexDim,
+    VertexDim, CECDim, E2C2VDim,
 )
 from icon4py.diffusion.diffusion import (
     Diffusion,
     DiffusionConfig,
-    DiffusionParams,
+    DiffusionParams, DiffusionType,
 )
 from icon4py.diffusion.diffusion_states import (
     DiffusionDiagnosticState,
@@ -41,7 +42,7 @@ from icon4py.diffusion.diffusion_states import (
 )
 from icon4py.grid.horizontal import CellParams, EdgeParams, HorizontalGridSize
 from icon4py.grid.icon_grid import GridConfig, IconGrid
-from icon4py.grid.vertical import VerticalModelParams
+from icon4py.grid.vertical import VerticalModelParams, VerticalGridSize
 from icon4py.py2f.cffi_utils import CffiMethod, to_fields
 
 
@@ -51,15 +52,13 @@ nproma = 50000
 field_sizes = {EdgeDim: nproma, CellDim: nproma, VertexDim: nproma}
 
 
-class VerticalGridConfig:
-    pass
 
 
 @to_fields(dim_sizes=field_sizes)
 @CffiMethod.register
 def diffusion_init(
     cvd_o_rd: float,  # unused, calculated internally
-    grav: float,  # unused
+    grav: float,  # unused, calculated internally
     jg: int,  # unused (no nesting)
     nproma: int,
     nlev: int,
@@ -98,7 +97,7 @@ def diffusion_init(
     l_zdiffu_t: bool,
     ltkeshs: bool,
     lsmag_3d: bool,
-    lvert_nest: bool,  # unused
+    lvert_nest: bool,  # unused no nesting
     ltimer: bool,  # unused
     ddt_vn_hdf_is_associated: bool,
     ddt_vn_dyn_is_associated: bool,  # unused,
@@ -111,51 +110,45 @@ def diffusion_init(
     geofac_grg_x: Field[[CellDim, C2E2CODim], float],
     geofac_grg_y: Field[[CellDim, C2E2CODim], float],
     nudgecoeff_e: Field[[EdgeDim], float],
-    enhfac_diffu: Field[[KDim], float],  #  unused, diffusion type 4 only
-    zd_intcoef: Field[[CellDim, C2E2CDim, KDim], float],
-    zd_geofac: Field[[CellDim, C2E2CODim], float],  # same as geofac_2ns
-    zd_diffcoef: Field[[CellDim, KDim], float],
+    enhfac_diffu: Field[[KDim], float],  #  unused, diffusion type  only
+    zd_intcoef: Field[[CellDim, C2E2CDim, KDim], float], # special DSL field:   zd_intcoef_dsl in mo_vertical_grid.f90
+    zd_geofac: Field[[CellDim, C2E2CODim], float],  # same as geofac_n2s
+    zd_diffcoef: Field[[CellDim, KDim], float],  # special DSL field mask instead of list: zd_diffcoef_dsl in mo_vertical_grid.f90
     wgtfac_c: Field[[CellDim, KDim], float],
     wgtfac_e: Field[[EdgeDim, KDim], float],  #  unused
     wgtfacq_e: Field[[EdgeDim, KDim], float],  #  unused
     wgtfacq1_e: Field[[EdgeDim, KDim], float],  #  unused
     ddqz_z_full_e: Field[[EdgeDim, KDim], float],  # unused
     theta_ref_mc: Field[[CellDim, KDim], float],
-    zd_indlist: Field[[CellDim, C2E2CDim, KDim], int],
-    zd_blklist: Field[[CellDim, C2E2CDim, KDim], int],  # unused,
-    zd_vertidx: Field[[CellDim, C2E2CDim, KDim], int],
+    zd_indlist: Field[[CellDim, C2E2CDim, KDim], int32], # unused, index list for  steep points, DSL uses mask_hdiff instead (see below)
+    zd_blklist: Field[[CellDim, C2E2CDim, KDim], int32],  # unused,
+    zd_vertidx: Field[[CellDim, C2E2CDim, KDim], int32],  # DSL uses offsets instead of absolute indices zd_vertoffset_dsl in mo_vertical_grid.f90
     zd_listdim: int,
-    edges_start_block: Field[
-        [KDim], int
-    ],  # unused, !!wrong dimension: probably is number of patches!!
-    edges_end_block: Field[
-        [KDim], int
-    ],  # unused, !!wrong dimension: probably is number of patches!!,
-    edges_vertex_idx: Field[[EdgeDim, E2VDim], int],
-    edges_vertex_blk: Field[[EdgeDim, E2VDim], int],  # unused, not blocking
-    edges_cell_idx: Field[[EdgeDim, E2CDim], int],
-    edges_cell_blk: Field[[EdgeDim, E2CDim], int],  # unused, not blocking
+    ### edges_start_block: np.ndarray, # unused, we need start, end indices instead, see below
+    ### edges_end_block:np.ndarray, # unused, we need start, end indices instead, see below
+    edges_vertex_idx: Field[[EdgeDim, E2VDim], int32],
+    edges_vertex_blk: Field[[EdgeDim, E2VDim], int32],  # unused, no blocking
+    edges_cell_idx: Field[[EdgeDim, E2CDim], int32],
+    edges_cell_blk: Field[[EdgeDim, E2CDim], int32],  # unused, no blocking
     edges_tangent_orientation: Field[[EdgeDim], float],
-    edges_primal_normal_vert_1: Field[[ECVDim], float],
-    edges_primal_normal_vert_2: Field[[ECVDim], float],
-    edges_dual_normal_vert_1: Field[[ECVDim], float],
-    edges_dual_normal_vert_2: Field[[ECVDim], float],
+    edges_primal_normal_vert_1: Field[[ECVDim], float], # shallow derived type in Fortran
+    edges_primal_normal_vert_2: Field[[ECVDim], float], # shallow derived type in Fortran
+    edges_dual_normal_vert_1: Field[[ECVDim], float], # shallow derived type in Fortran
+    edges_dual_normal_vert_2: Field[[ECVDim], float], # shallow derived type in Fortran
     edges_inv_vert_vert_lengths: Field[[EdgeDim], float],
     edges_inv_primal_edge_length: Field[[EdgeDim], float],
     edges_inv_dual_edge_length: Field[[EdgeDim], float],
     edges_area_edge: Field[[EdgeDim], float],
-    cells_start_block: Field[
-        [KDim], int
-    ],  # unused, !!wrong dimension: probably is number of patches!!
-    cells_end_block: Field[
-        [KDim], int
-    ],  # unused, !!wrong dimension: probably is number of patches!!
-    cells_neighbor_idx: Field[[CellDim, C2E2CDim], int],
-    cells_neighbor_blk: Field[[CellDim, C2E2CDim], int],  # unused, not blocking,
-    cells_edge_idx: Field[[CellDim, C2EDim], int],
-    cells_edge_blk: Field[[CellDim, C2EDim], int],  # unused, not blocking,
+    ### cells_start_block: np.ndarray,  # unused, we need start, end indices instead, see below
+    ### cells_end_block: np.ndarray,  # unused, we need start, end indices instead, see below
+    cells_neighbor_idx: Field[[CellDim, C2E2CDim], int32],
+    ### cells_neighbor_blk: Field[[CellDim, C2E2CDim], int32],  # unused, not blocking,
+    cells_edge_idx: Field[[CellDim, C2EDim], int32],
+    ### cells_edge_blk: np.ndarray,  # unused, not blocking,
     cells_area: Field[[CellDim], float],
-    mask_hdiff: Field[[CellDim, KDim], int],  # dsl specific
+    # dsl specific additional args
+    mask_hdiff: Field[[CellDim, KDim], bool],
+    zd_vertoffset: Field[[CECDim], int32],
     rbf_coeff_1: Field[
         [VertexDim, V2EDim], float
     ],  # -> used in rbf_vec_interpol_vertex
@@ -163,14 +156,25 @@ def diffusion_init(
         [VertexDim, V2EDim], float
     ],  # -> used in rbf_vec_interpol_vertex
     verts_edge_idx: Field[
-        [VertexDim, V2EDim], int
+        [VertexDim, V2EDim], int32
     ],  # -> mo_intp_rbf_rbf_vec_interpol_vertex
+    ### vertex_starts: np.ndarray, # start and endindices for horizontal domain, are through p_patch in Fortran granule and used implicitly in get_indices_
+    ### vertex_ends: np.ndarray,
+    ### edge_starts:np.ndarray,
+    ### edge_ends:np.ndarray,
+    ### cell_starts: np.ndarray,
+    ### cell_ends:np.ndarray,
 ):
     """
     Instantiate and Initialize the diffusion object.
 
-    should only accept simple fields as arguments for compatibility with the standalone
-    Fortran ICON Diffusion component (aka Diffusion granule)
+    Takes all the argument that are passed to the Fortran diffusion granule, see
+    https://gitlab.dkrz.de/icon/icon-cscs/-/blob/diffusion_granule/src/atm_dyn_iconam/mo_nh_diffusion_new.f90
+    It should only accept simple fields as arguments for compatibility with the standalone
+    Fortran ICON Diffusion component (aka Diffusion granule).
+    Open issues:
+    - It does not yet support shallow derived types so these have to passed as simple fields.
+    - How to handle DSL specific fields (which are restructured to match the need of the DSL)
 
     """
     if diffusion.initialized:
@@ -178,14 +182,14 @@ def diffusion_init(
             "Diffusion has already been already initialized"
         )
 
-    config: DiffusionConfig = DiffusionConfig(
-        diffusion_type=hdiff_order,
+    diffusion_config: DiffusionConfig = DiffusionConfig(
+        diffusion_type=DiffusionType(hdiff_order),
         type_t_diffu=itype_t_diffu,
         type_vn_diffu=itype_vn_diffu,
         smagorinski_scaling_factor=hdiff_smag_fac,
         hdiff_efdt_ratio=hdiff_efdt_ratio,
         max_nudging_coeff=nudge_max_coeff,
-        velocity_boundary_diffusion_denominator=denom_diffu_v,
+        velocity_boundary_diffusion_denom=denom_diffu_v,
         hdiff_rcf=lhdiff_rcf,
         hdiff_w=lhdiff_w,
         hdiff_temp=lhdiff_temp,
@@ -198,59 +202,71 @@ def diffusion_init(
     horizontal_size = HorizontalGridSize(
         num_cells=nproma, num_vertices=nproma, num_edges=nproma
     )
-    vertical_size = VerticalGridConfig(
-        num_lev=nlev, nshift=n_shift, nshift_total=n_shift_total
+    vertical_size = VerticalGridSize(
+        num_lev=nlev
     )
     mesh_config = GridConfig(
         horizontal_config=horizontal_size,
         vertical_config=vertical_size,
         limited_area=l_limited_area,
+        n_shift_total=n_shift_total
     )
-    c2e2c = np.asarray(cells_neighbor_idx)
-    c2e2c0 = np.column_stack((c2e2c, (np.asarray(range(c2e2c.shape[0])))))
-    connectivities = {
-        E2CDim: np.asarray(edges_cell_idx),
-        E2VDim: np.asarray(edges_vertex_idx),
-        C2EDim: np.asarray(cells_edge_idx),
-        C2E2CDim: c2e2c,
-        C2E2CODim: c2e2c0,
-        V2EDim: np.asarray(verts_edge_idx),
-    }
-    grid = IconGrid().with_config(mesh_config).with_connectivities(connectivities)
 
     edges_params = EdgeParams(
         tangent_orientation=edges_tangent_orientation,
+        primal_edge_lengths=None,
         inverse_primal_edge_lengths=edges_inv_primal_edge_length,
-        dual_normal_vert=(edges_dual_normal_vert_1, edges_dual_normal_vert_2),
+        dual_edge_lengths=None,
         inverse_dual_edge_lengths=edges_inv_dual_edge_length,
         inverse_vertex_vertex_lengths=edges_inv_vert_vert_lengths,
-        primal_normal_vert=(edges_primal_normal_vert_1, edges_primal_normal_vert_2),
+        dual_normal_vert_x=edges_dual_normal_vert_1,
+        dual_normal_vert_y=edges_dual_normal_vert_2,
+        primal_normal_vert_x=edges_primal_normal_vert_1,
+        primal_normal_vert_y=edges_primal_normal_vert_2,
         edge_areas=edges_area_edge,
+
     )
+    # we need the start, end indices in order for the grid to be functional those are not passed
+    # to init in the Fortran diffusion granule since they are hidden away in the get_indices_[c,e,v]
+    # diffusion_run does take p_patch in order to pass it on to other subroutines (interpolation, get_indices...
+
+    c2e2c0 = np.column_stack(((np.asarray(cells_neighbor_idx)), (np.asarray(range(
+        np.asarray(cells_neighbor_idx).shape[0])))))
+    e2c2v = np.asarray(edges_vertex_idx)
+    e2v=e2c2v[:, 0:2]
+    connectivities = {
+        E2CDim: np.asarray(edges_cell_idx),
+        E2C2VDim: e2c2v,
+        E2VDim: e2v,
+        C2EDim: np.asarray(cells_edge_idx),
+        C2E2CDim: (np.asarray(cells_neighbor_idx)),
+        C2E2CODim: c2e2c0,
+        V2EDim: np.asarray(verts_edge_idx),
+
+    }
+    grid = IconGrid().with_config(mesh_config).with_connectivities(connectivities).with_start_end_indices(VertexDim, vertex_starts, vertex_ends).with_start_end_indices(EdgeDim, edge_starts, edge_ends).with_start_end_indices(CellDim, cell_starts, cell_ends)
 
     cell_params = CellParams(cells_area)
     vertical_params = VerticalModelParams(vct_a=vct_a, rayleigh_damping_height=nrdmax)
-    config: DiffusionConfig = DiffusionConfig(grid, vertical_params)
-    derived_diffusion_params = DiffusionParams(config)
+    derived_diffusion_params = DiffusionParams(diffusion_config)
     metric_state = DiffusionMetricState(
         theta_ref_mc=theta_ref_mc,
         wgtfac_c=wgtfac_c,
         mask_hdiff=mask_hdiff,
-        zd_vertidx=zd_vertidx,
         zd_diffcoef=zd_diffcoef,
         zd_intcoef=zd_intcoef,
-        zd_indlist=zd_indlist,
-        zd_listdim=zd_listdim,
+        zd_vertoffset=zd_vertoffset,
+
     )
     interpolation_state = DiffusionInterpolationState(
-        e_bln_c_s,
-        rbf_coeff_1,
-        rbf_coeff_2,
-        geofac_div,
-        geofac_n2s,
-        geofac_grg_x,
-        geofac_grg_y,
-        nudgecoeff_e,
+        e_bln_c_s=e_bln_c_s,
+        rbf_coeff_1=rbf_coeff_1,
+        rbf_coeff_2=rbf_coeff_2,
+        geofac_div=geofac_div,
+        geofac_n2s=geofac_n2s,
+        geofac_grg_x=geofac_grg_x,
+        geofac_grg_y=geofac_grg_y,
+        nudgecoeff_e=nudgecoeff_e,
     )
 
     diffusion.init(
@@ -260,7 +276,7 @@ def diffusion_init(
         vertical_params=vertical_params,
         metric_state=metric_state,
         interpolation_state=interpolation_state,
-        edge_params=edge_params,
+        edge_params=edges_params,
         cell_params=cell_params,
     )
 
