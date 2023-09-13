6ed5a30337df7a07ea4b8b7e09f8da920195b4ab
nfarabullini <nicoletta.farabullini@c2sm.ethz.ch>
2023-09-13 16:10:40 +0200

    additional edits to diffusion test

--- model/atmosphere/diffusion/diffusion_tests/test_diffusion.py
+++ model/atmosphere/diffusion/diffusion_tests/test_diffusion.py
@@ -14,18 +14,12 @@
 import numpy as np
 import pytest
 
-from icon4py.model.common.test_utils.serialbox_utils import (
-    IconDiffusionExitSavepoint,
-    IconDiffusionInitSavepoint,
-)
 from icon4py.model.atmosphere.diffusion.diffusion import Diffusion, DiffusionParams
-from icon4py.model.atmosphere.diffusion.diffusion_states import (
-    DiffusionDiagnosticState,
-    PrognosticState,
-)
+
 from icon4py.model.atmosphere.diffusion.diffusion_utils import scale_k
 from icon4py.model.common.grid.horizontal import CellParams, EdgeParams
 from icon4py.model.common.grid.vertical import VerticalModelParams
+from icon4py.model.common.test_utils.serialbox_utils import IconDiffusionInitSavepoint
 
 from .utils import (
     diff_multfac_vn_numpy,
