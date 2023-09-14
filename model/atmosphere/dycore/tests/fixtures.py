60d29c21e99d4b4018cfc615a4e21248b959c1fc
samkellerhals <kellerhalssamuel@gmail.com>
2023-09-14 16:24:29 +0200

    Update URIs

--- model/atmosphere/dycore/tests/fixtures.py
+++ model/atmosphere/dycore/tests/fixtures.py
@@ -18,29 +18,6 @@ import pytest
 from .serialbox_utils import IconSerialDataProvider
 
 
-mch_ch_r04b09_dsl_grid_uri = (
-    "https://polybox.ethz.ch/index.php/s/vcsCYmCFA9Qe26p/download"
-)
-r02b04_global_grid_uri = "https://polybox.ethz.ch/index.php/s/0EM8O8U53GKGsst/download"
-data_uri = "https://polybox.ethz.ch/index.php/s/LcAbscZqnsx4WCf/download"
-data_path_old = Path(__file__).parent.joinpath("ser_icondata")
-base_path = Path(__file__).parent.parent.parent.parent.joinpath("testdata")
-
-data_path = base_path.joinpath("ser_icondata")
-extracted_path = data_path.joinpath("mpitask1/mch_ch_r04b09_dsl/ser_data")
-data_file = data_path.joinpath("mch_ch_r04b09_dsl.tar.gz").name
-
-grids_path = base_path.joinpath("grids")
-r04b09_dsl_grid_path = grids_path.joinpath("mch_ch_r04b09_dsl")
-r04b09_dsl_data_file = r04b09_dsl_grid_path.joinpath(
-    "mch_ch_r04b09_dsl_grids_v1.tar.gz"
-).name
-r02b04_global_grid_path = grids_path.joinpath("r02b04_global")
-r02b04_global_data_file = r02b04_global_grid_path.joinpath(
-    "icon_grid_0013_R02B04_G.tar.gz"
-).name
-
-
 @pytest.fixture
 def data_provider(setup_icon_data) -> IconSerialDataProvider:
     return IconSerialDataProvider("icon_pydycore", str(extracted_path), True)
--- model/common/src/icon4py/model/common/test_utils/fixtures.py
+++ model/common/src/icon4py/model/common/test_utils/fixtures.py
@@ -27,6 +27,28 @@ model = test_utils.parent.parent
 common = model.parent.parent.parent.parent
 base_path = common.parent.joinpath("testdata")
 
+mch_ch_r04b09_dsl_grid_uri = (
+    "https://polybox.ethz.ch/index.php/s/vcsCYmCFA9Qe26p/download"
+)
+r02b04_global_grid_uri = "https://polybox.ethz.ch/index.php/s/0EM8O8U53GKGsst/download"
+data_uri = "https://polybox.ethz.ch/index.php/s/LcAbscZqnsx4WCf/download"
+data_path_old = Path(__file__).parent.joinpath("ser_icondata")
+base_path = Path(__file__).parent.parent.parent.parent.joinpath("testdata")
+
+data_path = base_path.joinpath("ser_icondata")
+extracted_path = data_path.joinpath("mpitask1/mch_ch_r04b09_dsl/ser_data")
+data_file = data_path.joinpath("mch_ch_r04b09_dsl.tar.gz").name
+
+grids_path = base_path.joinpath("grids")
+r04b09_dsl_grid_path = grids_path.joinpath("mch_ch_r04b09_dsl")
+r04b09_dsl_data_file = r04b09_dsl_grid_path.joinpath(
+    "mch_ch_r04b09_dsl_grids_v1.tar.gz"
+).name
+r02b04_global_grid_path = grids_path.joinpath("r02b04_global")
+r02b04_global_data_file = r02b04_global_grid_path.joinpath(
+    "icon_grid_0013_R02B04_G.tar.gz"
+).name
+
 data_uris = {
     1: "https://polybox.ethz.ch/index.php/s/vcsCYmCFA9Qe26p/download",
     2: "https://polybox.ethz.ch/index.php/s/NUQjmJcMEoQxFiK/download",
