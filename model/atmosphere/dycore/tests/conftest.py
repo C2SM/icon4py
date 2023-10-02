fca9e3848b8fa07d0554ed68e959924f1c443e79
Rico Haeuselmann <r.haeuselmann@gmx.ch>
2023-10-02 11:54:28 +0200

    Add CSCS CI Runners (#275)
    Adds support for triggering CSCS CI Runners.
    
    ---------
    
    Co-authored-by: Rico Häuselmann <ricoh@cscs.ch>
    Co-authored-by: samkellerhals <kellerhalssamuel@gmail.com>
    Co-authored-by: Enrique G. Paredes <18477+egparedes@users.noreply.github.com>

--- model/atmosphere/dycore/tests/conftest.py
+++ model/atmosphere/dycore/tests/conftest.py
@@ -28,5 +28,6 @@ from icon4py.model.common.test_utils.fixtures import (  # noqa F401
 from icon4py.model.common.test_utils.pytest_config import (  # noqa: F401
     pytest_addoption,
     pytest_configure,
+    pytest_generate_tests,
     pytest_runtest_setup,
 )
