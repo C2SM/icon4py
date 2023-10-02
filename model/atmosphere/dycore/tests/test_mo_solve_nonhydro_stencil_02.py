9a7b55afb826069d609120cf89c052f1ee8105f3
nfarabullini <nicoletta.farabullini@c2sm.ethz.ch>
2023-10-02 11:03:04 +0200

    edits to tests

--- model/atmosphere/dycore/tests/test_mo_solve_nonhydro_stencil_02.py
+++ model/atmosphere/dycore/tests/test_mo_solve_nonhydro_stencil_02.py
@@ -13,6 +13,7 @@
 
 import numpy as np
 import pytest
+from gt4py.next.ffront.fbuiltins import int32
 
 from icon4py.model.atmosphere.dycore.mo_solve_nonhydro_stencil_02 import (
     mo_solve_nonhydro_stencil_02,
@@ -58,4 +59,8 @@ class TestMoSolveNonhydroStencil02(StencilTest):
             exner_ref_mc=exner_ref_mc,
             exner_pr=exner_pr,
             z_exner_ex_pr=z_exner_ex_pr,
+            horizontal_start=int32(0),
+            horizontal_end=int32(mesh.n_cells),
+            vertical_start=int32(1),
+            vertical_end=int32(mesh.k_level),
         )
--- model/atmosphere/dycore/tests/test_mo_solve_nonhydro_stencil_03.py
+++ model/atmosphere/dycore/tests/test_mo_solve_nonhydro_stencil_03.py
@@ -13,6 +13,7 @@
 
 import numpy as np
 import pytest
+from gt4py.next.ffront.fbuiltins import int32
 
 from icon4py.model.atmosphere.dycore.mo_solve_nonhydro_stencil_03 import (
     mo_solve_nonhydro_stencil_03,
@@ -36,4 +37,8 @@ class TestMoSolveNonhydroStencil03(StencilTest):
 
         return dict(
             z_exner_ex_pr=z_exner_ex_pr,
+            horizontal_start=int32(0),
+            horizontal_end=int32(mesh.n_cells),
+            vertical_start=int32(1),
+            vertical_end=int32(mesh.k_level),
         )
