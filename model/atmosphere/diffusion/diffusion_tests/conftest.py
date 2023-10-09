01cf13200c0c49eb71b3aa1bce547f364bc4c29e
Magdalena <luzm@ethz.ch>
2023-10-09 13:28:40 +0200

    pass hooks pytest_config.py via pytest config file (#287)
    Unify how pytest config is loaded in all the model packages and make it available also when running tests from the repository base directory: pass `pytest_config.py` through `addopts` in the `pytest` inif files.

--- model/atmosphere/diffusion/diffusion_tests/conftest.py
+++ model/atmosphere/diffusion/diffusion_tests/conftest.py
@@ -33,12 +33,6 @@ from icon4py.model.common.test_utils.fixtures import (  # noqa: F401  # import f
     step_date_exit,
     step_date_init,
 )
-from icon4py.model.common.test_utils.pytest_config import (  # noqa: F401
-    pytest_addoption,
-    pytest_configure,
-    pytest_generate_tests,
-    pytest_runtest_setup,
-)
 
 
 @pytest.fixture
