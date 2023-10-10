117b98c34d1eea456bf08b611932fec19ccde5d9
Magdalena Luz <luzm@ethz.ch>
2023-10-10 11:01:22 +0200

    port test_velocity_advection_stencil_20.py to StencilTest

--- model/atmosphere/dycore/tests/stencil_tests/test_mo_velocity_advection_stencil_20.py
+++ model/atmosphere/dycore/tests/stencil_tests/test_mo_velocity_advection_stencil_20.py
@@ -12,140 +12,115 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import numpy as np
+import pytest
 from gt4py.next.ffront.fbuiltins import int32
 
 from icon4py.model.atmosphere.dycore.mo_velocity_advection_stencil_20 import (
     mo_velocity_advection_stencil_20,
 )
 from icon4py.model.common.dimension import CellDim, E2C2EODim, E2CDim, EdgeDim, KDim, VertexDim
-from icon4py.model.common.test_utils.helpers import random_field, random_mask
-from icon4py.model.common.test_utils.simple_mesh import SimpleMesh
+from icon4py.model.common.test_utils.helpers import StencilTest, random_field, random_mask
 
 
-def mo_velocity_advection_stencil_20_numpy(
-    e2c: np.array,
-    e2c2eO: np.array,
-    e2v: np.array,
-    levelmask: np.array,
-    c_lin_e: np.array,
-    z_w_con_c_full: np.array,
-    ddqz_z_full_e: np.array,
-    area_edge: np.array,
-    tangent_orientation: np.array,
-    inv_primal_edge_length: np.array,
-    zeta: np.array,
-    geofac_grdiv: np.array,
-    vn: np.array,
-    ddt_vn_adv: np.array,
-    cfl_w_limit,
-    scalfac_exdiff,
-    dtime,
-):
-    w_con_e = np.zeros_like(vn)
-    difcoef = np.zeros_like(vn)
+class TestMoVelocityAdvectionStencil20(StencilTest):
+    PROGRAM = mo_velocity_advection_stencil_20
+    OUTPUTS = ("ddt_vn_apc",)
 
-    levelmask_offset_0 = levelmask[:-1]
-    levelmask_offset_1 = levelmask[1:]
+    @pytest.fixture
+    def input_data(self, mesh):
+        levelmask = random_mask(mesh, KDim, extend={KDim: 1})
+        c_lin_e = random_field(mesh, EdgeDim, E2CDim)
+        z_w_con_c_full = random_field(mesh, CellDim, KDim)
+        ddqz_z_full_e = random_field(mesh, EdgeDim, KDim)
+        area_edge = random_field(mesh, EdgeDim)
+        tangent_orientation = random_field(mesh, EdgeDim)
+        inv_primal_edge_length = random_field(mesh, EdgeDim)
+        zeta = random_field(mesh, VertexDim, KDim)
+        geofac_grdiv = random_field(mesh, EdgeDim, E2C2EODim)
+        vn = random_field(mesh, EdgeDim, KDim)
+        ddt_vn_apc = random_field(mesh, EdgeDim, KDim)
+        cfl_w_limit = 4.0
+        scalfac_exdiff = 6.0
+        dtime = 2.0
+        return dict(
+            levelmask=levelmask,
+            ddqz_z_full_e=ddqz_z_full_e,
+            area_edge=area_edge,
+            tangent_orientation=tangent_orientation,
+            inv_primal_edge_length=inv_primal_edge_length,
+            zeta=zeta,
+            geofac_grdiv=geofac_grdiv,
+            vn=vn,
+            ddt_vn_apc=ddt_vn_apc,
+            cfl_w_limit=cfl_w_limit,
+            scalfac_exdiff=scalfac_exdiff,
+            dtime=dtime,
+            c_lin_e=c_lin_e,
+            z_w_con_c_full=z_w_con_c_full,
+            horizontal_start=int32(0),
+            horizontal_end=int32(mesh.n_edges),
+            vertical_start=int32(0),
+            vertical_end=int32(mesh.k_level),
+        )
 
-    c_lin_e = np.expand_dims(c_lin_e, axis=-1)
-    geofac_grdiv = np.expand_dims(geofac_grdiv, axis=-1)
-    area_edge = np.expand_dims(area_edge, axis=-1)
-    tangent_orientation = np.expand_dims(tangent_orientation, axis=-1)
-    inv_primal_edge_length = np.expand_dims(inv_primal_edge_length, axis=-1)
-
-    w_con_e = np.where(
-        (levelmask_offset_0) | (levelmask_offset_1),
-        np.sum(c_lin_e * z_w_con_c_full[e2c], axis=1),
-        w_con_e,
-    )
-    difcoef = np.where(
-        ((levelmask_offset_0) | (levelmask_offset_1))
-        & (np.abs(w_con_e) > cfl_w_limit * ddqz_z_full_e),
-        scalfac_exdiff
-        * np.minimum(
-            0.85 - cfl_w_limit * dtime,
-            np.abs(w_con_e) * dtime / ddqz_z_full_e - cfl_w_limit * dtime,
-        ),
-        difcoef,
-    )
-    ddt_vn_adv = np.where(
-        ((levelmask_offset_0) | (levelmask_offset_1))
-        & (np.abs(w_con_e) > cfl_w_limit * ddqz_z_full_e),
-        ddt_vn_adv
-        + difcoef
-        * area_edge
-        * (
-            np.sum(geofac_grdiv * vn[e2c2eO], axis=1)
-            + tangent_orientation * inv_primal_edge_length * (zeta[e2v][:, 1] - zeta[e2v][:, 0])
-        ),
-        ddt_vn_adv,
-    )
-    return ddt_vn_adv
-
-
-def test_mo_velocity_advection_stencil_20():
-    mesh = SimpleMesh()
-
-    levelmask = random_mask(mesh, KDim, extend={KDim: 1})
-    c_lin_e = random_field(mesh, EdgeDim, E2CDim)
-    z_w_con_c_full = random_field(mesh, CellDim, KDim)
-    ddqz_z_full_e = random_field(mesh, EdgeDim, KDim)
-    area_edge = random_field(mesh, EdgeDim)
-    tangent_orientation = random_field(mesh, EdgeDim)
-    inv_primal_edge_length = random_field(mesh, EdgeDim)
-    zeta = random_field(mesh, VertexDim, KDim)
-    geofac_grdiv = random_field(mesh, EdgeDim, E2C2EODim)
-    vn = random_field(mesh, EdgeDim, KDim)
-    ddt_vn_adv = random_field(mesh, EdgeDim, KDim)
-    cfl_w_limit = 4.0
-    scalfac_exdiff = 6.0
-    dtime = 2.0
-
-    ddt_vn_adv_ref = mo_velocity_advection_stencil_20_numpy(
-        mesh.e2c,
-        mesh.e2c2eO,
-        mesh.e2v,
-        np.asarray(levelmask),
-        np.asarray(c_lin_e),
-        np.asarray(z_w_con_c_full),
-        np.asarray(ddqz_z_full_e),
-        np.asarray(area_edge),
-        np.asarray(tangent_orientation),
-        np.asarray(inv_primal_edge_length),
-        np.asarray(zeta),
-        np.asarray(geofac_grdiv),
-        np.asarray(vn),
-        np.asarray(ddt_vn_adv),
+    @staticmethod
+    def reference(
+        mesh,
+        levelmask: np.array,
+        c_lin_e: np.array,
+        z_w_con_c_full: np.array,
+        ddqz_z_full_e: np.array,
+        area_edge: np.array,
+        tangent_orientation: np.array,
+        inv_primal_edge_length: np.array,
+        zeta: np.array,
+        geofac_grdiv: np.array,
+        vn: np.array,
+        ddt_vn_apc: np.array,
         cfl_w_limit,
         scalfac_exdiff,
         dtime,
-    )
+        **kwargs,
+    ):
+        w_con_e = np.zeros_like(vn)
+        difcoef = np.zeros_like(vn)
 
-    mo_velocity_advection_stencil_20(
-        levelmask,
-        c_lin_e,
-        z_w_con_c_full,
-        ddqz_z_full_e,
-        area_edge,
-        tangent_orientation,
-        inv_primal_edge_length,
-        zeta,
-        geofac_grdiv,
-        vn,
-        ddt_vn_adv,
-        cfl_w_limit,
-        scalfac_exdiff,
-        dtime,
-        horizontal_start=int32(0),
-        horizontal_end=int32(mesh.n_edges),
-        vertical_start=int32(0),
-        vertical_end=int32(mesh.k_level),
-        offset_provider={
-            "Koff": KDim,
-            "E2C": mesh.get_e2c_offset_provider(),
-            "E2C2EO": mesh.get_e2c2eO_offset_provider(),
-            "E2V": mesh.get_e2v_offset_provider(),
-        },
-    )
+        levelmask_offset_0 = levelmask[:-1]
+        levelmask_offset_1 = levelmask[1:]
 
-    assert np.allclose(ddt_vn_adv, ddt_vn_adv_ref)
+        c_lin_e = np.expand_dims(c_lin_e, axis=-1)
+        geofac_grdiv = np.expand_dims(geofac_grdiv, axis=-1)
+        area_edge = np.expand_dims(area_edge, axis=-1)
+        tangent_orientation = np.expand_dims(tangent_orientation, axis=-1)
+        inv_primal_edge_length = np.expand_dims(inv_primal_edge_length, axis=-1)
+
+        w_con_e = np.where(
+            (levelmask_offset_0) | (levelmask_offset_1),
+            np.sum(c_lin_e * z_w_con_c_full[mesh.e2c], axis=1),
+            w_con_e,
+        )
+        difcoef = np.where(
+            ((levelmask_offset_0) | (levelmask_offset_1))
+            & (np.abs(w_con_e) > cfl_w_limit * ddqz_z_full_e),
+            scalfac_exdiff
+            * np.minimum(
+                0.85 - cfl_w_limit * dtime,
+                np.abs(w_con_e) * dtime / ddqz_z_full_e - cfl_w_limit * dtime,
+            ),
+            difcoef,
+        )
+        ddt_vn_apc = np.where(
+            ((levelmask_offset_0) | (levelmask_offset_1))
+            & (np.abs(w_con_e) > cfl_w_limit * ddqz_z_full_e),
+            ddt_vn_apc
+            + difcoef
+            * area_edge
+            * (
+                np.sum(geofac_grdiv * vn[mesh.e2c2eO], axis=1)
+                + tangent_orientation
+                * inv_primal_edge_length
+                * (zeta[mesh.e2v][:, 1] - zeta[mesh.e2v][:, 0])
+            ),
+            ddt_vn_apc,
+        )
+        return dict(ddt_vn_apc=ddt_vn_apc)
