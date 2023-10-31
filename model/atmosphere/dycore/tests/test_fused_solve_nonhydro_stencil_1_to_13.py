c044d57ac162a1c4c1ffd4cd1eaf95f725d54c1e
nfarabullini <nicoletta.farabullini@c2sm.ethz.ch>
2023-10-31 09:45:43 +0100

    edits to fused stencils test

--- model/atmosphere/dycore/tests/test_fused_solve_nonhydro_stencil_1_to_13.py
+++ model/atmosphere/dycore/tests/test_fused_solve_nonhydro_stencil_1_to_13.py
@@ -15,6 +15,23 @@ import numpy as np
 import pytest
 from gt4py.next import np_as_located_field
 from gt4py.next.ffront.fbuiltins import int32
+from stencil_tests.test_mo_solve_nonhydro_stencil_01 import mo_solve_nonhydro_stencil_01_numpy
+from stencil_tests.test_mo_solve_nonhydro_stencil_02 import mo_solve_nonhydro_stencil_02_numpy
+from stencil_tests.test_mo_solve_nonhydro_stencil_04 import mo_solve_nonhydro_stencil_04_numpy
+from stencil_tests.test_mo_solve_nonhydro_stencil_05 import mo_solve_nonhydro_stencil_05_numpy
+from stencil_tests.test_mo_solve_nonhydro_stencil_06 import mo_solve_nonhydro_stencil_06_numpy
+from stencil_tests.test_mo_solve_nonhydro_stencil_07 import mo_solve_nonhydro_stencil_07_numpy
+from stencil_tests.test_mo_solve_nonhydro_stencil_08 import mo_solve_nonhydro_stencil_08_numpy
+from stencil_tests.test_mo_solve_nonhydro_stencil_09 import mo_solve_nonhydro_stencil_09_numpy
+from stencil_tests.test_mo_solve_nonhydro_stencil_10 import mo_solve_nonhydro_stencil_10_numpy
+from stencil_tests.test_mo_solve_nonhydro_stencil_11_lower import (
+    mo_solve_nonhydro_stencil_11_lower_numpy,
+)
+from stencil_tests.test_mo_solve_nonhydro_stencil_11_upper import (
+    mo_solve_nonhydro_stencil_11_upper_numpy,
+)
+from stencil_tests.test_mo_solve_nonhydro_stencil_12 import mo_solve_nonhydro_stencil_12_numpy
+from stencil_tests.test_mo_solve_nonhydro_stencil_13 import mo_solve_nonhydro_stencil_13_numpy
 
 from icon4py.model.atmosphere.dycore.fused_mo_solve_nonhydro_stencils_01_to_13 import (
     fused_mo_solve_nonhydro_stencils_01_to_13,
@@ -59,9 +76,9 @@ class TestFusedMoSolveNonHydroStencil1To13(StencilTest):
     def reference(
         cls,
         mesh,
-        rho,
+        rho_nnow,
         rho_ref_mc,
-        theta_v,
+        theta_v_nnow,
         theta_ref_mc,
         z_rth_pr_1,
         z_rth_pr_2,
@@ -79,7 +96,7 @@ class TestFusedMoSolveNonHydroStencil1To13(StencilTest):
         rho_ic,
         z_exner_ic,
         exner_exfac,
-        exner,
+        exner_nnow,
         exner_ref_mc,
         z_exner_ex_pr,
         z_dexner_dz_c_1,
@@ -90,13 +107,10 @@ class TestFusedMoSolveNonHydroStencil1To13(StencilTest):
         vert_idx,
         limited_area,
         igradp_method,
-        k_field,
         w,
         w_concorr_c,
-        rho_now,
-        rho_var,
-        theta_now,
-        theta_var,
+        rho_nvar,
+        theta_v_nvar,
         dtime,
         wgt_nnow_rth,
         wgt_nnew_rth,
@@ -113,19 +127,205 @@ class TestFusedMoSolveNonHydroStencil1To13(StencilTest):
         horizontal_upper_03,
         horizontal_lower_11,
         horizontal_upper_11,
-        nlev,
+        n_lev,
         nflatlev,
         nflat_gradp,
     ):
-        iadv_rhotheta = 2
         horz_idx = horz_idx[:, np.newaxis]
-        z_grad_rth_1 = np_as_located_field(CellDim, KDim)(np.zeros_like(z_rth_pr_1))
-        z_grad_rth_2 = np_as_located_field(CellDim, KDim)(np.zeros_like(z_rth_pr_1))
-        exner_pr = np_as_located_field(CellDim, KDim)(np.zeros_like(z_rth_pr_1))
-        z_exner_ic = np_as_located_field(CellDim, KDim)(np.zeros_like(z_rth_pr_1))
-        rho_ic = np_as_located_field(CellDim, KDim)(np.zeros_like(z_rth_pr_1))
-        z_theta_v_pr_ic = np_as_located_field(CellDim, KDim)(np.zeros_like(z_rth_pr_1))
-        z_th_ddz_exner_c = np_as_located_field(CellDim, KDim)(np.zeros_like(z_rth_pr_1))
+
+        if istep == 1:
+            if limited_area:
+                (z_rth_pr_1, z_rth_pr_2) = np.where(
+                    (horizontal_lower_01 <= horz_idx) & (horz_idx < horizontal_upper_01),
+                    mo_solve_nonhydro_stencil_01_numpy(
+                        mesh=mesh,
+                        z_rth_pr_1=z_rth_pr_1,
+                        z_rth_pr_2=z_rth_pr_2,
+                    ),
+                    (z_rth_pr_1, z_rth_pr_2),
+                )
+
+            (z_exner_ex_pr, exner_pr) = np.where(
+                (horizontal_lower_02 <= horz_idx)
+                & (horz_idx < horizontal_upper_02)
+                & (vert_idx < (n_lev + int32(1))),
+                mo_solve_nonhydro_stencil_02_numpy(
+                    mesh=mesh,
+                    exner_exfac=exner_exfac,
+                    exner=exner_nnow,
+                    exner_ref_mc=exner_ref_mc,
+                    exner_pr=exner_pr,
+                ),
+                (z_exner_ex_pr, exner_pr),
+            )
+
+            z_exner_ex_pr = np.where(
+                (horizontal_lower_02 <= horz_idx)
+                & (horz_idx < horizontal_upper_02)
+                & (vert_idx == n_lev),
+                0.0,
+                z_exner_ex_pr,
+            )
+
+            vert_start = np.maximum(1, nflatlev)
+            if igradp_method == 3:
+                z_exner_ic[:, :n_lev] = np.where(
+                    (horizontal_lower_02 <= horz_idx)
+                    & (horz_idx < horizontal_upper_02)
+                    & (vert_idx == n_lev),
+                    mo_solve_nonhydro_stencil_04_numpy(
+                        mesh=mesh,
+                        z_exner_ex_pr=z_exner_ex_pr,
+                        wgtfacq_c=wgtfacq_c_dsl,
+                        z_exner_ic=z_exner_ic[:, :n_lev],
+                    ),
+                    z_exner_ic[:, :n_lev],
+                )
+                z_exner_ic[:, :n_lev] = np.where(
+                    (horizontal_lower_02 <= horz_idx)
+                    & (horz_idx < horizontal_upper_02)
+                    & (vert_start <= vert_idx)
+                    & (vert_idx < (n_lev + int32(1))),
+                    mo_solve_nonhydro_stencil_05_numpy(
+                        mesh=mesh, wgtfac_c=wgtfac_c, z_exner_ex_pr=z_exner_ex_pr
+                    ),
+                    z_exner_ic[:, :n_lev],
+                )
+
+                z_dexner_dz_c_1 = np.where(
+                    (horizontal_lower_02 <= horz_idx)
+                    & (horz_idx < horizontal_upper_02)
+                    & (vert_start <= vert_idx)
+                    & (vert_idx < (n_lev + int32(1))),
+                    mo_solve_nonhydro_stencil_06_numpy(
+                        mesh=mesh, z_exner_ic=z_exner_ic, inv_ddqz_z_full=inv_ddqz_z_full
+                    ),
+                    z_dexner_dz_c_1,
+                )
+
+            (z_rth_pr_1, z_rth_pr_2) = np.where(
+                (horizontal_lower_02 <= horz_idx)
+                & (horz_idx < horizontal_upper_02)
+                & (vert_idx == int32(0)),
+                mo_solve_nonhydro_stencil_07_numpy(
+                    mesh=mesh,
+                    rho=rho_nnow,
+                    rho_ref_mc=rho_ref_mc,
+                    theta_v=theta_v_nnow,
+                    theta_ref_mc=theta_ref_mc,
+                ),
+                (z_rth_pr_1, z_rth_pr_2),
+            )
+
+            (rho_ic, z_rth_pr_1, z_rth_pr_2) = np.where(
+                (horizontal_lower_02 <= horz_idx)
+                & (horz_idx < horizontal_upper_02)
+                & (vert_idx >= int32(0)),
+                mo_solve_nonhydro_stencil_08_numpy(
+                    mesh=mesh,
+                    wgtfac_c=wgtfac_c,
+                    rho=rho_nnow,
+                    rho_ref_mc=rho_ref_mc,
+                    theta_v=theta_v_nnow,
+                    theta_ref_mc=theta_ref_mc,
+                ),
+                (rho_ic, z_rth_pr_1, z_rth_pr_2),
+            )
+
+            (z_theta_v_pr_ic[:, :n_lev], theta_v_ic, z_th_ddz_exner_c) = np.where(
+                (horizontal_lower_02 <= horz_idx)
+                & (horz_idx < horizontal_upper_02)
+                & (vert_idx >= int32(0)),
+                mo_solve_nonhydro_stencil_09_numpy(
+                    mesh=mesh,
+                    wgtfac_c=wgtfac_c,
+                    z_rth_pr_2=z_rth_pr_2,
+                    theta_v=theta_v_nnow,
+                    vwind_expl_wgt=vwind_expl_wgt,
+                    exner_pr=exner_pr,
+                    d_exner_dz_ref_ic=d_exner_dz_ref_ic,
+                    ddqz_z_half=ddqz_z_half,
+                ),
+                (z_theta_v_pr_ic[:, :n_lev], theta_v_ic, z_th_ddz_exner_c),
+            )
+
+            z_theta_v_pr_ic[:, :n_lev] = np.where(
+                (horizontal_lower_02 <= horz_idx)
+                & (horz_idx < horizontal_upper_02)
+                & (vert_idx == int32(0)),
+                mo_solve_nonhydro_stencil_11_lower_numpy(
+                    mesh=mesh, z_theta_v_pr_ic=z_theta_v_pr_ic[:, :n_lev]
+                ),
+                z_theta_v_pr_ic[:, :n_lev],
+            )
+
+            (z_theta_v_pr_ic[:, :n_lev], theta_v_ic) = np.where(
+                (horizontal_lower_02 <= horz_idx)
+                & (horz_idx < horizontal_upper_02)
+                & (vert_idx == n_lev),
+                mo_solve_nonhydro_stencil_11_upper_numpy(
+                    mesh=mesh,
+                    wgtfacq_c=wgtfacq_c_dsl,
+                    z_rth_pr=z_rth_pr_2,
+                    theta_ref_ic=theta_ref_ic,
+                    z_theta_v_pr_ic=z_theta_v_pr_ic[:, :n_lev],
+                ),
+                (z_theta_v_pr_ic[:, :n_lev], theta_v_ic),
+            )
+
+            vert_start = nflat_gradp
+            if igradp_method == 3:
+                z_dexner_dz_c_2 = np.where(
+                    (horizontal_lower_02 <= horz_idx)
+                    & (horz_idx < horizontal_upper_02)
+                    & (vert_start <= vert_idx),
+                    mo_solve_nonhydro_stencil_12_numpy(
+                        mesh=mesh,
+                        z_theta_v_pr_ic=z_theta_v_pr_ic,
+                        d2dexdz2_fac1_mc=d2dexdz2_fac1_mc,
+                        d2dexdz2_fac2_mc=d2dexdz2_fac2_mc,
+                        z_rth_pr_2=z_rth_pr_2,
+                    ),
+                    z_dexner_dz_c_2,
+                )
+
+            (z_rth_pr_1, z_rth_pr_2) = np.where(
+                (horizontal_lower_03 <= horz_idx) & (horz_idx < horizontal_upper_03),
+                mo_solve_nonhydro_stencil_13_numpy(
+                    mesh=mesh,
+                    rho=rho_nnow,
+                    rho_ref_mc=rho_ref_mc,
+                    theta_v=theta_v_nnow,
+                    theta_ref_mc=theta_ref_mc,
+                ),
+                (z_rth_pr_1, z_rth_pr_2),
+            )
+
+        else:
+            (rho_ic, z_theta_v_pr_ic[:, :n_lev], theta_v_ic, z_th_ddz_exner_c) = np.where(
+                (horizontal_lower_11 <= horz_idx)
+                & (horz_idx < horizontal_upper_11)
+                & (int32(1) <= vert_idx),
+                mo_solve_nonhydro_stencil_10_numpy(
+                    mesh=mesh,
+                    w=w,
+                    w_concorr_c=w_concorr_c,
+                    ddqz_z_half=ddqz_z_half,
+                    rho_now=rho_nnow,
+                    rho_var=rho_nvar,
+                    theta_now=theta_v_nnow,
+                    theta_var=theta_v_nvar,
+                    wgtfac_c=wgtfac_c,
+                    theta_ref_mc=theta_ref_mc,
+                    vwind_expl_wgt=vwind_expl_wgt,
+                    exner_pr=exner_pr,
+                    d_exner_dz_ref_ic=d_exner_dz_ref_ic,
+                    dtime=dtime,
+                    wgt_nnow_rth=wgt_nnow_rth,
+                    wgt_nnew_rth=wgt_nnew_rth,
+                ),
+                (rho_ic, z_theta_v_pr_ic[:, :n_lev], theta_v_ic, z_th_ddz_exner_c),
+            )
 
         return dict(
             z_exner_ex_pr=z_exner_ex_pr,
@@ -143,16 +343,12 @@ class TestFusedMoSolveNonHydroStencil1To13(StencilTest):
 
     @pytest.fixture
     def input_data(self, mesh):
-
-        rho = random_field(mesh, CellDim, KDim)
         rho_ref_mc = random_field(mesh, CellDim, KDim)
-        theta_v = random_field(mesh, CellDim, KDim)
         theta_ref_mc = random_field(mesh, CellDim, KDim)
         z_rth_pr_1 = random_field(mesh, CellDim, KDim)
         z_rth_pr_2 = random_field(mesh, CellDim, KDim)
-        z_theta_v_pr_ic = random_field(mesh, CellDim, KDim)
+        z_theta_v_pr_ic = random_field(mesh, CellDim, KDim, extend={KDim: 1})
         theta_ref_ic = random_field(mesh, CellDim, KDim)
-
         d2dexdz2_fac1_mc = random_field(mesh, CellDim, KDim)
         d2dexdz2_fac2_mc = random_field(mesh, CellDim, KDim)
         wgtfacq_c_dsl = random_field(mesh, CellDim, KDim)
@@ -161,29 +357,23 @@ class TestFusedMoSolveNonHydroStencil1To13(StencilTest):
         exner_pr = random_field(mesh, CellDim, KDim)
         d_exner_dz_ref_ic = random_field(mesh, CellDim, KDim)
         ddqz_z_half = random_field(mesh, CellDim, KDim)
-
         z_th_ddz_exner_c = random_field(mesh, CellDim, KDim)
         rho_ic = random_field(mesh, CellDim, KDim)
-        z_exner_ic = random_field(mesh, CellDim, KDim)
+        z_exner_ic = random_field(mesh, CellDim, KDim, extend={KDim: 1})
         exner_exfac = random_field(mesh, CellDim, KDim)
-        exner = random_field(mesh, CellDim, KDim)
+        exner_nnow = random_field(mesh, CellDim, KDim)
         exner_ref_mc = random_field(mesh, CellDim, KDim)
         z_exner_ex_pr = random_field(mesh, CellDim, KDim)
-
         z_dexner_dz_c_1 = random_field(mesh, CellDim, KDim)
         z_dexner_dz_c_2 = random_field(mesh, CellDim, KDim)
-
         theta_v_ic = random_field(mesh, CellDim, KDim)
         inv_ddqz_z_full = random_field(mesh, CellDim, KDim)
-
         w_concorr_c = random_field(mesh, CellDim, KDim)
         w = random_field(mesh, CellDim, KDim)
-        rho_now = random_field(mesh, CellDim, KDim)
-        rho_var = random_field(mesh, CellDim, KDim)
-        theta_now = random_field(mesh, CellDim, KDim)
-        theta_var = random_field(mesh, CellDim, KDim)
-
-        k_field = zero_field(mesh, KDim, dtype=int32)  # TODO: @abishekg7 change later
+        rho_nnow = random_field(mesh, CellDim, KDim)
+        rho_nvar = random_field(mesh, CellDim, KDim)
+        theta_v_nnow = random_field(mesh, CellDim, KDim)
+        theta_v_nvar = random_field(mesh, CellDim, KDim)
 
         vert_idx = zero_field(mesh, KDim, dtype=int32)
         for level in range(mesh.k_level):
@@ -193,21 +383,16 @@ class TestFusedMoSolveNonHydroStencil1To13(StencilTest):
         for cell in range(mesh.n_cells):
             horz_idx[cell] = cell
 
-        grav_o_cpd = 9.80665 / 1004.64
         dtime = 0.9
-        p_dthalf = 0.5 * dtime
-        idiv_method = 1
         igradp_method = 3
         wgt_nnow_rth = 0.25
         wgt_nnew_rth = 0.75
-        cpd = 1004.64
-        iau_wgt_dyn = 1.0
-        is_iau_active = False
         limited_area = True
-        itime_scheme = 4
-        istep = 1
-        horizontal_lower = 5387
-        horizontal_upper = 31558
+        istep = 2
+        horizontal_start = 5387
+        horizontal_end = 31558
+        vertical_start = 0
+        vertical_end = mesh.k_level
         horizontal_lower_01 = 31558
         horizontal_upper_01 = 31558
         horizontal_lower_02 = 31558
@@ -216,15 +401,14 @@ class TestFusedMoSolveNonHydroStencil1To13(StencilTest):
         horizontal_upper_03 = 31558
         horizontal_lower_11 = 3777
         horizontal_upper_11 = 31558
-
-        nlev = mesh.k_level
+        n_lev = mesh.k_level
         nflatlev = 4
         nflat_gradp = 27
 
         return dict(
-            rho=rho,
+            rho_nnow=rho_nnow,
             rho_ref_mc=rho_ref_mc,
-            theta_v=theta_v,
+            theta_v_nnow=theta_v_nnow,
             theta_ref_mc=theta_ref_mc,
             z_rth_pr_1=z_rth_pr_1,
             z_rth_pr_2=z_rth_pr_2,
@@ -242,7 +426,7 @@ class TestFusedMoSolveNonHydroStencil1To13(StencilTest):
             rho_ic=rho_ic,
             z_exner_ic=z_exner_ic,
             exner_exfac=exner_exfac,
-            exner=exner,
+            exner_nnow=exner_nnow,
             exner_ref_mc=exner_ref_mc,
             z_exner_ex_pr=z_exner_ex_pr,
             z_dexner_dz_c_1=z_dexner_dz_c_1,
@@ -253,21 +437,18 @@ class TestFusedMoSolveNonHydroStencil1To13(StencilTest):
             vert_idx=vert_idx,
             limited_area=limited_area,
             igradp_method=igradp_method,
-            k_field=k_field,
             w=w,
             w_concorr_c=w_concorr_c,
-            rho_now=rho_now,
-            rho_var=rho_var,
-            theta_now=theta_now,
-            theta_var=theta_var,
+            rho_nvar=rho_nvar,
+            theta_v_nvar=theta_v_nvar,
             dtime=dtime,
             wgt_nnow_rth=wgt_nnow_rth,
             wgt_nnew_rth=wgt_nnew_rth,
             istep=istep,
-            horizontal_start=horizontal_lower,
-            horizontal_end=horizontal_upper,
-            vertical_start=0,
-            vertical_end=nlev,
+            horizontal_start=horizontal_start,
+            horizontal_end=horizontal_end,
+            vertical_start=vertical_start,
+            vertical_end=vertical_end,
             horizontal_lower_01=horizontal_lower_01,
             horizontal_upper_01=horizontal_upper_01,
             horizontal_lower_02=horizontal_lower_02,
@@ -276,7 +457,7 @@ class TestFusedMoSolveNonHydroStencil1To13(StencilTest):
             horizontal_upper_03=horizontal_upper_03,
             horizontal_lower_11=horizontal_lower_11,
             horizontal_upper_11=horizontal_upper_11,
-            nlev=nlev,
+            n_lev=n_lev,
             nflatlev=nflatlev,
             nflat_gradp=nflat_gradp,
         )
