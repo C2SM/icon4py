6bb84b44f5f3c6ec8e48319fd9fb0c400d57a9b0
Daniel Hupp <daniel.hupp@meteoswiss.ch>
2023-11-02 12:01:36 +0100

    pre-commit

--- model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/fused_velocity_advection_stencil_19_to_20.py
+++ model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/fused_velocity_advection_stencil_19_to_20.py
@@ -77,25 +77,26 @@ def _fused_velocity_advection_stencil_19_to_20(
     )
 
     if extra_diffu:
-      ddt_vn_apc = where( maximum(2, nrdmax - 2) <= k < nlev - 3,
-              _mo_velocity_advection_stencil_20(
-                  levelmask,
-                  c_lin_e,
-                  z_w_con_c_full,
-                  ddqz_z_full_e,
-                  area_edge,
-                  tangent_orientation,
-                  inv_primal_edge_length,
-                  zeta,
-                  geofac_grdiv,
-                  vn,
-                  ddt_vn_apc,
-                  cfl_w_limit,
-                  scalfac_exdiff,
-                  d_time,
-              ),
-              ddt_vn_apc,
-          )
+        ddt_vn_apc = where(
+            maximum(2, nrdmax - 2) <= k < nlev - 3,
+            _mo_velocity_advection_stencil_20(
+                levelmask,
+                c_lin_e,
+                z_w_con_c_full,
+                ddqz_z_full_e,
+                area_edge,
+                tangent_orientation,
+                inv_primal_edge_length,
+                zeta,
+                geofac_grdiv,
+                vn,
+                ddt_vn_apc,
+                cfl_w_limit,
+                scalfac_exdiff,
+                d_time,
+            ),
+            ddt_vn_apc,
+        )
 
     return ddt_vn_apc
 
