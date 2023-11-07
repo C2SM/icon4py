fa46f6c73e6550d079484bb74ba44a0760d4a003
Chia Rui Ong <chiarui.ong@c2sm.ethz.ch>
2023-11-07 15:49:29 +0100

    Attempting to resolve shadowing of Python builtin in stencil 14

--- model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/mo_velocity_advection_stencil_14.py
+++ model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/mo_velocity_advection_stencil_14.py
@@ -13,7 +13,12 @@
 
 from gt4py.next.common import GridType
 from gt4py.next.ffront.decorator import field_operator, program
-from gt4py.next.ffront.fbuiltins import Field, abs, broadcast, where
+from gt4py.next.ffront.fbuiltins import (
+    Field,
+    abs, # noqa: A004 abs is shadowing a Python builtin
+    broadcast,
+    where
+)
 
 from icon4py.model.common.dimension import CellDim, KDim
 
