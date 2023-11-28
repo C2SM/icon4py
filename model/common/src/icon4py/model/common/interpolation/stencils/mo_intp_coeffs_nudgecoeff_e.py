00efda3f8ae27208663e8c270b075852f2df1d2a
Jonas Jucker <jonas.jucker@c2sm.ethz.ch>
2023-11-28 16:59:18 +0100

    add nudgecoeffs_e

---
+++ model/common/src/icon4py/model/common/interpolation/stencils/mo_intp_coeffs_nudgecoeff_e.py
@@ -0,0 +1,50 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from gt4py.next.common import GridType
+from gt4py.next.ffront.decorator import field_operator, program
+from gt4py.next.ffront.fbuiltins import Field, int32, neighbor_sum
+
+from icon4py.model.common.dimension import V2E, EdgeDim, KDim, V2EDim, VertexDim
+from icon4py.model.common.type_alias import wpfloat
+
+
+@field_operator
+def _mo_intp_coeffs_nudgecoeff_e(
+    refin_ctrl: Field[[EdgeDim], int32],
+    grf_nudge_start_e: int32,
+    nudge_max_coeffs: wpfloat,
+) -> Field[[EdgeDim], wpfloat]:
+
+    return nudge_max_coeffs * EXP(-(refin_ctrl - grf_nudge_start_e)/ 4.0)
+
+
+@program(grid_type=GridType.UNSTRUCTURED)
+def mo_intp_coeffs_nudgecoeff_e(
+    refin_ctrl: Field[[EdgeDim], int32],
+    grf_nudge_start_e: int32,
+    nudge_max_coeffs: wpfloat,
+    nudgecoeffs_e: Field[[EdgeDim], wpfloat],
+    horizontal_start: int32,
+    horizontal_end: int32,
+):
+    _mo_intp_coeffs_nudgecoeff_e(
+        refin_ctrl,
+        grf_nudge_start_e,
+        nudge_max_coeffs,
+        out=nudgecoeffs_e,
+        domain={
+            EdgeDim: (horizontal_start, horizontal_end),
+        },
+    )
+
