Bugfixes due to wrong dimensions in greenline (#319)
(Fix) Part 2 of adapting to new `gt4py` field representation.

- remove remaining `np.asarray` 
- fix bugs due to run dimension in serialbox input in `Diffusion` and `SolveNonHydro`

Co-authored-by: Nina Burgdorfer <Nina.Burgdorfer@meteoswiss.ch>
Co-authored-by: ninaburg <nina.burg@hotmail.com>

model/atmosphere/diffusion/tests/diffusion_tests/test_diffusion_utils.py
@@ -49,7 +49,7 @@ def test_diff_multfac_vn_and_smag_limit_for_initial_step(backend):
     smag_limit_init = zero_field(grid, KDim)
     k4 = 1.0
     efdt_ratio = 24.0
-    shape = np.asarray(diff_multfac_vn_init).shape
+    shape = diff_multfac_vn_init.asnumpy().shape
 
     expected_diff_multfac_vn_init = initial_diff_multfac_vn_numpy(shape, k4, efdt_ratio)
     expected_smag_limit_init = smag_limit_numpy(
@@ -71,7 +71,7 @@ def test_diff_multfac_vn_smag_limit_for_time_step_with_const_value(backend):
     k4 = 1.0
     substeps = 5.0
     efdt_ratio = 24.0
-    shape = np.asarray(diff_multfac_vn).shape
+    shape = diff_multfac_vn.asnumpy().shape
 
     expected_diff_multfac_vn = diff_multfac_vn_numpy(shape, k4, substeps)
     expected_smag_limit = smag_limit_numpy(diff_multfac_vn_numpy, shape, k4, substeps)
@@ -92,7 +92,7 @@ def test_diff_multfac_vn_smag_limit_for_loop_run_with_k4_substeps(backend):
     k4 = 0.003
     substeps = 1.0
 
-    shape = np.asarray(diff_multfac_vn).shape
+    shape = diff_multfac_vn.asnumpy().shape
     expected_diff_multfac_vn = diff_multfac_vn_numpy(shape, k4, substeps)
     expected_smag_limit = smag_limit_numpy(diff_multfac_vn_numpy, shape, k4, substeps)
     _setup_runtime_diff_multfac_vn.with_backend(backend)(

model/atmosphere/dycore/tests/dycore_tests/mpi_tests/test_parallel_solve_nonhydro.py
@@ -235,50 +235,50 @@ def test_run_solve_nonhydro_single_step(
         expected_theta_v,
         calculated_theta_v,
     )
-    expected_exner = np.asarray(sp_step_exit.exner_new())
-    calculated_exner = np.asarray(prognostic_state_nnew.exner)
+    expected_exner = sp_step_exit.exner_new().asnumpy()
+    calculated_exner = prognostic_state_nnew.exner.asnumpy()
     assert dallclose(
         expected_exner,
         calculated_exner,
     )
     assert dallclose(
-        np.asarray(savepoint_nonhydro_exit.vn_new()),
-        np.asarray(prognostic_state_nnew.vn),
+        savepoint_nonhydro_exit.vn_new().asnumpy(),
+        prognostic_state_nnew.vn.asnumpy(),
         rtol=1e-10,
     )
     assert dallclose(
-        np.asarray(savepoint_nonhydro_exit.w_new()),
-        np.asarray(prognostic_state_nnew.w),
+        savepoint_nonhydro_exit.w_new().asnumpy(),
+        prognostic_state_nnew.w.asnumpy(),
         atol=8e-14,
     )
     assert dallclose(
-        np.asarray(savepoint_nonhydro_exit.rho_new()),
-        np.asarray(prognostic_state_nnew.rho),
+        savepoint_nonhydro_exit.rho_new().asnumpy(),
+        prognostic_state_nnew.rho.asnumpy(),
     )
 
     assert dallclose(
-        np.asarray(savepoint_nonhydro_exit.rho_ic()),
-        np.asarray(diagnostic_state_nh.rho_ic),
+        savepoint_nonhydro_exit.rho_ic().asnumpy(),
+        diagnostic_state_nh.rho_ic.asnumpy(),
     )
 
     assert dallclose(
-        np.asarray(savepoint_nonhydro_exit.theta_v_ic()),
-        np.asarray(diagnostic_state_nh.theta_v_ic),
+        savepoint_nonhydro_exit.theta_v_ic().asnumpy(),
+        diagnostic_state_nh.theta_v_ic.asnumpy(),
     )
 
     assert dallclose(
-        np.asarray(savepoint_nonhydro_exit.mass_fl_e()),
-        np.asarray(diagnostic_state_nh.mass_fl_e),
+        savepoint_nonhydro_exit.mass_fl_e().asnumpy(),
+        diagnostic_state_nh.mass_fl_e.asnumpy(),
         rtol=1e-10,
     )
 
     assert dallclose(
-        np.asarray(savepoint_nonhydro_exit.mass_flx_me()),
-        np.asarray(prep_adv.mass_flx_me),
+        savepoint_nonhydro_exit.mass_flx_me().asnumpy(),
+        prep_adv.mass_flx_me.asnumpy(),
         rtol=1e-10,
     )
     assert dallclose(
-        np.asarray(savepoint_nonhydro_exit.vn_traj()),
-        np.asarray(prep_adv.vn_traj),
+        savepoint_nonhydro_exit.vn_traj().asnumpy(),
+        prep_adv.vn_traj.asnumpy(),
         rtol=1e-10,
     )