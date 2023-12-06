35d0ef14fef0019f3d7f371e94ebbdcb476aed8c
Magdalena Luz <luzm@ethz.ch>
2023-12-06 08:38:18 +0100

    remove unused class

--- model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/state_utils/states.py
+++ model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/state_utils/states.py
@@ -12,7 +12,6 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 from dataclasses import dataclass
 
-from gt4py.next import Field
 from gt4py.next.common import Field
 
 from icon4py.model.common.dimension import (
@@ -29,35 +28,6 @@ from icon4py.model.common.dimension import (
     V2CDim,
     ECDim,
 )
-
-
-@dataclass
-class DiagnosticState:
-    # fields for 3D elements in turbdiff
-    hdef_ic: Field[
-        [CellDim, KDim], float
-    ]  # ! divergence at half levels(nproma,nlevp1,nblks_c)     [1/s]
-    div_ic: Field[
-        [CellDim, KDim], float
-    ]  # ! horizontal wind field deformation (nproma,nlevp1,nblks_c)     [1/s^2]
-    dwdx: Field[
-        [CellDim, KDim], float
-    ]  # zonal gradient of vertical wind speed (nproma,nlevp1,nblks_c)     [1/s]
-
-    dwdy: Field[
-        [CellDim, KDim], float
-    ]  # meridional gradient of vertical wind speed (nproma,nlevp1,nblks_c)
-
-    vt: Field[[EdgeDim, KDim], float]
-    vn_ie: Field[
-        [EdgeDim, KDim], float
-    ]  # normal wind at half levels (nproma,nlevp1,nblks_e)   [m/s] # TODO: change this back to KHalfDim, but how do we treat it wrt to field_operators and domain?
-    w_concorr_c: Field[
-        [CellDim, KDim], float
-    ]  # contravariant vert correction (nproma,nlevp1,nblks_c)[m/s] # TODO: change this back to KHalfDim, but how do we treat it wrt to field_operators and domain?
-    ddt_w_adv_pc: Field[[CellDim, KDim], float]
-    ddt_vn_apc_pc: Field[[EdgeDim, KDim], float]
-    ntnd: float
 
 
 @dataclass
