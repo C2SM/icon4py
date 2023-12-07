e1430767c4313a56e470c7fa4e1ba5ade408fb94
samkellerhals <kellerhalssamuel@gmail.com>
2023-12-07 13:49:01 +0100

    Add gpu benchmarks

--- model/common/src/icon4py/model/common/test_utils/pytest_config.py
+++ model/common/src/icon4py/model/common/test_utils/pytest_config.py
@@ -14,8 +14,8 @@
 import os
 
 import pytest
-from gt4py.next.program_processors.runners.gtfn import run_gtfn
-from gt4py.next.program_processors.runners.roundtrip import executor
+from gt4py.next.program_processors.runners.gtfn import run_gtfn, run_gtfn_gpu
+from gt4py.next.program_processors.runners.roundtrip import backend as run_embedded_iterator_ir
 
 
 def pytest_configure(config):
@@ -42,8 +42,8 @@ def pytest_addoption(parser):
         parser.addoption(
             "--backend",
             action="store",
-            default="embedded",
-            help="GT4Py backend to use when executing stencils. Defaults to embedded, other options include gtfn_cpu",
+            default=None,
+            help="GT4Py backend to use when executing stencils. Defaults to embedded backend, other options include gtfn_cpu, gtfn_gpu, embedded_ir",
         )
     except ValueError:
         pass
@@ -83,17 +83,18 @@ def pytest_generate_tests(metafunc):
         params = []
         ids = []
 
-        if backend_option == "gtfn_cpu":
+        if backend_option is None:
+            params.append(None)
+            ids.append("embedded")
+        elif backend_option == "gtfn_cpu":
             params.append(run_gtfn)
             ids.append("backend=gtfn_cpu")
-        elif backend_option == "embedded":
-            params.append(executor)
-            ids.append("backend=embedded")
-        # TODO (skellerhals): add gpu support
-        else:
-            raise Exception(
-                "Need to select a backend. Select from: ['embedded', 'gtfn_cpu'] and pass it as an argument to --backend when invoking pytest."
-            )
+        elif backend_option == "embedded_ir":
+            params.append(run_embedded_iterator_ir)
+            ids.append("backend=embedded_ir")
+        elif backend_option == "gtfn_gpu":
+            params.append(run_gtfn_gpu)
+            ids.append("backend=gtfn_gpu")
 
         metafunc.parametrize("backend", params, ids=ids)
 
