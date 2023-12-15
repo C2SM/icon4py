6e153b5350ad3418c1dd2613a11222ec62a3e0bf
Magdalena <luzm@ethz.ch>
2023-12-15 17:00:52 +0100

    Verify global model (#315)
    Add data tests for verification for a global model experiment (exclaim_ape_R02B04)
    Add additional parameter in apply_diffusion_to_w_and_compute_horizontal_gradients_for_turbulence.py for type_sher != 2
    Remove circular dependency from common upon diffusion

--- model/atmosphere/dycore/tests/dycore_tests/test_velocity_advection.py
+++ model/atmosphere/dycore/tests/dycore_tests/test_velocity_advection.py
@@ -16,9 +16,11 @@ from gt4py.next.ffront.fbuiltins import int32
 
 from icon4py.model.atmosphere.dycore.state_utils.states import DiagnosticStateNonHydro
 from icon4py.model.atmosphere.dycore.velocity.velocity_advection import VelocityAdvection
-from icon4py.model.common.grid.horizontal import CellParams, EdgeParams
+from icon4py.model.common.dimension import CellDim, EdgeDim
+from icon4py.model.common.grid.horizontal import CellParams, EdgeParams, HorizontalMarkerIndex
 from icon4py.model.common.grid.vertical import VerticalModelParams
 from icon4py.model.common.states.prognostic_state import PrognosticState
+from icon4py.model.common.test_utils.datatest_utils import GLOBAL_EXPERIMENT, REGIONAL_EXPERIMENT
 from icon4py.model.common.test_utils.helpers import dallclose
 
 from .utils import construct_interpolation_state_for_nonhydro, construct_nh_metric_state
@@ -69,15 +71,22 @@ def test_velocity_init(
         owner_mask=grid_savepoint.c_owner_mask(),
     )
 
-    assert dallclose(0.0, velocity_advection.cfl_clipping.asnumpy())
-    assert dallclose(False, velocity_advection.levmask.asnumpy())
-    assert dallclose(0.0, velocity_advection.vcfl_dsl.asnumpy())
+    assert dallclose(velocity_advection.cfl_clipping.asnumpy(), 0.0)
+    assert dallclose(velocity_advection.levmask.asnumpy(), False)
+    assert dallclose(velocity_advection.vcfl_dsl.asnumpy(), 0.0)
 
     assert velocity_advection.cfl_w_limit == 0.65
     assert velocity_advection.scalfac_exdiff == 0.05
 
 
 @pytest.mark.datatest
+@pytest.mark.parametrize(
+    "experiment, step_date_init, damping_height",
+    [
+        ("mch_ch_r04b09_dsl", "2021-06-20T12:00:10.000", 12500.0),
+        ("exclaim_ape_R02B04", "2000-01-01T00:00:02.000", 50000.0),
+    ],
+)
 def test_verify_velocity_init_against_regular_savepoint(
     savepoint_velocity_init,
     interpolation_savepoint,
@@ -85,6 +94,7 @@ def test_verify_velocity_init_against_regular_savepoint(
     icon_grid,
     metrics_savepoint,
     damping_height,
+    experiment,
 ):
     savepoint = savepoint_velocity_init
     dtime = savepoint.get_metadata("dtime").get("dtime")
@@ -114,11 +124,16 @@ def test_verify_velocity_init_against_regular_savepoint(
 
 
 @pytest.mark.datatest
+@pytest.mark.parametrize("istep_init, istep_exit", [(1, 1)])
 @pytest.mark.parametrize(
-    "istep_init, istep_exit, step_date_init, step_date_exit",
-    [(1, 1, "2021-06-20T12:00:10.000", "2021-06-20T12:00:10.000")],
+    "experiment,step_date_init, step_date_exit, damping_height",
+    [
+        (REGIONAL_EXPERIMENT, "2021-06-20T12:00:10.000", "2021-06-20T12:00:10.000", 12500.0),
+        (GLOBAL_EXPERIMENT, "2000-01-01T00:00:02.000", "2000-01-01T00:00:02.000", 50000.0),
+    ],
 )
 def test_velocity_predictor_step(
+    experiment,
     istep_init,
     istep_exit,
     step_date_init,
@@ -157,6 +172,7 @@ def test_velocity_predictor_step(
         rho_incr=None,  # sp.rho_incr(),
         vn_incr=None,  # sp.vn_incr(),
         exner_incr=None,  # sp.exner_incr(),
+        exner_dyn_incr=None,
     )
     prognostic_state = PrognosticState(
         w=sp_v.w(),
@@ -209,63 +225,76 @@ def test_velocity_predictor_step(
     icon_result_z_v_grad_w = savepoint_velocity_exit.z_v_grad_w().asnumpy()
 
     # stencil 01
-    assert dallclose(icon_result_vt, diagnostic_state.vt.asnumpy(), atol=1.0e-14)
+    assert dallclose(diagnostic_state.vt.asnumpy(), icon_result_vt, atol=1.0e-14)
     # stencil 02,05
-    assert dallclose(icon_result_vn_ie, diagnostic_state.vn_ie.asnumpy(), atol=1.0e-14)
+    assert dallclose(diagnostic_state.vn_ie.asnumpy(), icon_result_vn_ie, atol=1.0e-14)
+
+    start_edge_lateral_boundary_6 = icon_grid.get_start_index(
+        EdgeDim, HorizontalMarkerIndex.lateral_boundary(EdgeDim) + 6
+    )
     # stencil 07
     if not vn_only:
         assert dallclose(
-            icon_result_z_v_grad_w[3777:31558, :],
-            velocity_advection.z_v_grad_w.asnumpy()[3777:31558, :],
+            icon_result_z_v_grad_w[start_edge_lateral_boundary_6:, :],
+            velocity_advection.z_v_grad_w.asnumpy()[start_edge_lateral_boundary_6:, :],
             atol=1.0e-16,
         )
 
     # stencil 08
+    start_cell_nudging = icon_grid.get_start_index(CellDim, HorizontalMarkerIndex.nudging(CellDim))
     assert dallclose(
-        savepoint_velocity_exit.z_ekinh().asnumpy()[3316:20896, :],
-        velocity_advection.z_ekinh.asnumpy()[3316:20896, :],
+        savepoint_velocity_exit.z_ekinh().asnumpy()[start_cell_nudging:, :],
+        velocity_advection.z_ekinh.asnumpy()[start_cell_nudging:, :],
     )
     # stencil 09
     assert dallclose(
-        icon_result_z_w_concorr_mc[3316:20896, vertical_params.nflatlev : icon_grid.num_levels],
         velocity_advection.z_w_concorr_mc.asnumpy()[
-            3316:20896, vertical_params.nflatlev : icon_grid.num_levels
+            start_cell_nudging:, vertical_params.nflatlev : icon_grid.num_levels
+        ],
+        icon_result_z_w_concorr_mc[
+            start_cell_nudging:, vertical_params.nflatlev : icon_grid.num_levels
         ],
         atol=1.0e-15,
     )
     # stencil 10
     assert dallclose(
-        icon_result_w_concorr_c[3316:20896, vertical_params.nflatlev + 1 : icon_grid.num_levels],
         diagnostic_state.w_concorr_c.asnumpy()[
-            3316:20896, vertical_params.nflatlev + 1 : icon_grid.num_levels
+            start_cell_nudging:, vertical_params.nflatlev + 1 : icon_grid.num_levels
+        ],
+        icon_result_w_concorr_c[
+            start_cell_nudging:, vertical_params.nflatlev + 1 : icon_grid.num_levels
         ],
         atol=1.0e-15,
     )
     # stencil 11,12,13,14
     assert dallclose(
-        savepoint_velocity_exit.z_w_con_c().asnumpy()[3316:20896, :],
-        velocity_advection.z_w_con_c.asnumpy()[3316:20896, :],
+        velocity_advection.z_w_con_c.asnumpy()[start_cell_nudging:, :],
+        savepoint_velocity_exit.z_w_con_c().asnumpy()[start_cell_nudging:, :],
         atol=1.0e-15,
     )
     # stencil 16
     assert dallclose(
-        icon_result_ddt_w_adv_pc[3316:20896, :],
-        diagnostic_state.ddt_w_adv_pc[ntnd - 1].asnumpy()[3316:20896, :],
+        diagnostic_state.ddt_w_adv_pc[ntnd - 1].asnumpy()[start_cell_nudging:, :],
+        icon_result_ddt_w_adv_pc[start_cell_nudging:, :],
         atol=5.0e-16,
         rtol=1.0e-10,
     )
     # stencil 19
     assert dallclose(
-        icon_result_ddt_vn_apc_pc,
         diagnostic_state.ddt_vn_apc_pc[ntnd - 1].asnumpy(),
+        icon_result_ddt_vn_apc_pc,
         atol=1.0e-15,
     )
 
 
 @pytest.mark.datatest
+@pytest.mark.parametrize("istep_init, istep_exit", [(2, 2)])
 @pytest.mark.parametrize(
-    "istep_init, istep_exit, step_date_init, step_date_exit",
-    [(2, 2, "2021-06-20T12:00:10.000", "2021-06-20T12:00:10.000")],
+    "experiment, step_date_init, step_date_exit, damping_height",
+    [
+        (REGIONAL_EXPERIMENT, "2021-06-20T12:00:10.000", "2021-06-20T12:00:10.000", 12500.0),
+        (GLOBAL_EXPERIMENT, "2000-01-01T00:00:02.000", "2000-01-01T00:00:02.000", 50000.0),
+    ],
 )
 def test_velocity_corrector_step(
     istep_init,
@@ -306,6 +335,7 @@ def test_velocity_corrector_step(
         rho_incr=None,  # sp.rho_incr(),
         vn_incr=None,  # sp.vn_incr(),
         exner_incr=None,  # sp.exner_incr(),
+        exner_dyn_incr=None,
     )
     prognostic_state = PrognosticState(
         w=sp_v.w(),
@@ -354,31 +384,35 @@ def test_velocity_corrector_step(
     icon_result_z_v_grad_w = savepoint_velocity_exit.z_v_grad_w().asnumpy()
 
     # stencil 07
+    start_cells_lateral_boundary_d = icon_grid.get_start_index(
+        EdgeDim, HorizontalMarkerIndex.lateral_boundary(EdgeDim) + 6
+    )
     assert dallclose(
-        icon_result_z_v_grad_w[3777:31558, :],
-        velocity_advection.z_v_grad_w.asnumpy()[3777:31558, :],
+        velocity_advection.z_v_grad_w.asnumpy()[start_cells_lateral_boundary_d:, :],
+        icon_result_z_v_grad_w[start_cells_lateral_boundary_d:, :],
         atol=1e-16,
     )
     # stencil 08
+    start_cell_nudging = icon_grid.get_start_index(CellDim, HorizontalMarkerIndex.nudging(CellDim))
     assert dallclose(
-        savepoint_velocity_exit.z_ekinh().asnumpy()[3316:20896, :],
-        velocity_advection.z_ekinh.asnumpy()[3316:20896, :],
+        velocity_advection.z_ekinh.asnumpy()[start_cell_nudging:, :],
+        savepoint_velocity_exit.z_ekinh().asnumpy()[start_cell_nudging:, :],
     )
 
     # stencil 11,12,13,14
     assert dallclose(
-        savepoint_velocity_exit.z_w_con_c().asnumpy()[3316:20896, :],
-        velocity_advection.z_w_con_c.asnumpy()[3316:20896, :],
+        velocity_advection.z_w_con_c.asnumpy()[start_cell_nudging:, :],
+        savepoint_velocity_exit.z_w_con_c().asnumpy()[start_cell_nudging:, :],
     )
     # stencil 16
     assert dallclose(
-        icon_result_ddt_w_adv_pc[3316:20896, :],
-        diagnostic_state.ddt_w_adv_pc[ntnd - 1].asnumpy()[3316:20896, :],
+        diagnostic_state.ddt_w_adv_pc[ntnd - 1].asnumpy()[start_cell_nudging:, :],
+        icon_result_ddt_w_adv_pc[start_cell_nudging:, :],
         atol=5.0e-16,
     )
     # stencil 19
     assert dallclose(
-        icon_result_ddt_vn_apc_pc,
         diagnostic_state.ddt_vn_apc_pc[ntnd - 1].asnumpy(),
+        icon_result_ddt_vn_apc_pc,
         atol=5.0e-16,
     )
