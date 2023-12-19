a391067de5423c3d1eb455bc0dab93925860da4e
Chia Rui Ong <chiarui.ong@c2sm.ethz.ch>
2023-12-19 11:27:56 +0100

    Added RBF interpolation for computation of longitudinal and meridional winds

---
+++ model/common/src/icon4py/model/common/interpolation/stencils/mo_rbf_vec_interpol_cell.py
@@ -0,0 +1,54 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from gt4py.next.common import GridType
+from gt4py.next.ffront.decorator import field_operator, program
+from gt4py.next.ffront.fbuiltins import Field, int32, neighbor_sum
+
+from icon4py.model.common.dimension import C2E2C2E, CellDim, EdgeDim, KDim, C2E2C2EDim
+from icon4py.model.common.type_alias import wpfloat
+
+
+@field_operator
+def _mo_rbf_vec_interpol_cell(
+    p_e_in: Field[[EdgeDim, KDim], wpfloat],
+    ptr_coeff_1: Field[[CellDim, C2E2C2EDim], wpfloat],
+    ptr_coeff_2: Field[[CellDim, C2E2C2EDim], wpfloat],
+) -> tuple[Field[[CellDim, KDim], wpfloat], Field[[CellDim, KDim], wpfloat]]:
+    p_u_out = neighbor_sum(ptr_coeff_1 * p_e_in(C2E2C2E), axis=C2E2C2EDim)
+    p_v_out = neighbor_sum(ptr_coeff_2 * p_e_in(C2E2C2E), axis=C2E2C2EDim)
+    return p_u_out, p_v_out
+
+
+@program(grid_type=GridType.UNSTRUCTURED)
+def mo_rbf_vec_interpol_cell(
+    p_e_in: Field[[EdgeDim, KDim], wpfloat],
+    ptr_coeff_1: Field[[CellDim, C2E2C2EDim], wpfloat],
+    ptr_coeff_2: Field[[CellDim, C2E2C2EDim], wpfloat],
+    p_u_out: Field[[CellDim, KDim], wpfloat],
+    p_v_out: Field[[CellDim, KDim], wpfloat],
+    horizontal_start: int32,
+    horizontal_end: int32,
+    vertical_start: int32,
+    vertical_end: int32,
+):
+    _mo_rbf_vec_interpol_cell(
+        p_e_in,
+        ptr_coeff_1,
+        ptr_coeff_2,
+        out=(p_u_out, p_v_out),
+        domain={
+            CellDim: (horizontal_start, horizontal_end),
+            KDim: (vertical_start, vertical_end),
+        },
+    )
