8c6e2178dadc3182be84d7e8a50c03e829de0ff3
Nina Burgdorfer <Nina.Burgdorfer@meteoswiss.ch>
2024-01-15 15:27:16 +0100

    Refactor test_hflux_ffsl_hybrid_stencil_02.py

--- model/atmosphere/advection/tests/stencil_tests/test_hflux_ffsl_hybrid_stencil_02.py
+++ model/atmosphere/advection/tests/stencil_tests/test_hflux_ffsl_hybrid_stencil_02.py
@@ -12,42 +12,37 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import numpy as np
+import pytest
 
 from icon4py.model.atmosphere.advection.hflux_ffsl_hybrid_stencil_02 import (
     hflux_ffsl_hybrid_stencil_02,
 )
 from icon4py.model.common.dimension import EdgeDim, KDim
-from icon4py.model.common.grid.simple import SimpleGrid
-from icon4py.model.common.test_utils.helpers import random_field
+from icon4py.model.common.test_utils.helpers import StencilTest, random_field
 
+class TestHfluxFfslHybridStencil02(StencilTest):
+    PROGRAM = hflux_ffsl_hybrid_stencil_02
+    OUTPUTS = ("p_out_e_hybrid_2",)
 
-def hflux_ffsl_hybrid_stencil_02_numpy(
-    p_out_e_hybrid_2: np.ndarray,
-    p_mass_flx_e: np.ndarray,
-    z_dreg_area: np.ndarray,
-):
-    p_out_e_hybrid_2 = p_mass_flx_e * p_out_e_hybrid_2 / z_dreg_area
+    @staticmethod
+    def reference(
+        grid,
+        p_out_e_hybrid_2: np.array,
+        p_mass_flx_e: np.array,
+        z_dreg_area: np.array,
+        **kwargs,
+    ):
+        p_out_e_hybrid_2 = p_mass_flx_e * p_out_e_hybrid_2 / z_dreg_area
 
-    return p_out_e_hybrid_2
+        return dict(p_out_e_hybrid_2=p_out_e_hybrid_2)
 
-
-def test_hflux_ffsl_hybrid_stencil_02(backend):
-    grid = SimpleGrid()
-    p_out_e_hybrid_2 = random_field(grid, EdgeDim, KDim)
-    p_mass_flx_e = random_field(grid, EdgeDim, KDim)
-    z_dreg_area = random_field(grid, EdgeDim, KDim)
-
-    ref = hflux_ffsl_hybrid_stencil_02_numpy(
-        p_out_e_hybrid_2.asnumpy(),
-        p_mass_flx_e.asnumpy(),
-        z_dreg_area.asnumpy(),
-    )
-
-    hflux_ffsl_hybrid_stencil_02.with_backend(backend)(
-        p_out_e_hybrid_2,
-        p_mass_flx_e,
-        z_dreg_area,
-        offset_provider={},
-    )
-
-    assert np.allclose(p_out_e_hybrid_2.asnumpy(), ref)
+    @pytest.fixture
+    def input_data(self, grid):
+        p_out_e_hybrid_2 = random_field(grid, EdgeDim, KDim)
+        p_mass_flx_e = random_field(grid, EdgeDim, KDim)
+        z_dreg_area = random_field(grid, EdgeDim, KDim)
+        return dict(
+            p_mass_flx_e=p_mass_flx_e,
+            z_dreg_area=z_dreg_area,
+            p_out_e_hybrid_2=p_out_e_hybrid_2,
+            )
