9b85fa9aa1ff465b9b79ee8b35d4d95370556f5c
Samuel <kellerhalssamuel@gmail.com>
2024-01-17 14:37:04 +0100

    Refactor advection stencils to use `StencilTest` (#336)
    Refactor half of all advection stencil to use StencilTest class, and make outputs sliceable.
    
    ---------
    
    Co-authored-by: Nina Burgdorfer <Nina.Burgdorfer@meteoswiss.ch>

--- model/atmosphere/advection/tests/stencil_tests/test_rbf_intp_edge_stencil_01.py
+++ model/atmosphere/advection/tests/stencil_tests/test_rbf_intp_edge_stencil_01.py
@@ -12,42 +12,26 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import numpy as np
+import pytest
 
 from icon4py.model.atmosphere.advection.rbf_intp_edge_stencil_01 import rbf_intp_edge_stencil_01
 from icon4py.model.common.dimension import E2C2EDim, EdgeDim, KDim
-from icon4py.model.common.grid.simple import SimpleGrid
-from icon4py.model.common.test_utils.helpers import random_field, zero_field
+from icon4py.model.common.test_utils.helpers import StencilTest, random_field, zero_field
 
 
-def rbf_intp_edge_stencil_01_numpy(
-    e2c2e: np.array,
-    p_vn_in: np.array,
-    ptr_coeff: np.array,
-) -> np.array:
-    ptr_coeff = np.expand_dims(ptr_coeff, axis=-1)
-    p_vt_out = np.sum(p_vn_in[e2c2e] * ptr_coeff, axis=1)
-    return p_vt_out
+class TestRbfIntpEdgeStencil01(StencilTest):
+    PROGRAM = rbf_intp_edge_stencil_01
+    OUTPUTS = ("p_vt_out",)
 
+    @staticmethod
+    def reference(grid, p_vn_in: np.array, ptr_coeff: np.array, **kwargs) -> np.array:
+        ptr_coeff = np.expand_dims(ptr_coeff, axis=-1)
+        p_vt_out = np.sum(p_vn_in[grid.connectivities[E2C2EDim]] * ptr_coeff, axis=1)
+        return dict(p_vt_out=p_vt_out)
 
-def test_rbf_intp_edge_stencil_01(backend):
-    grid = SimpleGrid()
-
-    p_vn_in = random_field(grid, EdgeDim, KDim)
-    ptr_coeff = random_field(grid, EdgeDim, E2C2EDim)
-    p_vt_out = zero_field(grid, EdgeDim, KDim)
-
-    ref = rbf_intp_edge_stencil_01_numpy(
-        grid.connectivities[E2C2EDim],
-        p_vn_in.asnumpy(),
-        ptr_coeff.asnumpy(),
-    )
-
-    rbf_intp_edge_stencil_01.with_backend(backend)(
-        p_vn_in,
-        ptr_coeff,
-        p_vt_out,
-        offset_provider={
-            "E2C2E": grid.get_offset_provider("E2C2E"),
-        },
-    )
-    assert np.allclose(p_vt_out.asnumpy(), ref)
+    @pytest.fixture
+    def input_data(self, grid):
+        p_vn_in = random_field(grid, EdgeDim, KDim)
+        ptr_coeff = random_field(grid, EdgeDim, E2C2EDim)
+        p_vt_out = zero_field(grid, EdgeDim, KDim)
+        return dict(p_vn_in=p_vn_in, ptr_coeff=ptr_coeff, p_vt_out=p_vt_out)
