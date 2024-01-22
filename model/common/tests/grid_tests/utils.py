1c0749ab6f963e2160dd5d6dc687bf537bc795d4
Magdalena Luz <luzm@ethz.ch>
2024-01-22 17:57:41 +0100

    review fixes in utils.py

--- model/common/tests/grid_tests/utils.py
+++ model/common/tests/grid_tests/utils.py
@@ -10,6 +10,7 @@
 # distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
+from pathlib import Path
 
 from icon4py.model.common.test_utils.data_handling import download_and_extract
 from icon4py.model.common.test_utils.datatest_utils import TEST_DATA_ROOT
@@ -21,30 +22,35 @@ R02B04_GLOBAL = "r02b04_global"
 grids_path = TEST_DATA_ROOT.joinpath("grids")
 r04b09_dsl_grid_path = grids_path.joinpath(MCH_GRID_FILE)
 r04b09_dsl_data_file = r04b09_dsl_grid_path.joinpath("mch_ch_r04b09_dsl_grids_v1.tar.gz").name
+
 r02b04_global_grid_path = grids_path.joinpath(R02B04_GLOBAL)
 r02b04_global_data_file = r02b04_global_grid_path.joinpath("icon_grid_0013_R02B04_R.tar.gz").name
 
 
-mch_ch_r04b09_dsl_grid_uri = "https://polybox.ethz.ch/index.php/s/hD232znfEPBh4Oh/download"
-r02b04_global_grid_uri = "https://polybox.ethz.ch/index.php/s/AKAO6ImQdIatnkB/download"
+MC_CH_R04B09_DSL_GRID_URI = "https://polybox.ethz.ch/index.php/s/hD232znfEPBh4Oh/download"
+R02B04_GLOBAL_GRID_URI = "https://polybox.ethz.ch/index.php/s/AKAO6ImQdIatnkB/download"
 
 
-def resolve_file_from_gridfile_name(name: str):
+def resolve_file_from_gridfile_name(name: str) -> Path:
     if name == MCH_GRID_FILE:
-        if not r04b09_dsl_grid_path.joinpath("grid.nc").exists():
+        gridfile = r04b09_dsl_grid_path.joinpath("grid.nc")
+        if not gridfile.exists():
             download_and_extract(
-                mch_ch_r04b09_dsl_grid_uri,
+                MC_CH_R04B09_DSL_GRID_URI,
                 r04b09_dsl_grid_path,
                 r04b09_dsl_grid_path,
                 r04b09_dsl_data_file,
             )
-        return r04b09_dsl_grid_path.joinpath("grid.nc")
+        return gridfile
     elif name == R02B04_GLOBAL:
-        if not r02b04_global_grid_path.joinpath("icon_grid_0013_R02B04_R.nc").exists():
+        gridfile = r02b04_global_grid_path.joinpath("icon_grid_0013_R02B04_R.nc")
+        if not gridfile.exists():
             download_and_extract(
-                r02b04_global_grid_uri,
+                R02B04_GLOBAL_GRID_URI,
                 r02b04_global_grid_path,
                 r02b04_global_grid_path,
                 r02b04_global_data_file,
             )
-        return r02b04_global_grid_path.joinpath("icon_grid_0013_R02B04_R.nc")
+        return gridfile
+    else:
+        raise ValueError(f"invalid name: use one of {R02B04_GLOBAL, MCH_GRID_FILE}")
