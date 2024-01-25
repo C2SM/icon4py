924798f51c5954bee08e70f8b04145f2a2edd94d
ninaburg <83002751+ninaburg@users.noreply.github.com>
2024-01-25 11:25:14 +0100

    Refactor advection stencils to use `StencilTest` - Part 2 (#351)

--- model/atmosphere/advection/src/icon4py/model/atmosphere/advection/hflux_ffsl_hybrid_stencil_01a.py
+++ model/atmosphere/advection/src/icon4py/model/atmosphere/advection/hflux_ffsl_hybrid_stencil_01a.py
@@ -11,9 +11,9 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from gt4py.next.common import Field
+from gt4py.next.common import GridType
 from gt4py.next.ffront.decorator import field_operator, program
-from gt4py.next.ffront.fbuiltins import int32, where
+from gt4py.next.ffront.fbuiltins import Field, int32, where
 
 from icon4py.model.common.dimension import E2C, CellDim, EdgeDim, KDim
 
@@ -108,7 +108,7 @@ def _hflux_ffsl_hybrid_stencil_01a(
     return p_out_e_hybrid_1a
 
 
-@program
+@program(grid_type=GridType.UNSTRUCTURED)
 def hflux_ffsl_hybrid_stencil_01a(
     z_lsq_coeff_1: Field[[CellDim, KDim], float],
     z_lsq_coeff_2: Field[[CellDim, KDim], float],
