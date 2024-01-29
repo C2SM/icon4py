e32d41de2d23b6d85ce25df1fa87f3bc8cad4350
Daniel Hupp <daniel.hupp@meteoswiss.ch>
2024-01-29 18:14:43 +0100

    fixes

--- model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/mo_solve_nonhydro_stencil_11_upper.py
+++ model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/mo_solve_nonhydro_stencil_11_upper.py
@@ -16,7 +16,7 @@ from gt4py.next.ffront.decorator import field_operator, program
 from gt4py.next.ffront.fbuiltins import Field, astype, int32
 
 from icon4py.model.atmosphere.dycore.interpolate_to_surface import _interpolate_to_surface
-from icon4py.model.common.dimension import CellDim, KDim, Koff
+from icon4py.model.common.dimension import CellDim, KDim
 from icon4py.model.common.type_alias import vpfloat, wpfloat
 
 
--- model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/nh_solve/solve_nonhydro_program.py
+++ model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/nh_solve/solve_nonhydro_program.py
@@ -14,6 +14,9 @@ from gt4py.next.common import Field, GridType
 from gt4py.next.ffront.decorator import field_operator, program
 from gt4py.next.ffront.fbuiltins import int32, where
 
+from icon4py.model.atmosphere.dycore.compute_theta_v_prime_at_ic_and_theta_v_at_ic_and_pressure_gradient_theta import (
+    _compute_theta_v_prime_at_ic_and_theta_v_at_ic_and_pressure_gradient_theta,
+)
 from icon4py.model.atmosphere.dycore.compute_contravariant_correction import (
     _compute_contravariant_correction,
 )
@@ -32,15 +35,6 @@ from icon4py.model.atmosphere.dycore.extrapolate_temporally_exner_pressure impor
 )
 from icon4py.model.atmosphere.dycore.interpolate_to_half_levels_vp import (
     _interpolate_to_half_levels_vp,
-)
-from icon4py.model.atmosphere.dycore.compute_first_vertical_derivative import (
-    _compute_first_vertical_derivative,
-)
-from icon4py.model.atmosphere.dycore.compute_pertubation_of_rho_and_theta_and_rho_at_ic import (
-    _compute_pertubation_of_rho_and_theta_and_rho_at_ic,
-)
-from icon4py.model.atmosphere.dycore. import (
-    _compute_theta_v_prime_at_ic_and_theta_v_at_ic_and_pressure_gradient_theta,
 )
 from icon4py.model.atmosphere.dycore.mo_solve_nonhydro_stencil_11_upper import (
     _mo_solve_nonhydro_stencil_11_upper,
