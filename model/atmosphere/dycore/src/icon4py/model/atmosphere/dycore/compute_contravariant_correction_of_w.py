f7d29d5619495861104e55c88173d4b2a18746dc
Daniel Hupp <daniel.hupp@meteoswiss.ch>
2024-02-02 15:59:30 +0100

    Rename stencils from mo_solve_nondyro first batch. (#376)
    This PR introduces new names for many stencils from mo_solve_nonhydro.

--- model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/compute_contravariant_correction_of_w.py
+++ model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/compute_contravariant_correction_of_w.py
@@ -11,62 +11,56 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from gt4py.next.ffront.decorator import GridType, field_operator, program
-from gt4py.next.ffront.fbuiltins import Field, int32, where
+from gt4py.next.common import GridType
+from gt4py.next.ffront.decorator import field_operator, program
+from gt4py.next.ffront.fbuiltins import Field, astype, int32, neighbor_sum
 
-from icon4py.model.atmosphere.dycore.compute_contravariant_correction_of_w import (
-    _compute_contravariant_correction_of_w,
-)
-from icon4py.model.atmosphere.dycore.compute_contravariant_correction_of_w_for_lower_boundary import (
-    _compute_contravariant_correction_of_w_for_lower_boundary,
-)
-from icon4py.model.common.dimension import CEDim, CellDim, EdgeDim, KDim
+from icon4py.model.common.dimension import C2CE, C2E, C2EDim, CEDim, CellDim, EdgeDim, KDim, Koff
 from icon4py.model.common.type_alias import vpfloat, wpfloat
 
 
 @field_operator
-def _fused_solve_nonhydro_stencil_39_40(
+def _compute_contravariant_correction_of_w(
     e_bln_c_s: Field[[CEDim], wpfloat],
     z_w_concorr_me: Field[[EdgeDim, KDim], vpfloat],
     wgtfac_c: Field[[CellDim, KDim], vpfloat],
-    wgtfacq_c: Field[[CellDim, KDim], vpfloat],
-    vert_idx: Field[[KDim], int32],
-    nlev: int32,
-    nflatlev: int32,
 ) -> Field[[CellDim, KDim], vpfloat]:
-    w_concorr_c = where(
-        nflatlev + 1 <= vert_idx < nlev,
-        _compute_contravariant_correction_of_w(e_bln_c_s, z_w_concorr_me, wgtfac_c),
-        _compute_contravariant_correction_of_w_for_lower_boundary(
-            e_bln_c_s, z_w_concorr_me, wgtfacq_c
-        ),
+    """Formerly known as _mo_solve_nonhydro_stencil_39."""
+    z_w_concorr_me_offset_1 = z_w_concorr_me(Koff[-1])
+
+    z_w_concorr_me_wp, z_w_concorr_me_offset_1_wp = astype(
+        (z_w_concorr_me, z_w_concorr_me_offset_1), wpfloat
     )
-    return w_concorr_c
+
+    z_w_concorr_mc_m1_wp = neighbor_sum(
+        e_bln_c_s(C2CE) * z_w_concorr_me_offset_1_wp(C2E), axis=C2EDim
+    )
+    z_w_concorr_mc_m0_wp = neighbor_sum(e_bln_c_s(C2CE) * z_w_concorr_me_wp(C2E), axis=C2EDim)
+
+    z_w_concorr_mc_m1_vp, z_w_concorr_mc_m0_vp = astype(
+        (z_w_concorr_mc_m1_wp, z_w_concorr_mc_m0_wp), vpfloat
+    )
+    w_concorr_c_vp = (
+        wgtfac_c * z_w_concorr_mc_m0_vp + (vpfloat("1.0") - wgtfac_c) * z_w_concorr_mc_m1_vp
+    )
+    return w_concorr_c_vp
 
 
 @program(grid_type=GridType.UNSTRUCTURED)
-def fused_solve_nonhydro_stencil_39_40(
+def compute_contravariant_correction_of_w(
     e_bln_c_s: Field[[CEDim], wpfloat],
     z_w_concorr_me: Field[[EdgeDim, KDim], vpfloat],
     wgtfac_c: Field[[CellDim, KDim], vpfloat],
-    wgtfacq_c: Field[[CellDim, KDim], vpfloat],
-    vert_idx: Field[[KDim], int32],
-    nlev: int32,
-    nflatlev: int32,
     w_concorr_c: Field[[CellDim, KDim], vpfloat],
     horizontal_start: int32,
     horizontal_end: int32,
     vertical_start: int32,
     vertical_end: int32,
 ):
-    _fused_solve_nonhydro_stencil_39_40(
+    _compute_contravariant_correction_of_w(
         e_bln_c_s,
         z_w_concorr_me,
         wgtfac_c,
-        wgtfacq_c,
-        vert_idx,
-        nlev,
-        nflatlev,
         out=w_concorr_c,
         domain={
             CellDim: (horizontal_start, horizontal_end),
