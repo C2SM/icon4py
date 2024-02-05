8c001352e22338eee36cf6c943b224538150edec
samkellerhals <kellerhalssamuel@gmail.com>
2024-02-05 09:41:34 +0100

    Change wrapper name

--- compile.sh
+++ compile.sh
@@ -3,6 +3,6 @@ set -e -x
 
 export LIB=.
 
-py2fgen icon4pytools.py2f.wrappers.square_wrapper square
+py2fgen icon4pytools.py2f.wrappers.square_wrapper square_wrapper
 gfortran -c square_wrapper_plugin.f90 .
 gfortran -I$LIB -Wl,-rpath=$LIB -L$LIB  square_wrapper_plugin.f90 run_square.f90 -lsquare_wrapper_plugin
