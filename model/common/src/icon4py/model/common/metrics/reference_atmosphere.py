6a752cdea29d75f6ff4d03dc1582c87fb07e894e
Magdalena Luz <luzm@ethz.ch>
2024-02-09 19:42:30 +0100

    reference atmosphere on edges (WIP)

--- model/common/src/icon4py/model/common/metrics/reference_atmosphere.py
+++ model/common/src/icon4py/model/common/metrics/reference_atmosphere.py
@@ -12,10 +12,99 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from gt4py.next import Field, GridType, field_operator, program
-from gt4py.next.ffront.fbuiltins import exp, int32, log
+from gt4py.next.ffront.fbuiltins import exp, int32, log, neighbor_sum
 
-from icon4py.model.common.dimension import CellDim, KDim
+from icon4py.model.common.dimension import CellDim, KDim, E2CDim, EdgeDim, E2C
 from icon4py.model.common.type_alias import wpfloat
+
+
+@field_operator
+def _cell_2_edge_interpolation(
+    in_field: Field[[CellDim, KDim], wpfloat], coeff: Field[[EdgeDim, E2CDim], wpfloat]
+) -> Field[[EdgeDim, KDim], wpfloat]:
+    """
+    Interpolate a Cell Field to Edges.
+
+    There is a special handling of lateral boundary edges in `subroutine cells2edges_scalar`
+    where the value is set to the one valid in_field value without multiplication by coeff.
+    That essentially means: the skip value neighbor in the neighbor_sum is skipped and coeff needs to
+    be 1 for this Edge index.
+    """
+    return neighbor_sum(in_field(E2C) * coeff, axis=E2CDim)
+
+
+@program(grid_type=GridType.UNSTRUCTURED)
+def cell_2_edge_interpolation(
+    in_field: Field[[CellDim, KDim], wpfloat],
+    coeff: Field[[EdgeDim, E2CDim], wpfloat],
+    out_field: Field[[EdgeDim, KDim], wpfloat],
+    horizontal_start: int32,
+    horizontal_end: int32,
+    vertical_start: int32,
+    vertical_end: int32,
+):
+    _cell_2_edge_interpolation(
+        in_field,
+        coeff,
+        out=out_field,
+        domain={EdgeDim: (horizontal_start, horizontal_end), KDim: (vertical_start, vertical_end)},
+    )
+
+
+@field_operator
+def _compute_reference_atmosphere_edge_fields(
+    z_me: Field[[EdgeDim, KDim], wpfloat],
+    p0ref: wpfloat,
+    p0sl_bg: wpfloat,
+    grav: wpfloat,
+    cpd: wpfloat,
+    rd: wpfloat,
+    h_scal_bg: wpfloat,
+    t0sl_bg: wpfloat,
+    del_t_bg: wpfloat,
+) -> tuple[Field[[EdgeDim, KDim], wpfloat], Field[[EdgeDim, KDim], wpfloat]]:
+    denom = t0sl_bg - del_t_bg
+    exp_z_me = exp(z_me / h_scal_bg)
+    logval = log((exp_z_me * denom + del_t_bg) / t0sl_bg)
+    z_aux_1 = p0sl_bg * exp(-grav / rd * h_scal_bg / denom * logval)
+    z_temp = denom + del_t_bg * exp_z_me
+    rho_ref_me = z_aux_1 / (rd * z_temp)
+    rd_o_cpd = rd / cpd
+    theta_ref_me = z_temp / (z_aux_1 / p0ref) ** rd_o_cpd
+    return (rho_ref_me, theta_ref_me)
+
+
+@program(grid_type=GridType.UNSTRUCTURED)
+def compute_reference_atmosphere_edge_fields(
+    z_me: Field[[EdgeDim, KDim], wpfloat],
+    p0ref: wpfloat,
+    p0sl_bg: wpfloat,
+    grav: wpfloat,
+    cpd: wpfloat,
+    rd: wpfloat,
+    h_scal_bg: wpfloat,
+    t0sl_bg: wpfloat,
+    del_t_bg: wpfloat,
+    rho_ref_me: Field[[EdgeDim, KDim], wpfloat],
+    theta_ref_me: Field[[EdgeDim, KDim], wpfloat],
+    horizontal_start: int32,
+    horizontal_end: int32,
+    vertical_start: int32,
+    vertical_end: int32,
+):
+    _compute_reference_atmosphere_edge_fields(
+        z_me,
+        p0ref,
+        p0sl_bg,
+        grav,
+        cpd,
+        rd,
+        h_scal_bg,
+        t0sl_bg,
+        del_t_bg,
+        out=(rho_ref_me, theta_ref_me),
+        domain={EdgeDim: (horizontal_start, horizontal_end), KDim: (vertical_start, vertical_end)},
+    )
 
 
 @field_operator
@@ -31,7 +120,7 @@ def compute_z_temp(
 
 
 @field_operator
-def compute_z_aux1(
+def compute_z_aux1_cell(
     z_mc: Field[[CellDim, KDim], wpfloat],
     p0sl_bg: wpfloat,
     grav: wpfloat,
@@ -46,7 +135,7 @@ def compute_z_aux1(
 
 
 @field_operator
-def _compute_reference_atmosphere(
+def _compute_reference_atmosphere_cell_fields(
     z_mc: Field[[CellDim, KDim], wpfloat],
     p0ref: wpfloat,
     p0sl_bg: wpfloat,
@@ -61,7 +150,7 @@ def _compute_reference_atmosphere(
     Field[[CellDim, KDim], wpfloat],
     Field[[CellDim, KDim], wpfloat],
 ]:
-    z_aux1 = compute_z_aux1(
+    z_aux1 = compute_z_aux1_cell(
         z_mc=z_mc,
         p0sl_bg=p0sl_bg,
         grav=grav,
@@ -84,7 +173,7 @@ def _compute_reference_atmosphere(
 
 
 @program(grid_type=GridType.UNSTRUCTURED)
-def compute_reference_atmosphere(
+def compute_reference_atmosphere_cell_fields(
     z_height: Field[[CellDim, KDim], wpfloat],
     p0ref: wpfloat,
     p0sl_bg: wpfloat,
@@ -124,7 +213,7 @@ def compute_reference_atmosphere(
         vertical_end:int32 end index of vertical domain
 
     """
-    _compute_reference_atmosphere(
+    _compute_reference_atmosphere_cell_fields(
         z_height,
         p0ref,
         p0sl_bg,
@@ -152,4 +241,4 @@ def compute_d_exner_dz_ref_ic(
 
     Returns: first vertical derivative of reference exner pressure
     """
-    return -grav / cpd / theta_ref_ic
+    return -grav / (cpd * theta_ref_ic)
--- model/common/tests/metric_tests/test_reference_atmosphere.py
+++ model/common/tests/metric_tests/test_reference_atmosphere.py
@@ -13,14 +13,22 @@
 
 import pytest
 from gt4py.next.ffront.fbuiltins import int32
+from gt4py.next.program_processors.runners import roundtrip, gtfn
 
 from icon4py.model.common import constants
-from icon4py.model.common.dimension import CellDim, KDim
+from icon4py.model.common.dimension import CellDim, KDim, EdgeDim
 from icon4py.model.common.grid.icon import IconGrid
 from icon4py.model.common.metrics.metric_fields import compute_z_mc
-from icon4py.model.common.metrics.reference_atmosphere import compute_reference_atmosphere
+from icon4py.model.common.metrics.reference_atmosphere import (
+    compute_reference_atmosphere_cell_fields,
+    compute_d_exner_dz_ref_ic,
+    cell_2_edge_interpolation,
+    compute_reference_atmosphere_edge_fields,
+)
 from icon4py.model.common.test_utils.helpers import dallclose, zero_field
 from icon4py.model.common.type_alias import wpfloat
+
+gtfn_backend = gtfn.run_gtfn_cached
 
 
 @pytest.mark.datatest
@@ -48,7 +56,7 @@ def test_compute_reference_atmsophere_fields(grid_savepoint, metrics_savepoint):
         offset_provider={"Koff": grid.get_offset_provider("Koff")},
     )
 
-    compute_reference_atmosphere(
+    compute_reference_atmosphere_cell_fields(
         z_height=z_mc,
         p0ref=constants.P0REF,
         p0sl_bg=constants.SEAL_LEVEL_PRESSURE,
@@ -85,7 +93,7 @@ def test_compute_reference_atmsophere_on_half_level_mass_points(grid_savepoint, 
     horizontal_end = grid.num_cells
     vertical_end = grid.num_levels + 1
 
-    compute_reference_atmosphere(
+    compute_reference_atmosphere_cell_fields(
         z_height=z_ifc,
         p0ref=constants.P0REF,
         p0sl_bg=constants.SEAL_LEVEL_PRESSURE,
@@ -106,3 +114,79 @@ def test_compute_reference_atmsophere_on_half_level_mass_points(grid_savepoint, 
     )
 
     assert dallclose(theta_ref_ic.asnumpy(), theta_ref_ic_ref.asnumpy())
+
+
+def test_compute_d_exner_dz_ref_ic(grid_savepoint, metrics_savepoint):
+    grid = grid_savepoint.construct_icon_grid()
+    theta_ref_ic = metrics_savepoint.theta_ref_ic()
+    d_exner_dz_ref_ic_ref = metrics_savepoint.d_exner_dz_ref_ic()
+    d_exner_dz_ref_ic = zero_field(grid, CellDim, KDim, extend={KDim: 1})
+
+    compute_d_exner_dz_ref_ic(
+        theta_ref_ic=theta_ref_ic,
+        grav=constants.GRAVITATIONAL_ACCELERATION,
+        cpd=constants.CPD,
+        out=d_exner_dz_ref_ic,
+        offset_provider={},
+    )
+
+    assert dallclose(d_exner_dz_ref_ic.asnumpy(), d_exner_dz_ref_ic_ref.asnumpy())
+
+
+def test_compute_reference_atmosphere_on_full_level_edge_fields(
+    grid_savepoint, interpolation_savepoint, metrics_savepoint
+):
+    grid: IconGrid = grid_savepoint.construct_icon_grid()
+    rho_ref_me_ref = metrics_savepoint.rho_ref_me()
+    theta_ref_me_ref = metrics_savepoint.theta_ref_me()
+    rho_ref_me = metrics_savepoint.rho_ref_me()
+    theta_ref_me = metrics_savepoint.theta_ref_me()
+    c_lin_e = interpolation_savepoint.c_lin_e()
+
+    z_ifc = metrics_savepoint.z_ifc()
+    z_mc = zero_field(grid, CellDim, KDim, dtype=wpfloat)
+    z_me = zero_field(grid, EdgeDim, KDim, dtype=wpfloat)
+
+    start = int32(0)
+    horizontal_end = int32(grid.num_cells)
+    vertical_end = int32(grid.num_levels)
+    compute_z_mc(
+        z_ifc=z_ifc,
+        z_mc=z_mc,
+        horizontal_start=start,
+        horizontal_end=horizontal_end,
+        vertical_start=start,
+        vertical_end=vertical_end,
+        offset_provider={"Koff": grid.get_offset_provider("Koff")},
+    )
+
+    cell_2_edge_interpolation.with_backend(backend=gtfn_backend)(
+        z_mc,
+        c_lin_e,
+        z_me,
+        horizontal_start=start,
+        horizontal_end=horizontal_end,
+        vertical_start=start,
+        vertical_end=vertical_end,
+        offset_provider={"E2C": grid.get_offset_provider("E2C")},
+    )
+    compute_reference_atmosphere_edge_fields(
+        z_me=z_me,
+        p0ref=constants.P0REF,
+        p0sl_bg=constants.SEAL_LEVEL_PRESSURE,
+        grav=constants.GRAVITATIONAL_ACCELERATION,
+        cpd=constants.CPD,
+        rd=constants.RD,
+        t0sl_bg=constants.T0SL_BG,
+        h_scal_bg=constants._H_SCAL_BG,
+        del_t_bg=constants.DELTA_TEMPERATURE,
+        rho_ref_me=rho_ref_me,
+        theta_ref_me=theta_ref_me,
+        horizontal_start=start,
+        horizontal_end=horizontal_end,
+        vertical_start=start,
+        vertical_end=vertical_end,
+        offset_provider={},
+    )
+    assert dallclose(rho_ref_me.asnumpy(), rho_ref_me_ref.asnumpy())
+    assert dallclose(theta_ref_me.asnumpy(), theta_ref_me_ref.asnumpy())
