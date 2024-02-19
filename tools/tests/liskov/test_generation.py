e273bbed0ef261f454fe78ac0d11eb9fb72333f2
Daniel Hupp <daniel.hupp@meteoswiss.ch>
2024-02-19 16:39:28 +0100

    fix style

--- tools/tests/liskov/test_generation.py
+++ tools/tests/liskov/test_generation.py
@@ -76,7 +76,7 @@ def integration_code_interface():
         acc_present=False,
         mergecopy=False,
         copies=True,
-        optional_module="None"
+        optional_module="None",
     )
     end_stencil_data = EndStencilData(
         name="stencil1", startln=3, noendif=False, noprofile=False, noaccenddata=False
