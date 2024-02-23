7d24784432bf13b32c7249cd79282c22fd0619fc
samkellerhals <kellerhalssamuel@gmail.com>
2024-02-23 09:46:47 +0100

    rerun ruff

--- model/atmosphere/diffusion/src/icon4py/model/atmosphere/diffusion/diffusion.py
+++ model/atmosphere/diffusion/src/icon4py/model/atmosphere/diffusion/diffusion.py
@@ -383,7 +383,7 @@ class Diffusion:
         self._initialized = False
         self.rd_o_cvd: float = GAS_CONSTANT_DRY_AIR / (CPD - GAS_CONSTANT_DRY_AIR)
         #: threshold temperature deviation from neighboring grid points hat activates extra diffusion against runaway cooling
-        self.thresh_tdiff: float = -5.0  
+        self.thresh_tdiff: float = -5.0
         self.grid: Optional[IconGrid] = None
         self.config: Optional[DiffusionConfig] = None
         self.params: Optional[DiffusionParams] = None
