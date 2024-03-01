953cad3a758705f9d00aa2b1674e1d97c7b52e62
Till Ehrengruber <till.ehrengruber@cscs.ch>
2024-03-01 11:44:01 +0100

    Fix `uses_icon_grid_with_otf` fixture that broke with gt4py#1470 (#401)
    Fix for icon4py CI.
    
    ---------
    
    Co-authored-by: Edoardo Paone <edoardo.paone@cscs.ch>

--- model/common/src/icon4py/model/common/test_utils/helpers.py
+++ model/common/src/icon4py/model/common/test_utils/helpers.py
@@ -14,16 +14,13 @@
 from dataclasses import dataclass, field
 from typing import ClassVar, Optional
 
+import gt4py.next.program_processors.modular_executor
 import numpy as np
 import numpy.typing as npt
 import pytest
 from gt4py._core.definitions import is_scalar_type
 from gt4py.next import as_field, common as gt_common, constructors
 from gt4py.next.ffront.decorator import Program
-from gt4py.next.program_processors.otf_compile_executor import (
-    CachedOTFCompileExecutor,
-    OTFCompileExecutor,
-)
 
 from ..grid.base import BaseGrid
 from ..grid.icon import IconGrid
@@ -244,7 +241,9 @@ def uses_icon_grid_with_otf(backend, grid):
     Is needed to skip certain stencils where the execution domain needs to be restricted or boundary taken into account.
     """
     if hasattr(backend, "executor") and isinstance(grid, IconGrid):
-        if isinstance(backend.executor, (OTFCompileExecutor, CachedOTFCompileExecutor)):
+        if isinstance(
+            backend.executor, gt4py.next.program_processors.modular_executor.ModularExecutor
+        ):
             return True
         try:
             from gt4py.next.program_processors.runners import dace_iterator
