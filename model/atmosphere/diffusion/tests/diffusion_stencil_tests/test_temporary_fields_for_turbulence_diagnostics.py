1a3cc793525fb3c7cc806b5e6b99c11da6a4620d
Edoardo Paone <edoardo.paone@cscs.ch>
2024-03-07 10:47:23 +0100

    Fix test

--- model/atmosphere/diffusion/tests/diffusion_stencil_tests/test_temporary_fields_for_turbulence_diagnostics.py
+++ model/atmosphere/diffusion/tests/diffusion_stencil_tests/test_temporary_fields_for_turbulence_diagnostics.py
@@ -43,12 +43,13 @@ class TestTemporaryFieldsForTurbulenceDiagnostics(StencilTest):
         **kwargs,
     ) -> dict:
         c2e = grid.connectivities[C2EDim]
+        c2ce = np.asarray(grid.get_offset_provider("C2CE").table)
         geofac_div = np.expand_dims(geofac_div, axis=-1)
-        vn_geofac = vn[c2e] * geofac_div[grid.get_offset_provider("C2CE").table]
+        vn_geofac = vn[c2e] * geofac_div[c2ce]
         div = np.sum(vn_geofac, axis=1)
         e_bln_c_s = np.expand_dims(e_bln_c_s, axis=-1)
         diff_multfac_smag = np.expand_dims(diff_multfac_smag, axis=0)
-        mul = kh_smag_ec[c2e] * e_bln_c_s[grid.get_offset_provider("C2CE").table]
+        mul = kh_smag_ec[c2e] * e_bln_c_s[c2ce]
         summed = np.sum(mul, axis=1)
         kh_c = summed / diff_multfac_smag
 
