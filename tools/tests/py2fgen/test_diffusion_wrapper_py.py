782db41e55e33aa1e899ae798556699ea4608b93
samkellerhals <kellerhalssamuel@gmail.com>
2024-03-14 17:26:18 +0100

    Update diffusion wrapper

--- tools/tests/py2fgen/test_diffusion_wrapper_py.py
+++ tools/tests/py2fgen/test_diffusion_wrapper_py.py
@@ -12,15 +12,16 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # type: ignore
 import numpy as np
+import pytest
 from gt4py.next import np_as_located_field
 from icon4py.model.atmosphere.diffusion.diffusion import DiffusionType
 from icon4py.model.common.dimension import (
+    C2E2CDim,
     C2E2CODim,
-    CECDim,
-    CEDim,
+    C2EDim,
     CellDim,
-    ECDim,
-    ECVDim,
+    E2C2VDim,
+    E2CDim,
     EdgeDim,
     KDim,
     V2EDim,
@@ -30,7 +31,8 @@ from icon4py.model.common.dimension import (
 from icon4pytools.py2fgen.wrappers.diffusion import diffusion_init, diffusion_run
 
 
-if __name__ == "__main__":
+@pytest.mark.skip("Enable manually for local testing.")
+def test_diffusion_wrapper_py():
     # grid parameters
     num_cells = 20480
     num_edges = 30720
@@ -38,11 +40,10 @@ if __name__ == "__main__":
     num_levels = 60
     num_c2ec2o = 4
     num_v2e = 6
-    num_ce = num_edges * 2
-    num_ec = num_edges * 2
-    num_ecv = num_edges * 4
+    num_c2e = 2
+    num_e2c2v = 4
     num_c2e2c = 3
-    num_cec = num_cells * num_c2e2c
+    num_e2c = 3
     mean_cell_area = 24907282236.708576
 
     # other configuration parameters
@@ -50,7 +51,6 @@ if __name__ == "__main__":
     dtime = 2.0
     rayleigh_damping_height = 50000
     nflatlev = 30
-    nrdmax = 8
     nflat_gradp = 59
 
     # diffusion configuration
@@ -72,11 +72,11 @@ if __name__ == "__main__":
     )  # has to be from 0 to 75000, must have larger values than rayleigh damping height
     theta_ref_mc = rng.uniform(low=0, high=1, size=(num_cells, num_levels))
     wgtfac_c = rng.uniform(low=0, high=1, size=(num_cells, num_levels + 1))
-    e_bln_c_s = rng.uniform(low=0, high=1, size=(num_ce,))
-    geofac_div = rng.uniform(low=0, high=1, size=(num_ce,))
-    geofac_grg_x = rng.uniform(low=0, high=1, size=(num_cells, 4))
-    geofac_grg_y = rng.uniform(low=0, high=1, size=(num_cells, 4))
-    geofac_n2s = rng.uniform(low=0, high=1, size=(num_cells, 4))
+    e_bln_c_s = rng.uniform(low=0, high=1, size=(num_cells, num_c2e))
+    geofac_div = rng.uniform(low=0, high=1, size=(num_cells, num_c2e))
+    geofac_grg_x = rng.uniform(low=0, high=1, size=(num_cells, num_c2ec2o))
+    geofac_grg_y = rng.uniform(low=0, high=1, size=(num_cells, num_c2ec2o))
+    geofac_n2s = rng.uniform(low=0, high=1, size=(num_cells, num_c2ec2o))
     nudgecoeff_e = np.zeros((num_edges,))
     rbf_coeff_1 = rng.uniform(low=0, high=1, size=(num_vertices, num_v2e))
     rbf_coeff_2 = rng.uniform(low=0, high=1, size=(num_vertices, num_v2e))
@@ -89,14 +89,14 @@ if __name__ == "__main__":
     exner = rng.uniform(low=0, high=1, size=(num_cells, num_levels))
     theta_v = rng.uniform(low=0, high=1, size=(num_cells, num_levels))
     rho = rng.uniform(low=0, high=1, size=(num_cells, num_levels))
-    dual_normal_cell_x = rng.uniform(low=0, high=1, size=(num_ec))
-    dual_normal_cell_y = rng.uniform(low=0, high=1, size=(num_ec))
-    dual_normal_vert_x = rng.uniform(low=0, high=1, size=(num_ecv))
-    dual_normal_vert_y = rng.uniform(low=0, high=1, size=(num_ecv))
-    primal_normal_cell_x = rng.uniform(low=0, high=1, size=(num_ec))
-    primal_normal_cell_y = rng.uniform(low=0, high=1, size=(num_ec))
-    primal_normal_vert_x = rng.uniform(low=0, high=1, size=(num_ecv))
-    primal_normal_vert_y = rng.uniform(low=0, high=1, size=(num_ecv))
+    dual_normal_cell_x = rng.uniform(low=0, high=1, size=(num_edges, num_e2c))
+    dual_normal_cell_y = rng.uniform(low=0, high=1, size=(num_edges, num_e2c))
+    dual_normal_vert_x = rng.uniform(low=0, high=1, size=(num_edges, num_e2c2v))
+    dual_normal_vert_y = rng.uniform(low=0, high=1, size=(num_edges, num_e2c2v))
+    primal_normal_cell_x = rng.uniform(low=0, high=1, size=(num_edges, num_e2c))
+    primal_normal_cell_y = rng.uniform(low=0, high=1, size=(num_edges, num_e2c))
+    primal_normal_vert_x = rng.uniform(low=0, high=1, size=(num_edges, num_e2c))
+    primal_normal_vert_y = rng.uniform(low=0, high=1, size=(num_edges, num_e2c))
     tangent_orientation = rng.uniform(low=0, high=1, size=(num_edges))
     inverse_primal_edge_lengths = rng.uniform(low=0, high=1, size=(num_edges))
     inv_dual_edge_length = rng.uniform(low=0, high=1, size=(num_edges))
@@ -105,10 +105,10 @@ if __name__ == "__main__":
     f_e = rng.uniform(low=0, high=1, size=(num_edges))
     cell_areas = rng.uniform(low=0, high=1, size=(num_cells))
     zd_diffcoef = rng.uniform(low=0, high=1, size=(num_cells, num_levels))
-    zd_vertoffset = np.round(rng.uniform(low=0, high=1, size=(num_cec, num_levels))).astype(
-        np.int32
-    )
-    zd_intcoef = rng.uniform(low=0, high=1, size=(num_cec, num_levels))
+    zd_vertoffset = np.round(
+        rng.uniform(low=0, high=1, size=(num_cells, num_c2e2c, num_levels))
+    ).astype(np.int32)
+    zd_intcoef = rng.uniform(low=0, high=1, size=(num_cells, num_c2e2c, num_levels))
 
     # Create a boolean array based on a condition
     mask_hdiff = rng.uniform(low=0, high=1, size=(num_cells, num_levels))
@@ -118,8 +118,8 @@ if __name__ == "__main__":
     theta_ref_mc = np_as_located_field(CellDim, KDim)(theta_ref_mc)
     wgtfac_c = np_as_located_field(CellDim, KDim)(wgtfac_c)
     vct_a = np_as_located_field(KDim)(vct_a)
-    e_bln_c_s = np_as_located_field(CEDim)(e_bln_c_s)
-    geofac_div = np_as_located_field(CEDim)(geofac_div)
+    e_bln_c_s = np_as_located_field(CellDim, C2EDim)(e_bln_c_s)
+    geofac_div = np_as_located_field(CellDim, C2EDim)(geofac_div)
     geofac_grg_x = np_as_located_field(CellDim, C2E2CODim)(geofac_grg_x)
     geofac_grg_y = np_as_located_field(CellDim, C2E2CODim)(geofac_grg_y)
     geofac_n2s = np_as_located_field(CellDim, C2E2CODim)(geofac_n2s)
@@ -132,36 +132,44 @@ if __name__ == "__main__":
     div_ic = np_as_located_field(CellDim, KDim)(div_ic)
     mask_hdiff = np_as_located_field(CellDim, KDim)(mask_hdiff)
     zd_diffcoef = np_as_located_field(CellDim, KDim)(zd_diffcoef)
-    zd_vertoffset = np_as_located_field(CECDim, KDim)(zd_vertoffset)
-    zd_intcoef = np_as_located_field(CECDim, KDim)(zd_intcoef)
+    zd_vertoffset = np_as_located_field(CellDim, C2E2CDim, KDim)(zd_vertoffset)
+    zd_intcoef = np_as_located_field(CellDim, C2E2CDim, KDim)(zd_intcoef)
     w = np_as_located_field(CellDim, KDim)(w)
     vn = np_as_located_field(EdgeDim, KDim)(vn)
     exner = np_as_located_field(CellDim, KDim)(exner)
     theta_v = np_as_located_field(CellDim, KDim)(theta_v)
     rho = np_as_located_field(CellDim, KDim)(rho)
     dual_normal_cell_x = np_as_located_field(
-        ECDim,
+        EdgeDim,
+        E2CDim,
     )(dual_normal_cell_x)
     dual_normal_cell_y = np_as_located_field(
-        ECDim,
+        EdgeDim,
+        E2CDim,
     )(dual_normal_cell_y)
     dual_normal_vert_x = np_as_located_field(
-        ECVDim,
+        EdgeDim,
+        E2C2VDim,
     )(dual_normal_vert_x)
     dual_normal_vert_y = np_as_located_field(
-        ECVDim,
+        EdgeDim,
+        E2C2VDim,
     )(dual_normal_vert_y)
     primal_normal_cell_x = np_as_located_field(
-        ECDim,
+        EdgeDim,
+        E2CDim,
     )(primal_normal_cell_x)
     primal_normal_cell_y = np_as_located_field(
-        ECDim,
+        EdgeDim,
+        E2CDim,
     )(primal_normal_cell_y)
     primal_normal_vert_x = np_as_located_field(
-        ECVDim,
+        EdgeDim,
+        E2C2VDim,
     )(primal_normal_vert_x)
     primal_normal_vert_y = np_as_located_field(
-        ECVDim,
+        EdgeDim,
+        E2C2VDim,
     )(primal_normal_vert_y)
     tangent_orientation = np_as_located_field(
         EdgeDim,
