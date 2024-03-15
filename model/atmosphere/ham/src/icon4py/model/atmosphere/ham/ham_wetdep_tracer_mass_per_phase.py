4b1113ed862e97d323ad98b52e35081875338a50
Mikael Stellio <mikael.stellio@meteoswiss.ch>
2024-03-15 20:28:46 +0100

    Added implementation of stencil ham_wetdep_tracer_mass_per_phase

---
+++ model/atmosphere/ham/src/icon4py/model/atmosphere/ham/ham_wetdep_tracer_mass_per_phase.py
@@ -0,0 +1,55 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from gt4py.next.common import GridType
+from gt4py.next.ffront.decorator import field_operator, program
+from gt4py.next.ffront.fbuiltins import Field, int32
+
+from icon4py.model.common.dimension import CellDim, KDim
+from icon4py.model.common.type_alias import wpfloat
+
+
+@field_operator
+def _ham_wetdep_tracer_mass_per_phase(
+    pice     : Field[[CellDim, KDim], wpfloat],
+    pxtp1c_kt: Field[[CellDim, KDim], wpfloat],
+) -> (
+    tuple[Field[[CellDim, KDim], wpfloat],
+          Field[[CellDim, KDim], wpfloat]]
+):
+
+    zxtwat_tmp = pxtp1c_kt * (1. - pice)
+    zxtice_tmp = pxtp1c_kt * pice
+
+    return (zxtice_tmp, zxtwat_tmp)
+
+
+@program(grid_type=GridType.UNSTRUCTURED)
+def ham_wetdep_tracer_mass_per_phase(
+    pice            : Field[[CellDim, KDim], wpfloat],
+    pxtp1c_kt       : Field[[CellDim, KDim], wpfloat],
+    zxtice          : Field[[CellDim, KDim], wpfloat],
+    zxtwat          : Field[[CellDim, KDim], wpfloat],
+    horizontal_start: int32,
+    horizontal_end  : int32,
+    vertical_start  : int32,
+    vertical_end    : int32
+):
+
+    _ham_wetdep_tracer_mass_per_phase( pice, pxtp1c_kt,
+                                       out = (zxtice, zxtwat),
+                                       domain = {
+                                           CellDim: (horizontal_start, horizontal_end),
+                                           KDim: (vertical_start, vertical_end)
+                                       }
+    )
