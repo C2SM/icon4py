9b16b99f7b58414d1fe604ab3b97077c9fba29db
samkellerhals <kellerhalssamuel@gmail.com>
2024-04-05 09:13:09 +0200

    Skip diffusion test

--- tools/tests/py2fgen/test_diffusion_wrapper.py
+++ tools/tests/py2fgen/test_diffusion_wrapper.py
@@ -11,6 +11,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 # type: ignore
+import pytest
 from gt4py.next import as_field
 from icon4py.model.atmosphere.diffusion.diffusion import DiffusionType
 from icon4py.model.common.config import Icon4PyConfig
@@ -36,7 +37,7 @@ xp = config.array_ns
 
 
 # todo(samkellerhals): turn on and off using a marker/option
-# @pytest.mark.skip
+@pytest.mark.skip
 def test_diffusion_wrapper_py():
     # grid parameters
     num_cells = 20480
