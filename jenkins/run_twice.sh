0f694492eb7fabbbcf3f6b756159fdd3c8fcf2e4
Jonas Jucker <jonas.jucker@c2sm.ethz.ch>
2024-04-18 14:00:09 +0200

    run twice on balfrin

--- jenkins/run_twice.sh
+++ jenkins/run_twice.sh
@@ -1,16 +1,9 @@
 #!/usr/local/bin/bash -l
 
-# Function to run a command twice concurrently
 run_twice() {
   cmd=$@
-
-  # Run the command in the background
   $cmd &
-
-  # Run the command again in the background
   $cmd &
-
-  # Wait for all background jobs to finish
   wait
 }
 
