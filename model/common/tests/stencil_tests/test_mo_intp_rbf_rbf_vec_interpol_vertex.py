3608bb7f12b3bac7c8d8d555b60ad7010e156346
Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>
2024-04-30 13:04:34 +0200

    file split

--- model/common/tests/stencil_tests/test_mo_intp_rbf_rbf_vec_interpol_vertex.py
+++ model/common/tests/stencil_tests/test_mo_intp_rbf_rbf_vec_interpol_vertex.py
@@ -16,7 +16,7 @@ import pytest
 from gt4py.next.ffront.fbuiltins import int32
 
 from icon4py.model.common.dimension import EdgeDim, KDim, V2EDim, VertexDim
-from icon4py.model.common.interpolation.stencils.mo_intp_rbf_rbf_vec_interpol import (
+from icon4py.model.common.interpolation.stencils.mo_intp_rbf_rbf_vec_interpol_vertex import (
     mo_intp_rbf_rbf_vec_interpol_vertex,
 )
 from icon4py.model.common.test_utils.helpers import StencilTest, random_field, zero_field
