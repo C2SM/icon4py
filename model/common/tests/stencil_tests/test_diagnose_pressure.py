2b4548c30dd7c281aa375cf02d7c882479b20350
Ong Chia Rui <93439630+OngChia@users.noreply.github.com>
2024-05-06 10:13:29 +0200

    Initial condition for jw test (#446)
    * Pull changes for initial conditions from Jablonowski_Williamson_test branch.
    
    * Changes function names testcase_functions.py for better readability.
    
    * Added initial conditions for JW.
    
    * Add back io_utils which later will become initialization_utils.py
    
    * remove existing initialization
    
    * rename io_utils.py as initialization_utils.py
    
    * Add JW initial conditions in initialization_utils.py. Move non-compulsory test for JW initial condition to test_testcase.py. Fixed a few bugs.
    
    * Removed unecessary savepoints.
    
    * clean up. Remove unecessary codes that were used to output data for CDO remap.
    
    * precommit
    
    * Remove dummy log file that was accidentally added
    
    * Removed unwanted diagnostic metric state variables that were used to output the grid data for remapping.
    
    * add docstring to variables in horizontal.py
    
    * move referecen solution to second argument in allclose.
    
    * create diagnostic_stencils and their tests
    
    * Finalizing all stencil and data tests for diagnostic state variables, except pressure
    
    * Removed init_exner_pr and temporary printing statement in test_diagnostic_calculations
    
    * remove comment
    
    * remove unsued rbf numpy stencil in testcase_functions.py
    
    * remove bug in pressure stencil test
    
    * Resolved pressure problem
    
    * changes according to review
    
    * remove the pytest skip mark for test_jabw_initial_condition because jabw data is available for CI
    
    * skip test_diagnose_pressure when roundtrip backend

---
+++ model/common/tests/stencil_tests/test_diagnose_pressure.py
@@ -0,0 +1,84 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import numpy as np
+import pytest
+from gt4py.next.ffront.fbuiltins import int32
+
+from icon4py.model.common.constants import GRAV_O_RD
+from icon4py.model.common.diagnostic_calculations.stencils.diagnose_pressure import (
+    diagnose_pressure,
+)
+from icon4py.model.common.dimension import CellDim, KDim
+from icon4py.model.common.test_utils.helpers import (
+    StencilTest,
+    is_roundtrip,
+    random_field,
+    zero_field,
+)
+from icon4py.model.common.type_alias import vpfloat, wpfloat
+
+
+class TestDiagnosePressure(StencilTest):
+    PROGRAM = diagnose_pressure
+    OUTPUTS = ("pressure", "pressure_ifc")
+
+    @staticmethod
+    def reference(
+        grid,
+        pressure_sfc: np.array,
+        temperature: np.array,
+        ddqz_z_full: np.array,
+        **kwargs,
+    ) -> dict:
+        pressure_ifc = np.zeros_like(temperature)
+        pressure = np.zeros_like(temperature)
+        ground_level = temperature.shape[1] - 1
+        pressure_ifc[:, ground_level] = pressure_sfc * np.exp(
+            -GRAV_O_RD * ddqz_z_full[:, ground_level] / temperature[:, ground_level]
+        )
+        pressure[:, ground_level] = np.sqrt(pressure_ifc[:, ground_level] * pressure_sfc)
+        for k in range(ground_level - 1, -1, -1):
+            pressure_ifc[:, k] = pressure_ifc[:, k + 1] * np.exp(
+                -GRAV_O_RD * ddqz_z_full[:, k] / temperature[:, k]
+            )
+            pressure[:, k] = np.sqrt(pressure_ifc[:, k] * pressure_ifc[:, k + 1])
+
+        return dict(
+            pressure=pressure,
+            pressure_ifc=pressure_ifc,
+        )
+
+    @pytest.fixture
+    def input_data(self, grid):
+        if is_roundtrip:
+            pytest.xfail("This stencil currently does not work properly with roundtrip backend.")
+
+        ddqz_z_full = random_field(grid, CellDim, KDim, dtype=wpfloat)
+        temperature = random_field(grid, CellDim, KDim, dtype=vpfloat)
+        pressure_sfc = random_field(grid, CellDim, dtype=vpfloat)
+        pressure = zero_field(grid, CellDim, KDim, dtype=vpfloat)
+        pressure_ifc = zero_field(grid, CellDim, KDim, dtype=vpfloat)
+
+        return dict(
+            ddqz_z_full=ddqz_z_full,
+            temperature=temperature,
+            pressure_sfc=pressure_sfc,
+            pressure=pressure,
+            pressure_ifc=pressure_ifc,
+            grav_o_rd=GRAV_O_RD,
+            horizontal_start=int32(0),
+            horizontal_end=int32(grid.num_cells),
+            vertical_start=int32(0),
+            vertical_end=int32(grid.num_levels),
+        )
