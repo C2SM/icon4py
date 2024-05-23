d7a05cb46bafe22b1978044f6dc9061a3393bfef
Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>
2024-05-23 09:53:07 +0200

    partial updates following review

--- model/common/src/icon4py/model/common/metrics/stencils/compute_wgtfacq.py
+++ model/common/src/icon4py/model/common/metrics/stencils/compute_wgtfacq.py
@@ -13,10 +13,40 @@
 
 import numpy as np
 
-from icon4py.model.common.metrics.stencils.compute_wgtfacq_c_dsl import (
-    compute_wgtfacq_c_dsl,
-    compute_z1_z2_z3,
-)
+
+def compute_z1_z2_z3(z_ifc, i1, i2, i3, i4):
+    z1 = 0.5 * (z_ifc[:, i2] - z_ifc[:, i1])
+    z2 = 0.5 * (z_ifc[:, i2] + z_ifc[:, i3]) - z_ifc[:, i1]
+    z3 = 0.5 * (z_ifc[:, i3] + z_ifc[:, i4]) - z_ifc[:, i1]
+    return z1, z2, z3
+
+
+def compute_wgtfacq_c_dsl(
+    z_ifc: np.array,
+    nlev: int,
+) -> np.array:
+    """
+    Compute weighting factor for quadratic interpolation to surface.
+
+    Args:
+        z_ifc: Field[CellDim, KDim] (half levels), geometric height at the vertical interface of cells.
+        nlev: int, last k level
+    Returns:
+    Field[CellDim, KDim] (full levels)
+    """
+    wgtfacq_c = np.zeros((z_ifc.shape[0], nlev + 1))
+    wgtfacq_c_dsl = np.zeros((z_ifc.shape[0], nlev))
+    z1, z2, z3 = compute_z1_z2_z3(z_ifc, nlev, nlev - 1, nlev - 2, nlev - 3)
+
+    wgtfacq_c[:, 2] = z1 * z2 / (z2 - z3) / (z1 - z3)
+    wgtfacq_c[:, 1] = (z1 - wgtfacq_c[:, 2] * (z1 - z3)) / (z1 - z2)
+    wgtfacq_c[:, 0] = 1.0 - (wgtfacq_c[:, 1] + wgtfacq_c[:, 2])
+
+    wgtfacq_c_dsl[:, nlev - 1] = wgtfacq_c[:, 0]
+    wgtfacq_c_dsl[:, nlev - 2] = wgtfacq_c[:, 1]
+    wgtfacq_c_dsl[:, nlev - 3] = wgtfacq_c[:, 2]
+
+    return wgtfacq_c_dsl
 
 
 def compute_wgtfacq_e_dsl(
@@ -24,7 +54,7 @@ def compute_wgtfacq_e_dsl(
     z_ifc: np.array,
     z_aux_c: np.array,
     c_lin_e: np.array,
-    wgtfacq_e_dsl: np.array,
+    n_edges: int,
     nlev: int,
 ):
     """
@@ -40,6 +70,7 @@ def compute_wgtfacq_e_dsl(
     Returns:
     Field[EdgeDim, KDim] (full levels)
     """
+    wgtfacq_e_dsl = np.zeros(shape=(n_edges, nlev+1))
     z1, z2, z3 = compute_z1_z2_z3(z_ifc, nlev, nlev - 1, nlev - 2, nlev - 3)
     wgtfacq_c_dsl = compute_wgtfacq_c_dsl(z_ifc, nlev)
     z_aux_c[:, 2] = z1 * z2 / (z2 - z3) / (z1 - z3)
