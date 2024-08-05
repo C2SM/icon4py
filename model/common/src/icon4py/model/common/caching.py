91579bd697e9aff0017e0eb4a877ec0bfd88146f
Rico Haeuselmann <r.haeuselmann@gmx.ch>
2024-08-05 13:54:18 +0200

    update gt4py dep to PR #1545. (#513)
    * update gt4py dep to PR #1545.
    
    * update CachedProgram
    
    * remove unused field size handling
    
    ---------
    
    Co-authored-by: Samuel <kellerhalssamuel@gmail.com>

--- model/common/src/icon4py/model/common/caching.py
+++ model/common/src/icon4py/model/common/caching.py
@@ -16,50 +16,29 @@ from typing import Any, Callable, Optional
 
 import numpy as np
 from gt4py import next as gtx
-from gt4py.next.otf import workflow
+from gt4py.next.otf import arguments, workflow
+from gt4py.next.ffront import signature
 from gt4py.next.program_processors.runners.gtfn import extract_connectivity_args
 
 from icon4py.model.common.settings import device
-
-
-try:
-    import cupy as cp
-    from gt4py.next.embedded.nd_array_field import CuPyArrayField
-except ImportError:
-    cp: Optional = None  # type:ignore[no-redef]
-
-from gt4py.next.embedded.nd_array_field import NumPyArrayField
 
 
 def handle_numpy_integer(value):
     return int(value)
 
 
-def handle_common_field(value, sizes):
-    sizes.extend(value.shape)
-    return value  # Return the value unmodified, but side-effect on sizes
-
-
 def handle_default(value):
     return value  # Return the value unchanged
 
 
-if cp:
-    type_handlers = {
-        np.integer: handle_numpy_integer,
-        NumPyArrayField: handle_common_field,
-        CuPyArrayField: handle_common_field,
-    }
-else:
-    type_handlers = {
-        np.integer: handle_numpy_integer,
-        NumPyArrayField: handle_common_field,
-    }
+type_handlers = {
+    np.integer: handle_numpy_integer,
+}
 
 
-def process_arg(value, sizes):
+def process_arg(value):
     handler = type_handlers.get(type(value), handle_default)
-    return handler(value, sizes) if handler == handle_common_field else handler(value)
+    return handler(value)
 
 
 @dataclasses.dataclass
@@ -89,7 +68,6 @@ class CachedProgram:
     with_domain: bool = True
     _compiled_program: Optional[Callable] = None
     _conn_args: Any = None
-    _compiled_args: tuple = dataclasses.field(default_factory=tuple)
 
     @property
     def compiled_program(self) -> Callable:
@@ -102,16 +80,9 @@ class CachedProgram:
     def compile_the_program(
         self, *args, offset_provider: dict[str, gtx.Dimension], **kwargs: Any
     ) -> Callable:
-        backend = self.program.backend
-        program_call = backend.transforms_prog(
-            workflow.InputWithArgs(
-                data=self.program.definition_stage,
-                args=args,
-                kwargs=kwargs | {"offset_provider": offset_provider},
-            )
+        return self.program.backend.jit(
+            self.program.definition_stage, *args, **kwargs | {"offset_provider": offset_provider}
         )
-        self._compiled_args = program_call.args
-        return backend.executor.otf_workflow(program_call)
 
     def __call__(self, *args, offset_provider: dict[str, gtx.Dimension], **kwargs: Any) -> None:
         if not self.compiled_program:
@@ -120,16 +91,12 @@ class CachedProgram:
             )
             self._conn_args = extract_connectivity_args(offset_provider, device)
 
-        kwargs_as_tuples = tuple(kwargs.values())
-        program_args = list(args) + list(kwargs_as_tuples)
-        sizes = []
+        args, kwargs = signature.convert_to_positional(
+            self.program.definition_stage, *args, **kwargs
+        )
 
         # Convert numpy integers in args to int and handle gtx.common.Field
-        for i in range(len(program_args)):
-            program_args[i] = process_arg(program_args[i], sizes)
-
-        if not self.with_domain:
-            program_args.extend(sizes)
+        args = tuple(process_arg(arg) for arg in args)
 
         # todo(samkellerhals): if we merge gt4py PR we can also pass connectivity args here conn_args=self.conn_args
-        return self.compiled_program(*program_args, offset_provider=offset_provider)
+        return self.compiled_program(*args, offset_provider=offset_provider)
