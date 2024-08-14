e02f08f95c670bf9714064b574e3d26965702f15
Hannes Vogt <vogt@hey.com>
2024-08-14 14:24:38 +0200

    Add license header to Fortran test files

--- tools/tests/py2fgen/fortran_samples/test_square.f90
+++ tools/tests/py2fgen/fortran_samples/test_square.f90
@@ -1,3 +1,11 @@
+! ICON4Py - ICON inspired code in Python and GT4Py
+!
+! Copyright (c) 2022-2024, ETH Zurich and MeteoSwiss
+! All rights reserved.
+!
+! Please, refer to the LICENSE file in the root directory.
+! SPDX-License-Identifier: BSD-3-Clause
+
 program call_square_wrapper_cffi_plugin
    use, intrinsic :: iso_c_binding
    use square_plugin
