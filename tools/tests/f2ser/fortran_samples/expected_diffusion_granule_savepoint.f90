262294f6fde9c29b9e86f4c28857bdf43092cd38
Hannes Vogt <vogt@hey.com>
2024-08-28 15:18:00 +0200

    fix tests that rely on stable line no

--- tools/tests/f2ser/fortran_samples/expected_diffusion_granule_savepoint.f90
+++ tools/tests/f2ser/fortran_samples/expected_diffusion_granule_savepoint.f90
@@ -1,12 +1,4 @@
 
-! ICON4Py - ICON inspired code in Python and GT4Py
-!
-! Copyright (c) 2022-2024, ETH Zurich and MeteoSwiss
-! All rights reserved.
-!
-! Please, refer to the LICENSE file in the root directory.
-! SPDX-License-Identifier: BSD-3-Clause
-
     !$ser verbatim real, dimension(:,:,:), allocatable :: edges_primal_normal_vert_v1(:,:,:)
 
     !$ser verbatim real, dimension(:,:,:), allocatable :: edges_primal_normal_vert_v2(:,:,:)
