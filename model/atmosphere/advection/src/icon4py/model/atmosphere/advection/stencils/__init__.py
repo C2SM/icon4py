6f0c732f6bef5ebbe3cb80c6d79a588a23d33437
David Strassmann <david.strassmann@c2sm.ethz.ch>
2024-09-05 15:40:29 +0200

    Add missing python infrastructure files

--- model/atmosphere/advection/src/icon4py/model/atmosphere/advection/stencils/__init__.py
+++ model/atmosphere/advection/src/icon4py/model/atmosphere/advection/stencils/__init__.py
@@ -6,24 +6,3 @@
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
 
-from typing import Final
-
-from packaging import version as pkg_version
-
-
-__all__ = [
-    "__author__",
-    "__copyright__",
-    "__license__",
-    "__version__",
-    "__version_info__",
-]
-
-
-__author__: Final = "ETH Zurich,  MeteoSwiss and individual contributors"
-__copyright__: Final = "Copyright (c) 2022-2024 ETH Zurich and MeteoSwiss"
-__license__: Final = "BSD-3-Clause"
-
-
-__version__: Final = "0.0.6"
-__version_info__: Final = pkg_version.parse(__version__)
