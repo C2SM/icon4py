7de9ea1341f2b1a5a6f4d53b34d619230606a019
samkellerhals <kellerhalssamuel@gmail.com>
2024-09-18 15:51:35 +0200

    Add wrapper input test

--- tools/tests/py2fgen/conftest.py
+++ tools/tests/py2fgen/conftest.py
@@ -9,8 +9,30 @@
 from pathlib import Path
 
 import pytest
+from icon4py.model.common.settings import xp
 
 
 @pytest.fixture
 def samples_path():
     return Path(__file__).parent / "fortran_samples"
+
+
+def compare_objects(obj1: object, obj2: object) -> bool:
+    # Check if both objects are instances of the same class
+    if obj1.__class__ != obj2.__class__:
+        return False
+
+    # Compare the attributes of both objects
+    for attr, value in vars(obj1).items():
+        other_value = getattr(obj2, attr, None)
+
+        if hasattr(value, "__dict__") and hasattr(other_value, "__dict__"):
+            # special case for arrays
+            if hasattr(value, "ndarray") and hasattr(other_value, "ndarray"):
+                return xp.all(value == other_value)
+            return value.__dict__ == other_value.__dict__
+
+        if value != other_value:
+            return False
+
+    return True
