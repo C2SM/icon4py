317c9016aaca6136663dee51adcfc36745aaaca5
Samuel <kellerhalssamuel@gmail.com>
2024-09-30 14:11:36 +0200

    [py2f]: Add diffusion grid initialisation and datatest (#548)
    Add diffusion grid initialization in the wrapper as well as a datatest.

---
+++ tools/src/icon4pytools/py2fgen/wrappers/wrapper_dimension.py
@@ -0,0 +1,14 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022-2024, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# Please, refer to the LICENSE file in the root directory.
+# SPDX-License-Identifier: BSD-3-Clause
+
+from gt4py.next import Dimension
+
+
+CellIndexDim = Dimension("CellIndex")
+EdgeIndexDim = Dimension("EdgeIndex")
+VertexIndexDim = Dimension("VertexIndex")
