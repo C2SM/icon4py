2ff713473adde52ded925b335d9507ead04cf7dc
Jacopo <jacopo.canton@gmail.com>
2024-10-03 18:58:24 +0200

    more wip, `grid` works (not with savepoint); `icon_grid` doesn't (at all)

--- model/common/tests/grid_tests/test_topography.py
+++ model/common/tests/grid_tests/test_topography.py
@@ -20,42 +20,76 @@ from icon4py.model.common.test_utils import helpers as test_helpers
 from icon4py.model.common.settings import xp
 
 
+# @pytest.mark.datatest
+# ## @pytest.mark.parametrize("experiment", [dt_utils.REGIONAL_EXPERIMENT, dt_utils.GLOBAL_EXPERIMENT])
+# def test_topography_smoothing(
+#     icon_grid,
+#     grid_savepoint,
+#     interpolation_savepoint,
+# ):
+# 
+#     cell_geometry: geometry.CellParams = grid_savepoint.construct_cell_geometry()
+#     cell_domain = h_grid.domain(dims.CellDim)
+# 
+#     dual_edge_length = grid_savepoint.dual_edge_length() # TODO (Jacopo): add dual_edge_length to torus serialized data
+#     geofac_div = interpolation_savepoint.geofac_div()
+#     c2e = icon_grid.connectivities[dims.C2EDim]
+#     e2c = icon_grid.connectivities[dims.E2CDim]
+#     c2e2c = icon_grid.connectivities[dims.C2E2CDim]
+#     horizontal_start = icon_grid.start_index(cell_domain(h_grid.Zone.LATERAL_BOUNDARY_LEVEL_2))
+#     geofac_n2s_np = interp_fields.compute_geofac_n2s(
+#         dual_edge_length.asnumpy(),
+#         geofac_div.asnumpy(),
+#         c2e,
+#         e2c,
+#         c2e2c,
+#         horizontal_start,
+#     )
+#     geofac_n2s = gtx.as_field((dims.CellDim, dims.C2E2CODim), geofac_n2s_np)
+# 
+#     topography_np = xp.zeros((icon_grid.num_cells), dtype=ta.wpfloat)
+#     topography = gtx.as_field((dims.CellDim,), topography_np)
+# 
+#     topography_smoothed = topo.compute_smooth_topo(
+#         topography=topography,
+#         grid=icon_grid,
+#         cell_areas=cell_geometry.area,
+#         geofac_n2s=geofac_n2s,
+#         num_iterations=25,
+#     )
+# 
+#     assert test_helpers.dallclose(topography.asnumpy(), topography_smoothed.asnumpy())
+
+
+from icon4py.model.common.test_utils.helpers import StencilTest, constant_field, zero_field
+
+def calculate_nabla2_for_w_numpy(grid, psi_c: xp.array, geofac_n2s: xp.array):
+    c2e2cO = grid.connectivities[dims.C2E2CODim]
+    geofac_n2s = xp.expand_dims(geofac_n2s, axis=-1)
+    nabla2_psi_c = xp.sum(
+        xp.where((c2e2cO != -1)[:, :, xp.newaxis], psi_c[c2e2cO] * geofac_n2s, 0), axis=1
+    )
+    return nabla2_psi_c
 @pytest.mark.datatest
 ## @pytest.mark.parametrize("experiment", [dt_utils.REGIONAL_EXPERIMENT, dt_utils.GLOBAL_EXPERIMENT])
-def test_topography_smoothing(
-    icon_grid,
-    grid_savepoint,
-    interpolation_savepoint,
-):
+class TestCalculateNabla2ForW(StencilTest):
+    PROGRAM = topo.nabla2_scalar
+    OUTPUTS = ("nabla2_psi_c",)
 
-    cell_geometry: geometry.CellParams = grid_savepoint.construct_cell_geometry()
-    cell_domain = h_grid.domain(dims.CellDim)
+    @staticmethod
+    def reference(grid, psi_c: xp.array, geofac_n2s: xp.array, **kwargs) -> dict:
+        nabla2_psi_c = calculate_nabla2_for_w_numpy(grid, psi_c, geofac_n2s)
+        return dict(nabla2_psi_c=nabla2_psi_c)
 
-    dual_edge_length = grid_savepoint.dual_edge_length() # TODO (Jacopo): add dual_edge_length to torus serialized data
-    geofac_div = interpolation_savepoint.geofac_div()
-    c2e = icon_grid.connectivities[dims.C2EDim]
-    e2c = icon_grid.connectivities[dims.E2CDim]
-    c2e2c = icon_grid.connectivities[dims.C2E2CDim]
-    horizontal_start = icon_grid.start_index(cell_domain(h_grid.Zone.LATERAL_BOUNDARY_LEVEL_2))
-    geofac_n2s_np = interp_fields.compute_geofac_n2s(
-        dual_edge_length.asnumpy(),
-        geofac_div.asnumpy(),
-        c2e,
-        e2c,
-        c2e2c,
-        horizontal_start,
-    )
-    geofac_n2s = gtx.as_field((dims.CellDim, dims.C2E2CODim), geofac_n2s_np)
+    @pytest.fixture
+    def input_data(self, grid, interpolation_savepoint):
+        psi_c = constant_field(grid, 1.0, dims.CellDim, dims.KDim)
+        geofac_n2s = constant_field(grid, 2.0, dims.CellDim, dims.C2E2CODim)
+        #geofac_n2s = interpolation_savepoint.geofac_n2s()
+        nabla2_psi_c = zero_field(grid, dims.CellDim, dims.KDim)
 
-    topography_np = xp.zeros((icon_grid.num_cells), dtype=ta.wpfloat)
-    topography = gtx.as_field((dims.CellDim,), topography_np)
-
-    topography_smoothed = topo.compute_smooth_topo(
-        topography=topography,
-        grid=icon_grid,
-        cell_areas=cell_geometry.area,
-        geofac_n2s=geofac_n2s,
-        num_iterations=25,
-    )
-
-    assert test_helpers.dallclose(topography.asnumpy(), topography_smoothed.asnumpy())
+        return dict(
+            psi_c=psi_c,
+            geofac_n2s=geofac_n2s,
+            nabla2_psi_c=nabla2_psi_c,
+        )
