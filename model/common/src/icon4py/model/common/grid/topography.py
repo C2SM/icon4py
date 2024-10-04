efbd294c9ea78d61f0041d2484da2c2ccf530411
Jacopo <jacopo.canton@gmail.com>
2024-10-04 14:42:58 +0200

    fix KDim

--- model/common/src/icon4py/model/common/grid/topography.py
+++ model/common/src/icon4py/model/common/grid/topography.py
@@ -7,7 +7,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import gt4py.next as gtx
-from gt4py.next.ffront.fbuiltins import neighbor_sum
+from gt4py.next.ffront.fbuiltins import broadcast, neighbor_sum
 from icon4py.model.common.dimension import C2E2CO, C2E2CODim
 
 from icon4py.model.common import dimension as dims, field_type_aliases as fa, type_alias as ta
@@ -52,10 +52,13 @@ def compute_smooth_topo(
     coordinate.
     """
 
-    topography_smoothed = gtx.as_field((dims.CellDim,), topography.asnumpy())
+    #topography_smoothed = gtx.as_field((dims.CellDim,), topography.asnumpy())
+    topography_smoothed_np = xp.zeros((grid.num_cells, grid.num_levels), dtype=ta.wpfloat)
+    topography_smoothed_np[:,0] = topography.asnumpy()
+    topography_smoothed = gtx.as_field((dims.CellDim, dims.KDim), topography_smoothed_np)
 
-    nabla2_topo_np = xp.zeros(grid.num_cells, dtype=ta.wpfloat)
-    nabla2_topo = gtx.as_field((dims.CellDim,), nabla2_topo_np)
+    nabla2_topo_np = xp.zeros((grid.num_cells, grid.num_levels), dtype=ta.wpfloat)
+    nabla2_topo = gtx.as_field((dims.CellDim, dims.KDim), nabla2_topo_np)
 
     for iter in range(num_iterations):
 
@@ -66,7 +69,7 @@ def compute_smooth_topo(
             offset_provider={"C2E2CO":grid.get_offset_provider("C2E2CO"),}
         )
         
-        topography_smoothed_np = topography_smoothed.asnumpy() + 0.125 * nabla2_topo.asnumpy() * cell_areas.asnumpy()
-        topography_smoothed = gtx.as_field((dims.CellDim,), topography_smoothed_np)
+        topography_smoothed_np[:,0] = topography_smoothed.asnumpy()[:,0] + 0.125 * nabla2_topo.asnumpy()[:,0] * cell_areas.asnumpy()
+        topography_smoothed = gtx.as_field((dims.CellDim, dims.KDim), topography_smoothed_np)
 
     return topography_smoothed
