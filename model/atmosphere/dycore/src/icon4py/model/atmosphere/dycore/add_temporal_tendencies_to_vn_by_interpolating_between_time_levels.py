89e24334ef9e047a62d7b68c3d81600502fc1817
Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>
2024-10-23 09:28:21 +0200

    Solve nonhydro backend edits (#567)
    * backend edits for solveNonHydro and VelocityAdvection

--- model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/add_temporal_tendencies_to_vn_by_interpolating_between_time_levels.py
+++ model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/add_temporal_tendencies_to_vn_by_interpolating_between_time_levels.py
@@ -11,7 +11,6 @@ from gt4py.next.ffront.decorator import field_operator, program
 from gt4py.next.ffront.fbuiltins import astype
 
 from icon4py.model.common import dimension as dims, field_type_aliases as fa
-from icon4py.model.common.settings import backend
 from icon4py.model.common.type_alias import vpfloat, wpfloat
 
 
@@ -42,7 +41,7 @@ def _add_temporal_tendencies_to_vn_by_interpolating_between_time_levels(
     return vn_nnew_wp
 
 
-@program(grid_type=GridType.UNSTRUCTURED, backend=backend)
+@program(grid_type=GridType.UNSTRUCTURED)
 def add_temporal_tendencies_to_vn_by_interpolating_between_time_levels(
     vn_nnow: fa.EdgeKField[wpfloat],
     ddt_vn_apc_ntl1: fa.EdgeKField[vpfloat],
--- model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/compute_exner_from_rhotheta.py
+++ model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/compute_exner_from_rhotheta.py
@@ -11,7 +11,6 @@ from gt4py.next.ffront.decorator import field_operator, program
 from gt4py.next.ffront.fbuiltins import exp, log
 
 from icon4py.model.common import dimension as dims, field_type_aliases as fa
-from icon4py.model.common.settings import backend
 from icon4py.model.common.type_alias import wpfloat
 
 
@@ -29,7 +28,7 @@ def _compute_exner_from_rhotheta(
     return theta_v_wp, exner_wp
 
 
-@program(grid_type=GridType.UNSTRUCTURED, backend=backend)
+@program(grid_type=GridType.UNSTRUCTURED)
 def compute_exner_from_rhotheta(
     rho: fa.CellKField[wpfloat],
     theta_v: fa.CellKField[wpfloat],
