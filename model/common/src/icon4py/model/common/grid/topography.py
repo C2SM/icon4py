d2918c622271e5a177ccd885c9fb46370625ec1d
Jacopo <jacopo.canton@gmail.com>
2024-10-25 14:16:44 +0200

    tests working, now cleanup and refactor

--- model/common/src/icon4py/model/common/grid/topography.py
+++ model/common/src/icon4py/model/common/grid/topography.py
@@ -11,8 +11,13 @@ from gt4py.next.ffront.fbuiltins import broadcast, neighbor_sum
 from icon4py.model.common.dimension import C2E2CO, C2E2CODim
 
 from icon4py.model.common import dimension as dims, field_type_aliases as fa, type_alias as ta
-from icon4py.model.common.grid import icon as icon_grid
+from icon4py.model.common.grid import icon as icon_grid, horizontal as h_grid
 from icon4py.model.common.settings import xp
+
+# TODO: this will have to be removed once domain allows for imports
+CellDim = dims.CellDim
+EdgeDim = dims.EdgeDim
+KDim = dims.KDim
 
 @gtx.field_operator
 def _nabla2_scalar(
@@ -32,11 +37,19 @@ def nabla2_scalar(
     psi_c: fa.CellKField[ta.wpfloat],
     geofac_n2s: gtx.Field[gtx.Dims[dims.CellDim, dims.C2E2CODim], ta.wpfloat],
     nabla2_psi_c: fa.CellKField[ta.wpfloat],
+    horizontal_start: gtx.int32,
+    horizontal_end: gtx.int32,
+    vertical_start: gtx.int32,
+    vertical_end: gtx.int32,
 ):
     _nabla2_scalar(
         psi_c,
         geofac_n2s,
         out=nabla2_psi_c,
+        domain={
+            CellDim: (horizontal_start, horizontal_end),
+            KDim: (vertical_start, vertical_end),
+        },
     )
 
 
@@ -45,6 +58,7 @@ def compute_smooth_topo(
     grid: icon_grid.IconGrid,
     cell_areas: fa.CellField[ta.wpfloat],
     geofac_n2s: gtx.Field[gtx.Dims[dims.CellDim, dims.C2E2CODim], ta.wpfloat],
+    backend,
     num_iterations: int = 25,
 ) -> fa.CellField[ta.wpfloat]:
     """
@@ -52,7 +66,7 @@ def compute_smooth_topo(
     coordinate.
     """
 
-    #topography_smoothed = gtx.as_field((dims.CellDim,), topography.asnumpy())
+
     topography_smoothed_np = xp.zeros((grid.num_cells, grid.num_levels), dtype=ta.wpfloat)
     topography_smoothed_np[:,0] = topography.asnumpy()
     topography_smoothed = gtx.as_field((dims.CellDim, dims.KDim), topography_smoothed_np)
@@ -60,16 +74,23 @@ def compute_smooth_topo(
     nabla2_topo_np = xp.zeros((grid.num_cells, grid.num_levels), dtype=ta.wpfloat)
     nabla2_topo = gtx.as_field((dims.CellDim, dims.KDim), nabla2_topo_np)
 
+    cell_domain = h_grid.domain(dims.CellDim)
+    end_cell_end = grid.end_index(cell_domain(h_grid.Zone.END))
+
     for iter in range(num_iterations):
 
-        nabla2_scalar(
+        nabla2_scalar.with_backend(backend)(
             psi_c=topography_smoothed,
             geofac_n2s=geofac_n2s,
             nabla2_psi_c=nabla2_topo,
+            horizontal_start=0,
+            horizontal_end=end_cell_end,
+            vertical_start=0,
+            vertical_end=1,
             offset_provider={"C2E2CO":grid.get_offset_provider("C2E2CO"),}
         )
         
         topography_smoothed_np[:,0] = topography_smoothed.asnumpy()[:,0] + 0.125 * nabla2_topo.asnumpy()[:,0] * cell_areas.asnumpy()
         topography_smoothed = gtx.as_field((dims.CellDim, dims.KDim), topography_smoothed_np)
 
-    return topography_smoothed
+    return gtx.as_field((dims.CellDim,), topography_smoothed_np[:,0])
--- model/common/tests/grid_tests/test_topography.py
+++ model/common/tests/grid_tests/test_topography.py
@@ -54,65 +54,59 @@ class TestCalculateNabla2ForW(StencilTest):
         )
 
 @pytest.mark.datatest
-## @pytest.mark.parametrize("experiment", [dt_utils.REGIONAL_EXPERIMENT, dt_utils.GLOBAL_EXPERIMENT])
 def test_nabla2_scalar(
-    grid,
+    icon_grid,
     interpolation_savepoint,
+    backend,
 ):
-        psi_c = constant_field(grid, 1.0, dims.CellDim, dims.KDim)
-        geofac_n2s = constant_field(grid, 2.0, dims.CellDim, dims.C2E2CODim)
-        #geofac_n2s = interpolation_savepoint.geofac_n2s()
-        nabla2_psi_c = zero_field(grid, dims.CellDim, dims.KDim)
+        psi_c = constant_field(icon_grid, 1.0, dims.CellDim, dims.KDim)
+        #h = constant_field(icon_grid, 2.0, dims.CellDim, dims.C2E2CODim)
+        geofac_n2s = interpolation_savepoint.geofac_n2s()
+        nabla2_psi_c = zero_field(icon_grid, dims.CellDim, dims.KDim)
 
-        topo.nabla2_scalar(
+        topo.nabla2_scalar.with_backend(backend)(
             psi_c=psi_c,
             geofac_n2s=geofac_n2s,
             nabla2_psi_c=nabla2_psi_c,
-            offset_provider={"C2E2CO":grid.get_offset_provider("C2E2CO"),}
+            offset_provider={"C2E2CO":icon_grid.get_offset_provider("C2E2CO"),}
         )
 
-        nabla2_psi_c_np = nabla2_scalar_numpy(grid, psi_c.asnumpy(), geofac_n2s.asnumpy())
+        nabla2_psi_c_np = nabla2_scalar_numpy(icon_grid, psi_c.asnumpy(), geofac_n2s.asnumpy())
 
         assert test_helpers.dallclose(nabla2_psi_c.asnumpy(), nabla2_psi_c_np)
 
 
 @pytest.mark.datatest
-## @pytest.mark.parametrize("experiment", [dt_utils.REGIONAL_EXPERIMENT, dt_utils.GLOBAL_EXPERIMENT])
 def test_topography_smoothing(
     icon_grid,
     grid_savepoint,
     interpolation_savepoint,
+    backend,
 ):
 
+    num_iterations = 2
     cell_geometry: geometry.CellParams = grid_savepoint.construct_cell_geometry()
-    cell_domain = h_grid.domain(dims.CellDim)
+    geofac_n2s = interpolation_savepoint.geofac_n2s()
 
-    dual_edge_length = grid_savepoint.dual_edge_length() # TODO (Jacopo): add dual_edge_length to torus serialized data
-    geofac_div = interpolation_savepoint.geofac_div()
-    c2e = icon_grid.connectivities[dims.C2EDim]
-    e2c = icon_grid.connectivities[dims.E2CDim]
-    c2e2c = icon_grid.connectivities[dims.C2E2CDim]
-    horizontal_start = icon_grid.start_index(cell_domain(h_grid.Zone.LATERAL_BOUNDARY_LEVEL_2))
-    geofac_n2s_np = interp_fields.compute_geofac_n2s(
-        dual_edge_length.asnumpy(),
-        geofac_div.asnumpy(),
-        c2e,
-        e2c,
-        c2e2c,
-        horizontal_start,
-    )
-    geofac_n2s = gtx.as_field((dims.CellDim, dims.C2E2CODim), geofac_n2s_np)
-    #geofac_n2s = interpolation_savepoint.geofac_n2s()
+    topography = test_helpers.random_field(icon_grid, dims.CellDim, dtype=ta.wpfloat)
+    topography_np = topography.asnumpy()
 
-    topography_np = xp.zeros((icon_grid.num_cells), dtype=ta.wpfloat)
-    topography = gtx.as_field((dims.CellDim,), topography_np)
+    # numpy implementation
+    topography_smoothed_np = xp.zeros((icon_grid.num_cells, icon_grid.num_levels), dtype=ta.wpfloat)
+    topography_smoothed_np[:,0] = topography_np.copy()
+    for iter in range(num_iterations):
+        nabla2_topo_np = nabla2_scalar_numpy(icon_grid, topography_smoothed_np, geofac_n2s.asnumpy())
+        topography_smoothed_np[:,0] = topography_smoothed_np[:,0] + 0.125 * nabla2_topo_np[:,0] * cell_geometry.area.asnumpy()
+    topography_smoothed_np = topography_smoothed_np[:,0]
 
+    # GT4Py implementation
     topography_smoothed = topo.compute_smooth_topo(
         topography=topography,
         grid=icon_grid,
         cell_areas=cell_geometry.area,
         geofac_n2s=geofac_n2s,
-        num_iterations=25,
+        backend=backend,
+        num_iterations=num_iterations,
     )
 
-    assert test_helpers.dallclose(topography.asnumpy(), topography_smoothed.asnumpy())
+    assert test_helpers.dallclose(topography_smoothed_np, topography_smoothed.asnumpy())
