1049ef7d0f2baae364ff2f98421f47db8fbdf79d
Jacopo <jacopo.canton@gmail.com>
2024-10-29 18:54:14 +0100

    now processing comment blocks

--- model/atmosphere/dycore/docs/icon4py_sphinx.py
+++ model/atmosphere/dycore/docs/icon4py_sphinx.py
@@ -22,6 +22,7 @@ class FullMethodDocumenter(autodoc.MethodDocumenter):
     priority = autodoc.MethodDocumenter.priority - 1
 
     # Configuration options
+    docstring_keyword = 'scidoc'
     var_type_in_inputs = True
     var_type_formatting = '``'
 
@@ -30,24 +31,21 @@ class FullMethodDocumenter(autodoc.MethodDocumenter):
         # source code
         source = inspect.getsource(self.object) # this is only the source of the method, not the whole file
 
-        docstrings = re.findall(r'"""(.*?)"""', source, re.DOTALL)
+        docstrings = self.get_docstring_blocks(source, self.docstring_keyword)
+
         docstrings_list = []
         for idocstr, docstring in enumerate(docstrings):
             formatted_docstr = None
-            if idocstr==0:
-                # "Usual" docstring at the beginning of the method
-                formatted_docstr = docstring.splitlines()
 
-            elif docstring.startswith('_scidoc_'):
-                # Get some useful information
-                call_string = self.get_next_method_call(source, source.find(docstring) + len(docstring))
-                next_method_info = self.get_method_info(call_string)
-                # and process a scientific documentation docstring
-                formatted_docstr = docstring.splitlines()
-                formatted_docstr = self.format_source_code(formatted_docstr)
-                formatted_docstr = self.add_header(formatted_docstr, next_method_info)
-                formatted_docstr = self.process_scidocstrlines(formatted_docstr, next_method_info)
-                formatted_docstr = self.add_next_method_call(formatted_docstr, call_string)
+            # Get some useful information on the following method call
+            call_string = self.get_next_method_call(source, source.find(docstring) + len(docstring))
+            next_method_info = self.get_method_info(call_string)
+            # and process a scientific documentation docstring
+            formatted_docstr = docstring.splitlines()
+            formatted_docstr = self.format_docstring_block(formatted_docstr, self.docstring_keyword)
+            formatted_docstr = self.add_header(formatted_docstr, next_method_info)
+            formatted_docstr = self.process_scidocstrlines(formatted_docstr, next_method_info)
+            formatted_docstr = self.add_next_method_call(formatted_docstr, call_string)
 
             if formatted_docstr is not None:
                 if idocstr < len(docstrings)-1: # Add footer
@@ -56,25 +54,51 @@ class FullMethodDocumenter(autodoc.MethodDocumenter):
                 docstrings_list.append(formatted_docstr)
 
         return docstrings_list
+    
+    def get_docstring_blocks(self, source, keyword):
+        # which in this implementation are comment blocks
+        comment_blocks = []
+        source_lines = source.splitlines()
+        in_block = False
+        current_block = []
+        for line in source_lines:
+            stripped_line = line.strip()
+            if stripped_line.startswith(f'# {keyword}:'):
+                if in_block:
+                    comment_blocks.append('\n'.join(current_block))
+                    current_block = []
+                in_block = True
+                current_block.append(line)
+            elif in_block:
+                if stripped_line.startswith('#'):
+                    current_block.append(line)
+                else:
+                    in_block = False
+                    comment_blocks.append('\n'.join(current_block))
+                    current_block = []
+        if current_block:
+            comment_blocks.append('\n'.join(current_block))
+        return comment_blocks
 
-    def format_source_code(self, source_lines):
+    def format_docstring_block(self, docstr_lines, keyword):
         # Clean up and format
-        if source_lines[0].startswith('_scidoc_'):
-            source_lines.pop(0) # remove the _scidoc_ prefix
-        while source_lines[0] == '':
+        if docstr_lines[0].strip().startswith(f'# {keyword}:'):
+            docstr_lines.pop(0) # remove the {keyword} prefix
+        while docstr_lines[0].strip() == '#':
             # strip empty lines from the beginning
-            source_lines.pop(0)
+            docstr_lines.pop(0)
         # strip leading and trailing whitespace of every line (maintain indentation)
-        indent = len(source_lines[0]) - len(source_lines[0].lstrip(' '))
-        source_lines = [line[indent:].rstrip() for line in source_lines]
+        # as well as comment character and space (indent+2)
+        indent = len(docstr_lines[0]) - len(docstr_lines[0].lstrip(' '))
+        docstr_lines = [line[min(indent+2,len(line)):].rstrip() for line in docstr_lines]
         # strip empty lines from the end
-        while source_lines[-1] == '':
-            source_lines.pop(-1)
-        return source_lines
-
+        while docstr_lines[-1] == '':
+            docstr_lines.pop(-1)
+        return docstr_lines
+    
     def add_header(self, docstr_lines, method_info):
         # Add a title with ReST formatting
-        method_name = method_info['method_name']
+        method_name = method_info['module_local_name'] + '.' + method_info['method_name']
         method_full_name = method_info['module_full_name'] + '.' + method_name
         title = f':meth:`{method_name}()<{method_full_name}>`'
         docstr_lines.insert(0, title)
@@ -168,9 +192,23 @@ class FullMethodDocumenter(autodoc.MethodDocumenter):
         if start_index is not None and end_index is not None:
             call_start_line = source[:index_start].count('\n')+1
             call_end_line = call_start_line + remaining_source[:end_index].count('\n')-1
-            call_string = self.format_source_code(source.splitlines()[call_start_line:call_end_line+1])
+            call_string = self.format_code_block(source.splitlines()[call_start_line:call_end_line+1])
             return call_string
         return None
+
+    def format_code_block(self, code_lines):
+        # Clean up and format
+        while code_lines[0] == '':
+            # strip empty lines from the beginning
+            code_lines.pop(0)
+        # strip leading and trailing whitespace of every line (maintain indentation)
+        indent = len(code_lines[0]) - len(code_lines[0].lstrip(' '))
+        code_lines = [line[indent:].rstrip() for line in code_lines]
+        # strip empty lines from the end
+        while code_lines[-1] == '':
+            code_lines.pop(-1)
+        return code_lines
+
 
     def get_method_info(self, call_string):
         method_info = {}
