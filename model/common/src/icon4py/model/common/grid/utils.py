aa2f1d6ec7dc5aef7d65aad424338b1fdab7949d
Hannes Vogt <vogt@hey.com>
2024-10-29 14:09:29 +0100

    proper memory layout for connectivities

--- model/common/src/icon4py/model/common/grid/utils.py
+++ model/common/src/icon4py/model/common/grid/utils.py
@@ -10,6 +10,7 @@ import numpy as np
 from gt4py.next import Dimension, NeighborTableOffsetProvider
 
 from icon4py.model.common.settings import xp
+from gt4py.next import constructors
 
 
 def neighbortable_offset_provider_for_1d_sparse_fields(
@@ -19,6 +20,7 @@ def neighbortable_offset_provider_for_1d_sparse_fields(
     has_skip_values: bool,
 ):
     table = xp.asarray(np.arange(old_shape[0] * old_shape[1], dtype=gtx.int32).reshape(old_shape))
+    table = gtx.as_field([origin_axis, gtx.Dimension("neigh", kind=gtx.DimensionKind.LOCAL)], table).ndarray
     assert (
         table.dtype == gtx.int32
     ), 'Neighbor table\'s ("{}" to "{}") data type for 1d sparse fields must be gtx.int32. Instead it\'s "{}"'.format(
