188994edbcfc29b1ccad266bdad484b36b5b8a96
Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>
2024-11-01 10:06:10 +0100

    added xp.asarray to fields

--- model/common/src/icon4py/model/common/grid/vertical.py
+++ model/common/src/icon4py/model/common/grid/vertical.py
@@ -131,12 +131,12 @@ class VerticalGrid:
         object.__setattr__(
             self,
             "_start_index_for_moist_physics",
-            self._determine_start_level_of_moist_physics(vct_a_array, self.config.htop_moist_proc),
+            self._determine_start_level_of_moist_physics(xp.asarray(vct_a_array), self.config.htop_moist_proc),
         )
         object.__setattr__(
             self,
             "_end_index_of_flat_layer",
-            self._determine_end_index_of_flat_layers(vct_a_array, self.config.flat_height),
+            self._determine_end_index_of_flat_layers(xp.asarray(vct_a_array), self.config.flat_height),
         )
         log.info(f"computation of moist physics start on layer: {self.kstart_moist}")
         log.info(f"end index of Rayleigh damping layer for w: {self.nrdmax} ")
