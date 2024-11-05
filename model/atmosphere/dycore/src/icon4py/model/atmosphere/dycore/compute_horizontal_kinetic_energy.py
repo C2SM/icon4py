2be4b2bf5354b5cebdc626577b89620171cfc698
Jacopo <jacopo.canton@gmail.com>
2024-11-05 16:24:08 +0100

    add better notes

--- model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/compute_horizontal_kinetic_energy.py
+++ model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/compute_horizontal_kinetic_energy.py
@@ -25,6 +25,8 @@ def _compute_horizontal_kinetic_energy(
     fa.EdgeKField[vpfloat],
 ]:
     """Formerly known as _mo_solve_nonhydro_stencil_37 or _mo_velocity_advection_stencil_05."""
+    # TODO: This stencil doesn't only do what the name implies. It also
+    # assigns to vn_ie_wp and z_vt_ie_vp. These things should be separated.
     vn_ie_wp = vn
     z_vt_ie_vp = vt
     z_kin_hor_e_wp = wpfloat("0.5") * (vn * vn + astype(vt * vt, wpfloat))
