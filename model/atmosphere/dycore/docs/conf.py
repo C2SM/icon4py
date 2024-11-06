55388807a8d73e28953a93635eb7ead9852b92c5
Jacopo <jacopo.canton@gmail.com>
2024-11-06 09:10:38 +0100

    noqa was missing

--- model/atmosphere/dycore/docs/conf.py
+++ model/atmosphere/dycore/docs/conf.py
@@ -7,9 +7,9 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 # fmt: off
+# ruff: noqa
 import os
 import sys
-
 
 # Add the directory containing icon4py_sphinx to sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "_ext")))
