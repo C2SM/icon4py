e07a404cd010d92f31032974dba098ad36753ac8
Jacopo <jacopo.canton@gmail.com>
2024-11-14 16:57:05 +0100

    keep np in tests instead of xp

--- model/common/src/icon4py/model/common/test_utils/reference_funcs.py
+++ model/common/src/icon4py/model/common/test_utils/reference_funcs.py
@@ -7,7 +7,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 from icon4py.model.common import dimension as dims
-from icon4py.model.common.settings import xp
+import numpy as np
 
 
 def enhanced_smagorinski_factor_numpy(factor_in, heigths_in, a_vec):
@@ -19,22 +19,22 @@ def enhanced_smagorinski_factor_numpy(factor_in, heigths_in, a_vec):
     bqdr = (df42 * dz32 - df32 * dz42) / (dz32 * dz42 * (dz42 - dz32))
     aqdr = df32 / dz32 - bqdr * dz32
     zf = 0.5 * (a_vec[:-1] + a_vec[1:])
-    max0 = xp.maximum(0.0, zf - heigths_in[0])
-    dzlin = xp.minimum(heigths_in[1] - heigths_in[0], max0)
-    max1 = xp.maximum(0.0, zf - heigths_in[1])
-    dzqdr = xp.minimum(heigths_in[3] - heigths_in[1], max1)
+    max0 = np.maximum(0.0, zf - heigths_in[0])
+    dzlin = np.minimum(heigths_in[1] - heigths_in[0], max0)
+    max1 = np.maximum(0.0, zf - heigths_in[1])
+    dzqdr = np.minimum(heigths_in[3] - heigths_in[1], max1)
     return factor_in[0] + dzlin * alin + dzqdr * (aqdr + dzqdr * bqdr)
 
 
-def nabla2_on_cell_numpy(grid, psi_c: xp.array, geofac_n2s: xp.array):
+def nabla2_on_cell_numpy(grid, psi_c: np.array, geofac_n2s: np.array):
     c2e2cO = grid.connectivities[dims.C2E2CODim]
-    nabla2_psi_c = xp.sum( xp.where((c2e2cO != -1), psi_c[c2e2cO] * geofac_n2s, 0), axis=1)
+    nabla2_psi_c = np.sum( np.where((c2e2cO != -1), psi_c[c2e2cO] * geofac_n2s, 0), axis=1)
     return nabla2_psi_c
 
-def nabla2_on_cell_k_numpy(grid, psi_c: xp.array, geofac_n2s: xp.array):
+def nabla2_on_cell_k_numpy(grid, psi_c: np.array, geofac_n2s: np.array):
     c2e2cO = grid.connectivities[dims.C2E2CODim]
-    geofac_n2s = xp.expand_dims(geofac_n2s, axis=-1)
-    nabla2_psi_c = xp.sum(
-        xp.where((c2e2cO != -1)[:, :, xp.newaxis], psi_c[c2e2cO] * geofac_n2s, 0), axis=1
+    geofac_n2s = np.enpand_dims(geofac_n2s, axis=-1)
+    nabla2_psi_c = np.sum(
+        np.where((c2e2cO != -1)[:, :, np.newaxis], psi_c[c2e2cO] * geofac_n2s, 0), axis=1
     )
     return nabla2_psi_c
