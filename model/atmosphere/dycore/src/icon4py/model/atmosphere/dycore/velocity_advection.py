9bc8c6ec547af86683951a74a3509566a07a9f08
Jacopo <jacopo.canton@gmail.com>
2024-11-21 15:07:37 +0100

    understanding wcc now

--- model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/velocity_advection.py
+++ model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/velocity_advection.py
@@ -464,14 +464,30 @@ class VelocityAdvection:
             offset_provider=self.grid.offset_providers,
         )
 
-        """
-        z_w_con_c (0:nlev):
-            This is the vertical wind with contravariant correction at half levels (cell center).
-            It is first simply set equal to vertical wind, w, from level 0 to nlev-1.
-            At level nlev, it is first set to zero.
-            From flat_lev+1 to nlev-1, it is subtracted from contravariant correction, which is w_concorr_c computed
-            previously, at half levels at cell center. TODO (Chia Rui): Check why is it subtraction.
-        """
+        # scidoc:
+        # Outputs:
+        #  - z_w_con_c :
+        #     $$
+        #     (\w{\n}{\c}{\k-1/2} - \wcc{\n}{\c}{\k-1/2}) = 
+        #     \begin{cases}
+        #         \w{\n}{\c}{\k-1/2},                        & \k \in [0, \nflatlev+1)     \\
+        #         \w{\n}{\c}{\k-1/2} - \wcc{\n}{\c}{\k-1/2}, & \k \in [\nflatlev+1, \nlev) \\
+        #         0,                                         & \k = \nlev
+        #     \end{cases}
+        #     $$
+        #     Subtract the contravariant correction $\wcc{\n}{\c}{\k\pm1/2}$
+        #     from the vertical wind $\w{\n}{\c}{\k\pm1/2}$ in the
+        #     terrain-following levels. This is done for convevnience as the
+        #     result needs to be interpolated to edge centers and full levels
+        #     for later use.
+        #     The papers do not use a new symbol for this variable, and the code
+        #     ambiguosly mixes the variable names used for
+        #     $\wcc{}{}{}$ and $\w{}{}{} - \wcc{}{}{}$.
+        #     
+        # Inputs:
+        #  - $\w{\n}{\c}{\k\pm1/2}$ : w
+        #  - $\wcc{\n}{\c}{\k\pm1/2}$ : w_concorr_c
+        #
         self._fused_stencils_11_to_13(
             w=prognostic_state.w,
             w_concorr_c=diagnostic_state.w_concorr_c,
@@ -523,13 +539,14 @@ class VelocityAdvection:
         # Outputs:
         #  - z_w_con_c_full :
         #     $$
-        #     \wcc{\n}{\c}{\k} = \frac{1}{2} ( \wcc{\n}{\c}{\k-1/2} + \wcc{\n}{\c}{\k+1/2} ), \quad \k \in [0, \nlev)
+        #     (\w{\n}{\c}{\k} - \wcc{\n}{\c}{\k}) = \frac{1}{2} [ (\w{\n}{\c}{\k-1/2} - \wcc{\n}{\c}{\k-1/2})
+        #                                                     + (\w{\n}{\c}{\k+1/2} - \wcc{\n}{\c}{\k+1/2}) ], \quad \k \in [0, \nlev)
         #     $$
-        #     Compute the vertical wind with contravariant correction at full
-        #     levels by averaging the values at the neighboring half levels.
+        #     Interpolate the vertical wind with contravariant correction at
+        #     full levels by averaging the values at half levels.
         #
         # Inputs:
-        #  - $\wcc{\n}{\c}{\k\pm1/2}$ : z_w_con_c
+        #  - $(\w{\n}{\c}{\k\pm1/2} - \wcc{\n}{\c}{\k\pm1/2})$ : z_w_con_c
         #
         self._interpolate_contravariant_vertical_velocity_to_full_levels(
             z_w_con_c=self.z_w_con_c,
@@ -605,19 +622,19 @@ class VelocityAdvection:
         # Outputs:
         #  - ddt_vn_apc_pc[ntnd] :
         #     $$
-        #     \advvn{\n}{\e}{\k} &&= \pdxn{\kinehori{\n}{}{}} + \vt{\n}{}{} (\vortvert{\n}{}{} + \coriolis{}) + \pdz{\vn{\n}{}{}} \w{\n}{}{}, \quad \k \in [0, \nlev) \\
-        #                        &&= \Gradn_{\offProv{e2c}} \Cgrad \kinehori{\n}{c}{\k} + \kinehori{\n}{\e}{\k} \Gradn_{\offProv{e2c}} \Cgrad\\
-        #                        &&+ \vt{\n}{\e}{\k} (\coriolis{\e} + 1/2 \sum_{\offProv{e2v}} \vortvert{\n}{\v}{\k}) \\
+        #     \advvn{\n}{\e}{\k} &&= \pdxn{\kinehori{}{}{}} + \vt{}{}{} (\vortvert{}{}{} + \coriolis{}) + \pdz{\vn{}{}{}} (\w{}{}{} - \wcc{}{}{}), \quad \k \in [0, \nlev) \\
+        #                        &&= \Gradn_{\offProv{e2c}} \Cgrad \kinehori{\n}{c}{\k} + \kinehori{\n}{\e}{\k} \Gradn_{\offProv{e2c}} \Cgrad \\
+        #                        &&+ \vt{\n}{\e}{\k} (\coriolis{\e} + 1/2 \sum_{\offProv{e2v}} \vortvert{\n}{\v}{\k})                         \\
         #                        &&+ \frac{\vn{\n}{\e}{\k-1/2} - \vn{\n}{\e}{\k+1/2}}{\Dz{k}}
-        #                            \sum_{\offProv{e2c}} \Whor \wcc{\n}{\c}{\k}
+        #                            \sum_{\offProv{e2c}} \Whor (\w{\n}{\c}{\k} - \wcc{\n}{\c}{\k})
         #     $$
-        #     Compute the advective tendency of the normal wind.
+        #     Compute the advective tendency of the normal wind (eq. 13 in
+        #     |ICONdycorePaper|).
         #     The edge-normal derivative of kinetic energy is computed by
         #     combining the first order approximation across adiacent cell
         #     centres (eq. 7 in |BonaventuraRingler2005|) with the edge
-        #     value of the kinetic energy.
-        #     $\vortvert{}{}{}$ is the vorticity and $\wcc{}{}{}$ is the
-        #     vertical wind with contravariant correction.
+        #     value of the kinetic energy (TODO: this needs explaining and a
+        #     reference).
         #
         # Inputs:
         #  - $\Cgrad$ : coeff_gradekin
@@ -627,7 +644,7 @@ class VelocityAdvection:
         #  - $\coriolis{\e}$ : f_e
         #  - $\vortvert{\n}{\v}{\k}$ : zeta
         #  - $\Whor$ : c_lin_e
-        #  - $\wcc{\n}{\c}{\k}$ : z_w_con_c_full
+        #  - $(\w{\n}{\c}{\k} - \wcc{\n}{\c}{\k})$ : z_w_con_c_full
         #  - $\vn{\n}{\e}{\k\pm1/2}$ : vn_ie
         #  - $\Dz{\k}$ : ddqz_z_full_e
         #
