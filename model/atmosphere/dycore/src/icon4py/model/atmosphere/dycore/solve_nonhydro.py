0a6772d80475418f1bb0d8b0b52c05ec89e21b65
Enrique Gonzalez Paredes <enriqueg@cscs.ch>
2024-11-22 09:48:01 +0100

    Enhance diagnostic states swap documentation.

--- model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/solve_nonhydro.py
+++ model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/solve_nonhydro.py
@@ -803,13 +803,16 @@ class SolveNonhydro:
     ) -> Literal[0, 1]:
         """Set time levels of ddt_adv fields for call to velocity_tendencies."""
         if self._config.itime_scheme == TimeSteppingScheme.MOST_EFFICIENT:
-            # Use one of the pairs items for the predictor output (.first) and
-            # the other one for the corrector (.second)
+            # When using `TimeSteppingScheme.MOST_EFFICIENT` (itime_scheme=4 in ICON Fortran)
+            # `ddt_w_adv_pc.first` is not computed at the beginning of each substep but
+            # the value computed in the previous substep is used instead for efficiency
+            # (except, of course, the very first substep of the first time step).
+            # In this case, we use one of the pairs items (.first) for the predictor output
+            # and the other one (.second) for the corrector. 
             if not at_first_substep:
-                # Swap buffers from the previous substep so `.first` contains the output
-                # of the corrector in the previous substep
                 diagnostic_state_nh.ddt_w_adv_pc.swap()
-                diagnostic_state_nh.ddt_vn_apc_pc.swap()
+                # diagnostic_state_nh.ddt_vn_apc_pc does not need to be swapped
+                # because it is always recomputed in the predictor step
             return 1
         else:
             # Use only the first item of the pairs for both predictor and corrector outputs
