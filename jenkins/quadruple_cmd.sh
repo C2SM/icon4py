1a435eceacf721c26e143dd9e87652279d99dc56
Jonas Jucker <jonas.jucker@c2sm.ethz.ch>
2024-11-27 13:03:53 +0100

    check exit codes

--- jenkins/quadruple_cmd.sh
+++ jenkins/quadruple_cmd.sh
@@ -3,10 +3,26 @@
 quadruple_command() {
   cmd=$@
   $cmd &
+  pid1=$!
   $cmd &
+  pid2=$!
   $cmd &
+  pid3=$!
   $cmd &
-  wait
+  pid4=$!
+
+  wait $pid1
+  status1=$?
+  wait $pid2
+  status2=$?
+  wait $pid3
+  status3=$?
+  wait $pid4
+  status4=$?
+
+  if [ $status1 -ne 0 ] || [ $status2 -ne 0 ] || [ $status3 -ne 0 ] || [ $status4 -ne 0 ]; then
+    exit 1
+  fi
 }
 
 quadruple_command "$@"
