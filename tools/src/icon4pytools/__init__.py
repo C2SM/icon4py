0f8639148d0c1b423bb5bffe1af8edb3225a7a4e
Enrique Gonzalez Paredes <enriqueg@cscs.ch>
2024-11-29 15:07:53 +0100

    Update license and versions

--- tools/src/icon4pytools/__init__.py
+++ tools/src/icon4pytools/__init__.py
@@ -12,7 +12,6 @@ from typing import Final
 
 from packaging import version as pkg_version
 
-
 __all__ = [
     "__author__",
     "__copyright__",
@@ -21,11 +20,9 @@ __all__ = [
     "__version_info__",
 ]
 
-
 __author__: Final = "ETH Zurich,  MeteoSwiss and individual contributors"
 __copyright__: Final = "Copyright (c) 2022-2024 ETH Zurich and MeteoSwiss"
 __license__: Final = "BSD-3-Clause"
-
 
 __version__: Final = "0.0.6"
 __version_info__: Final = pkg_version.parse(__version__)
