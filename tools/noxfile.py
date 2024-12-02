e60d7e22274932acc7a301773b33b6c364a837c6
Enrique Gonzalez Paredes <enriqueg@cscs.ch>
2024-12-02 16:42:14 +0100

    WIP common

--- tools/noxfile.py
+++ tools/noxfile.py
@@ -16,8 +16,8 @@ nox.options.default_venv_backend = "uv|virtualenv"
 @nox.parametrize("python", ["3.10", "3.11"])
 def tests(session):
     root_project = nox.project.load_toml("../pyproject.toml")
-    dev_workspace_group = nox.project.dependency_groups(root_project, "test")
-    session.install(*dev_workspace_group)
+    session.install(*nox.project.dependency_groups(root_project, "test"))
+
     session.install(".")
     num_processes = session.env.get("NUM_PROCESSES", "1")
     session.run("pytest", "-v", "-n", num_processes)
