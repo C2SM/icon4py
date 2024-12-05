74ea99c5e782641a95a6137d329d6b05c89c08db
Enrique Gonzalez Paredes <enriqueg@cscs.ch>
2024-12-05 12:00:41 +0100

    WIP Fixes to pyprojects

--- noxfile.py
+++ noxfile.py
@@ -39,51 +39,51 @@ def test_tools(session: nox.Session) -> None:
     session.run("pytest", "-sv", "-n", session.env.get("NUM_PROCESSES", "1"), *session.posargs)
 
 
-@nox.session(python=["3.10", "3.11"])
-@nox.parametrize("package", ["atmosphere.dycore", "atmosphere.advection", "atmosphere.diffusion"])
-def tests_model(session: nox.Session, package: str) -> None:
-    """Run the unit and regular tests for the model."""
-    session_install(session, groups=("test",))
-    session.run(
-        "pytest", "-v", "-m", "not slow_tests", "--cov", "--cov-append", "--benchmark-skip", "-n",  session.env.get("NUM_PROCESSES", "1"),
-        "atmosphere/diffusion/tests/diffusion_stencil_tests", *session.posargs
-    )
-    pytest -v -m "not slow_tests" --cov --cov-append atmosphere/dcore/tests/dycore_stencil_tests --benchmark-skip -n {env:NUM_PROCESSES:1} {posargs}
-    pytest -v -m "not slow_tests" --cov --cov-append atmosphere/advection/tests/advection_stencil_tests --benchmark-skip -n {env:NUM_PROCESSES:1} {posargs}
+# @nox.session(python=["3.10", "3.11"])
+# @nox.parametrize("package", ["atmosphere.dycore", "atmosphere.advection", "atmosphere.diffusion"])
+# def tests_model(session: nox.Session, package: str) -> None:
+#     """Run the unit and regular tests for the model."""
+#     session_install(session, groups=("test",))
+#     session.run(
+#         "pytest", "-v", "-m", "not slow_tests", "--cov", "--cov-append", "--benchmark-skip", "-n",  session.env.get("NUM_PROCESSES", "1"),
+#         "atmosphere/diffusion/tests/diffusion_stencil_tests", *session.posargs
+#     )
+#     pytest -v -m "not slow_tests" --cov --cov-append atmosphere/dcore/tests/dycore_stencil_tests --benchmark-skip -n {env:NUM_PROCESSES:1} {posargs}
+#     pytest -v -m "not slow_tests" --cov --cov-append atmosphere/advection/tests/advection_stencil_tests --benchmark-skip -n {env:NUM_PROCESSES:1} {posargs}
 
 
 
 
 
-@nox.session(python=["3.10", "3.11"])
-@nox.parametrize("package", ["atmosphere.dycore", "atmosphere.advection", "atmosphere.diffusion"])
-def tests_model(session: nox.Session, package: str) -> None:
-    """Run the unit and regular tests for the model."""
-    session_install(session, groups=("test",))
-    session.run(
-        "pytest", "-v", "-m", "not slow_tests", "--cov", "--cov-append", "--benchmark-skip", "-n",  session.env.get("NUM_PROCESSES", "1"),
-        "atmosphere/diffusion/tests/diffusion_stencil_tests", *session.posargs
-    )
-    pytest -v -m "not slow_tests" --cov --cov-append atmosphere/dcore/tests/dycore_stencil_tests --benchmark-skip -n {env:NUM_PROCESSES:1} {posargs}
-    pytest -v -m "not slow_tests" --cov --cov-append atmosphere/advection/tests/advection_stencil_tests --benchmark-skip -n {env:NUM_PROCESSES:1} {posargs}
+# @nox.session(python=["3.10", "3.11"])
+# @nox.parametrize("package", ["atmosphere.dycore", "atmosphere.advection", "atmosphere.diffusion"])
+# def tests_model(session: nox.Session, package: str) -> None:
+#     """Run the unit and regular tests for the model."""
+#     session_install(session, groups=("test",))
+#     session.run(
+#         "pytest", "-v", "-m", "not slow_tests", "--cov", "--cov-append", "--benchmark-skip", "-n",  session.env.get("NUM_PROCESSES", "1"),
+#         "atmosphere/diffusion/tests/diffusion_stencil_tests", *session.posargs
+#     )
+#     pytest -v -m "not slow_tests" --cov --cov-append atmosphere/dcore/tests/dycore_stencil_tests --benchmark-skip -n {env:NUM_PROCESSES:1} {posargs}
+#     pytest -v -m "not slow_tests" --cov --cov-append atmosphere/advection/tests/advection_stencil_tests --benchmark-skip -n {env:NUM_PROCESSES:1} {posargs}
 
 
 
-[testenv:run_stencil_tests]
-commands =
-    pytest -v -m "not slow_tests" --cov --cov-append atmosphere/diffusion/tests/diffusion_stencil_tests --benchmark-skip -n {env:NUM_PROCESSES:1} {posargs}
-    pytest -v -m "not slow_tests" --cov --cov-append atmosphere/dycore/tests/dycore_stencil_tests --benchmark-skip -n {env:NUM_PROCESSES:1} {posargs}
-    pytest -v -m "not slow_tests" --cov --cov-append atmosphere/advection/tests/advection_stencil_tests --benchmark-skip -n {env:NUM_PROCESSES:1} {posargs}
+# [testenv:run_stencil_tests]
+# commands =
+#     pytest -v -m "not slow_tests" --cov --cov-append atmosphere/diffusion/tests/diffusion_stencil_tests --benchmark-skip -n {env:NUM_PROCESSES:1} {posargs}
+#     pytest -v -m "not slow_tests" --cov --cov-append atmosphere/dycore/tests/dycore_stencil_tests --benchmark-skip -n {env:NUM_PROCESSES:1} {posargs}
+#     pytest -v -m "not slow_tests" --cov --cov-append atmosphere/advection/tests/advection_stencil_tests --benchmark-skip -n {env:NUM_PROCESSES:1} {posargs}
 
-[testenv:run_benchmarks]
-commands =
-    pytest -v -m "not slow_tests" atmosphere/diffusion/tests/diffusion_stencil_tests --benchmark-only {posargs}
-    pytest -v -m "not slow_tests" atmosphere/dycore/tests/dycore_stencil_tests --benchmark-only {posargs}
-    pytest -v -m "not slow_tests" atmosphere/advection/tests/advection_stencil_tests --benchmark-only {posargs}
+# [testenv:run_benchmarks]
+# commands =
+#     pytest -v -m "not slow_tests" atmosphere/diffusion/tests/diffusion_stencil_tests --benchmark-only {posargs}
+#     pytest -v -m "not slow_tests" atmosphere/dycore/tests/dycore_stencil_tests --benchmark-only {posargs}
+#     pytest -v -m "not slow_tests" atmosphere/advection/tests/advection_stencil_tests --benchmark-only {posargs}
 
-[testenv:run_model_tests]
-commands =
-    pytest -v -m "not slow_tests" --datatest {posargs}
+# [testenv:run_model_tests]
+# commands =
+#     pytest -v -m "not slow_tests" --datatest {posargs}
 
 
-addopts = ["-p", "icon4py.model.testing.pytest_config"]
+# addopts = ["-p", "icon4py.model.testing.pytest_config"]
