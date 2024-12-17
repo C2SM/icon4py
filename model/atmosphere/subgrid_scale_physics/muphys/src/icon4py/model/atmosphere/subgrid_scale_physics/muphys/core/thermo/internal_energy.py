e31723f5b59472fc84944caf32c1049a134bfd37
Will Sawyer <wsawyer@cscs.ch>
2024-12-17 17:26:38 +0100

    Added internal energy stencils and tests, but both are still failing

--- model/atmosphere/subgrid_scale_physics/muphys/src/icon4py/model/atmosphere/subgrid_scale_physics/muphys/core/thermo/internal_energy.py
+++ model/atmosphere/subgrid_scale_physics/muphys/src/icon4py/model/atmosphere/subgrid_scale_physics/muphys/core/thermo/internal_energy.py
@@ -29,7 +29,7 @@ def _internal_energy(
 
     return rho * dz * ( cv * t - qliq * LVC - qice * LSC )
 
-@gtx.program(grid_type=gtx.GridType.UNSTRUCTURED, backend=backend)
+@gtx.program(grid_type=gtx.GridType.UNSTRUCTURED)
 def internal_energy(
     t:         fa.CellField[ta.wpfloat],             # Temperature
     qv:        fa.CellField[ta.wpfloat],             # Specific mass of vapor
@@ -39,11 +39,10 @@ def internal_energy(
     dz:        fa.CellField[ta.wpfloat],             # Extent of grid cell
     CI:        ta.wpfloat,
     CLW:       ta.wpfloat,
-    CV:        ta.wpfloat,
     CVD:       ta.wpfloat,
     CVV:       ta.wpfloat,
     LSC:       ta.wpfloat,
     LVC:       ta.wpfloat,
     internal_energy: fa.CellField[ta.wpfloat]  # output
 ):
-    _internal_energy( t, qv, qliq, qice, rho, dz, CI, CLW, CV, CVD, CVV, LSC, LVC, out=internal_energy )
+    _internal_energy( t, qv, qliq, qice, rho, dz, CI, CLW, CVD, CVV, LSC, LVC, out=internal_energy )
