042f7dd227f76108db90ad822ac6f17de269e99a
Edoardo Paone <edoardo.paone@cscs.ch>
2025-01-10 14:57:04 +0100

    disable orchestration tests

--- model/atmosphere/diffusion/tests/diffusion_tests/test_diffusion.py
+++ model/atmosphere/diffusion/tests/diffusion_tests/test_diffusion.py
@@ -403,8 +403,11 @@ def test_run_diffusion_single_step(
     backend,
     orchestration,
 ):
-    if orchestration and ("dace" not in backend.name.lower()):
+    if orchestration and not helpers.is_dace(backend):
         pytest.skip(f"running backend = '{backend.name}': orchestration only on dace backends")
+    if orchestration and helpers.is_dace(backend):
+        # TODO(edopao): remove once https://github.com/GridTools/gt4py/pull/1742 is merged
+        pytest.xfail(f"dace orchestration not supported yet in GTIR")
     grid = get_grid_for_experiment(experiment, backend)
     cell_geometry = get_cell_geometry_for_experiment(experiment, backend)
     edge_geometry = get_edge_geometry_for_experiment(experiment, backend)
@@ -639,8 +642,11 @@ def test_run_diffusion_initial_step(
     backend,
     orchestration,
 ):
-    if orchestration and ("dace" not in backend.name.lower()):
+    if orchestration and not helpers.is_dace(backend):
         pytest.skip(f"running backend = '{backend.name}': orchestration only on dace backends")
+    if orchestration and helpers.is_dace(backend):
+        # TODO(edopao): remove once https://github.com/GridTools/gt4py/pull/1742 is merged
+        pytest.xfail(f"dace orchestration not supported yet in GTIR")
     grid = get_grid_for_experiment(experiment, backend)
     cell_geometry = get_cell_geometry_for_experiment(experiment, backend)
     edge_geometry = get_edge_geometry_for_experiment(experiment, backend)
