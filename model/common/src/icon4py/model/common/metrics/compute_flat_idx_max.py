5977587a0ad4c4265469a1118bc221e772baa695
Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>
2025-01-16 13:56:00 +0100

    Update model/common/src/icon4py/model/common/metrics/compute_flat_idx_max.py
    Co-authored-by: Magdalena <luzm@ethz.ch>

--- model/common/src/icon4py/model/common/metrics/compute_flat_idx_max.py
+++ model/common/src/icon4py/model/common/metrics/compute_flat_idx_max.py
@@ -25,7 +25,7 @@ def compute_flat_idx_max(
     z_ifc_e_k_0 = np.roll(z_ifc_e_0, -1, axis=1)
     z_ifc_e_1 = z_ifc[e2c[:, 1]]
     z_ifc_e_k_1 = np.roll(z_ifc_e_1, -1, axis=1)
-    flat_idx = np.zeros_like(z_me)
+    flat_idx = array_ns.zeros_like(z_me)
     for je in range(horizontal_lower, horizontal_upper):
         for jk in range(k_lev.shape[0] - 1):
             if (
