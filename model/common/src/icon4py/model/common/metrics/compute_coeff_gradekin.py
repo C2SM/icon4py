c198b99b1f1f279f195b61b850d85349ab65f42c
Magdalena Luz <luzm@ethz.ch>
2025-01-17 09:13:06 +0100

    fix type annotations, add array namespace interface

--- model/common/src/icon4py/model/common/metrics/compute_coeff_gradekin.py
+++ model/common/src/icon4py/model/common/metrics/compute_coeff_gradekin.py
@@ -5,19 +5,20 @@
 #
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
+from types import ModuleType
 
 import numpy as np
 
-from icon4py.model.common import dimension as dims
 from icon4py.model.common.utils import data_allocation as data_alloc
 
 
 def compute_coeff_gradekin(
-    edge_cell_length: np.array,
-    inv_dual_edge_length: np.array,
-    horizontal_start: float,
-    horizontal_end: float,
-) -> np.array:
+    edge_cell_length: data_alloc.NDArray,
+    inv_dual_edge_length: data_alloc.NDArray,
+    horizontal_start: gtx.int32,
+    horizontal_end: gtx.int32,
+    array_ns: ModuleType = np,
+) -> data_alloc.NDArray:
     """
     Compute coefficients for improved calculation of kinetic energy gradient
 
@@ -27,8 +28,8 @@ def compute_coeff_gradekin(
         horizontal_start: horizontal start index
         horizontal_end: horizontal end index
     """
-    coeff_gradekin_0 = np.zeros_like(inv_dual_edge_length)
-    coeff_gradekin_1 = np.zeros_like(inv_dual_edge_length)
+    coeff_gradekin_0 = array_ns.zeros_like(inv_dual_edge_length)
+    coeff_gradekin_1 = array_ns.zeros_like(inv_dual_edge_length)
     for e in range(horizontal_start, horizontal_end):
         coeff_gradekin_0[e] = (
             edge_cell_length[e, 1] / edge_cell_length[e, 0] * inv_dual_edge_length[e]
@@ -36,5 +37,5 @@ def compute_coeff_gradekin(
         coeff_gradekin_1[e] = (
             edge_cell_length[e, 0] / edge_cell_length[e, 1] * inv_dual_edge_length[e]
         )
-    coeff_gradekin_full = np.column_stack((coeff_gradekin_0, coeff_gradekin_1))
-    return data_alloc.numpy_to_1D_sparse_field(coeff_gradekin_full, dims.ECDim)
+    coeff_gradekin_full = array_ns.column_stack((coeff_gradekin_0, coeff_gradekin_1))
+    return coeff_gradekin_full
