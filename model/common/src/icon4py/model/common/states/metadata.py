90f2bc1c0663fcce4810c51e8df7f10db3b85ccc
Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>
2025-01-22 13:11:10 +0100

    Update model/common/src/icon4py/model/common/states/metadata.py
    Co-authored-by: Magdalena <luzm@ethz.ch>

--- model/common/src/icon4py/model/common/states/metadata.py
+++ model/common/src/icon4py/model/common/states/metadata.py
@@ -124,14 +124,6 @@ attrs: Final[dict[str, model.FieldMetaData]] = {
         icon_var_name="e_refin_ctrl",
         long_name="refinement contorl fields on edges",
     ),
-    "tangent_orientation": dict(
-        standard_name="tangent_orientation",
-        units="",
-        dims=(dims.EdgeDim),
-        dtype=ta.wpfloat,
-        icon_var_name="tangent_orientation",
-        long_name="grid savepoint field",
-    ),
     "inv_primal_edge_length": dict(
         standard_name="inv_primal_edge_length",
         units="",
