ec6d1bbe76c4700dfdabe2de9dae149a774b4b4b
Will Sawyer <wsawyer@cscs.ch>
2025-01-29 16:50:44 +0100

    Started to transition to FrozenNamespace

---
+++ model/atmosphere/subgrid_scale_physics/muphys/src/icon4py/model/atmosphere/subgrid_scale_physics/muphys/core/common/frozen.py
@@ -0,0 +1,81 @@
+from gt4py.eve.utils import FrozenNamespace
+
+g_ct = FrozenNamespace(
+    rho_00 = 1.225,  # reference air density
+    q1 = 8.e-6,
+    qmin = 1.0e-15,  # threshold for computation
+    ams = 0.069,  # Formfactor in the mass-size relation of snow particles
+    bms = 2.0,  # Exponent in the mass-size relation of snow particles
+    v0s = 25.0,  # prefactor in snow fall speed
+    v1s = 0.5,  # Exponent in the terminal velocity for snow
+    m0_ice = 1.0e-12,  # initial crystal mass for cloud ice nucleation
+    ci = 2108.0,  # specific heat of ice
+    tx = 3339.5,
+    tfrz_het1 = 267.15,  # temperature for het. freezing of cloud water with supersat => TMELT - 6.0
+    tfrz_het2 = 248.15,  # temperature for het. freezing of cloud water => TMELT - 25.0
+    tfrz_hom = 236.15,   # temperature for hom. freezing of cloud water => TMELT - 37.0
+    lvc = 3135383.2031928,  # invariant part of vaporization enthalpy => alv - (cpv - clw) * tmelt
+    lsc = 2899657.201,     # invariant part of vaporization enthalpy => als - (cpv - ci) * tmelt
+)
+
+t_d = FrozenNamespace(
+    # Thermodynamic constants for the dry and moist atmosphere
+
+    # Dry air
+    rd = 287.04,      # [J/K/kg] gas constant
+    cpd = 1004.64,    # [J/K/kg] specific heat at constant pressure
+    cvd = 717.60,     # [J/K/kg] specific heat at constant volume => cpd - rd
+    con_m = 1.50E-5,  # [m^2/s]  kinematic viscosity of dry air
+    con_h = 2.20E-5,  # [m^2/s]  scalar conductivity of dry air
+    con0_h = 2.40e-2, # [J/m/s/K] thermal conductivity of dry air
+    eta0d = 1.717e-5, # [N*s/m2] dyn viscosity of dry air at tmelt
+
+    # H2O
+    # gas
+    rv = 461.51,      # [J/K/kg] gas constant for water vapor
+    cpv = 1869.46,    # [J/K/kg] specific heat at constant pressure
+    cvv = 1407.95,    # [J/K/kg] specific heat at constant volume => cpv - rv
+    dv0 = 2.22e-5,    # [m^2/s]  diff coeff of H2O vapor in dry air at tmelt
+    # liquid / water
+    rhoh2o = 1000.0,  # [kg/m3]  density of liquid water
+    # solid / ice
+    rhoice = 916.7,   # [kg/m3]  density of pure ice
+
+    cv_i = 2000.0,
+
+    # phase changes
+    alv = 2.5008e6,   # [J/kg]   latent heat for vaporisation
+    als = 2.8345e6,   # [J/kg]   latent heat for sublimation
+    alf = 333700.0,   # [J/kg]   latent heat for fusion => als - alv              
+    tmelt = 273.15,   # [K]      melting temperature of ice/snow 
+    t3 = 273.16,      # [K]      Triple point of water at 611hPa
+
+    # Auxiliary constants
+    rdv = 0.6219583540985028,                    # [ ] rd / rv
+    vtmpc1 = 0.6078246934225193,                 # [ ] rv / rd - 1.0
+    vtmpc2 = 0.8608257684344642,                 # [ ] cpv / cpd - 1.0
+    rcpv = -0.46260417446749336,                 # [ ] cpd / cpv - 1.0
+    alvdcp = 2489.2498805542286,                 # [K] alv / cpd
+    alsdcp = 2821.408663799968,                  # [K] als / cpd
+    rcpd = 0.000995381430164039,                 # [K*kg/J] 1.0 / cpd
+    rcvd = 0.0013935340022296545,                # [K*kg/J] 1.0 / cvd
+    rcpl = 3.1733,                               # cp_d / cp_l - 1
+    
+    clw = 4192.6641119999995,                    # specific heat capacity of liquid water (rcpl + 1.0) * cpd
+    cv_v = 78.37934216297742,                    # (rcpv + 1.0) * cpd - rv
+)
+
+idx = FrozenNamespace(
+    prefactor_r  = 14.58,
+    exponent_r   =  0.111,
+    offset_r     =  1.0e-12,
+    prefactor_i  =  1.25,
+    exponent_i   =  0.160,
+    offset_i     =  1.0e-12,
+    prefactor_s  = 57.80,
+    exponent_s   =  0.5,
+    offset_s     =  1.0e-12,
+    prefactor_g  = 12.24,
+    exponent_g   =  0.217,
+    offset_g     =  1.0e-08,
+) 
--- model/atmosphere/subgrid_scale_physics/muphys/src/icon4py/model/atmosphere/subgrid_scale_physics/muphys/implementations/graupel.py
+++ model/atmosphere/subgrid_scale_physics/muphys/src/icon4py/model/atmosphere/subgrid_scale_physics/muphys/implementations/graupel.py
@@ -9,6 +9,8 @@ import gt4py.next as gtx
 
 from gt4py.next.ffront.fbuiltins import where, maximum, minimum, power, sqrt
 from icon4py.model.common import field_type_aliases as fa, type_alias as ta
+from icon4py.model.common.dimension import Koff
+from icon4py.model.atmosphere.subgrid_scale_physics.muphys.core.common.frozen import idx, g_ct, t_d
 from icon4py.model.atmosphere.subgrid_scale_physics.muphys.core.properties import _fall_speed, _snow_number, _snow_lambda, _ice_number, _ice_mass, _ice_sticking, _deposition_factor, _deposition_auto_conversion, _ice_deposition_nucleation, _vel_scale_factor_default, _vel_scale_factor_ice, _vel_scale_factor_snow
 from icon4py.model.atmosphere.subgrid_scale_physics.muphys.core.thermo import _qsat_rho, _qsat_ice_rho
 from icon4py.model.atmosphere.subgrid_scale_physics.muphys.core.transitions import _cloud_to_rain, _rain_to_vapor, _cloud_x_ice, _cloud_to_snow, _cloud_to_graupel, _vapor_x_ice, _ice_to_snow, _ice_to_graupel, _rain_to_graupel, _snow_to_graupel, _vapor_x_snow, _vapor_x_graupel, _snow_to_rain, _graupel_to_rain
@@ -44,41 +46,33 @@ def _temperature_update(
     state:     tuple[ta.wpfloat, ta.wpfloat, ta.wpfloat],
     t:         ta.wpfloat,
     t_kp1:     ta.wpfloat,
-    qv_old:    ta.wpfloat,
-    qliq_old:  ta.wpfloat,
-    qice_old:  ta.wpfloat,
-    qr:        ta.wpfloat,
+    ei_old:    ta.wpfloat,
     qv:        ta.wpfloat,
     qliq:      ta.wpfloat,
     qice:      ta.wpfloat,
+    pr:        ta.wpfloat,
     rho:       ta.wpfloat,             # density
     dz:        ta.wpfloat,
     dt:        ta.wpfloat,
-    CI:        ta.wpfloat,
-    CLW:       ta.wpfloat,
-    CVD:       ta.wpfloat,
-    CVV:       ta.wpfloat,
-    LSC:       ta.wpfloat,
-    LVC:       ta.wpfloat,
 ) -> tuple[ta.wpfloat,ta.wpfloat,ta.wpfloat]:
     _, eflx, pflx = state
 
-#    e_int   = internal_energy( t_old, qv_old, qliq_old, qice_old, rho, dz, CI, CLW, CVD, CVV, LSC, LVC ) + eflx
+#    e_int   = internal_energy( t_old, qv_old, qliq_old, qice_old, rho, dz, g_ct.ci, CLW, CVD, CVV, LSC, LVC ) + eflx
 #   inlined in order to avoid scan_operator -> field_operator
-    qtot    = qliq_old + qice_old + qv_old
-    cv      = CVD * ( 1.0 - qtot ) + CVV * qv_old + CLW * qliq_old + CI * qice_old
-    e_int   = rho * dz * ( cv * t - qliq * LVC - qice * LSC ) + eflx
+#    qtot    = qliq_old + qice_old + qv_old
+#    cv      = t_d.cvd * ( 1.0 - qtot ) + t_d.cvv * qv_old + t_d.clw * qliq_old + g_ct.ci * qice_old
+    e_int   = ei_old + eflx
 
-    eflx    = dt * ( qr * ( CLW * t - CVD * t_kp1 - LVC ) + pflx * ( CI * t - CVD * t_kp1 - LSC ) )
+    eflx    = dt * ( pr * ( t_d.clw * t - t_d.cvd * t_kp1 - g_ct.lvc ) + pflx * ( g_ct.ci * t - t_d.cvd * t_kp1 - g_ct.lsc ) )
     e_int   = e_int - eflx
 
-    pflx    = pflx + qr  # pflx[oned_vec_index] = pflx[oned_vec_index] + q[lqr].p[iv];
+    pflx    = pflx + pr  # pflx[oned_vec_index] = pflx[oned_vec_index] + q[lqr].p[iv];
 
 #    t       = T_from_internal_energy( min_k, e_int, qv, qliq, qice, rho, dz, CI, CLW, CVD, CVV, LSC, LVC )
 #   inlined in order to avoid scan_operator -> field_operator
     qtot    = qliq + qice + qv                          # total water specific mass
-    cv      = ( CVD * ( 1.0 - qtot ) + CVV * qv + CLW * qliq + CI * qice ) * rho * dz # Moist isometric specific heat
-    t       = ( e_int + rho * dz * ( qliq * LVC + qice * LSC )) / cv
+    cv      = ( t_d.cvd * ( 1.0 - qtot ) + t_d.cvv * qv + t_d.clw * qliq + g_ct.ci * qice ) * rho * dz # Moist isometric specific heat
+    t       = ( e_int + rho * dz * ( qliq * g_ct.lvc + qice * g_ct.lsc )) / cv
 
     return t, eflx, pflx
 
@@ -289,13 +283,14 @@ def _graupel_loop3_if_lrain(
     QMIN:   ta.wpfloat,
     AMS:    ta.wpfloat,
     TMELT:  ta.wpfloat,
-) -> [fa.CellKField[ta.wpfloat],fa.CellKField[ta.wpfloat]]:
+) -> tuple[fa.CellKField[ta.wpfloat],fa.CellKField[ta.wpfloat]]:
 
     # Store current fields for later temperature update
-    qv_old   = qv
-    t_old    = t
-    qliq_old = qc + qr
-    qice_old = qs + qi + qg
+    qliq    = qc + qr
+    qice    = qs + qi + qg
+    qtot    = qliq + qice + qv
+    cv      = t_d.cvd * ( 1.0 - qtot ) + t_d.cvv * qv + t_d.clw * qliq + g_ct.ci * qice                                    
+    ei_old  = rho * dz * ( cv * t - qliq * g_ct.lvc - qice * g_ct.lsc )
 
     zeta  = dt / (2.0 * dz )
     xrho  = sqrt( RHO_00 / rho )
@@ -307,24 +302,24 @@ def _graupel_loop3_if_lrain(
     vc_g = where( kmin_g, _vel_scale_factor_default( xrho ), 0.0 )
 
     q_kp1    = qr(Koff[1])
-    qr, pr, _ = _precip( prefactor_r, exponent_r, offset_r, zeta, vc_r, qr, q_kp1, rho )
+    qr, pr, _ = _precip( idx.prefactor_r, idx.exponent_r, idx.offset_r, zeta, vc_r, qr, q_kp1, rho )
     q_kp1    = qi(Koff[1])
-    qi, pi, _ = _precip( prefactor_i, exponent_i, offset_i, zeta, vi_r, qi, q_kp1, rho )
+    qi, pi, _ = _precip( idx.prefactor_i, idx.exponent_i, idx.offset_i, zeta, vc_i, qi, q_kp1, rho )
     q_kp1    = qs(Koff[1])
-    qs, ps, _ = _precip( prefactor_s, exponent_s, offset_s, zeta, vc_r, qs, q_kp1, rho )
+    qs, ps, _ = _precip( idx.prefactor_s, idx.exponent_s, idx.offset_s, zeta, vc_r, qs, q_kp1, rho )
     q_kp1    = qg(Koff[1])
-    qg, pg, _ = _precip( prefactor_g, exponent_g, offset_g, zeta, vc_g, qg, q_kp1, rho )
+    qg, pg, _ = _precip( idx.prefactor_g, idx.exponent_g, idx.offset_g, zeta, vc_g, qg, q_kp1, rho )
 
     pflx      = ps + pi + pg   # pflx[oned_vec_index] = q[lqs].p[iv] + q[lqi].p[iv] + q[lqg].p[iv];
 
-    qliq  = qc + qr
-    qice  = qs + qi + qg
-
+    qliq    = qc + qr
+    qice    = qs + qi + qg
+    qtot    = qliq + qice + qv
     t_kp1   = t(Koff[1])
-    t, eflx = _temperature_update( t, t_kp1, qv_old, qliq_old, qice_old, qv, qliq, qice, rho, dz )
+    t, _, _ = _temperature_update( t, t_kp1, ei_old, qv, qliq, qice, pr, rho, dz, dt )
 
     # TODO: here we have to return a single layer for pre_gsp
-    return qr, qi, qs, qg
+    return qr, qi, qs, qg, t
     
 @gtx.field_operator
 def _output_calculation(
