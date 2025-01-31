e039f9c47ae4c7948a5b8146a0a6aeff6e6eaa2c
Edoardo Paone <edoardo.paone@cscs.ch>
2025-01-31 16:49:39 +0100

    Re-enable diffusion tests when orchestration is False

--- model/atmosphere/diffusion/tests/diffusion_tests/test_diffusion.py
+++ model/atmosphere/diffusion/tests/diffusion_tests/test_diffusion.py
@@ -420,8 +420,8 @@ def test_run_diffusion_single_step(
     backend,
     orchestration,
 ):
-    if not orchestration or not helpers.is_dace(backend):
-        pytest.skip("This test is only executed for orchestration on dace backends")
+    if orchestration and not helpers.is_dace(backend):
+        pytest.skip("Orchestration test requires a dace backend.")
     grid = get_grid_for_experiment(experiment, backend)
     cell_geometry = get_cell_geometry_for_experiment(experiment, backend)
     edge_geometry = get_edge_geometry_for_experiment(experiment, backend)
@@ -665,8 +665,8 @@ def test_run_diffusion_initial_step(
     backend,
     orchestration,
 ):
-    if not orchestration or not helpers.is_dace(backend):
-        pytest.skip("This test is only executed for orchestration only on dace backends")
+    if orchestration and not helpers.is_dace(backend):
+        pytest.skip("Orchestration test requires a dace backend.")
     grid = get_grid_for_experiment(experiment, backend)
     cell_geometry = get_cell_geometry_for_experiment(experiment, backend)
     edge_geometry = get_edge_geometry_for_experiment(experiment, backend)
