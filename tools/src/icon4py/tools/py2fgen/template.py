cd34b18239ac5945ff98a8284181f40fbc9f60dc
Hannes Vogt <vogt@hey.com>
2025-02-14 11:53:57 +0100

    this icon4py version works with  diffusion_granule_optional_arrays at commit 7d939db

--- tools/src/icon4py/tools/py2fgen/template.py
+++ tools/src/icon4py/tools/py2fgen/template.py
@@ -235,7 +235,9 @@ from {{ plugin_name }} import ffi
 from gt4py.next.iterator.embedded import np_as_located_field
 from icon4py.tools.py2fgen.settings import config
 from icon4py.tools.py2fgen import wrapper_utils
+from icon4py.tools.py2fgen.wrappers.wrapper_dimension import *
 from icon4py.model.common import dimension as dims
+xp = config.array_ns
 
 # logger setup
 log_format = '%(asctime)s.%(msecs)03d - %(levelname)s - %(message)s'
