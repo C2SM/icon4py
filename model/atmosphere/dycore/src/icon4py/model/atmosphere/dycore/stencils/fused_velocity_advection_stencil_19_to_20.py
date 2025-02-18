472f60ba5ecd2de33c6f8f4e8201747118a50ed4
Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>
2025-02-18 13:26:42 +0100

    small edit for fused 19 to 20

--- model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/stencils/fused_velocity_advection_stencil_19_to_20.py
+++ model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/stencils/fused_velocity_advection_stencil_19_to_20.py
@@ -79,27 +79,28 @@ def _fused_velocity_advection_stencil_19_to_20(
             ddt_vn_apc,
         )
 
-
-
     ddt_vn_apc =  where(
-            (maximum(2, nrdmax - 2) <= k < nlev - 3) & (start_edge_nudging_level_2 <= edge < end_edge_local),
-            _add_extra_diffusion_for_normal_wind_tendency_approaching_cfl(
-                levelmask,
-                c_lin_e,
-                z_w_con_c_full,
-                ddqz_z_full_e,
-                area_edge,
-                tangent_orientation,
-                inv_primal_edge_length,
-                zeta,
-                geofac_grdiv,
-                vn,
-                ddt_vn_apc,
-                cfl_w_limit,
-                scalfac_exdiff,
-                d_time,
-            ),
-            ddt_vn_apc,
+            (maximum(2, nrdmax - 2) <= k < nlev - 3),
+                where((start_edge_nudging_level_2 <= edge < end_edge_local),
+                    _add_extra_diffusion_for_normal_wind_tendency_approaching_cfl(
+                        levelmask,
+                        c_lin_e,
+                        z_w_con_c_full,
+                        ddqz_z_full_e,
+                        area_edge,
+                        tangent_orientation,
+                        inv_primal_edge_length,
+                        zeta,
+                        geofac_grdiv,
+                        vn,
+                        ddt_vn_apc,
+                        cfl_w_limit,
+                        scalfac_exdiff,
+                        d_time,
+                    ),
+                    ddt_vn_apc
+                ),
+            ddt_vn_apc
         )
         # if extra_diffu
         # else ddt_vn_apc
