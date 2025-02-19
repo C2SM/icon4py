d5c309a3bc187e5a64fd92c489b6a78d8d344b94
Magdalena Luz <luzm@ethz.ch>
2025-02-19 15:10:17 +0100

    fix: use array instead of list

--- model/common/tests/metric_tests/test_compute_zdiff_gradp_dsl.py
+++ model/common/tests/metric_tests/test_compute_zdiff_gradp_dsl.py
@@ -23,22 +23,23 @@ from icon4py.model.testing.helpers import (
 
 @pytest.mark.datatest
 @pytest.mark.parametrize("experiment", [dt_utils.REGIONAL_EXPERIMENT, dt_utils.GLOBAL_EXPERIMENT])
-def test_compute_zdiff_gradp_dsl(icon_grid, metrics_savepoint, interpolation_savepoint, backend, experiment):
+def test_compute_zdiff_gradp_dsl(
+    icon_grid, metrics_savepoint, interpolation_savepoint, backend, experiment
+):
     xp = data_alloc.import_array_ns(backend)
     zdiff_gradp_ref = metrics_savepoint.zdiff_gradp()
-    
+
     c_lin_e = interpolation_savepoint.c_lin_e()
     z_ifc = metrics_savepoint.z_ifc()
     z_ifc_ground_level = z_ifc.ndarray[:, icon_grid.num_levels]
     z_mc = metrics_savepoint.z_mc()
-    k_lev = data_alloc.index_field(icon_grid, 
-        dims.KDim, dtype=gtx.int32, backend=backend
+    k_lev = data_alloc.index_field(icon_grid, dims.KDim, dtype=gtx.int32, backend=backend)
+    flat_idx = data_alloc.zero_field(
+        icon_grid, dims.EdgeDim, dims.KDim, dtype=gtx.int32, backend=backend
     )
-    flat_idx = data_alloc.zero_field(icon_grid, dims.EdgeDim, dims.KDim, dtype=gtx.int32, backend=backend)
     edge_domain = h_grid.domain(dims.EdgeDim)
     horizontal_start_edge = icon_grid.start_index(edge_domain(h_grid.Zone.LATERAL_BOUNDARY_LEVEL_2))
     start_nudging = icon_grid.start_index(edge_domain(h_grid.Zone.NUDGING_LEVEL_2))
-
 
     compute_flat_idx.with_backend(backend)(
         z_mc=z_mc,
@@ -49,7 +50,7 @@ def test_compute_zdiff_gradp_dsl(icon_grid, metrics_savepoint, interpolation_sav
         horizontal_start=horizontal_start_edge,
         horizontal_end=icon_grid.num_edges,
         vertical_start=0,
-        vertical_end=icon_grid.num_levels-1,
+        vertical_end=icon_grid.num_levels - 1,
         offset_provider={
             "E2C": icon_grid.get_offset_provider("E2C"),
             "Koff": icon_grid.get_offset_provider("Koff"),
@@ -57,7 +58,6 @@ def test_compute_zdiff_gradp_dsl(icon_grid, metrics_savepoint, interpolation_sav
     )
 
     flat_idx_np = xp.amax(flat_idx.ndarray, axis=1)
-
 
     zdiff_gradp_full_field = compute_zdiff_gradp_dsl(
         e2c=icon_grid.connectivities[dims.E2CDim],
@@ -69,8 +69,12 @@ def test_compute_zdiff_gradp_dsl(icon_grid, metrics_savepoint, interpolation_sav
         nlev=icon_grid.num_levels,
         horizontal_start=horizontal_start_edge,
         horizontal_start_1=start_nudging,
-        array_ns=xp
+        array_ns=xp,
     )
 
-    assert dallclose(data_alloc.as_numpy(zdiff_gradp_full_field),
-                     zdiff_gradp_ref.asnumpy(), atol=1e-10, rtol=1.0e-9)
+    assert dallclose(
+        data_alloc.as_numpy(zdiff_gradp_full_field),
+        zdiff_gradp_ref.asnumpy(),
+        atol=1e-10,
+        rtol=1.0e-9,
+    )
