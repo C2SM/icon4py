afb52622c5159d2bad00fef9b4e4647968086295
Edoardo Paone <edoardo.paone@cscs.ch>
2025-02-25 09:13:36 +0100

    fix previous commit

--- model/atmosphere/advection/src/icon4py/model/atmosphere/advection/stencils/compute_ppm_all_face_values.py
+++ model/atmosphere/advection/src/icon4py/model/atmosphere/advection/stencils/compute_ppm_all_face_values.py
@@ -8,6 +8,7 @@
 
 import gt4py.next as gtx
 from gt4py.next.ffront.experimental import concat_where
+from gt4py.next.ffront.fbuiltins import where
 
 from icon4py.model.atmosphere.advection.stencils.compute_ppm_quadratic_face_values import (
     _compute_ppm_quadratic_face_values,
@@ -31,8 +32,8 @@ def _compute_ppm_all_face_values(
     slevp1: gtx.int32,
     elevp1: gtx.int32,
 ) -> fa.CellKField[ta.wpfloat]:
-    p_face = concat_where(
-        (dims.KDim == slevp1) | (dims.KDim == elev),
+    p_face = where(
+        (k == slevp1) | (k == elev),
         _compute_ppm_quadratic_face_values(p_cc, p_cellhgt_mc_now),
         p_face_in,
     )
