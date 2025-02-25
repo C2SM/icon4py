0a80e76b53bd90a293d4217c919ec86f8154695d
Magdalena Luz <luzm@ethz.ch>
2025-02-25 13:00:33 +0100

    enable c_lin_e

--- model/common/tests/interpolation_tests/test_interpolation_factory.py
+++ model/common/tests/interpolation_tests/test_interpolation_factory.py
@@ -98,7 +98,6 @@ def get_interpolation_factory(
         (dt_utils.R02B04_GLOBAL, dt_utils.GLOBAL_EXPERIMENT, 1e-12),
     ],
 )
-@pytest.mark.cpu_only  # TODO (any): This test does not work on gpu backend because the field operator is run with embedded backend
 @pytest.mark.datatest
 def test_get_geofac_div(interpolation_savepoint, grid_file, experiment, backend, rtol):
     field_ref = interpolation_savepoint.geofac_div()
