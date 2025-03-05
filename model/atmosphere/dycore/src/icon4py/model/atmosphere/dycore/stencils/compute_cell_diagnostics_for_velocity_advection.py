3bfce9563050ff32b7b99836bf227eebdb383635
Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>
2025-03-05 11:23:57 +0100

    Update model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/stencils/compute_cell_diagnostics_for_velocity_advection.py

--- model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/stencils/compute_cell_diagnostics_for_velocity_advection.py
+++ model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/stencils/compute_cell_diagnostics_for_velocity_advection.py
@@ -7,7 +7,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 import gt4py.next as gtx
 from gt4py.next.common import GridType
-from gt4py.next.ffront.decorator import field_operator, program
 from gt4py.next.ffront.fbuiltins import astype, broadcast, where
 
 from icon4py.model.atmosphere.dycore.stencils.correct_contravariant_vertical_velocity import (
