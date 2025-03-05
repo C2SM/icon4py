89a08fd4c1eb763ddc5902586b27611bf13631a6
Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>
2025-03-05 11:24:20 +0100

    Update model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/stencils/compute_cell_diagnostics_for_velocity_advection.py

--- model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/stencils/compute_cell_diagnostics_for_velocity_advection.py
+++ model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/stencils/compute_cell_diagnostics_for_velocity_advection.py
@@ -22,7 +22,6 @@ from icon4py.model.common import dimension as dims, field_type_aliases as fa
 from icon4py.model.common.type_alias import vpfloat, wpfloat
 
 
-# TODO (Chia RUi): Rename and clean up the individual stencils used by the combined stencil
 @gtx.field_operator
 def _compute_horizontal_kinetic_energy_and_khalf_contravariant_correction(
     horizontal_kinetic_energy_at_edge: fa.EdgeKField[vpfloat],
