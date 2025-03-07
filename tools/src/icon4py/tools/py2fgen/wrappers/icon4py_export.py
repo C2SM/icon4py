c7f32e1015f2be318b04e14a03ff122f49e747c0
Hannes Vogt <vogt@hey.com>
2025-03-07 22:47:15 +0100

    missing file

---
+++ tools/src/icon4py/tools/py2fgen/wrappers/icon4py_export.py
@@ -0,0 +1,110 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022-2024, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# Please, refer to the LICENSE file in the root directory.
+# SPDX-License-Identifier: BSD-3-Clause
+
+import dataclasses
+import functools
+import typing
+from collections.abc import Sequence
+from types import NoneType
+from typing import Any, Callable, Optional, Union
+
+import cffi
+from gt4py import next as gtx
+from gt4py.next.type_system import (
+    type_specifications as ts,
+    type_translation as gtx_type_translation,
+)
+
+from icon4py.tools import py2fgen
+from icon4py.tools.py2fgen import _template, wrapper_utils
+
+
+@dataclasses.dataclass
+class FieldDescriptor:
+    dims: list[gtx.Dimension]
+    array_param: _template.ArrayParameter
+
+
+def _parse_type_spec(type_spec: ts.TypeSpec) -> tuple[list[gtx.Dimension], ts.ScalarKind]:
+    if isinstance(type_spec, ts.ScalarType):
+        return [], type_spec.kind
+    elif isinstance(type_spec, ts.FieldType):
+        return type_spec.dims, type_spec.dtype.kind
+    else:
+        raise ValueError(f"Unsupported type specification: {type_spec}")
+
+
+def _is_optional_type_hint(type_hint: Any) -> bool:
+    return typing.get_origin(type_hint) is Union and typing.get_args(type_hint)[1] is NoneType
+
+
+def _unpack_optional_type_hint(type_hint: Any) -> tuple[Any, bool]:
+    if _is_optional_type_hint(type_hint):
+        return typing.get_args(type_hint)[0], True
+    else:
+        return type_hint, False
+
+
+def _get_gt4py_type(type_hint: Any) -> Optional[tuple[ts.TypeSpec, bool]]:
+    non_optional_type, is_optional = _unpack_optional_type_hint(type_hint)
+    try:
+        return gtx_type_translation.from_type_hint(non_optional_type), is_optional
+    except ValueError:
+        return None
+
+
+def field_annotation_descriptor_hook(annotation: Any) -> Optional[py2fgen.ParamDescriptor]:
+    maybe_gt4py_type = _get_gt4py_type(annotation)
+    if maybe_gt4py_type is None:
+        return None
+
+    gt4py_type, is_optional = maybe_gt4py_type
+    dims, dtype = _parse_type_spec(gt4py_type)
+    if len(dims) > 0:
+        return py2fgen.ArrayParamDescriptor(
+            rank=len(dims),
+            dtype=dtype,
+            device=_template.DeviceType.MAYBE_DEVICE,
+            is_optional=is_optional,
+        )
+    else:
+        return py2fgen.ScalarParamDescriptor(dtype=dtype)
+
+
+def _as_field(dims: Sequence[gtx.Dimension], scalar_kind: ts.ScalarKind) -> Callable:
+    @functools.lru_cache(maxsize=None)
+    def impl(
+        array_descriptor: wrapper_utils.ArrayDescriptor, *, ffi: cffi.FFI
+    ) -> Optional[gtx.Field]:
+        domain = {d: s for d, s in zip(dims, array_descriptor.shape, strict=True)}
+        return wrapper_utils.as_field(
+            ffi,
+            array_descriptor.on_gpu,
+            array_descriptor.ptr,
+            scalar_kind,
+            domain,
+            is_optional=False,
+        )
+
+    return impl
+
+
+def field_annotation_mapping_hook(annotation: Any, _: py2fgen.ParamDescriptor) -> Callable:
+    maybe_gt4py_type = _get_gt4py_type(annotation)
+    if maybe_gt4py_type is None:
+        return None
+    else:
+        gt4py_type, is_optional = maybe_gt4py_type
+        dims, dtype = _parse_type_spec(gt4py_type)
+        return _as_field(dims, dtype)
+
+
+export = py2fgen.export(
+    annotation_descriptor_hook=field_annotation_descriptor_hook,
+    annotation_mapping_hook=field_annotation_mapping_hook,
+)
