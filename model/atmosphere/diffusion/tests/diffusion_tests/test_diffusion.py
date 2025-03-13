40b4e97c49a710b617381df6398cec41ee840e6e
Edoardo Paone <edoardo.paone@cscs.ch>
2025-03-13 22:16:10 +0100

    test diffusion orchestration on gpu

--- model/atmosphere/diffusion/tests/diffusion_tests/test_diffusion.py
+++ model/atmosphere/diffusion/tests/diffusion_tests/test_diffusion.py
@@ -429,8 +429,6 @@ def test_run_diffusion_single_step(
 ):
     if orchestration and not helpers.is_dace(backend):
         pytest.skip("Orchestration test requires a dace backend.")
-    if orchestration and data_alloc.is_cupy_device(backend):
-        pytest.xfail("GPU compilation fails.")
     grid = get_grid_for_experiment(experiment, backend)
     cell_geometry = get_cell_geometry_for_experiment(experiment, backend)
     edge_geometry = get_edge_geometry_for_experiment(experiment, backend)
@@ -538,8 +536,6 @@ def test_run_diffusion_multiple_steps(
 ):
     if not helpers.is_dace(backend):
         raise pytest.skip("This test is only executed for dace backends")
-    if data_alloc.is_cupy_device(backend):
-        pytest.xfail("GPU compilation fails.")
     ######################################################################
     # Diffusion initialization
     ######################################################################
@@ -687,8 +683,6 @@ def test_run_diffusion_initial_step(
 ):
     if orchestration and not helpers.is_dace(backend):
         pytest.skip("Orchestration test requires a dace backend.")
-    if orchestration and data_alloc.is_cupy_device(backend):
-        pytest.xfail("GPU compilation fails.")
     grid = get_grid_for_experiment(experiment, backend)
     cell_geometry = get_cell_geometry_for_experiment(experiment, backend)
     edge_geometry = get_edge_geometry_for_experiment(experiment, backend)
