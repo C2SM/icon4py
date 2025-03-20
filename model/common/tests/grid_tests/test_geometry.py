68bf12f94cd1f00f21835f5dbbccd1cb04f6a3bf
Edoardo Paone <edoardo.paone@cscs.ch>
2025-03-20 15:10:05 +0100

    fix rtol values

--- model/common/tests/grid_tests/test_geometry.py
+++ model/common/tests/grid_tests/test_geometry.py
@@ -97,7 +97,7 @@ def test_compute_inverse_edge_length(backend, grid_savepoint, grid_file, experim
 @pytest.mark.parametrize(
     "grid_file, experiment, rtol",
     [
-        (dt_utils.REGIONAL_EXPERIMENT, dt_utils.REGIONAL_EXPERIMENT, 1e-8),
+        (dt_utils.REGIONAL_EXPERIMENT, dt_utils.REGIONAL_EXPERIMENT, 1e-7),
         (dt_utils.R02B04_GLOBAL, dt_utils.GLOBAL_EXPERIMENT, 1e-11),
     ],
 )
