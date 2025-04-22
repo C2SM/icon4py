f0dd28380db26e761f942db2b48c0d29b8d74dd1
Christos Kotsalos <kotsaloscv@gmail.com>
2025-04-22 11:02:52 +0200

    Benchmarking framework

--- model/atmosphere/advection/tests/advection_tests/test_advection.py
+++ model/atmosphere/advection/tests/advection_tests/test_advection.py
@@ -5,14 +5,12 @@
 #
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
-import functools
 
 import pytest
 
 from icon4py.model.atmosphere.advection import advection
 from icon4py.model.common import dimension as dims
 from icon4py.model.common.utils import data_allocation as data_alloc
-from icon4py.model.testing import helpers
 
 from .utils import (
     construct_config,
@@ -96,7 +94,6 @@ def test_advection_run_single_step(
     horizontal_advection_limiter,
     vertical_advection_type,
     vertical_advection_limiter,
-    benchmark,
 ):
     # TODO (Chia Rui): the last datatest fails on GPU (or even CPU) backend when there is no advection because the horizontal flux is not zero. Further check required.
     if (
@@ -106,11 +103,6 @@ def test_advection_run_single_step(
         pytest.xfail(
             "This test is skipped until the cause of nonzero horizontal advection if revealed."
         )
-
-    if ntracer == 1:
-        # Skip benchmarks for this experiment
-        benchmark = None
-
     config = construct_config(
         horizontal_advection_type=horizontal_advection_type,
         horizontal_advection_limiter=horizontal_advection_limiter,
@@ -145,28 +137,24 @@ def test_advection_run_single_step(
 
     log_serialized(diagnostic_state, prep_adv, p_tracer_now, dtime)
 
+    advection_granule.run(
+        diagnostic_state=diagnostic_state,
+        prep_adv=prep_adv,
+        p_tracer_now=p_tracer_now,
+        p_tracer_new=p_tracer_new,
+        dtime=dtime,
+    )
+
     diagnostic_state_ref = construct_diagnostic_exit_state(
         icon_grid, advection_exit_savepoint, ntracer, backend=backend
     )
     p_tracer_new_ref = advection_exit_savepoint.tracer(ntracer)
 
-    helpers.run_verify_and_benchmark(
-        functools.partial(
-            advection_granule.run,
-            diagnostic_state=diagnostic_state,
-            prep_adv=prep_adv,
-            p_tracer_now=p_tracer_now,
-            p_tracer_new=p_tracer_new,
-            dtime=dtime,
-        ),
-        functools.partial(
-            verify_advection_fields,
-            grid=icon_grid,
-            diagnostic_state=diagnostic_state,
-            diagnostic_state_ref=diagnostic_state_ref,
-            p_tracer_new=p_tracer_new,
-            p_tracer_new_ref=p_tracer_new_ref,
-            even_timestep=even_timestep,
-        ),
-        benchmark,
+    verify_advection_fields(
+        grid=icon_grid,
+        diagnostic_state=diagnostic_state,
+        diagnostic_state_ref=diagnostic_state_ref,
+        p_tracer_new=p_tracer_new,
+        p_tracer_new_ref=p_tracer_new_ref,
+        even_timestep=even_timestep,
     )
