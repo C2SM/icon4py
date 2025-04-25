53833819c237dff2f4274471dabae67e42e45401
Jacopo <jacopo.canton@gmail.com>
2025-04-25 17:34:30 +0200

    move after log.debug

--- model/atmosphere/diffusion/src/icon4py/model/atmosphere/diffusion/diffusion.py
+++ model/atmosphere/diffusion/src/icon4py/model/atmosphere/diffusion/diffusion.py
@@ -705,10 +705,10 @@ class Diffusion:
             vertical_end=self._grid.num_levels,
             offset_provider=self._grid.offset_providers,
         )
+        log.debug("running stencil 01 (calculate_nabla2_and_smag_coefficients_for_vn): end")
         #---> IBM
         self._ibm.set_bcs_khsmag(self.kh_smag_e)
         #<--- IBM
-        log.debug("running stencil 01 (calculate_nabla2_and_smag_coefficients_for_vn): end")
         if (
             self.config.shear_type
             >= TurbulenceShearForcingType.VERTICAL_HORIZONTAL_OF_HORIZONTAL_WIND
