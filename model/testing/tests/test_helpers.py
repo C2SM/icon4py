2f97bc1b1cd355733887b113211f3b3550eb855e
Christos Kotsalos <christos.kotsalos@cscs.ch>
2025-04-25 13:47:22 +0200

    Updated Benchmarking Infrastructure (#703)
    Co-authored-by: Enrique González Paredes <enriqueg@cscs.ch>

---
+++ model/testing/tests/test_helpers.py
@@ -0,0 +1,51 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022-2024, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# Please, refer to the LICENSE file in the root directory.
+# SPDX-License-Identifier: BSD-3-Clause
+import functools
+from unittest import mock
+
+import pytest
+
+from icon4py.model.testing import helpers
+
+
+def required_args_func(req_arg):
+    pass
+
+
+@pytest.mark.parametrize("benchmark_enabled", [True, False])
+def test_run_verify_and_benchmark(benchmark_enabled):
+    test_func = mock.Mock(side_effect=required_args_func)
+    verification_func = mock.Mock()
+    benchmark = mock.Mock(enabled=benchmark_enabled)
+
+    helpers.run_verify_and_benchmark(
+        functools.partial(test_func, req_arg=mock.Mock()),
+        verification_func,
+        benchmark_fixture=benchmark,
+    )
+
+    test_func.assert_called_once()
+    verification_func.assert_called_once()
+    if benchmark_enabled:
+        benchmark.assert_called_once()
+    else:
+        benchmark.assert_not_called()
+
+
+def test_run_verify_and_benchmark_no_fixture():
+    test_func = mock.Mock()
+    verification_func = mock.Mock()
+
+    helpers.run_verify_and_benchmark(
+        test_func,
+        verification_func,
+        benchmark_fixture=None,  # No benchmark fixture provided
+    )
+
+    test_func.assert_called_once()
+    verification_func.assert_called_once()
