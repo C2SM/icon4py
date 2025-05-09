0e3743d0b978751c4b3ca8c869116e3e75e28fa7
Jacopo <jacopo.canton@gmail.com>
2025-05-09 16:35:54 +0200

    update submission script

--- submit_gauss.sh
+++ submit_gauss.sh
@@ -1,34 +1,21 @@
 #! /bin/bash
 #=============================================================================
 
-# balfrin cpu batch job parameters
+# santis batch job parameters
 # ------------------------------
 #SBATCH --partition=debug
 #SBATCH --time=0:30:00
 #SBATCH --job-name=torus_test
-#SBATCH --output=logs/torus_%j.o
-#SBATCH --error=logs/torus_%j.o
+#SBATCH --output=logs/torus_log.o
+#SBATCH --error=logs/torus_log.o
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-
-# OpenMP environment variables
-# ----------------------------
-export OMP_NUM_THREADS=1
-
-module use /mch-environment/v5/modules
-module load netcdf-c/4.8.1-nvhpc
-module load hdf5/1.12.2-nvhpc
-module load python/3.10.8
-module load gcc/11.3.0
-module load cuda/11.8.0-nvhpc
-export BOOST_ROOT=/scratch/mch/cong/spack-c2sm/spack/opt/spack/linux-sles15-zen3/gcc-11.3.0/boost-1.83.0-irmtcaa6tdqltmaibi7xectycnhowpex/
+#SBATCH --gpus-per-node=4
+## #SBATCH --ntasks-per-node=1
 
 # Cuda architecture
-export CUDAARCHS="80"
+#export CUDAARCHS="90" # not needed anymore
 
-# uv sync --extra cuda11
-
-# uenv
+# # uenv
 # # Path to your boost installation
 # export BOOST_ROOT=/user-environment/env/icon/include/boost/
 # # Path to your cuda installation
@@ -36,8 +23,18 @@ export CUDAARCHS="80"
 # # Path to shared libraries in user environment
 # export LD_LIBRARY_PATH=/user-environment/env/icon/lib64:$LD_LIBRARY_PATH
 
-rm imgs/*
+# export PATH=$HOME/.local/$(uname -m)/bin:$PATH
+# unset -f uenv
 
-# source .venv/bin/activate
+export PYTHONOPTIMIZE=2
+export GT4PY_UNSTRUCTURED_HORIZONTAL_HAS_UNIT_STRIDE=1
+export GT4PY_BUILD_CACHE_LIFETIME=persistent
+export GT4PY_BUILD_CACHE_DIR=/capstor/scratch/cscs/jcanton/gt4py_cache
 
-srun -n 1 --ntasks-per-node 1 --threads-per-core=1 python model/driver/src/icon4py/model/driver/icon4py_driver.py testdata/ser_icondata/mpitask1/gauss3d_torus/ser_data --icon4py_driver_backend=gtfn_gpu --experiment_type=gauss3d_torus --grid_root=2 --grid_level=0 --enable_output
+source .venv/bin/activate
+
+srun python model/driver/src/icon4py/model/driver/icon4py_driver.py testdata/ser_icondata/mpitask1/gauss3d_torus/ser_data --icon4py_driver_backend=gtfn_gpu --experiment_type=gauss3d_torus --grid_root=2 --grid_level=0 --enable_output
+
+#srun python model/driver/src/icon4py/model/driver/icon4py_driver.py testdata/ser_icondata/mpitask1/jabw_R02B04/ser_data --icon4py_driver_backend=gtfn_gpu --experiment_type=jablonowski_williamson --grid_root=2 --grid_level=0 --enable_output
+
+#srun pytest -sv --backend=gtfn_gpu model/driver/tests/initial_condition_tests/test_jablonowski_williamson.py
