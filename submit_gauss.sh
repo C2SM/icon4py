175740cb679307a6052f5286eae71f91c331642f
Jacopo <jacopo.canton@gmail.com>
2025-05-14 17:24:19 +0200

    add pytest and clean srun

--- submit_gauss.sh
+++ submit_gauss.sh
@@ -20,9 +20,15 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/user-environment/linux-sles15-neoverse_
 
 source venv_uv/bin/activate
 
+#srun \
+#	python model/driver/src/icon4py/model/driver/icon4py_driver.py \
+#	testdata/ser_icondata/mpitask1/gauss3d_torus/ser_data \
+#	--icon4py_driver_backend=gtfn_gpu \
+#	--experiment_type=gauss3d_torus \
+#	--grid_root=2 --grid_level=0 #--enable_output
+
 srun \
-	python model/driver/src/icon4py/model/driver/icon4py_driver.py \
-	testdata/ser_icondata/mpitask1/gauss3d_torus/ser_data \
-	--icon4py_driver_backend=gtfn_gpu \
-	--experiment_type=gauss3d_torus \
-	--grid_root=2 --grid_level=0 #--enable_output
+	pytest -sv \
+	--backend=gtfn_gpu \
+	model/atmosphere/diffusion/tests/diffusion_tests/test_diffusion.py \
+  -k test_verify_diffusion_init_against_savepoint
