aee92fa27d1cb41976d892466f9e1bd220585bd6
Mikael Simberg <mikael.simberg@iki.fi>
2025-05-15 11:03:59 +0200

    Add comment for ignored cupy import warning

--- tools/src/icon4py/tools/py2fgen/_definitions.py
+++ tools/src/icon4py/tools/py2fgen/_definitions.py
@@ -94,7 +94,7 @@ else:
     ArrayInfo: TypeAlias = tuple[Any, tuple[int, ...], bool, bool]
 
 if TYPE_CHECKING:
-    import cupy as cp  # type: ignore[import-not-found]
+    import cupy as cp  # type: ignore[import-not-found] # cupy is not always available for type-checking
 
     NDArray: TypeAlias = cp.ndarray | np.ndarray
 else:
