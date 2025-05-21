865f7f5ffe9a4325dd22e46299fb62a6e21f00c9
Ioannis Magkanaris <ioannis.magkanaris@cscs.ch>
2025-05-21 13:05:34 +0200

    Fix issue with _compute_results_for_thermodynamic_variables

--- model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/stencils/vertically_implicit_dycore_solver.py
+++ model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/stencils/vertically_implicit_dycore_solver.py
@@ -368,22 +368,20 @@ def _vertically_implicit_solver_at_predictor_step_after_solving_w(
         else next_w
     )
 
-    next_rho, next_exner, next_theta_v = (
-        _compute_results_for_thermodynamic_variables(
-            z_rho_expl=rho_explicit_term,
-            vwind_impl_wgt=exner_w_implicit_weight_parameter,
-            inv_ddqz_z_full=inv_ddqz_z_full,
-            rho_ic=rho_at_cells_on_half_levels,
-            w=next_w,
-            z_exner_expl=exner_explicit_term,
-            exner_ref_mc=reference_exner_at_cells_on_model_levels,
-            z_alpha=tridiagonal_alpha_coeff_at_cells_on_half_levels,
-            z_beta=tridiagonal_beta_coeff_at_cells_on_model_levels,
-            rho_now=current_rho,
-            theta_v_now=current_theta_v,
-            exner_now=current_exner,
-            dtime=dtime,
-        ),
+    next_rho, next_exner, next_theta_v = _compute_results_for_thermodynamic_variables(
+        z_rho_expl=rho_explicit_term,
+        vwind_impl_wgt=exner_w_implicit_weight_parameter,
+        inv_ddqz_z_full=inv_ddqz_z_full,
+        rho_ic=rho_at_cells_on_half_levels,
+        w=next_w,
+        z_exner_expl=exner_explicit_term,
+        exner_ref_mc=reference_exner_at_cells_on_model_levels,
+        z_alpha=tridiagonal_alpha_coeff_at_cells_on_half_levels,
+        z_beta=tridiagonal_beta_coeff_at_cells_on_model_levels,
+        rho_now=current_rho,
+        theta_v_now=current_theta_v,
+        exner_now=current_exner,
+        dtime=dtime,
     )
 
     # compute dw/dz for divergence damping term
@@ -643,22 +641,20 @@ def _vertically_implicit_solver_at_corrector_step_after_solving_w(
         else next_w
     )
 
-    next_rho, next_exner, next_theta_v = (
-        _compute_results_for_thermodynamic_variables(
-            z_rho_expl=rho_explicit_term,
-            vwind_impl_wgt=exner_w_implicit_weight_parameter,
-            inv_ddqz_z_full=inv_ddqz_z_full,
-            rho_ic=rho_at_cells_on_half_levels,
-            w=next_w,
-            z_exner_expl=exner_explicit_term,
-            exner_ref_mc=reference_exner_at_cells_on_model_levels,
-            z_alpha=tridiagonal_alpha_coeff_at_cells_on_half_levels,
-            z_beta=tridiagonal_beta_coeff_at_cells_on_model_levels,
-            rho_now=current_rho,
-            theta_v_now=current_theta_v,
-            exner_now=current_exner,
-            dtime=dtime,
-        ),
+    next_rho, next_exner, next_theta_v = _compute_results_for_thermodynamic_variables(
+        z_rho_expl=rho_explicit_term,
+        vwind_impl_wgt=exner_w_implicit_weight_parameter,
+        inv_ddqz_z_full=inv_ddqz_z_full,
+        rho_ic=rho_at_cells_on_half_levels,
+        w=next_w,
+        z_exner_expl=exner_explicit_term,
+        exner_ref_mc=reference_exner_at_cells_on_model_levels,
+        z_alpha=tridiagonal_alpha_coeff_at_cells_on_half_levels,
+        z_beta=tridiagonal_beta_coeff_at_cells_on_model_levels,
+        rho_now=current_rho,
+        theta_v_now=current_theta_v,
+        exner_now=current_exner,
+        dtime=dtime,
     )
 
     (
