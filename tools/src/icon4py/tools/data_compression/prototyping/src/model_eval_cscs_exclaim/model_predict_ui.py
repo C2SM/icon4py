e44189302ccc9ec1a941f30b4fa778c140a06916
Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>
2025-05-30 11:53:42 +0200

    model predictions

---
+++ tools/src/icon4py/tools/data_compression/prototyping/src/model_eval_cscs_exclaim/model_predict_ui.py
@@ -0,0 +1,135 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022-2024, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# Please, refer to the LICENSE file in the root directory.
+# SPDX-License-Identifier: BSD-3-Clause
+
+import streamlit as st
+from data_compression_cscs_exclaim import utils
+from model_eval_cscs_exclaim.cli import calc_dwt_dist
+from numcodecs_combinators.stack import CodecStack
+from numcodecs_wasm_asinh import Asinh
+from numcodecs_wasm_bit_round import BitRound
+from numcodecs_wasm_linear_quantize import LinearQuantize
+from numcodecs_wasm_sz3 import Sz3
+from numcodecs_wasm_zfp import Zfp
+from numcodecs_wasm_zlib import Zlib
+
+
+#  prototyping % streamlit run ./src/model_eval_cscs_exclaim/model_predict_ui.py
+#  prototyping % model_eval_cscs_exclaim models_evaluation netCDF_files/tigge_pl_t_q_dx=2_2024_08_02.nc t parameters.yaml
+
+# Page title
+st.title("Upload a File and Choose a compressor")
+
+# File uploader
+uploaded_file = st.file_uploader("Choose a file")
+
+# Dropdown options
+options = ["t", "q"]
+selected_option_field = st.selectbox("Choose field to compress", options)
+
+options = [
+    "linear_quantization_zlib_compressors",
+    "bitround_zlib_compressors",
+    "zfp_asinh_compressors",
+    "sz3_eb_compressors",
+]
+selected_option_compressor = st.multiselect("Choose compression method(s)", options)
+
+# Submit button
+if st.button("Submit"):
+    if uploaded_file is not None:
+        if len(selected_option_compressor) == 0:
+            st.warning("Please select at least one compressor")
+
+        netcdf_file = uploaded_file
+        field_to_compress = selected_option_field
+
+        ds = utils.open_netcdf(netcdf_file, field_to_compress)
+        ds_coords = dict(ds.coords).keys()
+        lat_key = [key for key in ds_coords if key.startswith("lat")][0]
+        lon_key = [key for key in ds_coords if key.startswith("lon")][0]
+        lat_upper = round(ds[field_to_compress][lat_key].shape[0] * 0.2)
+        lon_upper = round(ds[field_to_compress][lon_key].shape[0] * 0.2)
+        ds = ds.isel(latitude=slice(0, lat_upper), longitude=slice(0, lon_upper))
+        dwt_dists = {}
+
+        # Perform action based on dropdown
+        if "linear_quantization_zlib_compressors" in selected_option_compressor:
+            linear_quantization_bits, zlib_level = utils.get_filter_parameters(
+                "parameters.yaml", "linear_quantization_zlib_compressors"
+            )
+            ds_linquant = {}
+            linquant_compressor = CodecStack(
+                LinearQuantize(
+                    bits=linear_quantization_bits, dtype=str(ds[field_to_compress].dtype)
+                ),
+                Zlib(level=zlib_level),
+            )
+            ds_linquant[field_to_compress] = linquant_compressor.encode_decode_data_array(
+                ds[field_to_compress]
+            ).compute()
+            lquant_dwt_distances = calc_dwt_dist(
+                ds_linquant[field_to_compress], ds[field_to_compress], n_levels=4
+            )
+            dwt_dists["lquant_dwt_distances"] = lquant_dwt_distances
+            st.write("Linear Quantization DWT disctance: " + f"{lquant_dwt_distances}")
+
+        if "bitround_zlib_compressors" in selected_option_compressor:
+            bitround_bits, zlib_level = utils.get_filter_parameters(
+                "parameters.yaml", "bitround_zlib_compressors"
+            )
+            ds_bitround = {}
+            bitround_compressor = CodecStack(
+                BitRound(keepbits=bitround_bits),
+                Zlib(level=zlib_level),
+            )
+            ds_bitround[field_to_compress] = bitround_compressor.encode_decode_data_array(
+                ds[field_to_compress]
+            ).compute()
+            bitround_dwt_distances = calc_dwt_dist(
+                ds_bitround[field_to_compress], ds[field_to_compress], n_levels=4
+            )
+            dwt_dists["bitround_dwt_distances"] = bitround_dwt_distances
+            st.write("Bit Round DWT disctance: " + f"{bitround_dwt_distances}")
+
+        if "zfp_asinh_compressors" in selected_option_compressor:
+            asinh_linear_width, zfp_mode, zfp_tolerance = utils.get_filter_parameters(
+                "parameters.yaml", "zfp_asinh_compressors"
+            )
+            ds_zfp = {}
+            zfp_compressor = CodecStack(
+                Asinh(linear_width=asinh_linear_width),
+                Zfp(mode=zfp_mode, tolerance=zfp_tolerance),
+            )
+            ds_zfp[field_to_compress] = zfp_compressor.encode_decode_data_array(
+                ds[field_to_compress]
+            ).compute()
+            zfp_dwt_distances = calc_dwt_dist(
+                ds_zfp[field_to_compress], ds[field_to_compress], n_levels=4
+            )
+            dwt_dists["zfp_dwt_distances"] = zfp_dwt_distances
+            st.write("Zfp DWT disctance: " + f"{zfp_dwt_distances}")
+
+        if "sz3_eb_compressors" in selected_option_compressor:
+            sz3_eb_mode, sz3_eb_rel = utils.get_filter_parameters(
+                "parameters.yaml", "sz3_eb_compressors"
+            )
+            ds_sz3 = {}
+            sz3_compressor = CodecStack(Sz3(eb_mode=sz3_eb_mode, eb_rel=sz3_eb_rel))
+            ds_sz3[field_to_compress] = sz3_compressor.encode_decode_data_array(
+                ds[field_to_compress]
+            ).compute()
+            sz3_dwt_distances = calc_dwt_dist(
+                ds_sz3[field_to_compress], ds[field_to_compress], n_levels=4
+            )
+            dwt_dists["sz3_eb_compressors"] = sz3_dwt_distances
+            st.write("SZ3 DWT disctance: " + f"{sz3_dwt_distances}")
+
+        min_key = min(dwt_dists, key=dwt_dists.get)
+        st.write("Best compression method: " + f"{min_key}")
+    else:
+        st.warning("Please upload a file before submitting.")
