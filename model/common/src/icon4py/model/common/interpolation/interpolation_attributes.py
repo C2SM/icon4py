faf55e3322c4d18537d9a28c06c0903a8d5032aa
Mikael Simberg <mikael.simberg@iki.fi>
2025-06-05 17:22:50 +0200

    Remove naming todos for rbf coefficient fields

--- model/common/src/icon4py/model/common/interpolation/interpolation_attributes.py
+++ model/common/src/icon4py/model/common/interpolation/interpolation_attributes.py
@@ -25,11 +25,11 @@ E_FLX_AVG: Final[str] = "e_flux_average"
 POS_ON_TPLANE_E_X: Final[str] = "pos_on_tplane_e_x"
 POS_ON_TPLANE_E_Y: Final[str] = "pos_on_tplane_e_y"
 CELL_AW_VERTS: Final[str] = "cell_to_vertex_interpolation_factor_by_area_weighting"
-RBF_VEC_COEFF_C1: Final[str] = "rbf_interpolation_coefficient_cell_1"  # TODO: long name?
-RBF_VEC_COEFF_C2: Final[str] = "rbf_interpolation_coefficient_cell_2"  # TODO: long name?
-RBF_VEC_COEFF_E: Final[str] = "rbf_interpolation_coefficient_edge"  # TODO: long name?
-RBF_VEC_COEFF_V1: Final[str] = "rbf_interpolation_coefficient_vertex_1"  # TODO: long name?
-RBF_VEC_COEFF_V2: Final[str] = "rbf_interpolation_coefficient_vertex_2"  # TODO: long name?
+RBF_VEC_COEFF_C1: Final[str] = "rbf_interpolation_coefficient_cell_1"
+RBF_VEC_COEFF_C2: Final[str] = "rbf_interpolation_coefficient_cell_2"
+RBF_VEC_COEFF_E: Final[str] = "rbf_interpolation_coefficient_edge"
+RBF_VEC_COEFF_V1: Final[str] = "rbf_interpolation_coefficient_vertex_1"
+RBF_VEC_COEFF_V2: Final[str] = "rbf_interpolation_coefficient_vertex_2"
 
 attrs: dict[str, model.FieldMetaData] = {
     C_LIN_E: dict(
@@ -138,7 +138,7 @@ attrs: dict[str, model.FieldMetaData] = {
     ),
     RBF_VEC_COEFF_C1: dict(
         standard_name=RBF_VEC_COEFF_C1,
-        long_name="rbf interpolation coefficient from cells to edges 1",  # TODO: name
+        long_name="rbf interpolation coefficient from cells to edges 1",
         units="",
         dims=(dims.CellDim, dims.C2E2C2EDim),
         icon_var_name="rbf_vec_coeff_c1",
@@ -146,7 +146,7 @@ attrs: dict[str, model.FieldMetaData] = {
     ),
     RBF_VEC_COEFF_C2: dict(
         standard_name=RBF_VEC_COEFF_C2,
-        long_name="rbf interpolation coefficient from cells to edges 2",  # TODO: name
+        long_name="rbf interpolation coefficient from cells to edges 2",
         units="",
         dims=(dims.CellDim, dims.C2E2C2EDim),
         icon_var_name="rbf_vec_coeff_c2",
@@ -154,7 +154,7 @@ attrs: dict[str, model.FieldMetaData] = {
     ),
     RBF_VEC_COEFF_E: dict(
         standard_name=RBF_VEC_COEFF_E,
-        long_name="rbf interpolation coefficient from edges to edges",  # TODO: name
+        long_name="rbf interpolation coefficient from edges to edges",
         units="",
         dims=(dims.EdgeDim, dims.E2C2EDim),
         icon_var_name="rbf_vec_coeff_e",
@@ -162,7 +162,7 @@ attrs: dict[str, model.FieldMetaData] = {
     ),
     RBF_VEC_COEFF_V1: dict(
         standard_name=RBF_VEC_COEFF_V1,
-        long_name="rbf interpolation coefficient from vertices to edges 1",  # TODO: name
+        long_name="rbf interpolation coefficient from vertices to edges 1",
         units="",
         dims=(dims.VertexDim, dims.V2EDim),
         icon_var_name="rbf_vec_coeff_v1",
@@ -170,7 +170,7 @@ attrs: dict[str, model.FieldMetaData] = {
     ),
     RBF_VEC_COEFF_V2: dict(
         standard_name=RBF_VEC_COEFF_V2,
-        long_name="rbf interpolation coefficient from vertices to edges 2",  # TODO: name
+        long_name="rbf interpolation coefficient from vertices to edges 2",
         units="",
         dims=(dims.VertexDim, dims.V2EDim),
         icon_var_name="rbf_vec_coeff_v2",
