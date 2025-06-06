0954843931b315a35d3eef76b16bfc6659121697
Magdalena Luz <luzm@ethz.ch>
2025-06-06 14:19:57 +0200

    fix test: array_ns

--- model/common/tests/grid_tests/test_base.py
+++ model/common/tests/grid_tests/test_base.py
@@ -6,7 +6,7 @@
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
 
-import numpy as np
+import gt4py.next as gtx
 
 from icon4py.model.common import dimension as dims
 from icon4py.model.common.grid import base, gridfile, simple
@@ -16,13 +16,12 @@ from icon4py.model.common.utils import data_allocation as data_alloc
 def test_replace_skip_values(grid_file, caplog, backend):
     grid = simple.SimpleGrid()
     domain = (dims.CellDim, dims.C2E2CDim)
+    xp = data_alloc.array_ns(backend)
     neighbor_table = data_alloc.random_field(
-        grid, *domain, low=0, high=grid.num_cells, dtype=np.int32
+        grid, *domain, low=0, high=grid.num_cells, dtype=gtx.int32, backend=backend
     )
     neighbor_table[0, 1:] = gridfile.GridFile.INVALID_INDEX
 
-    assert np.any(neighbor_table == gridfile.GridFile.INVALID_INDEX)
-    neighbor_table = base.replace_skip_values(
-        domain, neighbor_table.asnumpy(), array_ns=data_alloc.array_ns(backend)
-    )
-    assert not np.any(neighbor_table == gridfile.GridFile.INVALID_INDEX)
+    assert xp.any(neighbor_table == gridfile.GridFile.INVALID_INDEX)
+    neighbor_table = base.replace_skip_values(domain, neighbor_table.asnumpy(), array_ns=xp)
+    assert not xp.any(neighbor_table == gridfile.GridFile.INVALID_INDEX)
