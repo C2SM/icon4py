dc4fa7b863c1a4b5b0304f30d5329315d085ca95
Mikael Simberg <mikael.simberg@iki.fi>
2025-06-12 12:16:16 +0200

    Move mean_cell_area test

--- model/common/tests/grid_tests/test_icon.py
+++ model/common/tests/grid_tests/test_icon.py
@@ -213,3 +213,28 @@ def test_when_replace_skip_values_then_only_pentagon_points_remain(grid_file, di
             not np.any(connectivity.asnumpy() == gridfile.GridFile.INVALID_INDEX).item()
         ), f"Connectivity {dim.value} for {grid_file} contains skip values, but none are expected."
         assert connectivity.skip_value is None
+
+
+@pytest.mark.parametrize(
+    "grid_root,grid_level,num_cells,mean_cell_area,expected",
+    [
+        (2, 4, None, None, 24907282236.708576),
+        (4, 9, None, None, 6080879.45232143),
+        (2, 4, 42, 123.456, 123.456),
+        (4, 9, None, 123.456, 123.456),
+    ],
+)
+def test_mean_cell_area_calculation(grid_root, grid_level, num_cells, mean_cell_area, expected):
+    params = icon.GlobalGridParams(grid_root, grid_level, num_cells, mean_cell_area)
+    assert expected == params.mean_cell_area
+
+
+@pytest.mark.parametrize(
+    "num_cells,mean_cell_area",
+    [
+        (42, 123.456),
+    ],
+)
+def test_mean_cell_area_from_cells(num_cells, mean_cell_area):
+    params = icon.GlobalGridParams.from_cells(num_cells, mean_cell_area)
+    assert mean_cell_area == params.mean_cell_area
