08f4f054c96a40e4e3a2e0de8f877c80b23f762f
Hannes Vogt <vogt@hey.com>
2025-06-12 13:06:31 +0200

    refactor horizontal_pressure_gradient

--- model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/stencils/compute_edge_diagnostics_for_dycore_and_update_vn.py
+++ model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/stencils/compute_edge_diagnostics_for_dycore_and_update_vn.py
@@ -68,6 +68,71 @@ divergence_damp_order: Final = dycore_states.DivergenceDampingOrder()
 dycore_consts: Final = constants.PhysicsConstants()
 
 
+# TODO better name
+@gtx.field_operator
+def level_switch(
+    nflatlev: gtx.int32,
+    nflat_gradp: gtx.int32,
+    on_flatlevels: fa.EdgeKField[ta.wpfloat],
+    between_flat_and_flatgradp: fa.EdgeKField[ta.wpfloat],
+    below_flatgradp: fa.EdgeKField[ta.wpfloat],
+) -> fa.EdgeKField[ta.wpfloat]:
+    concat_where(
+        dims.KDim < nflatlev,
+        on_flatlevels,
+        concat_where(nflat_gradp + 1 <= dims.KDIM, below_flatgradp, between_flat_and_flatgradp),
+    )
+
+
+@gtx.field_operator
+def _compute_horizontal_pressure_gradient(
+    temporal_extrapolation_of_perturbed_exner: fa.CellKField[ta.vpfloat],
+    ddz_of_temporal_extrapolation_of_perturbed_exner_on_model_levels: fa.CellKField[ta.vpfloat],
+    d2dz2_of_temporal_extrapolation_of_perturbed_exner_on_model_levels: fa.CellKField[ta.vpfloat],
+    hydrostatic_correction_on_lowest_level: fa.EdgeField[ta.wpfloat],
+    ddxn_z_full: fa.EdgeKField[ta.vpfloat],
+    c_lin_e: gtx.Field[[dims.EdgeDim, dims.E2CDim], ta.wpfloat],
+    ikoffset: gtx.Field[[dims.ECDim, dims.KDim], gtx.int32],
+    zdiff_gradp: gtx.Field[[dims.ECDim, dims.KDim], ta.vpfloat],
+    ipeidx_dsl: fa.EdgeKField[bool],
+    pg_exdist: fa.EdgeKField[ta.vpfloat],
+    inv_dual_edge_length: fa.EdgeField[ta.wpfloat],
+    nflatlev: gtx.int32,
+    nflat_gradp: gtx.int32,
+) -> fa.EdgeKField[ta.wpfloat]:
+    # Note: we only support `TAYLOR_HYDRO`
+    horizontal_pressure_gradient = level_switch(
+        nflatlev,
+        nflat_gradp,
+        _compute_horizontal_gradient_of_exner_pressure_for_flat_coordinates(
+            inv_dual_edge_length=inv_dual_edge_length,
+            z_exner_ex_pr=temporal_extrapolation_of_perturbed_exner,
+        ),
+        _compute_horizontal_gradient_of_exner_pressure_for_nonflat_coordinates(
+            inv_dual_edge_length=inv_dual_edge_length,
+            z_exner_ex_pr=temporal_extrapolation_of_perturbed_exner,
+            ddxn_z_full=ddxn_z_full,
+            c_lin_e=c_lin_e,
+            z_dexner_dz_c_1=ddz_of_temporal_extrapolation_of_perturbed_exner_on_model_levels,
+        ),
+        _compute_horizontal_gradient_of_exner_pressure_for_multiple_levels(
+            inv_dual_edge_length=inv_dual_edge_length,
+            z_exner_ex_pr=temporal_extrapolation_of_perturbed_exner,
+            zdiff_gradp=zdiff_gradp,
+            ikoffset=ikoffset,
+            z_dexner_dz_c_1=ddz_of_temporal_extrapolation_of_perturbed_exner_on_model_levels,
+            z_dexner_dz_c_2=d2dz2_of_temporal_extrapolation_of_perturbed_exner_on_model_levels,
+        ),
+    )
+
+    return _apply_hydrostatic_correction_to_horizontal_gradient_of_exner_pressure(
+        ipeidx_dsl=ipeidx_dsl,
+        pg_exdist=pg_exdist,
+        z_hydro_corr=hydrostatic_correction_on_lowest_level,
+        z_gradh_exner=horizontal_pressure_gradient,
+    )
+
+
 @gtx.field_operator
 def _compute_theta_rho_face_values_and_pressure_gradient_and_update_vn(
     next_vn: fa.EdgeKField[ta.wpfloat],
@@ -163,64 +228,21 @@ def _compute_theta_rho_face_values_and_pressure_gradient_and_update_vn(
         ),
     )
 
-    # we are overcomputing `horizontal_pressure_gradient` here (it is only needed < end_edge_local)
-    # TODO(havogt): if we never consume `horizontal_pressure_gradient` in the lateral boundary region,
-    # we could further simplify this expression (i.e. remove the horizontal `concat_where` completely).
-    horizontal_pressure_gradient = concat_where(
-        dims.KDim < nflatlev,
-        _compute_horizontal_gradient_of_exner_pressure_for_flat_coordinates(
-            inv_dual_edge_length=inv_dual_edge_length,
-            z_exner_ex_pr=temporal_extrapolation_of_perturbed_exner,
-        ),
-        # horizontal_pressure_gradient,
-        broadcast(wpfloat("0.0"), (dims.EdgeDim, dims.KDim)),  # this will go away with a k switch
+    horizontal_pressure_gradient = _compute_horizontal_pressure_gradient(
+        temporal_extrapolation_of_perturbed_exner=temporal_extrapolation_of_perturbed_exner,
+        ddz_of_temporal_extrapolation_of_perturbed_exner_on_model_levels=ddz_of_temporal_extrapolation_of_perturbed_exner_on_model_levels,
+        d2dz2_of_temporal_extrapolation_of_perturbed_exner_on_model_levels=d2dz2_of_temporal_extrapolation_of_perturbed_exner_on_model_levels,
+        hydrostatic_correction_on_lowest_level=hydrostatic_correction_on_lowest_level,
+        ddxn_z_full=ddxn_z_full,
+        c_lin_e=c_lin_e,
+        ikoffset=ikoffset,
+        zdiff_gradp=zdiff_gradp,
+        ipeidx_dsl=ipeidx_dsl,
+        pg_exdist=pg_exdist,
+        inv_dual_edge_length=inv_dual_edge_length,
+        nflatlev=nflatlev,
+        nflat_gradp=nflat_gradp,
     )
-
-    if igradp_method == horzpres_discr_type.TAYLOR_HYDRO:
-        if nflatlev < (nflat_gradp + 1):  # TODO check this condition
-            horizontal_pressure_gradient = concat_where(
-                (nflatlev <= dims.KDim) & (dims.KDim < (nflat_gradp + 1)),
-                concat_where(
-                    (start_edge_nudging_level_2 <= dims.EdgeDim),
-                    _compute_horizontal_gradient_of_exner_pressure_for_nonflat_coordinates(
-                        inv_dual_edge_length=inv_dual_edge_length,
-                        z_exner_ex_pr=temporal_extrapolation_of_perturbed_exner,
-                        ddxn_z_full=ddxn_z_full,
-                        c_lin_e=c_lin_e,
-                        z_dexner_dz_c_1=ddz_of_temporal_extrapolation_of_perturbed_exner_on_model_levels,
-                    ),
-                    horizontal_pressure_gradient,
-                ),
-                horizontal_pressure_gradient,
-            )
-
-        horizontal_pressure_gradient = concat_where(
-            (nflat_gradp + 1) <= dims.KDim,
-            concat_where(
-                (start_edge_nudging_level_2 <= dims.EdgeDim),
-                _compute_horizontal_gradient_of_exner_pressure_for_multiple_levels(
-                    inv_dual_edge_length=inv_dual_edge_length,
-                    z_exner_ex_pr=temporal_extrapolation_of_perturbed_exner,
-                    zdiff_gradp=zdiff_gradp,
-                    ikoffset=ikoffset,
-                    z_dexner_dz_c_1=ddz_of_temporal_extrapolation_of_perturbed_exner_on_model_levels,
-                    z_dexner_dz_c_2=d2dz2_of_temporal_extrapolation_of_perturbed_exner_on_model_levels,
-                ),
-                horizontal_pressure_gradient,
-            ),
-            horizontal_pressure_gradient,
-        )
-
-        horizontal_pressure_gradient = concat_where(
-            (start_edge_nudging_level_2 <= dims.EdgeDim),
-            _apply_hydrostatic_correction_to_horizontal_gradient_of_exner_pressure(
-                ipeidx_dsl=ipeidx_dsl,
-                pg_exdist=pg_exdist,
-                z_hydro_corr=hydrostatic_correction_on_lowest_level,
-                z_gradh_exner=horizontal_pressure_gradient,
-            ),
-            horizontal_pressure_gradient,
-        )
 
     # we are overcomputing `next_vn` here (it is only needed < end_edge_local)
     next_vn = concat_where(
