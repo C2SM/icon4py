c646f5d6004985660e1e2e63856410510df32c9d
Jacopo <jacopo.canton@gmail.com>
2025-06-13 10:31:36 +0200

    remove run gauss

--- run_gauss.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm imgs/*
-python model/driver/src/icon4py/model/driver/icon4py_driver.py testdata/ser_icondata/mpitask1/gauss3d_torus/ser_data --icon4py_driver_backend=gtfn_cpu --experiment_type=gauss3d_torus --grid_root=2 --grid_level=0 --enable_output
