517366cf1d6494100941b0d7e2b44e3edd61cc65
Edoardo Paone <edoardo.paone@cscs.ch>
2025-06-14 14:59:08 +0200

    undo extra changes

--- model/common/src/icon4py/model/common/model_backends.py
+++ model/common/src/icon4py/model/common/model_backends.py
@@ -11,8 +11,6 @@ from typing import Final
 import gt4py.next.backend as gtx_backend
 from gt4py.next import gtfn_cpu, gtfn_gpu, itir_python
 
-from icon4py.model.common import dimension as dims
-
 
 DEFAULT_BACKEND: Final = "embedded"
 
@@ -23,32 +21,20 @@ BACKENDS: dict[str, gtx_backend.Backend | None] = {
     "gtfn_gpu": gtfn_gpu,
 }
 
-
 try:
-    from gt4py.next.program_processors.runners.dace import make_dace_backend
-
-    # In icon4py we want to make an asynchronous SDFG call on gpu to allow overlapping
-    #   of gpu kernel execution with the Python driver code (same behavior as in GTFN).
-    # Besides, it is safe to assume that the field layout does not change
-    #   between multiple calls to a gt4py program, therefore we can make temporary
-    #   arrays persistent (thus, allocated at SDFG initialization) and we do not
-    #   need to update the array shape and strides on each SDFG call.
-    # We also enable loop-blocking on the vertical dimension for gpu target.
-    def make_custom_dace_backend(gpu: bool) -> gtx_backend.Backend:
-        return make_dace_backend(
-            auto_optimize=True,
-            cached=True,
-            gpu=gpu,
-            async_sdfg_call=True,
-            make_persistent=True,
-            blocking_dim=(dims.KDim if gpu else None),
-            blocking_size=10,
-        )
+    from gt4py.next.program_processors.runners.dace import (
+        run_dace_cpu_cached as run_dace_cpu,
+        run_dace_cpu_noopt,
+        run_dace_gpu_cached as run_dace_gpu,
+        run_dace_gpu_noopt,
+    )
 
     BACKENDS.update(
         {
-            "dace_cpu": make_custom_dace_backend(gpu=False),
-            "dace_gpu": make_custom_dace_backend(gpu=True),
+            "dace_cpu": run_dace_cpu,
+            "dace_gpu": run_dace_gpu,
+            "dace_cpu_noopt": run_dace_cpu_noopt,
+            "dace_gpu_noopt": run_dace_gpu_noopt,
         }
     )
 
