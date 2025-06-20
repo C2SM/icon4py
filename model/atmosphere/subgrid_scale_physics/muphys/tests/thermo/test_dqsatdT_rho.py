a6e6b80d19f855982022484e827ad4b15f53470f
Will Sawyer <vectorflux@gmail.com>
2025-06-20 10:26:28 +0200

    Update model/atmosphere/subgrid_scale_physics/muphys/tests/thermo/test_dqsatdT_rho.py
    Zero fields should be zeroed out like this.
    
    Co-authored-by: Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>

--- model/atmosphere/subgrid_scale_physics/muphys/tests/thermo/test_dqsatdT_rho.py
+++ model/atmosphere/subgrid_scale_physics/muphys/tests/thermo/test_dqsatdT_rho.py
@@ -28,5 +28,5 @@ class TestQsatRho(StencilTest):
         return dict(
             qs               = data_alloc.constant_field(grid, 0.00448941, dims.CellDim, dims.KDim, dtype=wpfloat),
             t                = data_alloc.constant_field(grid, 273.909, dims.CellDim, dims.KDim, dtype=wpfloat),
-            derivative       = data_alloc.constant_field(grid, 0., dims.CellDim, dims.KDim, dtype=wpfloat)
+            derivative       = data_alloc.zero_field(grid, dims.CellDim, dims.KDim, dtype=wpfloat)
         )
