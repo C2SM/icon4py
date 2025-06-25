5ee90fd1e5ffd77c6af2ca7a75854e355f3f3a95
Magdalena Luz <luzm@ethz.ch>
2025-06-25 16:16:22 +0200

    switch to using typer commands instead of option

--- .github/validation_grid_utils.py
+++ .github/validation_grid_utils.py
@@ -12,7 +12,6 @@
 
 
 import typer
-import enum
 import sys
 
 from icon4py.model.testing import grid_utils
@@ -21,48 +20,22 @@ import hashlib
 
 VALIDATION_GRIDS = (definitions.Grids.R02B04_GLOBAL, definitions.Grids.MCH_CH_R04B09_DSL, definitions.Grids.MCH_OPR_R04B07_DOMAIN01) # change to MCH_OPR_R04B07_DOMAIN01
 app = typer.Typer()
-
+@app.command(name="cache-key")
 def cache_key()->None:
+    """ Generate a cache key for the Github action cache based on grid file name and download URI."""
     d = "_".join(grid.name + grid.uri for grid in VALIDATION_GRIDS)
     hexdigest = hashlib.md5(d.encode()).hexdigest()
     print(hexdigest)
 
 
-
+@app.command(name="download")
 def download_validation_grids()->None:
+    """Effectively download the validation grid files."""
     for grid in VALIDATION_GRIDS:
         print(f"downloading and unpacking {grid.name}")
         fname = grid_utils._download_grid_file(grid.name)
         print(f"done - downloaded {fname}")
 
-class Mode(enum.Enum):
-    DOWNLOAD = "download"
-    CACHE_KEY = "cache-key"
-
-@app.command()
-def main(option: Mode = Mode.DOWNLOAD) -> int:
-    """
-    Functionality for grid file download.
-
-    The script has to options:
-
-    - `cache-key`: generate the cache key for the github cache action. The cache key is based on the download URI and the grid name
-
-    - `download` : effectively download the files
-    """
-    try:
-        match(option):
-            case Mode.DOWNLOAD:
-                download_validation_grids()
-            case Mode.CACHE_KEY:
-                cache_key()
-            case _:
-                raise ValueError(f"Unknown option: {option}")
-        return 0
-    except ValueError as e:
-        message, exit_code = e.args
-        print(f"[ERROR] {message}")
-        return exit_code
 
 
 
