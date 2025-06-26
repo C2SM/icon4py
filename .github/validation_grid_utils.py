602e8446a9a48d4a8377987a37388441a6e9a20b
Magdalena Luz <luzm@ethz.ch>
2025-06-26 08:37:16 +0200

    add .github to pre-commit config and run pre-commit hooks

--- .github/validation_grid_utils.py
+++ .github/validation_grid_utils.py
@@ -1,5 +1,13 @@
 #! /usr/bin/env -S uv run -q --script
 
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022-2024, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# Please, refer to the LICENSE file in the root directory.
+# SPDX-License-Identifier: BSD-3-Clause
+
 # /// script
 # requires-python = ">=3.10"
 # dependencies = [
@@ -11,25 +19,32 @@
 # ///
 
 
-import typer
+import hashlib
 import sys
 
-from icon4py.model.testing import grid_utils
-from icon4py.model.testing import definitions
-import hashlib
+import typer
 
-VALIDATION_GRIDS = (definitions.Grids.R02B04_GLOBAL, definitions.Grids.MCH_CH_R04B09_DSL, definitions.Grids.MCH_OPR_R04B07_DOMAIN01) # change to MCH_OPR_R04B07_DOMAIN01
+from icon4py.model.testing import definitions, grid_utils
+
+
+VALIDATION_GRIDS = (
+    definitions.Grids.R02B04_GLOBAL,
+    definitions.Grids.MCH_CH_R04B09_DSL,
+    definitions.Grids.MCH_OPR_R04B07_DOMAIN01,
+)  # change to MCH_OPR_R04B07_DOMAIN01
 app = typer.Typer()
+
+
 @app.command(name="cache-key")
-def cache_key()->None:
-    """ Generate a cache key for the Github action cache based on grid file name and download URI."""
+def cache_key() -> None:
+    """Generate a cache key for the Github action cache based on grid file name and download URI."""
     d = "_".join(grid.name + grid.uri for grid in VALIDATION_GRIDS)
     hexdigest = hashlib.md5(d.encode()).hexdigest()
     print(hexdigest)
 
 
 @app.command(name="download")
-def download_validation_grids()->None:
+def download_validation_grids() -> None:
     """Effectively download the validation grid files."""
     for grid in VALIDATION_GRIDS:
         print(f"downloading and unpacking {grid.name}")
@@ -37,7 +52,5 @@ def download_validation_grids()->None:
         print(f"done - downloaded {fname}")
 
 
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     sys.exit(app())
