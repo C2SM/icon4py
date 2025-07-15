95610f42e1fed9e2bb702d0246e3db45da859ba8
Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>
2025-07-15 11:29:48 +0200

    small edit

--- model/atmosphere/diffusion/src/icon4py/model/atmosphere/diffusion/diffusion.py
+++ model/atmosphere/diffusion/src/icon4py/model/atmosphere/diffusion/diffusion.py
@@ -459,7 +459,6 @@ class Diffusion:
             self._backend
         ).compile(
             enable_jit=False,
-            apply_zdiffusion_t=[self.config.apply_zdiffusion_t],
             vertical_start=[0],
             vertical_end=[self._grid.num_levels],
             offset_provider=self._grid.connectivities,
