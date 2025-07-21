af27b84159144b9fdbc0025f8b273b170db6c848
Edoardo Paone <edoardo.paone@cscs.ch>
2025-07-21 13:18:30 +0200

    Revert "Merge remote-tracking branch 'origin/restrict_connectivities' into update_gt4py_version"
    This reverts commit aa96c6c93e0979a0ac06d3ebe1d33655178ce656, reversing
    changes made to d9f5890f0988123fd198386fa2717598ba874207.

--- model/common/src/icon4py/model/common/grid/utils.py
+++ model/common/src/icon4py/model/common/grid/utils.py
@@ -5,14 +5,10 @@
 #
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
-import logging
 from types import ModuleType
 
 import gt4py.next as gtx
 import numpy as np
-
-
-_log = logging.getLogger(__name__)
 
 
 def connectivity_for_1d_sparse_fields(
@@ -20,7 +16,6 @@ def connectivity_for_1d_sparse_fields(
     old_shape: tuple[int, ...],
     origin_axis: gtx.Dimension,
     neighbor_axis: gtx.Dimension,
-    neighbor_axis_size: int,
     has_skip_values: bool,
     array_ns: ModuleType = np,
 ):
@@ -30,13 +25,6 @@ def connectivity_for_1d_sparse_fields(
     ), 'Neighbor table\'s ("{}" to "{}") data type for 1d sparse fields must be gtx.int32. Instead it\'s "{}"'.format(
         origin_axis, neighbor_axis, table.dtype
     )
-    if neighbor_axis_size <= table.shape[0]:
-        # TODO centralize the restriction in base.py
-        _log.info(
-            f"Restricting connectivity for {dim} to size of {neighbor_axis} ({neighbor_axis_size})."
-        )
-        table = table[:neighbor_axis_size, :]
-
     return gtx.as_connectivity(
         [origin_axis, dim],
         neighbor_axis,
