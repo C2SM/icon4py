e507f3a013a4cc500ed84ff1da465dab62aedc58
Enrique Gonzalez Paredes <enriqueg@cscs.ch>
2025-07-24 21:05:01 +0200

    More fixes after merging main

--- model/testing/src/icon4py/model/testing/fixtures/general.py
+++ model/testing/src/icon4py/model/testing/fixtures/general.py
@@ -6,14 +6,35 @@
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
 
+import dataclasses
 
 import gt4py.next as gtx
 import numpy as np
 import pytest
 
 from icon4py.model.common.utils import data_allocation as data_alloc
+from icon4py.model.common.grid import base
+
+
+
+@dataclasses.dataclass
+class _ConnectivityConceptFixer:
+    """
+    This works around a misuse of dimensions as an identifier for connectivities.
+    Since GT4Py might change the way the mesh is represented, we could
+    keep this for a while, otherwise we need to touch all StencilTests.
+    """
+
+    _grid: base.Grid
+
+    def __getitem__(self, dim: gtx.Dimension | str) -> np.ndarray:
+        if isinstance(dim, gtx.Dimension):
+            dim = dim.value
+        return self._grid.connectivities[dim].asnumpy()
+
 
 
 @pytest.fixture(scope="session")
-def connectivities_as_numpy(grid) -> dict[gtx.Dimension, np.ndarray]:
-    return {dim: data_alloc.as_numpy(table) for dim, table in grid.neighbor_tables.items()}
+def connectivities_as_numpy(grid: base.Grid) -> dict[gtx.Dimension, np.ndarray]:
+    return _ConnectivityConceptFixer(grid)
+
--- testing/README.md
+++
@@ -1,1 +0,0 @@
-# Testing utils for Python ICON port
