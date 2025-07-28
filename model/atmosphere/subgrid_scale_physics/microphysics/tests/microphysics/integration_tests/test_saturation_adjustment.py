79e095899969e0e9b921a1717704fea6cbfd5106
Hannes Vogt <vogt@hey.com>
2025-07-28 19:06:02 +0200

    fix missing fixtures import

--- model/atmosphere/subgrid_scale_physics/microphysics/tests/microphysics/integration_tests/test_saturation_adjustment.py
+++ model/atmosphere/subgrid_scale_physics/microphysics/tests/microphysics/integration_tests/test_saturation_adjustment.py
@@ -16,6 +16,8 @@ from icon4py.model.common.grid import vertical as v_grid
 from icon4py.model.common.utils import data_allocation as data_alloc
 from icon4py.model.testing import datatest_utils as dt_utils, helpers
 
+from ..fixtures import *  # noqa: F403
+
 
 @pytest.mark.datatest
 @pytest.mark.parametrize(
