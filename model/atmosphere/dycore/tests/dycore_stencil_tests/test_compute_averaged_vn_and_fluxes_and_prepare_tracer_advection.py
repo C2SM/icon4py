31986e97fe2889f4c33ad953dfbc7bfd10db1510
muellch <60387010+muellch@users.noreply.github.com>
2025-07-29 08:58:37 +0200

    Update model/atmosphere/dycore/tests/dycore_stencil_tests/test_compute_averaged_vn_and_fluxes_and_prepare_tracer_advection.py
    `prepare_advection` if should be statement, not expression.
    
    Co-authored-by: yiluchen1066 <67939569+yiluchen1066@users.noreply.github.com>

--- model/atmosphere/dycore/tests/dycore_stencil_tests/test_compute_averaged_vn_and_fluxes_and_prepare_tracer_advection.py
+++ model/atmosphere/dycore/tests/dycore_stencil_tests/test_compute_averaged_vn_and_fluxes_and_prepare_tracer_advection.py
@@ -66,20 +66,19 @@ class TestComputeAveragedVnAndFluxesAndPrepareTracerAdvection(test_helpers.Stenc
             theta_v_at_edges_on_model_levels,
         )
 
-        substep_and_spatially_averaged_vn, substep_averaged_mass_flux = (
+        if prepare_advection:
+         substep_and_spatially_averaged_vn, substep_averaged_mass_flux = (
             (
-                (r_nsubsteps * spatially_averaged_vn, r_nsubsteps * mass_fl_e)
+                (r_nsubsteps * spatially_averaged_vn, r_nsubsteps * mass_flux_at_edges_on_model_levels)
                 if at_first_substep
-                else accumulate_prep_adv_fields_numpy(
+                else _accumulate_prep_adv_fields(
                     spatially_averaged_vn,
-                    mass_fl_e,
+                    mass_flux_at_edges_on_model_levels,
                     substep_and_spatially_averaged_vn,
                     substep_averaged_mass_flux,
                     r_nsubsteps,
                 )
             )
-            if prepare_advection
-            else (substep_and_spatially_averaged_vn, substep_averaged_mass_flux)
         )
 
         return dict(
