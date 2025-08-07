8a00bcd60d5c658f38667e3826ee0b442ec5474c
Jacopo <jacopo.canton@gmail.com>
2025-08-07 17:03:00 +0200

    postprocessing

--- submit_gauss.sh
+++ submit_gauss.sh
@@ -11,7 +11,7 @@
 #SBATCH --partition=debug
 #SBATCH --time=00:30:00
 
-#SBATCH --job-name=channel_950x350x100_5m_nlev20_leeMoser
+#SBATCH --job-name=channel_950x350x100_5m_nlev20_leeMoser_debug
 
 #SBATCH --output=../runs_icon4py/logs/%x.log
 #SBATCH --error=../runs_icon4py/logs/%x.log
@@ -59,6 +59,9 @@ export ICON4PY_SAVEPOINT_PATH="ser_data/exclaim_channel_950x350x100_5m_nlev20/se
 export ICON4PY_GRID_FILE_PATH="testdata/grids/gauss3d_torus/Channel_950m_x_350m_res5m.nc"
 export TOTAL_WORKERS=$((SLURM_NNODES * SLURM_TASKS_PER_NODE))
 
+# ==============================================================================
+# Run simulation
+#
 python \
 	model/driver/src/icon4py/model/driver/icon4py_driver.py \
 	$ICON4PY_SAVEPOINT_PATH \
@@ -66,7 +69,16 @@ python \
 	--experiment_type=gauss3d_torus \
 	--grid_root=2 --grid_level=0 --enable_output
 
-## generate vtu files
-#python ../python-scripts/plot_vtk.py "$TOTAL_WORKERS" "$ICON4PY_OUTPUT_DIR" "$ICON4PY_SAVEPOINT_PATH" "$ICON4PY_GRID_FILE_PATH"
+# ==============================================================================
+# Postprocess
+#
+deactivate
+source "$PROJECTS_DIR/python-scripts/.venv/bin/activate"
+
+# generate vtu files
+python ../python-scripts/plot_vtk.py "$TOTAL_WORKERS" "$ICON4PY_OUTPUT_DIR" "$ICON4PY_SAVEPOINT_PATH" "$ICON4PY_GRID_FILE_PATH"
+
+# compute temporal averages
+python ../python-scripts/temporal_average.py "$TOTAL_WORKERS" "$ICON4PY_OUTPUT_DIR"
 
 echo "Finished running job: $SLURM_JOB_NAME, one way or another"
