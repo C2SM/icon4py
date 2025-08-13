d077f450d15c012850bc8dc71ab2411b71bfe752
Christoph Müller <Christoph.Mueller@meteoswiss.ch>
2025-08-13 13:19:56 +0200

    Fix unit test

--- model/atmosphere/dycore/tests/dycore/stencil_tests/test_spatially_average_flux_or_velocity.py
+++ model/atmosphere/dycore/tests/dycore/stencil_tests/test_spatially_average_flux_or_velocity.py
@@ -30,14 +30,14 @@ def spatially_average_flux_or_velocity_numpy(
 ) -> np.ndarray:
     e2c2eO = connectivities[dims.E2C2EODim]
     e_flx_avg = np.expand_dims(e_flx_avg, axis=-1)
-    z_vn_avg = np.sum(flux_or_velocity[e2c2eO] * e_flx_avg, axis=1)
+    spatially_averaged_flux_or_velocity = np.sum(flux_or_velocity[e2c2eO] * e_flx_avg, axis=1)
 
-    return z_vn_avg
+    return spatially_averaged_flux_or_velocity
 
 
 class TestSpatiallyAverageFluxOrVelocity(StencilTest):
     PROGRAM = spatially_average_flux_or_velocity
-    OUTPUTS = ("z_vn_avg",)
+    OUTPUTS = ("spatially_averaged_flux_or_velocity",)
     MARKERS = (pytest.mark.embedded_remap_error,)
 
     @staticmethod
@@ -47,22 +47,24 @@ class TestSpatiallyAverageFluxOrVelocity(StencilTest):
         flux_or_velocity: np.ndarray,
         **kwargs: Any,
     ) -> dict:
-        z_vn_avg = spatially_average_flux_or_velocity_numpy(
+        spatially_averaged_flux_or_velocity = spatially_average_flux_or_velocity_numpy(
             connectivities, e_flx_avg, flux_or_velocity
         )
 
-        return dict(z_vn_avg=z_vn_avg)
+        return dict(spatially_averaged_flux_or_velocity=spatially_averaged_flux_or_velocity)
 
     @pytest.fixture
     def input_data(self, grid: base.Grid) -> dict[str, gtx.Field | state_utils.ScalarType]:
         e_flx_avg = random_field(grid, dims.EdgeDim, dims.E2C2EODim, dtype=wpfloat)
         flux_or_velocity = random_field(grid, dims.EdgeDim, dims.KDim, dtype=wpfloat)
-        z_vn_avg = zero_field(grid, dims.EdgeDim, dims.KDim, dtype=wpfloat)
+        spatially_averaged_flux_or_velocity = zero_field(
+            grid, dims.EdgeDim, dims.KDim, dtype=wpfloat
+        )
 
         return dict(
             e_flx_avg=e_flx_avg,
-            vn=flux_or_velocity,
-            z_vn_avg=z_vn_avg,
+            flux_or_velocity=flux_or_velocity,
+            spatially_averaged_flux_or_velocity=spatially_averaged_flux_or_velocity,
             horizontal_start=0,
             horizontal_end=gtx.int32(grid.num_edges),
             vertical_start=0,
