6a2085fdeeef6515ff60532754961d819fb1ba86
Hannes Vogt <vogt@hey.com>
2025-08-13 17:53:01 +0200

    fix sync

--- model/common/src/icon4py/model/common/utils/device_utils.py
+++ model/common/src/icon4py/model/common/utils/device_utils.py
@@ -34,7 +34,7 @@ def sync(backend: gtx_backend.Backend | None = None) -> None:
 
     Note: this is and ad-hoc interface, maybe the function should get the device to sync for.
     """
-    if is_cupy_device(backend.allocator):
+    if backend is not None and is_cupy_device(backend.allocator):
         cp.cuda.runtime.deviceSynchronize()
 
 
