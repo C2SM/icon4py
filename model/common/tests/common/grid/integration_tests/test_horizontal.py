f507594c32efc9a8265f5aedda93fdc819f77f7d
Magdalena Luz <luzm@ethz.ch>
2025-08-14 09:27:07 +0200

    do not use function under test in test  assertion

--- model/common/tests/common/grid/integration_tests/test_horizontal.py
+++ model/common/tests/common/grid/integration_tests/test_horizontal.py
@@ -34,7 +34,37 @@ def test_map_domain_bounds_end_index(experiment, dim, grid_savepoint):
 
 def _map_and_assert_array(dim, index_array):
     index_map = h_grid.map_icon_domain_bounds(dim, index_array)
+    same_index = False
     for d, index in index_map.items():
-        icon_index = h_grid._map_to_icon_index(d.dim, d.zone)
-        assert index == index_array[icon_index]
-        assert isinstance(index, gtx.int32)
+        if d.zone == h_grid.Zone.INTERIOR:
+            same_index = index == index_array[h_grid._INTERIOR[d.dim]]
+        if d.zone == h_grid.Zone.LOCAL:
+            same_index = index == index_array[h_grid._LOCAL[d.dim]]
+        if d.zone == h_grid.Zone.END:
+            same_index = index == index_array[h_grid._END[d.dim]]
+        if d.zone == h_grid.Zone.LATERAL_BOUNDARY:
+            same_index = index == index_array[h_grid._LATERAL_BOUNDARY[d.dim]]
+        if d.zone == h_grid.Zone.LATERAL_BOUNDARY_LEVEL_2:
+            same_index = index == index_array[h_grid._LATERAL_BOUNDARY[d.dim] + 1]
+        if d.zone == h_grid.Zone.LATERAL_BOUNDARY_LEVEL_3:
+            same_index = index == index_array[h_grid._LATERAL_BOUNDARY[d.dim] + 2]
+        if d.zone == h_grid.Zone.LATERAL_BOUNDARY_LEVEL_4:
+            same_index = index == index_array[h_grid._LATERAL_BOUNDARY[d.dim] + 3]
+        if d.zone == h_grid.Zone.LATERAL_BOUNDARY_LEVEL_5:
+            same_index = index == index_array[h_grid._LATERAL_BOUNDARY[d.dim] + 4]
+        if d.zone == h_grid.Zone.LATERAL_BOUNDARY_LEVEL_6:
+            same_index = index == index_array[h_grid._LATERAL_BOUNDARY[d.dim] + 5]
+        if d.zone == h_grid.Zone.LATERAL_BOUNDARY_LEVEL_7:
+            same_index = index == index_array[h_grid._LATERAL_BOUNDARY[d.dim] + 6]
+        if d.zone == h_grid.Zone.LATERAL_BOUNDARY_LEVEL_8:
+            same_index = index == index_array[h_grid._LATERAL_BOUNDARY[d.dim] + 7]
+        if d.zone == h_grid.Zone.NUDGING:
+            same_index = index == index_array[h_grid._NUDGING[d.dim]]
+        if d.zone == h_grid.Zone.NUDGING_LEVEL_2:
+            same_index = index == index_array[h_grid._NUDGING[d.dim] + 1]
+        if d.zone == h_grid.Zone.HALO:
+            same_index = index == index_array[h_grid._HALO[d.dim]]
+        if d.zone == h_grid.Zone.HALO_LEVEL_2:
+            same_index = index == index_array[h_grid._HALO[d.dim] + 1]
+        if not same_index:
+            raise AssertionError(f"Wrong index for {d.zone} zone in dimension {d.dim}: ")
