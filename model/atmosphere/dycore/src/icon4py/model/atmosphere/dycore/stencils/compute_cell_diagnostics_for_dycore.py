670e801b4d76c24a1e60b5dae310b435853122d9
Christoph Müller <Christoph.Mueller@meteoswiss.ch>
2025-08-26 11:52:10 +0200

    bla

--- model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/stencils/compute_cell_diagnostics_for_dycore.py
+++ model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/stencils/compute_cell_diagnostics_for_dycore.py
@@ -63,32 +63,16 @@ horzpres_discr_type: Final = HorizontalPressureDiscretizationType()
 
 
 @field_operator
-def _initialize_zero_if_limited_area(
-    limited_area: bool,
-) -> tuple[
-    fa.CellKField[ta.vpfloat],
-    fa.CellKField[ta.vpfloat],
-]:
-    (perturbed_rho_at_cells_on_model_levels, perturbed_theta_v_at_cells_on_model_levels) = (
-        _init_two_cell_kdim_fields_with_zero_vp()
-        if limited_area
-        else _init_two_cell_kdim_fields_with_zero_vp()
-    )
-
-    return perturbed_rho_at_cells_on_model_levels, perturbed_theta_v_at_cells_on_model_levels
-
-
-@field_operator
 def _compute_perturbed_quantities_and_interpolation(
     current_rho: fa.CellKField[ta.wpfloat],
     reference_rho_at_cells_on_model_levels: fa.CellKField[ta.wpfloat],
     current_theta_v: fa.CellKField[ta.wpfloat],
     reference_theta_at_cells_on_model_levels: fa.CellKField[ta.wpfloat],
-    wgtfac_c: fa.CellKField[ta.wpfloat],
+    wgtfac_c: fa.CellKField[ta.vpfloat],
     exner_w_explicit_weight_parameter: fa.CellField[ta.wpfloat],
     perturbed_exner_at_cells_on_model_levels: fa.CellKField[ta.wpfloat],
     ddz_of_reference_exner_at_cells_on_half_levels: fa.CellKField[ta.wpfloat],
-    ddqz_z_half: fa.CellKField[ta.wpfloat],
+    ddqz_z_half: fa.CellKField[ta.vpfloat],
     pressure_buoyancy_acceleration_at_cells_on_half_levels: fa.CellKField[ta.vpfloat],
     rho_at_cells_on_half_levels: fa.CellKField[ta.wpfloat],
     exner_at_cells_on_half_levels: fa.CellKField[ta.vpfloat],
@@ -134,27 +118,55 @@ def _compute_perturbed_quantities_and_interpolation(
         rho_at_cells_on_half_levels,
     )
 
-    (
-        perturbed_theta_v_at_cells_on_half_levels,
-        theta_v_at_cells_on_half_levels,
-        pressure_buoyancy_acceleration_at_cells_on_half_levels,
-    ) = concat_where(
+    wgtfac_c_wp = astype(wgtfac_c, wpfloat)
+
+    perturbed_theta_v_at_cells_on_half_levels = concat_where(
         dims.KDim >= 1,
-        _compute_virtual_potential_temperatures_and_pressure_gradient(
-            wgtfac_c,
-            perturbed_theta_v_at_cells_on_model_levels,
-            current_theta_v,
-            exner_w_explicit_weight_parameter,
-            perturbed_exner_at_cells_on_model_levels,
-            ddz_of_reference_exner_at_cells_on_half_levels,
-            ddqz_z_half,
+        _interpolate_cell_field_to_half_levels_vp(
+            wgtfac_c=wgtfac_c, interpolant=perturbed_theta_v_at_cells_on_model_levels
         ),
-        (
-            broadcast(0.0, (dims.CellDim, dims.KDim)),
-            theta_v_at_cells_on_half_levels,
-            pressure_buoyancy_acceleration_at_cells_on_half_levels,
-        ),
+        broadcast(0.0, (dims.CellDim, dims.KDim)),
     )
+
+    theta_v_at_cells_on_half_levels = concat_where(
+        dims.KDim >= 1,
+        _interpolate_cell_field_to_half_levels_wp(
+            wgtfac_c=wgtfac_c_wp, interpolant=current_theta_v
+        ),
+        theta_v_at_cells_on_half_levels,
+    )
+
+    ddqz_z_half_wp = astype(ddqz_z_half, wpfloat)
+
+    pressure_buoyancy_acceleration_at_cells_on_half_levels = concat_where(
+        dims.KDim >= 1,
+        exner_w_explicit_weight_parameter * theta_v_at_cells_on_half_levels * (
+            perturbed_exner_at_cells_on_model_levels(Koff[-1]) - perturbed_exner_at_cells_on_model_levels
+        ) / ddqz_z_half_wp + astype(perturbed_theta_v_at_cells_on_half_levels * ddz_of_reference_exner_at_cells_on_half_levels, wpfloat),
+        pressure_buoyancy_acceleration_at_cells_on_half_levels,
+    )
+
+    #(
+    #    perturbed_theta_v_at_cells_on_half_levels,
+    #    theta_v_at_cells_on_half_levels,
+    #    pressure_buoyancy_acceleration_at_cells_on_half_levels,
+    #) = concat_where(
+    #    dims.KDim >= 1,
+    #    _compute_virtual_potential_temperatures_and_pressure_gradient(
+    #        wgtfac_c,
+    #        perturbed_theta_v_at_cells_on_model_levels,
+    #        current_theta_v,
+    #        exner_w_explicit_weight_parameter,
+    #        perturbed_exner_at_cells_on_model_levels,
+    #        ddz_of_reference_exner_at_cells_on_half_levels,
+    #        ddqz_z_half,
+    #    ),
+    #    (
+    #        broadcast(0.0, (dims.CellDim, dims.KDim)),
+    #        theta_v_at_cells_on_half_levels,
+    #        pressure_buoyancy_acceleration_at_cells_on_half_levels,
+    #    ),
+    #)
 
     return (
         perturbed_rho_at_cells_on_model_levels,
@@ -378,8 +390,7 @@ def compute_perturbed_quantities_and_interpolation(
         - ddz_of_temporal_extrapolation_of_perturbed_exner_on_model_level
         - d2dz2_of_temporal_extrapolation_of_perturbed_exner_on_model_levels
     """
-    _initialize_zero_if_limited_area(
-        limited_area=limited_area,
+    _init_two_cell_kdim_fields_with_zero_vp(
         out=(perturbed_rho_at_cells_on_model_levels, perturbed_theta_v_at_cells_on_model_levels),
         domain={
             dims.CellDim: (start_cell_lateral_boundary, start_cell_lateral_boundary_level_3),
