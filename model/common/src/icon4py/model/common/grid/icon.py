C2SM/icon4py

commit a9f71c9eeb4987eb5d4cdd52a76591d0f360602f

RBF interpolation stencils improvement (#709)
Adds computation of RBF interpolation coefficient fields for cells,
edges, and vertices. Adds a few missing cartesian coordinate fields used
as inputs for the computation. The R04B09 vertex and cell fields are
quite far off from the reference fields (~1e-2) but this seems to be
inherent to the method.

This does not port the gradient RBF field.

This is using numpy/cupy for the computation and is mostly vectorized.
The cholesky decomposition/solve is always done on CPU, and is not
vectorized/parallelized. The RBF matrices are small (n=4-9) and don't
offer enough parallelism. I haven't made any attempts at parallelizing
this or profiling in general, so this may be suboptimal, but I would
leave potential optimizations for a follow-up, if needed.

---------

Co-authored-by: Mikael Simberg <mikael.simberg@iki.fi>
Co-authored-by: Mikael Simberg <simberg@cscs.ch>

model/common/src/icon4py/model/common/grid/icon.py
@@ -8,8 +8,9 @@
 import dataclasses
 import functools
 import logging
+import math
 import uuid
-from typing import Final
+from typing import Final, Optional
 
 import gt4py.next as gtx
 import numpy as np
@@ -35,29 +36,74 @@ CONNECTIVITIES_ON_PENTAGONS = (dims.V2EDim, dims.V2CDim, dims.V2E2VDim)
 
 @dataclasses.dataclass(frozen=True)
 class GlobalGridParams:
-    root: int
-    level: int
+    root: Optional[int] = None
+    level: Optional[int] = None
+    _num_cells: Optional[int] = None
+    _mean_cell_area: Optional[float] = None
     geometry_type: Final[base.GeometryType] = base.GeometryType.ICOSAHEDRON
-    radius = constants.EARTH_RADIUS
+    radius: float = constants.EARTH_RADIUS
+
+    @classmethod
+    def from_mean_cell_area(
+        cls,
+        mean_cell_area: float,
+        root: Optional[int] = None,
+        level: Optional[int] = None,
+        num_cells: Optional[int] = None,
+        geometry_type: Final[base.GeometryType] = base.GeometryType.ICOSAHEDRON,
+        radius: float = constants.EARTH_RADIUS,
+    ):
+        return cls(root, level, num_cells, mean_cell_area, geometry_type)
 
     @functools.cached_property
     def num_cells(self):
-        match self.geometry_type:
-            case base.GeometryType.ICOSAHEDRON:
-                return compute_icosahedron_num_cells(self.root, self.level)
-            case base.GeometryType.TORUS:
-                return compute_torus_num_cells(1000, 1000)
-            case _:
-                NotImplementedError(f"Unknown gemoetry type {self.geometry_type}")
+        if self._num_cells is None:
+            match self.geometry_type:
+                case base.GeometryType.ICOSAHEDRON:
+                    assert self.root is not None and self.level is not None
+                    return compute_icosahedron_num_cells(self.root, self.level)
+                case base.GeometryType.TORUS:
+                    raise NotImplementedError("TODO : lookup torus cell number computation")
+                case _:
+                    raise NotImplementedError(f"Unknown geometry type {self.geometry_type}")
+
+        return self._num_cells
+
+    @functools.cached_property
+    def characteristic_length(self):
+        return math.sqrt(self.mean_cell_area)
+
+    @functools.cached_property
+    def mean_cell_area(self):
+        if self._mean_cell_area is None:
+            match self.geometry_type:
+                case base.GeometryType.ICOSAHEDRON:
+                    return compute_mean_cell_area_for_sphere(constants.EARTH_RADIUS, self.num_cells)
+                case base.GeometryType.TORUS:
+                    NotImplementedError(f"mean_cell_area not implemented for {self.geometry_type}")
+                case _:
+                    NotImplementedError(f"Unknown geometry type {self.geometry_type}")
+
+        return self._mean_cell_area
 
 
 def compute_icosahedron_num_cells(root: int, level: int):
     return 20.0 * root**2 * 4.0**level
 
 
-def compute_torus_num_cells(x: int, y: int):
-    # TODO (@halungge) add implementation
-    raise NotImplementedError("TODO : lookup torus cell number computation")
+def compute_mean_cell_area_for_sphere(radius, num_cells):
+    """
+    Compute the mean cell area.
+
+    Computes the mean cell area by dividing the sphere by the number of cells in the
+    global grid.
+
+    Args:
+        radius: average earth radius, might be rescaled by a scaling parameter
+        num_cells: number of cells on the global grid
+    Returns: mean area of one cell [m^2]
+    """
+    return 4.0 * math.pi * radius**2 / num_cells
 
 
 class IconGrid(base.BaseGrid):

model/atmosphere/diffusion/tests/diffusion_tests/test_diffusion.py
@@ -60,11 +60,10 @@ def _get_or_initialize(experiment, backend, name):
         geometry_ = grid_utils.get_grid_geometry(backend, experiment, grid_file)
         grid = geometry_.grid
 
-        cell_params = grid_states.CellParams.from_global_num_cells(
+        cell_params = grid_states.CellParams(
             cell_center_lat=geometry_.get(geometry_meta.CELL_LAT),
             cell_center_lon=geometry_.get(geometry_meta.CELL_LON),
             area=geometry_.get(geometry_meta.CELL_AREA),
-            global_num_cells=grid.global_num_cells,
         )
         edge_params = grid_states.EdgeParams(
             edge_center_lat=geometry_.get(geometry_meta.EDGE_LAT),

model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/solve_nonhydro.py
@@ -1350,13 +1350,13 @@ class SolveNonhydro:
         # delta_x**2 is approximated by the mean cell area
         # Coefficient for reduced fourth-order divergence d
         second_order_divdamp_scaling_coeff = (
-            second_order_divdamp_factor * self._cell_params.mean_cell_area
+            second_order_divdamp_factor * self._grid.global_properties.mean_cell_area
         )
 
         dycore_utils._calculate_divdamp_fields(
             self.interpolated_fourth_order_divdamp_factor,
             gtx.int32(self._config.divdamp_order),
-            self._cell_params.mean_cell_area,
+            self._grid.global_properties.mean_cell_area,
             second_order_divdamp_factor,
             self._config.nudge_max_coeff,
             constants.DBL_EPS,

model/common/src/icon4py/model/common/grid/geometry.py
@@ -489,6 +489,48 @@ class GridGeometry(factory.FieldSource):
             pairs=(("u_cell_1", "u_cell_2"), ("v_cell_1", "v_cell_2")),
         )
         self.register_provider(tangent_cell_wrapper)
+        cartesian_vertices = factory.EmbeddedFieldOperatorProvider(
+            func=math_helpers.geographical_to_cartesian_on_vertices.with_backend(self.backend),
+            domain=(dims.VertexDim,),
+            fields={
+                attrs.VERTEX_X: attrs.VERTEX_X,
+                attrs.VERTEX_Y: attrs.VERTEX_Y,
+                attrs.VERTEX_Z: attrs.VERTEX_Z,
+            },
+            deps={
+                "lat": attrs.VERTEX_LAT,
+                "lon": attrs.VERTEX_LON,
+            },
+        )
+        self.register_provider(cartesian_vertices)
+        cartesian_edge_centers = factory.EmbeddedFieldOperatorProvider(
+            func=math_helpers.geographical_to_cartesian_on_edges.with_backend(self.backend),
+            domain=(dims.EdgeDim,),
+            fields={
+                attrs.EDGE_CENTER_X: attrs.EDGE_CENTER_X,
+                attrs.EDGE_CENTER_Y: attrs.EDGE_CENTER_Y,
+                attrs.EDGE_CENTER_Z: attrs.EDGE_CENTER_Z,
+            },
+            deps={
+                "lat": attrs.EDGE_LAT,
+                "lon": attrs.EDGE_LON,
+            },
+        )
+        self.register_provider(cartesian_edge_centers)
+        cartesian_cell_centers = factory.EmbeddedFieldOperatorProvider(
+            func=math_helpers.geographical_to_cartesian_on_cells.with_backend(self.backend),
+            domain=(dims.CellDim,),
+            fields={
+                attrs.CELL_CENTER_X: attrs.CELL_CENTER_X,
+                attrs.CELL_CENTER_Y: attrs.CELL_CENTER_Y,
+                attrs.CELL_CENTER_Z: attrs.CELL_CENTER_Z,
+            },
+            deps={
+                "lat": attrs.CELL_LAT,
+                "lon": attrs.CELL_LON,
+            },
+        )
+        self.register_provider(cartesian_cell_centers)
 
     def _inverse_field_provider(self, field_name: str):
         meta = attrs.metadata_for_inverse(attrs.attrs[field_name])

model/common/src/icon4py/model/common/grid/geometry_attributes.py
@@ -24,9 +24,16 @@ EDGE_LON: Final[str] = "grid_longitude_of_edge_midpoint"
 EDGE_LAT: Final[str] = "grid_latitude_of_edge_midpoint"
 VERTEX_LON: Final[str] = "grid_longitude_of_vertex"
 VERTEX_LAT: Final[str] = "grid_latitude_of_vertex"
+VERTEX_X: Final[str] = "x_component_of_vertex"
+VERTEX_Y: Final[str] = "y_component_of_vertex"
+VERTEX_Z: Final[str] = "z_component_of_vertex"
 
 CELL_LON: Final[str] = "grid_longitude_of_cell_center"
 CELL_LAT: Final[str] = "grid_latitude_of_cell_center"
+CELL_CENTER_X: Final[str] = "x_component_of_cell_center"
+CELL_CENTER_Y: Final[str] = "y_component_of_cell_center"
+CELL_CENTER_Z: Final[str] = "z_component_of_cell_center"
+
 CELL_AREA: Final[str] = "cell_area"
 EDGE_AREA: Final[str] = "edge_area"
 DUAL_AREA: Final[str] = "dual_area"
@@ -62,6 +69,10 @@ EDGE_NORMAL_VERTEX_U: Final[str] = "eastward_component_of_edge_normal_on_vertex"
 EDGE_NORMAL_VERTEX_V: Final[str] = "northward_component_of_edge_normal_on_vertex"
 EDGE_NORMAL_CELL_U: Final[str] = "eastward_component_of_edge_normal_on_cell"
 EDGE_NORMAL_CELL_V: Final[str] = "northward_component_of_edge_normal_on_cell"
+EDGE_CENTER_X: Final[str] = "x_coordinate_of_edge_center"
+EDGE_CENTER_Y: Final[str] = "y_coordinate_of_edge_center"
+EDGE_CENTER_Z: Final[str] = "z_coordinate_of_edge_center"
+
 
 attrs: dict[str, model.FieldMetaData] = {
     CELL_LAT: dict(
@@ -90,6 +101,30 @@ attrs: dict[str, model.FieldMetaData] = {
         units="radian",
         dims=(dims.VertexDim,),
         icon_var_name="t_grid_vertices%vertex%lon",
+        dtype=ta.wpfloat,
+    ),
+    VERTEX_X: dict(
+        standard_name=VERTEX_X,
+        long_name="x component of cartesian coordinates of vertex",
+        units="1",
+        dims=(dims.VertexDim,),
+        icon_var_name="t_grid_vertices%vertex%x(1)",
+        dtype=ta.wpfloat,
+    ),
+    VERTEX_Y: dict(
+        standard_name=VERTEX_Y,
+        long_name="y component of cartesian coordinates of vertex",
+        units="1",
+        dims=(dims.VertexDim,),
+        icon_var_name="t_grid_vertices%vertex%x(2)",
+        dtype=ta.wpfloat,
+    ),
+    VERTEX_Z: dict(
+        standard_name=VERTEX_Z,
+        long_name="z component of cartesian coordinates of vertex",
+        units="1",
+        dims=(dims.VertexDim,),
+        icon_var_name="t_grid_vertices%vertex%x(3)",
         dtype=ta.wpfloat,
     ),
     EDGE_LAT: dict(
@@ -167,6 +202,30 @@ attrs: dict[str, model.FieldMetaData] = {
         units="m2",
         dims=(dims.CellDim,),
         icon_var_name="t_grid_cells%area",
+        dtype=ta.wpfloat,
+    ),
+    CELL_CENTER_X: dict(
+        standard_name=CELL_CENTER_X,
+        long_name="x component of cartesian coordinates of cell center",
+        units="",
+        dims=(dims.CellDim,),
+        icon_var_name="t_grid_cells%%cartesian_center%x(1)",
+        dtype=ta.wpfloat,
+    ),
+    CELL_CENTER_Y: dict(
+        standard_name=CELL_CENTER_Y,
+        long_name="y component of cartesian coordinates of cell center",
+        units="",
+        dims=(dims.CellDim,),
+        icon_var_name="t_grid_cells%%cartesian_center%x(2)",
+        dtype=ta.wpfloat,
+    ),
+    CELL_CENTER_Z: dict(
+        standard_name=CELL_CENTER_Z,
+        long_name="z component of cartesian coordinates of cell center",
+        units="",
+        dims=(dims.CellDim,),
+        icon_var_name="t_grid_cells%%cartesian_center%x(3)",
         dtype=ta.wpfloat,
     ),
     DUAL_AREA: dict(
@@ -343,6 +402,30 @@ attrs: dict[str, model.FieldMetaData] = {
         units="",  # TODO
         dims=(dims.EdgeDim,),
         icon_var_name="ptr_patch%edges%dual_normal%v2",
+        dtype=ta.wpfloat,
+    ),
+    EDGE_CENTER_X: dict(
+        standard_name=EDGE_CENTER_X,
+        long_name="x component of cartesian coordinates of edge centers",
+        units="1",
+        dims=(dims.EdgeDim,),
+        icon_var_name="t_grid_edges%cartesian_center%x(1)",
+        dtype=ta.wpfloat,
+    ),
+    EDGE_CENTER_Y: dict(
+        standard_name=EDGE_CENTER_Y,
+        long_name="y component of cartesian coordinates of edge centers",
+        units="1",
+        dims=(dims.EdgeDim,),
+        icon_var_name="t_grid_edges%cartesian_center%x(2)",
+        dtype=ta.wpfloat,
+    ),
+    EDGE_CENTER_Z: dict(
+        standard_name=EDGE_CENTER_Z,
+        long_name="z component of cartesian coordinates of edge centers",
+        units="1",
+        dims=(dims.EdgeDim,),
+        icon_var_name="t_grid_edges%cartesian_center%x(3)",
         dtype=ta.wpfloat,
     ),
 }

model/common/src/icon4py/model/common/grid/geometry_stencils.py
@@ -15,10 +15,10 @@ from gt4py.next import sin, where
 from icon4py.model.common import dimension as dims, field_type_aliases as fa, type_alias as ta
 from icon4py.model.common.dimension import E2C, E2C2V, E2V, EdgeDim
 from icon4py.model.common.math.helpers import (
-    arc_length,
+    arc_length_on_edges,
     cross_product_on_edges,
     geographical_to_cartesian_on_edges,
-    geographical_to_cartesian_on_vertex,
+    geographical_to_cartesian_on_vertices,
     normalize_cartesian_vector_on_edges,
     zonal_and_meridional_components_on_edges,
 )
@@ -47,7 +47,7 @@ def cartesian_coordinates_of_edge_tangent(
           y: y coordinate of normalized tangent vector
           z: z coordinate of normalized tangent vector
     """
-    vertex_x, vertex_y, vertex_z = geographical_to_cartesian_on_vertex(vertex_lat, vertex_lon)
+    vertex_x, vertex_y, vertex_z = geographical_to_cartesian_on_vertices(vertex_lat, vertex_lon)
 
     x = edge_orientation * (vertex_x(E2V[1]) - vertex_x(E2V[0]))
     y = edge_orientation * (vertex_y(E2V[1]) - vertex_y(E2V[0]))
@@ -362,7 +362,7 @@ def cell_center_arc_distance(
     x0, y0, z0 = geographical_to_cartesian_on_edges(lat_neighbor_0, lon_neighbor_0)
     x1, y1, z1 = geographical_to_cartesian_on_edges(lat_neighbor_1, lon_neighbor_1)
     # (xi, yi, zi) are normalized by construction
-    arc = arc_length(x0, x1, y0, y1, z0, z1, radius)
+    arc = arc_length_on_edges(x0, x1, y0, y1, z0, z1, radius)
     return arc
 
 
@@ -397,7 +397,7 @@ def arc_distance_of_far_edges_in_diamond(
         arc length between the "far" vertices in the diamond.
 
     """
-    x, y, z = geographical_to_cartesian_on_vertex(vertex_lat, vertex_lon)
+    x, y, z = geographical_to_cartesian_on_vertices(vertex_lat, vertex_lon)
     x2 = x(E2C2V[2])
     x3 = x(E2C2V[3])
     y2 = y(E2C2V[2])
@@ -406,7 +406,7 @@ def arc_distance_of_far_edges_in_diamond(
     z3 = z(E2C2V[3])
     # (xi, yi, zi) are normalized by construction
 
-    far_vertex_vertex_length = arc_length(x2, x3, y2, y3, z2, z3, radius)
+    far_vertex_vertex_length = arc_length_on_edges(x2, x3, y2, y3, z2, z3, radius)
     return far_vertex_vertex_length
 
 
@@ -431,7 +431,7 @@ def edge_length(
     Returns:
         edge length
     """
-    x, y, z = geographical_to_cartesian_on_vertex(vertex_lat, vertex_lon)
+    x, y, z = geographical_to_cartesian_on_vertices(vertex_lat, vertex_lon)
     x0 = x(E2V[0])
     x1 = x(E2V[1])
     y0 = y(E2V[0])
@@ -440,7 +440,7 @@ def edge_length(
     z1 = z(E2V[1])
     # (xi, yi, zi) are normalized by construction
 
-    length = arc_length(x0, x1, y0, y1, z0, z1, radius)
+    length = arc_length_on_edges(x0, x1, y0, y1, z0, z1, radius)
     return length
 
 

model/common/src/icon4py/model/common/grid/grid_manager.py
@@ -393,9 +393,9 @@ class GridManager:
         num_edges = self._reader.dimension(gridfile.DimensionName.EDGE_NAME)
         num_vertices = self._reader.dimension(gridfile.DimensionName.VERTEX_NAME)
         uuid = self._reader.attribute(gridfile.MandatoryPropertyName.GRID_UUID)
-        grid_level = self._reader.attribute(gridfile.MandatoryPropertyName.LEVEL)
         grid_root = self._reader.attribute(gridfile.MandatoryPropertyName.ROOT)
-        global_params = icon.GlobalGridParams(level=grid_level, root=grid_root)
+        grid_level = self._reader.attribute(gridfile.MandatoryPropertyName.LEVEL)
+        global_params = icon.GlobalGridParams(root=grid_root, level=grid_level)
         grid_size = base.HorizontalGridSize(
             num_vertices=num_vertices, num_edges=num_edges, num_cells=num_cells
         )

model/common/src/icon4py/model/common/grid/states.py
@@ -7,12 +7,10 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import dataclasses
-import functools
-import math
 
 from gt4py import next as gtx
 
-from icon4py.model.common import constants, dimension as dims, field_type_aliases as fa
+from icon4py.model.common import dimension as dims, field_type_aliases as fa
 
 
 class EdgeParams:
@@ -208,56 +206,3 @@ class CellParams:
     cell_center_lon: fa.CellField[float] = None
     #: Area of a cell, defined in ICON in mo_model_domain.f90:t_grid_cells%area
     area: fa.CellField[float] = None
-    #: Mean area of a cell [m^2] = total surface area / numer of cells defined in ICON in in mo_model_domimp_patches.f90
-    mean_cell_area: float = None
-    length_rescale_factor: float = 1.0
-
-    @classmethod
-    def from_global_num_cells(
-        cls,
-        cell_center_lat: fa.CellField[float],
-        cell_center_lon: fa.CellField[float],
-        area: fa.CellField[float],
-        global_num_cells: int,
-        length_rescale_factor: float = 1.0,
-    ):
-        if global_num_cells == 0:
-            # Compute from the area array (should be a torus grid)
-            # TODO (Magdalena) this would not work for a distributed setup (at
-            # least not for a sphere) for the torus it would because cell area
-            # is constant.
-            mean_cell_area = area.asnumpy().mean()
-        else:
-            mean_cell_area = compute_mean_cell_area_for_sphere(
-                constants.EARTH_RADIUS, global_num_cells
-            )
-        return cls(
-            cell_center_lat=cell_center_lat,
-            cell_center_lon=cell_center_lon,
-            area=area,
-            mean_cell_area=mean_cell_area,
-            length_rescale_factor=length_rescale_factor,
-        )
-
-    @functools.cached_property
-    def characteristic_length(self):
-        return math.sqrt(self.mean_cell_area)
-
-    @functools.cached_property
-    def mean_cell_area(self):
-        return self.mean_cell_area
-
-
-def compute_mean_cell_area_for_sphere(radius, num_cells):
-    """
-    Compute the mean cell area.
-
-    Computes the mean cell area by dividing the sphere by the number of cells in the
-    global grid.
-
-    Args:
-        radius: average earth radius, might be rescaled by a scaling parameter
-        num_cells: number of cells on the global grid
-    Returns: mean area of one cell [m^2]
-    """
-    return 4.0 * math.pi * radius**2 / num_cells

commit 8f07ed33205349c2402d5a32ba8b1723c29b0a95

Fix missing raises on exception statements (#821)
Fixes a couple I introduced in #709 and another one that existed from
before. These are triggered by PLW0133 for the linter
(https://docs.astral.sh/ruff/rules/useless-exception-statement/,
equivalent to
https://pylint.readthedocs.io/en/latest/user_guide/messages/warning/pointless-exception-statement.html
from pylint). PLW will be enabled by #842.

model/common/src/icon4py/model/common/grid/icon.py
@@ -82,9 +82,11 @@ class GlobalGridParams:
                 case base.GeometryType.ICOSAHEDRON:
                     return compute_mean_cell_area_for_sphere(constants.EARTH_RADIUS, self.num_cells)
                 case base.GeometryType.TORUS:
-                    NotImplementedError(f"mean_cell_area not implemented for {self.geometry_type}")
+                    raise NotImplementedError(
+                        f"mean_cell_area not implemented for {self.geometry_type}"
+                    )
                 case _:
-                    NotImplementedError(f"Unknown geometry type {self.geometry_type}")
+                    raise NotImplementedError(f"Unknown geometry type {self.geometry_type}")
 
         return self._mean_cell_area
 

model/common/src/icon4py/model/common/io/__init__.py
@@ -197,4 +197,4 @@ import importlib
 
 if not importlib.util.find_spec("xarray"):
     print("Optional icon4py-common[io] dependencies are missing!")
-    SystemExit(1)
+    raise SystemExit(1)