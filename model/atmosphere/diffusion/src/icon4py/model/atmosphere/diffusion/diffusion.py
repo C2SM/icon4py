d6ee9edd1cbc6a142bac1e353815ac0c2379507f
Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>
2025-08-27 09:56:57 +0200

    ran pre-commit

--- model/atmosphere/diffusion/src/icon4py/model/atmosphere/diffusion/diffusion.py
+++ model/atmosphere/diffusion/src/icon4py/model/atmosphere/diffusion/diffusion.py
@@ -434,7 +434,7 @@ class Diffusion:
                 "primal_normal_vert_y": self._edge_params.primal_normal_vert[1],
                 "dual_normal_vert_x": self._edge_params.dual_normal_vert[0],
                 "dual_normal_vert_y": self._edge_params.dual_normal_vert[1],
-                "smag_offset": self.smag_offset
+                "smag_offset": self.smag_offset,
             },
             horizontal_sizes={
                 "horizontal_start": self._edge_start_lateral_boundary_level_5,
