f5af4ef9bac24620763f61523423ff673b084a55
Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>
2025-08-28 12:39:08 +0200

    edits to diffusion

--- model/atmosphere/diffusion/src/icon4py/model/atmosphere/diffusion/diffusion.py
+++ model/atmosphere/diffusion/src/icon4py/model/atmosphere/diffusion/diffusion.py
@@ -422,7 +422,6 @@ class Diffusion:
                 "primal_normal_vert_y": self._edge_params.primal_normal_vert[1],
                 "dual_normal_vert_x": self._edge_params.dual_normal_vert[0],
                 "dual_normal_vert_y": self._edge_params.dual_normal_vert[1],
-                "smag_offset": self.smag_offset,
             },
             horizontal_sizes={
                 "horizontal_start": self._edge_start_lateral_boundary_level_5,
@@ -785,6 +784,7 @@ class Diffusion:
             kh_smag_e=self.kh_smag_e,
             kh_smag_ec=self.kh_smag_ec,
             z_nabla2_e=self.z_nabla2_e,
+            smag_offset=smag_offset,
         )
         log.debug("running stencil 01 (calculate_nabla2_and_smag_coefficients_for_vn): end")
         if (
