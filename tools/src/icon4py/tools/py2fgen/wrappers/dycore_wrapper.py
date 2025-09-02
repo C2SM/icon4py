ed00830e6983797f715f33b7b7787a14f007cecc
Magdalena Luz <luzm@ethz.ch>
2025-09-02 18:11:33 +0200

    fix tools tests: remove nflat_gradp from diffusion references

--- tools/src/icon4py/tools/py2fgen/wrappers/dycore_wrapper.py
+++ tools/src/icon4py/tools/py2fgen/wrappers/dycore_wrapper.py
@@ -225,7 +225,7 @@ def solve_nh_init(
         ddxn_z_full=ddxn_z_full,
         zdiff_gradp=zdiff_gradp,
         vertoffset_gradp=vertoffset_gradp,
-        nflat_gradp=nflat_gradp,
+        nflat_gradp=gtx.int32(nflat_gradp-1), # Fortran vs Python indexing
         pg_edgeidx_dsl=ipeidx_dsl,
         pg_exdist=pg_exdist,
         ddqz_z_full_e=ddqz_z_full_e,
@@ -253,10 +253,7 @@ def solve_nh_init(
     vertical_params = VerticalGrid(
         config=vertical_config,
         vct_a=vct_a,
-        vct_b=vct_b,
-        _min_index_flat_horizontal_grad_pressure=gtx.int32(
-            nflat_gradp - 1
-        ),  # Fortran vs Python indexing
+        vct_b=vct_b
     )
 
     global granule  # noqa: PLW0603 [global-statement]
--- tools/tests/tools/py2fgen/wrappers/test_dycore_wrapper.py
+++ tools/tests/tools/py2fgen/wrappers/test_dycore_wrapper.py
@@ -547,7 +547,6 @@ def test_dycore_wrapper_granule_inputs(
         config=expected_vertical_config,
         vct_a=grid_savepoint.vct_a(),
         vct_b=grid_savepoint.vct_b(),
-        _min_index_flat_horizontal_grad_pressure=grid_savepoint.nflat_gradp(),
     )
     expected_config = utils.construct_solve_nh_config(experiment)
     expected_additional_parameters = solve_nh.NonHydrostaticParams(expected_config)
