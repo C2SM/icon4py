366af2400486381adc2c5f1898f76304bbb8035e
Jacopo <jacopo.canton@gmail.com>
2025-09-30 17:32:02 +0200

    new grid.connectivities

--- model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/ibm.py
+++ model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/ibm.py
@@ -388,17 +388,16 @@ class ImmersedBoundaryMethodMasks:
 
             log.info(f"IBM: nr. of masked cells: {xp.sum(full_cell_mask_np)}")
 
-            #c2e = grid.connectivities[dims.C2EDim]
-            c2e = grid.connectivities["C2E"]
+            c2e = grid.connectivities["C2E"].ndarray
             for k in range(grid.num_levels + 1):
                 half_edge_mask_np[xp.unique(c2e[xp.where(half_cell_mask_np[:, k])[0]]), k] = True
             full_edge_mask_np = half_edge_mask_np[:, :-1]
 
-            c2v = grid.connectivities[dims.C2VDim]
+            c2v = grid.connectivities["C2V"].ndarray
             for k in range(grid.num_levels):
                 full_vertex_mask_np[xp.unique(c2v[xp.where(full_cell_mask_np[:, k])[0]]), k] = True
 
-            c2e2c = grid.connectivities[dims.C2E2CDim]
+            c2e2c = grid.connectivities["C2E2C"].ndarray
             for k in range(grid.num_levels):
                 neigh_full_cell_mask_np[
                     xp.unique(c2e2c[xp.where(full_cell_mask_np[:, k])[0]]), k
