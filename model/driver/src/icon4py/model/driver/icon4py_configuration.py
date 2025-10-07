9f0b3193ef9176b51cbac8645ca6525c39fcb89e
Philipp Stark <starkp@ethz.ch>
2025-10-07 15:42:10 +0200

    use shorter test case to be also able to run on cpu

--- model/driver/src/icon4py/model/driver/icon4py_configuration.py
+++ model/driver/src/icon4py/model/driver/icon4py_configuration.py
@@ -144,7 +144,7 @@ def read_config(
     def _jablownoski_Williamson_config():
         icon_run_config = Icon4pyRunConfig(
             dtime=datetime.timedelta(seconds=30.0),
-            end_date=datetime.datetime(1, 1, 1, 12, 0, 0),
+            end_date=datetime.datetime(1, 1, 1, 2, 0, 0),
             apply_initial_stabilization=False,
             n_substeps=5,
             backend=backend,
