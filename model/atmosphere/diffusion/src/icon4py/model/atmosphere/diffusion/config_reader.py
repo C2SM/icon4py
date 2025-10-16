fccd524722af65139b9130f9f051bebc0dab1f09
Magdalena Luz <luzm@ethz.ch>
2025-10-16 14:07:20 +0200

    implement update on path

--- model/atmosphere/diffusion/src/icon4py/model/atmosphere/diffusion/config_reader.py
+++ model/atmosphere/diffusion/src/icon4py/model/atmosphere/diffusion/config_reader.py
@@ -1,6 +1,16 @@
+import logging
 import pathlib
 from typing import TypeVar, Generic, overload
 import omegaconf as oc
+
+#TODO (@halungge):
+#  - interpolation
+
+# update from json, dataclass
+# error cases for update...
+
+log = logging.getLogger(__file__)
+
 
 T = TypeVar('T')
 """ T is a data class to which needs to have all its members type annotated with types that
@@ -17,18 +27,24 @@ class ConfigReader(Generic[T]):
     @overload
     def update(self, patch:T):
         assert type(patch) == self._schema, f"wrong type for config, expected {self._schema} but got {type(patch)}"
-        update = oc.OmegaConf.structured(patch)
-        self._config = oc.OmegaConf.merge(self._config, update)
+
+        try:
+            update = oc.OmegaConf.structured(patch)
+            self._config = oc.OmegaConf.merge(self._config, update)
+        except oc.ValidationError as e:
+            log.error(f"Patch {patch} does not validate against configuration {self._schema}")
 
     @overload
-    def update(self, file: str):
-        ...
+    def update(self, file: pathlib.Path|str):
+        try:
+            config_from_file = oc.OmegaConf.load(file)
+            merged = oc.OmegaConf.merge(self._config, config_from_file)
+            self._config = merged
+        except oc.ValidationError as e:
+            log.error(f"file {file} does not validate against configuration {self._schema}")
 
-    @overload
-    def update(self, file: pathlib.Path):
-        ...
 
-    def update(self, update: T| str| pathlib.Path):
+    def update(self, update: T | str| pathlib.Path):
         ...
 
     def get_config(self)->T:
--- model/atmosphere/diffusion/tests/diffusion/unit_tests/test_config_reader.py
+++ model/atmosphere/diffusion/tests/diffusion/unit_tests/test_config_reader.py
@@ -1,6 +1,8 @@
 import dataclasses
+import enum
+import pathlib
+from typing import Optional
 
-import boltons.funcutils
 import pytest
 
 from icon4py.model.atmosphere.diffusion import config_reader
@@ -8,30 +10,74 @@ import omegaconf as oc
 
 
 @dataclasses.dataclass
+class OptionalFoo:
+    a: int = oc.MISSING
+    b: str | None = None
+    c: list[int] = dataclasses.field(default_factory=list)
+
+@dataclasses.dataclass
 class Foo:
     a: int
     b: str
     c: list[int]
 
-def test_config_reader_raises_missing_value():
-    reader = config_reader.ConfigReader(Foo)
-    with pytest.raises(oc.MissingMandatoryValue):
-        reader.get_config()
 
 
 
+class Meridiem(enum.Enum):
+    AM = 1
+    PM = 2
+
+@dataclasses.dataclass
+class Time:
+    hour:int = 0
+    minutes: int = 0
+    seconds: int = 0
+    meridiem: Meridiem = Meridiem.AM
 
 
-
-def test_config_reader():
+def test_config_reader_validate_default_config()->None:
     reader = config_reader.ConfigReader(Foo(1, "b", [1,2,3]))
     foo = reader.get_config()
     assert foo.a == 1
     assert foo.b == "b"
     assert foo.c == [1,2,3]
 
+def test_config_reader_raises_missing_value()->None:
+    reader = config_reader.ConfigReader(Foo)
+    with pytest.raises(oc.MissingMandatoryValue):
+        reader.get_config()
 
-def test_config_reader_update():
+
+def test_config_reader_raises_for_missing()->None:
+    reader = config_reader.ConfigReader(OptionalFoo)
+    with pytest.raises(oc.MissingMandatoryValue) as e:
+        reader.get_config()
+
+def test_config_reader_type_validates()->None:
+    reader = config_reader.ConfigReader(Foo)
+    wrong_foo = Foo("a", "b", [1, 2, 3])
+    with pytest.raises(oc.ValidationError):
+        reader.update(wrong_foo)
+
+
+def test_config_reader_supports_optional()->None:
+    reader = config_reader.ConfigReader(OptionalFoo(a=3))
+    config = reader.get_config()
+    assert len(config.c) == 0
+    assert config.a == 3
+    assert config.b is None
+
+
+def test_config_reader_default_config_equals_config()->None:
+    reader = config_reader.ConfigReader(Foo(1, "b", [1,2,3]))
+    foo = reader.get_config()
+    default = reader.default
+    assert default == foo
+
+
+
+def test_config_reader_update_from_dataclass()->None:
     reader = config_reader.ConfigReader(Foo(1, "b", [1, 2]))
     original_config = reader.get_config()
     assert original_config.a == 1
@@ -41,18 +87,19 @@ def test_config_reader_update():
     update = reader.get_config()
     assert update.a == 2
     assert update.b == "b*"
-    # TODO should Sequences replace or append?, same question for general dicts
+    # TODO (@halungge): should Sequences replace or append?, same question for general dicts
     assert update.c == [8,7]
 
-def test_config_reader_type_validates():
-    reader = config_reader.ConfigReader(Foo)
-    wrong_foo = Foo("a", "b", [1, 2, 3])
-    with pytest.raises(oc.ValidationError):
-        reader.update(wrong_foo)
+def test_config_reader_update_from_file()->None:
+    reader = config_reader.ConfigReader(Foo(1, "b", [1, 2]))
+    original_config = reader.get_config()
+    file = pathlib.Path(__file__).parent.joinpath("foo_update.yaml")
+    reader.update(file)
+    config = reader.get_config()
+    assert config.a == 42
+    assert config.b == "this is the update"
+    assert config.c == original_config.c
 
 
-def test_config_reader_default_config_equals_config():
-    reader = config_reader.ConfigReader(Foo(1, "b", [1,2,3]))
-    foo = reader.get_config()
-    default = reader.default
-    assert default == foo
+
+
