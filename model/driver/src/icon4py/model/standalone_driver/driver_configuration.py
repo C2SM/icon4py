0f8bd45c629092c08a62bd023cf2efad01155726
yiluchen1066 <yilu.chen@c2sm.ethz.ch>
2025-10-21 14:13:45 +0200

    [WIP] configuration refactor in

--- model/driver/src/icon4py/model/standalone_driver/driver_configuration.py
+++ model/driver/src/icon4py/model/standalone_driver/driver_configuration.py
@@ -9,6 +9,7 @@ import dataclasses
 import datetime
 import functools
 import logging
+import os
 
 import gt4py.next.typing as gtx_typing
 from gt4py.next import metrics as gtx_metrics
@@ -17,6 +18,7 @@ from icon4py.model.atmosphere.diffusion import diffusion
 from icon4py.model.atmosphere.dycore import solve_nonhydro as solve_nh
 from icon4py.model.common import type_alias as ta
 from icon4py.model.common.grid import vertical as v_grid
+from pathlib import Path
 
 
 log = logging.getLogger(__name__)
@@ -25,6 +27,10 @@ log = logging.getLogger(__name__)
 @dataclasses.dataclass(frozen=True)
 class Icon4pyRunConfig:  # TODO (Yilu) think of a better name
     backend: gtx_typing.Backend
+    grid_path: Path
+    configuration_file_path: Path
+    output_path: Path
+
     dtime: datetime.timedelta = datetime.timedelta(seconds=600.0)  # length of a time step
     start_date: datetime.datetime = datetime.datetime(1, 1, 1, 0, 0, 0)
     end_date: datetime.datetime = datetime.datetime(1, 1, 1, 1, 0, 0)
@@ -35,36 +41,25 @@ class Icon4pyRunConfig:  # TODO (Yilu) think of a better name
 
     output_statistics: bool = False
 
-    apply_initial_stabilization: bool = True  # TODO (Yilu) remove
-    """
-    ltestcase in ICON
-        ltestcase has been renamed as apply_initial_stabilization because it is only used for extra damping for
-        initial steps in timeloop.
-    """
-
-    restart_mode: bool = False  # TODO (Yilu) remove
-
-    # TODO (Yilu)
-    # TODO (Yilu) add three paths: grid_path, configuration_file_path, output_path
+    def __post_init__(self):
+        for name in ["grid_path", "configuration_file_path", "output_path"]:
+            path = Path(os.path.expandvars(str(getattr(self, name)))).expanduser()
+            object.__setattr__(self, name, path)
+            if not path.exists():
+                log.warning(f"The path for {name} does not exist: {path}")
+            elif not path.is_socket():
+                raise ValueError(f"{name} must be a file: {path}")
 
     @functools.cached_property
     def backend(self):
         return self.backend
 
 
-# TODO (Yilu) remove Icon4pyConfig
-@dataclasses.dataclass
-class Icon4pyConfig:
-    run_config: Icon4pyRunConfig
-    vertical_grid_config: v_grid.VerticalGridConfig
-    diffusion_config: diffusion.DiffusionConfig
-    solve_nonhydro_config: solve_nh.NonHydrostaticConfig
-
-
 # TODO (Yilu) the read_config should only return the configure for jabw
 def read_config(
     backend: gtx_typing.Backend,
 ) -> Icon4pyConfig:
+
     def _jabw_vertical_config():
         return v_grid.VerticalGridConfig(
             num_levels=35,
@@ -98,6 +93,7 @@ def read_config(
         jabw_vertical_config = _jabw_vertical_config()
         jabw_diffusion_config = _jabw_diffusion_config(icon_run_config.ndyn_substeps)
         jabw_nonhydro_config = _jabw_nonhydro_config()
+
         return (
             icon_run_config,
             jabw_vertical_config,
