8928f732ca6164606b714c7375b014bba77bdd7c
yiluchen1066 <yilu.chen@c2sm.ethz.ch>
2025-10-23 17:46:13 +0200

    small grid (try)

--- model/testing/src/icon4py/model/testing/fixtures/benchmark.py
+++ model/testing/src/icon4py/model/testing/fixtures/benchmark.py
@@ -34,7 +34,7 @@ if TYPE_CHECKING:
 
 @pytest.fixture(
     scope="session",
-    params=[definitions.Grids.R02B07_GLOBAL, definitions.Grids.MCH_OPR_R19B08_DOMAIN01],
+    params=[definitions.Grids.R02B04_GLOBAL, definitions.Grids.MCH_CH_R04B09_DSL],
 )
 def benchmark_grid(request: pytest.FixtureRequest) -> definitions.GridDescription:
     """Default parametrization for benchmark testing.
