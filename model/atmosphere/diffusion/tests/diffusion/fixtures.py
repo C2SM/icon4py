64415800e984cfeaff712f007adb371ebe71f693
yiluchen1066 <yilu.chen@c2sm.ethz.ch>
2025-10-24 14:30:21 +0200

    small edit

--- model/atmosphere/diffusion/tests/diffusion/fixtures.py
+++ model/atmosphere/diffusion/tests/diffusion/fixtures.py
@@ -13,7 +13,6 @@ from icon4py.model.testing import serialbox as sb
 from icon4py.model.testing.fixtures.benchmark import (
     benchmark_grid,
     geometry_field_source,
-    grid_manager,
     interpolation_field_source,
     metrics_field_source,
 )
