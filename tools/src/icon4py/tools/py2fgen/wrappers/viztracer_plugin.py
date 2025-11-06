8ef2a3413a75de11d3ebe3ccc8472ca731aeb731
Hannes Vogt <vogt@hey.com>
2025-11-06 15:15:00 +0100

    separate tracer per function

--- tools/src/icon4py/tools/py2fgen/wrappers/viztracer_plugin.py
+++ tools/src/icon4py/tools/py2fgen/wrappers/viztracer_plugin.py
@@ -27,9 +27,10 @@ from icon4py.tools.py2fgen import runtime_config
 
 @dataclasses.dataclass
 class _FunctionTracer:
+    name: str
     start: int
     stop: int
-    _tracer: viztracer.VizTracer
+    _tracer: viztracer.VizTracer = dataclasses.field(default_factory=viztracer.VizTracer)
     _counter: int = 0
 
     def enter(self) -> None:
@@ -41,49 +42,35 @@ class _FunctionTracer:
         if self.start < self._counter <= self.stop:
             # "flush_as_finish" to avoid incomplete calls to `disable` which would visualize as nested calls
             self._tracer.stop(stop_option="flush_as_finish")
+        if self._counter == self.stop:
+            self._tracer.save(f"viztracer_{self.name}.json")
 
 
 def function_tracer(
-    tracer: viztracer.VizTracer, start: int, stop: int
+    name: str, start: int, stop: int
 ) -> tuple[Callable[[], None], Callable[[], None]]:
-    ft = _FunctionTracer(start, stop, tracer)
+    ft = _FunctionTracer(name, start, stop)
     return ft.enter, ft.exit
-
-
-class Init:
-    def __init__(self) -> None:
-        """Initialize the VizTracer plugin."""
-
-        self.tracer = viztracer.VizTracer()
-
-        if "ICON4PY_TRACING_RANGES" in os.environ:
-            tracing_ranges_str = os.environ["ICON4PY_TRACING_RANGES"]
-            tracing_ranges = {
-                name: (int(start), int(stop))
-                for range_spec in tracing_ranges_str.split(",")
-                for name, start, stop in [range_spec.split(":")]
-            }
-        else:
-            # Some useful defaults for the ICON4Py dycore and diffusion granules
-            tracing_ranges = {
-                "solve_nh_run": (10, 15),
-                "diffusion_run": (2, 4),
-            }
-
-        for name, (start, stop) in tracing_ranges.items():
-            enter, exit_ = function_tracer(self.tracer, start, stop)
-            runtime_config.HOOK_BINDINGS_FUNCTION_ENTER[name] = enter
-            runtime_config.HOOK_BINDINGS_FUNCTION_EXIT[name] = exit_
-
-    def __del__(self) -> None:
-        """Save the VizTracer trace on program exit."""
-        self.tracer.save("viztracer.json")
-
-
-keep_alive: Init | None = None
 
 
 def init() -> None:
     """Initialize the VizTracer plugin."""
-    global keep_alive  # noqa: PLW0603
-    keep_alive = Init()
+
+    if "ICON4PY_TRACING_RANGES" in os.environ:
+        tracing_ranges_str = os.environ["ICON4PY_TRACING_RANGES"]
+        tracing_ranges = {
+            name: (int(start), int(stop))
+            for range_spec in tracing_ranges_str.split(",")
+            for name, start, stop in [range_spec.split(":")]
+        }
+    else:
+        # Some useful defaults for the ICON4Py dycore and diffusion granules
+        tracing_ranges = {
+            "solve_nh_run": (10, 15),
+            "diffusion_run": (2, 4),
+        }
+
+    for name, (start, stop) in tracing_ranges.items():
+        enter, exit_ = function_tracer(name, start, stop)
+        runtime_config.HOOK_BINDINGS_FUNCTION_ENTER[name] = enter
+        runtime_config.HOOK_BINDINGS_FUNCTION_EXIT[name] = exit_
