1e9dc7510ae2639e5bc55576932d50015bfbc82c
Magdalena Luz <luzm@ethz.ch>
2025-11-12 13:53:05 +0100

    fix unit tests

--- model/common/tests/common/metrics/unit_tests/test_metric_fields.py
+++ model/common/tests/common/metrics/unit_tests/test_metric_fields.py
@@ -394,35 +394,23 @@ def test_compute_pressure_gradient_downward_extrapolation_mask_distance(
     horizontal_start_edge = icon_grid.start_index(
         edge_domain(horizontal.Zone.LATERAL_BOUNDARY_LEVEL_3)
     )
+    xp = data_alloc.import_array_ns(backend)
+    flat_idx_max = mf.compute_flat_max_idx(
+        e2c=icon_grid.get_connectivity("E2C").ndarray,
+        z_mc=z_mc.ndarray,
+        c_lin_e=c_lin_e.ndarray,
+        z_ifc=z_ifc.ndarray,
+        k_lev=k.ndarray,
+        array_ns=xp)
 
-    mf.compute_flat_idx.with_backend(backend)(
-        z_mc=z_mc,
-        c_lin_e=c_lin_e,
-        z_ifc=z_ifc,
-        k_lev=k,
-        flat_idx=flat_idx,
-        horizontal_start=horizontal_start_edge,
-        horizontal_end=icon_grid.num_edges,
-        vertical_start=gtx.int32(0),
-        vertical_end=icon_grid.num_levels,
-        offset_provider={
-            "E2C": icon_grid.get_connectivity("E2C"),
-            "Koff": dims.KDim,
-        },
-    )
-    flat_idx_max = gtx.as_field(
-        (dims.EdgeDim,),
-        xp.max(flat_idx.asnumpy(), axis=1),
-        dtype=gtx.int32,
-        allocator=backend,
-    )
+
 
     mf.compute_pressure_gradient_downward_extrapolation_mask_distance.with_backend(backend)(
         z_mc=z_mc,
         topography=topography,
         c_lin_e=c_lin_e,
         e_owner_mask=grid_savepoint.e_owner_mask(),
-        flat_idx_max=flat_idx_max,
+        flat_idx_max=gtx.as_field((dims.EdgeDim, ), data=flat_idx_max,allocator=backend),
         e_lev=edges,
         k_lev=k,
         pg_edgeidx_dsl=edge_mask,
@@ -520,47 +508,3 @@ def test_compute_horizontal_mask_for_3d_divdamp(
     assert testing_helpers.dallclose(
         horizontal_mask_for_3d_divdamp.asnumpy(), hmask_dd3d_ref.asnumpy()
     )
-
-
-@pytest.mark.level("unit")
-@pytest.mark.datatest
-def test_compute_theta_exner_ref_mc(
-    metrics_savepoint: sb.MetricSavepoint,
-    icon_grid: base_grid.Grid,
-    backend: gtx_typing.Backend,
-) -> None:
-    exner_ref_mc_full = data_alloc.zero_field(icon_grid, dims.CellDim, dims.KDim, allocator=backend)
-    theta_ref_mc_full = data_alloc.zero_field(icon_grid, dims.CellDim, dims.KDim, allocator=backend)
-    t0sl_bg = constants.SEA_LEVEL_TEMPERATURE
-    del_t_bg = constants.DELTA_TEMPERATURE
-    h_scal_bg = constants.HEIGHT_SCALE_FOR_REFERENCE_ATMOSPHERE
-    grav = constants.GRAV
-    rd = constants.RD
-    p0sl_bg = constants.SEA_LEVEL_PRESSURE
-    rd_o_cpd = constants.RD_O_CPD
-    p0ref = constants.REFERENCE_PRESSURE
-    exner_ref_mc_ref = metrics_savepoint.exner_ref_mc()
-    theta_ref_mc_ref = metrics_savepoint.theta_ref_mc()
-    z_mc = metrics_savepoint.z_mc()
-
-    mf.compute_theta_exner_ref_mc.with_backend(backend)(
-        z_mc=z_mc,
-        exner_ref_mc=exner_ref_mc_full,
-        theta_ref_mc=theta_ref_mc_full,
-        t0sl_bg=t0sl_bg,
-        del_t_bg=del_t_bg,
-        h_scal_bg=h_scal_bg,
-        grav=grav,
-        rd=rd,
-        p0sl_bg=p0sl_bg,
-        rd_o_cpd=rd_o_cpd,
-        p0ref=p0ref,
-        horizontal_start=0,
-        horizontal_end=icon_grid.num_cells,
-        vertical_start=0,
-        vertical_end=icon_grid.num_levels,
-        offset_provider={},
-    )
-
-    assert testing_helpers.dallclose(exner_ref_mc_ref.asnumpy(), exner_ref_mc_full.asnumpy())
-    assert testing_helpers.dallclose(theta_ref_mc_ref.asnumpy(), theta_ref_mc_full.asnumpy())
