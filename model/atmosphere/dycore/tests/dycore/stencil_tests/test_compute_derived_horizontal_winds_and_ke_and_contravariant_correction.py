69fb25b9ef4b4013d747c43f55cb023154232fed
Ioannis Magkanaris <iomagkanaris@gmail.com>
2025-11-14 11:29:57 +0100

    Cleaned up TestComputeDerivedHorizontalWindsAndKEAndHorizontalAdvectionofWAndContravariantCorrection

--- model/atmosphere/dycore/tests/dycore/stencil_tests/test_compute_derived_horizontal_winds_and_ke_and_contravariant_correction.py
+++ model/atmosphere/dycore/tests/dycore/stencil_tests/test_compute_derived_horizontal_winds_and_ke_and_contravariant_correction.py
@@ -142,6 +142,7 @@ def extrapolate_to_surface_numpy(wgtfacq_e: np.ndarray, vn: np.ndarray) -> np.nd
 
 
 @pytest.mark.embedded_remap_error
+@pytest.mark.continuous_benchmarking
 class TestComputeDerivedHorizontalWindsAndKEAndHorizontalAdvectionofWAndContravariantCorrection(
     stencil_tests.StencilTest
 ):
@@ -314,14 +315,17 @@ class TestComputeDerivedHorizontalWindsAndKEAndHorizontalAdvectionofWAndContrava
         c_intp = data_alloc.random_field(grid, dims.VertexDim, dims.V2CDim)
 
         nlev = grid.num_levels
-        nflatlev = 11
+        nflatlev = 5
 
         skip_compute_predictor_vertical_advection = request.param[
             "skip_compute_predictor_vertical_advection"
         ]
 
-        horizontal_start = 0
-        horizontal_end = grid.num_edges
+        edge_domain = h_grid.domain(dims.EdgeDim)
+        horizontal_start = grid.start_index(
+            edge_domain(h_grid.Zone.LATERAL_BOUNDARY_LEVEL_5)
+        )
+        horizontal_end = grid.end_index(edge_domain(h_grid.Zone.HALO_LEVEL_2))
         vertical_start = 0
         vertical_end = nlev + 1
 
@@ -350,31 +354,3 @@ class TestComputeDerivedHorizontalWindsAndKEAndHorizontalAdvectionofWAndContrava
             vertical_start=vertical_start,
             vertical_end=vertical_end,
         )
-
-
-@pytest.mark.continuous_benchmarking
-class TestComputeDerivedHorizontalWindsAndKEAndHorizontalAdvectionofWAndContravariantCorrectionContinuousBenchmarking(
-    TestComputeDerivedHorizontalWindsAndKEAndHorizontalAdvectionofWAndContravariantCorrection
-):
-    @pytest.fixture(
-        params=[{"skip_compute_predictor_vertical_advection": value} for value in [True, False]],
-        ids=lambda param: f"skip_compute_predictor_vertical_advection[{param['skip_compute_predictor_vertical_advection']}]",
-    )
-    def input_data(
-        self, grid: base.Grid, request: pytest.FixtureRequest
-    ) -> dict[str, gtx.Field | state_utils.ScalarType]:
-        base_data = TestComputeDerivedHorizontalWindsAndKEAndHorizontalAdvectionofWAndContravariantCorrection.input_data.__wrapped__(
-            self, grid, request
-        )
-        base_data["skip_compute_predictor_vertical_advection"] = request.param[
-            "skip_compute_predictor_vertical_advection"
-        ]
-        base_data["nflatlev"] = 6
-        edge_domain = h_grid.domain(dims.EdgeDim)
-        base_data["horizontal_start"] = grid.start_index(
-            edge_domain(h_grid.Zone.LATERAL_BOUNDARY_LEVEL_5)
-        )
-        base_data["horizontal_end"] = grid.end_index(edge_domain(h_grid.Zone.HALO_LEVEL_2))
-        base_data["vertical_start"] = 0
-        base_data["vertical_end"] = grid.num_levels + 1
-        return base_data
