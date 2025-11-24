eb1f80bb85ae9f0ee4b173de31d65acc8e4dd63f
Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>
2025-11-24 14:25:08 +0100

    test edits

--- model/common/tests/common/metrics/mpi_tests/test_parallel_metrics.py
+++ model/common/tests/common/metrics/mpi_tests/test_parallel_metrics.py
@@ -80,3 +80,57 @@ def test_distributed_metrics_attrs(
     field = factory.get(attrs_name).asnumpy()
     field_ref = metrics_savepoint.__getattribute__(metrics_name)().asnumpy()
     assert test_utils.dallclose(field, field_ref, rtol=1e-8, atol=1.0e-8)
+
+@pytest.mark.datatest
+@pytest.mark.mpi
+@pytest.mark.parametrize("processor_props", [True], indirect=True)
+@pytest.mark.parametrize(
+    "attrs_name, metrics_name",
+    [
+        # (attrs.NFLAT_GRADP, "nflat_gradp"), # this is a value, move to another test
+        (attrs.DDQZ_Z_FULL, "ddqz_z_full"),
+        (attrs.INV_DDQZ_Z_FULL, "inv_ddqz_z_full"),
+        (attrs.COEFF1_DWDZ, "coeff1_dwdz"),
+        (attrs.COEFF2_DWDZ, "coeff2_dwdz"),
+        (attrs.THETA_REF_MC, "theta_ref_mc"),
+        (attrs.EXNER_REF_MC, "exner_ref_mc"),
+        (attrs.D2DEXDZ2_FAC1_MC, "d2dexdz2_fac1_mc"),
+        (attrs.D2DEXDZ2_FAC2_MC, "d2dexdz2_fac2_mc"),
+        (attrs.DDXN_Z_FULL, "ddxn_z_full"),
+        (attrs.DDXT_Z_FULL, "ddxt_z_full"),
+        (attrs.EXNER_W_IMPLICIT_WEIGHT_PARAMETER, "vwind_impl_wgt"),
+        (attrs.EXNER_W_EXPLICIT_WEIGHT_PARAMETER, "vwind_expl_wgt"),
+        # (attrs.EXNER_EXFAC, "exner_exfac"),
+        (attrs.PG_EDGEDIST_DSL, "pg_exdist"),
+        (attrs.PG_EDGEIDX_DSL, "pg_edgeidx_dsl"),
+        (attrs.MASK_PROG_HALO_C, "mask_prog_halo_c"),
+        (attrs.BDY_HALO_C, "bdy_halo_c"),
+        (attrs.HORIZONTAL_MASK_FOR_3D_DIVDAMP, "hmask_dd3d"),
+        # (attrs.WGTFACQ_E, "wgtfacq_e_dsl"), # field_ref = metrics_savepoint.wgtfacq_e_dsl(field.shape[1])
+        (attrs.WGTFAC_C, "wgtfac_c"),
+        (attrs.MASK_HDIFF, "mask_hdiff"),
+        (attrs.ZD_DIFFCOEF_DSL, "zd_diffcoef"),
+        (attrs.ZD_INTCOEF_DSL, "zd_intcoef"),
+        (attrs.ZD_VERTOFFSET_DSL, "zd_vertoffset"),
+    ],
+)
+def test_distributed_metrics_attrs_no_halo(
+    backend: gtx_typing.Backend,
+    metrics_savepoint: sb.MetricSavepoint,
+    grid_savepoint: sb.IconGridSavepoint,
+    processor_props: decomposition.ProcessProperties,
+    decomposition_info: decomposition.DecompositionInfo,
+    metrics_factory_from_savepoint: metrics_factory.MetricsFieldsFactory,
+    attrs_name: str,
+    metrics_name: str,
+    experiment: test_defs.Experiment,
+) -> None:
+    parallel_helpers.check_comm_size(processor_props)
+    parallel_helpers.log_process_properties(processor_props)
+    parallel_helpers.log_local_field_size(decomposition_info)
+    factory = metrics_factory_from_savepoint
+
+    field = factory.get(attrs_name).asnumpy()
+    field_ref = metrics_savepoint.__getattribute__(metrics_name)().asnumpy()
+    assert test_utils.dallclose(field, field_ref, rtol=1e-7, atol=1.0e-8)
+
