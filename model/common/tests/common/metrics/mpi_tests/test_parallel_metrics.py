d88663891c252b84c42b5e6af5dff402824e3b23
Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>
2025-11-28 10:01:36 +0100

    ran pre-commit

--- model/common/tests/common/metrics/mpi_tests/test_parallel_metrics.py
+++ model/common/tests/common/metrics/mpi_tests/test_parallel_metrics.py
@@ -43,93 +43,93 @@ if TYPE_CHECKING:
     from icon4py.model.testing import serialbox as sb
 
 
-# @pytest.mark.datatest
-# @pytest.mark.mpi
-# @pytest.mark.parametrize("processor_props", [True], indirect=True)
-# @pytest.mark.parametrize(
-#     "attrs_name, metrics_name",
-#     [
-#         (attrs.CELL_HEIGHT_ON_HALF_LEVEL, "z_ifc"),
-#         (attrs.DDQZ_Z_FULL_E, "ddqz_z_full_e"),
-#         (attrs.ZDIFF_GRADP, "zdiff_gradp"),
-#         (attrs.VERTOFFSET_GRADP, "vertoffset_gradp"),
-#         (attrs.Z_MC, "z_mc"),
-#         (attrs.DDQZ_Z_HALF, "ddqz_z_half"),
-#         (attrs.SCALING_FACTOR_FOR_3D_DIVDAMP, "scalfac_dd3d"),
-#         (attrs.RAYLEIGH_W, "rayleigh_w"),
-#         (attrs.COEFF_GRADEKIN, "coeff_gradekin"),
-#     ],
-# )
-# def test_distributed_metrics_attrs(
-#     backend: gtx_typing.Backend,
-#     metrics_savepoint: sb.MetricSavepoint,
-#     grid_savepoint: sb.IconGridSavepoint,
-#     processor_props: decomposition.ProcessProperties,
-#     decomposition_info: decomposition.DecompositionInfo,
-#     metrics_factory_from_savepoint: metrics_factory.MetricsFieldsFactory,
-#     attrs_name: str,
-#     metrics_name: str,
-#     experiment: test_defs.Experiment,
-# ) -> None:
-#     parallel_helpers.check_comm_size(processor_props)
-#     parallel_helpers.log_process_properties(processor_props)
-#     parallel_helpers.log_local_field_size(decomposition_info)
-#     factory = metrics_factory_from_savepoint
-#     print(f"computed flatlev {factory.vertical_grid.nflatlev}, expected{grid_savepoint.nflatlev()}")
-#
-#     field = factory.get(attrs_name).asnumpy()
-#     field_ref = metrics_savepoint.__getattribute__(metrics_name)().asnumpy()
-#     assert test_utils.dallclose(field, field_ref, rtol=1e-8, atol=1.0e-8)
-#
-#
-# @pytest.mark.datatest
-# @pytest.mark.mpi
-# @pytest.mark.parametrize("processor_props", [True], indirect=True)
-# @pytest.mark.parametrize(
-#     "attrs_name, metrics_name",
-#     [
-#         (attrs.DDQZ_Z_FULL, "ddqz_z_full"),
-#         (attrs.INV_DDQZ_Z_FULL, "inv_ddqz_z_full"),
-#         (attrs.COEFF1_DWDZ, "coeff1_dwdz"),
-#         (attrs.COEFF2_DWDZ, "coeff2_dwdz"),
-#         (attrs.THETA_REF_MC, "theta_ref_mc"),
-#         (attrs.EXNER_REF_MC, "exner_ref_mc"),
-#         (attrs.RHO_REF_ME, "rho_ref_me"),
-#         (attrs.THETA_REF_ME, "theta_ref_me"),
-#         (attrs.D2DEXDZ2_FAC1_MC, "d2dexdz2_fac1_mc"),
-#         (attrs.D2DEXDZ2_FAC2_MC, "d2dexdz2_fac2_mc"),
-#         (attrs.DDXN_Z_FULL, "ddxn_z_full"),
-#         (attrs.DDXT_Z_FULL, "ddxt_z_full"),
-#         (attrs.EXNER_W_IMPLICIT_WEIGHT_PARAMETER, "vwind_impl_wgt"),
-#         (attrs.EXNER_W_EXPLICIT_WEIGHT_PARAMETER, "vwind_expl_wgt"),
-#         (attrs.PG_EDGEDIST_DSL, "pg_exdist"),
-#         (attrs.PG_EDGEIDX_DSL, "pg_edgeidx_dsl"),
-#         (attrs.MASK_PROG_HALO_C, "mask_prog_halo_c"),
-#         (attrs.BDY_HALO_C, "bdy_halo_c"),
-#         (attrs.HORIZONTAL_MASK_FOR_3D_DIVDAMP, "hmask_dd3d"),
-#         (attrs.WGTFAC_C, "wgtfac_c"),
-#         (attrs.EXNER_EXFAC, "exner_exfac"),
-#     ],
-# )
-# def test_distributed_metrics_attrs_no_halo(
-#     backend: gtx_typing.Backend,
-#     metrics_savepoint: sb.MetricSavepoint,
-#     grid_savepoint: sb.IconGridSavepoint,
-#     processor_props: decomposition.ProcessProperties,
-#     decomposition_info: decomposition.DecompositionInfo,
-#     metrics_factory_from_savepoint: metrics_factory.MetricsFieldsFactory,
-#     attrs_name: str,
-#     metrics_name: str,
-#     experiment: test_defs.Experiment,
-# ) -> None:
-#     parallel_helpers.check_comm_size(processor_props)
-#     parallel_helpers.log_process_properties(processor_props)
-#     parallel_helpers.log_local_field_size(decomposition_info)
-#     factory = metrics_factory_from_savepoint
-#
-#     field = factory.get(attrs_name).asnumpy()
-#     field_ref = metrics_savepoint.__getattribute__(metrics_name)().asnumpy()
-#     assert test_utils.dallclose(field, field_ref, rtol=1e-7, atol=1.0e-8)
+@pytest.mark.datatest
+@pytest.mark.mpi
+@pytest.mark.parametrize("processor_props", [True], indirect=True)
+@pytest.mark.parametrize(
+    "attrs_name, metrics_name",
+    [
+        (attrs.CELL_HEIGHT_ON_HALF_LEVEL, "z_ifc"),
+        (attrs.DDQZ_Z_FULL_E, "ddqz_z_full_e"),
+        (attrs.ZDIFF_GRADP, "zdiff_gradp"),
+        (attrs.VERTOFFSET_GRADP, "vertoffset_gradp"),
+        (attrs.Z_MC, "z_mc"),
+        (attrs.DDQZ_Z_HALF, "ddqz_z_half"),
+        (attrs.SCALING_FACTOR_FOR_3D_DIVDAMP, "scalfac_dd3d"),
+        (attrs.RAYLEIGH_W, "rayleigh_w"),
+        (attrs.COEFF_GRADEKIN, "coeff_gradekin"),
+    ],
+)
+def test_distributed_metrics_attrs(
+    backend: gtx_typing.Backend,
+    metrics_savepoint: sb.MetricSavepoint,
+    grid_savepoint: sb.IconGridSavepoint,
+    processor_props: decomposition.ProcessProperties,
+    decomposition_info: decomposition.DecompositionInfo,
+    metrics_factory_from_savepoint: metrics_factory.MetricsFieldsFactory,
+    attrs_name: str,
+    metrics_name: str,
+    experiment: test_defs.Experiment,
+) -> None:
+    parallel_helpers.check_comm_size(processor_props)
+    parallel_helpers.log_process_properties(processor_props)
+    parallel_helpers.log_local_field_size(decomposition_info)
+    factory = metrics_factory_from_savepoint
+    print(f"computed flatlev {factory.vertical_grid.nflatlev}, expected{grid_savepoint.nflatlev()}")
+
+    field = factory.get(attrs_name).asnumpy()
+    field_ref = metrics_savepoint.__getattribute__(metrics_name)().asnumpy()
+    assert test_utils.dallclose(field, field_ref, rtol=1e-8, atol=1.0e-8)
+
+
+@pytest.mark.datatest
+@pytest.mark.mpi
+@pytest.mark.parametrize("processor_props", [True], indirect=True)
+@pytest.mark.parametrize(
+    "attrs_name, metrics_name",
+    [
+        (attrs.DDQZ_Z_FULL, "ddqz_z_full"),
+        (attrs.INV_DDQZ_Z_FULL, "inv_ddqz_z_full"),
+        (attrs.COEFF1_DWDZ, "coeff1_dwdz"),
+        (attrs.COEFF2_DWDZ, "coeff2_dwdz"),
+        (attrs.THETA_REF_MC, "theta_ref_mc"),
+        (attrs.EXNER_REF_MC, "exner_ref_mc"),
+        (attrs.RHO_REF_ME, "rho_ref_me"),
+        (attrs.THETA_REF_ME, "theta_ref_me"),
+        (attrs.D2DEXDZ2_FAC1_MC, "d2dexdz2_fac1_mc"),
+        (attrs.D2DEXDZ2_FAC2_MC, "d2dexdz2_fac2_mc"),
+        (attrs.DDXN_Z_FULL, "ddxn_z_full"),
+        (attrs.DDXT_Z_FULL, "ddxt_z_full"),
+        (attrs.EXNER_W_IMPLICIT_WEIGHT_PARAMETER, "vwind_impl_wgt"),
+        (attrs.EXNER_W_EXPLICIT_WEIGHT_PARAMETER, "vwind_expl_wgt"),
+        (attrs.PG_EDGEDIST_DSL, "pg_exdist"),
+        (attrs.PG_EDGEIDX_DSL, "pg_edgeidx_dsl"),
+        (attrs.MASK_PROG_HALO_C, "mask_prog_halo_c"),
+        (attrs.BDY_HALO_C, "bdy_halo_c"),
+        (attrs.HORIZONTAL_MASK_FOR_3D_DIVDAMP, "hmask_dd3d"),
+        (attrs.WGTFAC_C, "wgtfac_c"),
+        (attrs.EXNER_EXFAC, "exner_exfac"),
+    ],
+)
+def test_distributed_metrics_attrs_no_halo(
+    backend: gtx_typing.Backend,
+    metrics_savepoint: sb.MetricSavepoint,
+    grid_savepoint: sb.IconGridSavepoint,
+    processor_props: decomposition.ProcessProperties,
+    decomposition_info: decomposition.DecompositionInfo,
+    metrics_factory_from_savepoint: metrics_factory.MetricsFieldsFactory,
+    attrs_name: str,
+    metrics_name: str,
+    experiment: test_defs.Experiment,
+) -> None:
+    parallel_helpers.check_comm_size(processor_props)
+    parallel_helpers.log_process_properties(processor_props)
+    parallel_helpers.log_local_field_size(decomposition_info)
+    factory = metrics_factory_from_savepoint
+
+    field = factory.get(attrs_name).asnumpy()
+    field_ref = metrics_savepoint.__getattribute__(metrics_name)().asnumpy()
+    assert test_utils.dallclose(field, field_ref, rtol=1e-7, atol=1.0e-8)
 
 
 @pytest.mark.datatest
@@ -164,8 +164,6 @@ def test_distributed_metrics_attrs_no_halo_regional(
 
     field = factory.get(attrs_name).asnumpy()
     field_ref = metrics_savepoint.__getattribute__(metrics_name)().asnumpy()
-    import numpy as np
-    breakpoint()
     assert test_utils.dallclose(field, field_ref)
 
 
