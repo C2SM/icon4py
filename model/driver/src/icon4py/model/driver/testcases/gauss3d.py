b0f28c7ef7a593f8a773463e1092c00dfe894a84
yiluchen1066 <yilu.chen@c2sm.ethz.ch>
2025-12-03 16:01:41 +0100

    fix the error on CI

--- model/driver/src/icon4py/model/driver/testcases/gauss3d.py
+++ model/driver/src/icon4py/model/driver/testcases/gauss3d.py
@@ -203,7 +203,7 @@ def model_initialization_gauss3d(  # noqa: PLR0915 [too-many-statements]
         pressure_ndarray,
         pressure_ifc_ndarray,
         grid=grid,
-        backend=backend,
+        allocator=backend,
     )
 
     edge_2_cell_vector_rbf_interpolation.edge_2_cell_vector_rbf_interpolation.with_backend(backend)(
@@ -258,15 +258,15 @@ def model_initialization_gauss3d(  # noqa: PLR0915 [too-many-statements]
     )
 
     diffusion_diagnostic_state = diffusion_states.initialize_diffusion_diagnostic_state(
-        grid=grid, backend=backend
+        grid=grid, allocator=backend
     )
     solve_nonhydro_diagnostic_state = dycore_states.initialize_solve_nonhydro_diagnostic_state(
         perturbed_exner_at_cells_on_model_levels=perturbed_exner,
         grid=grid,
-        backend=backend,
+        allocator=backend,
     )
 
-    prep_adv = dycore_states.initialize_prep_advection(grid=grid, backend=backend)
+    prep_adv = dycore_states.initialize_prep_advection(grid=grid, allocator=backend)
     log.info("Initialization completed.")
 
     return (
