1c541ac58aaee45f45f8d23d7be9d5b3caf87cd4
Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>
2025-12-04 15:55:48 +0100

    Update model/common/src/icon4py/model/common/decomposition/definitions.py
    Co-authored-by: Magdalena <luzm@ethz.ch>

--- model/common/src/icon4py/model/common/decomposition/definitions.py
+++ model/common/src/icon4py/model/common/decomposition/definitions.py
@@ -326,7 +326,7 @@ class SingleNodeRun(RunType):
 
 
 class Reductions(Protocol):
-    def min(self, buffer: data_alloc.NDArray) -> state_utils.ScalarType: ...
+    def min(self, buffer: data_alloc.NDArray, array_ns: ModuleType=np) -> state_utils.ScalarType: ...
 
 
 class SingleNodeReductions(Reductions):
