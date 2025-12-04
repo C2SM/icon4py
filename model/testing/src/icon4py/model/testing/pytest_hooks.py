050bbb5a841e40f86ddd77f53363105e10ef3cfa
Philipp Stark <starkp@ethz.ch>
2025-12-04 13:59:04 +0100

    add single_precision_marker to pytests

--- model/testing/src/icon4py/model/testing/pytest_hooks.py
+++ model/testing/src/icon4py/model/testing/pytest_hooks.py
@@ -35,10 +35,9 @@ def pytest_configure(config):
         "markers",
         "level(name): marks test as unit or integration tests, mostly applicable where both are available",
     )
-
-    # Check if the --enable-mixed-precision option is set and set the environment variable accordingly
-    if config.getoption("--enable-mixed-precision"):
-        os.environ["FLOAT_PRECISION"] = "mixed"
+    config.addinivalue_line(
+        "markers", "single_precision_ready: intended to run if single precision is selected"
+    )
 
     # Handle datatest options: --datatest-only  and --datatest-skip
     if m_option := config.getoption("-m", []):
@@ -48,6 +47,10 @@ def pytest_configure(config):
 
     if config.getoption("--datatest-skip"):
         config.option.markexpr = " and ".join(["not datatest", *m_option])
+
+    if os.environ.get("FLOAT_PRECISION", "double").lower() == "single":
+        # if precision is set to single per env variable, only run tests marked as single_precision_ready
+        config.option.markexpr = " and ".join(["single_precision_ready", *m_option])
 
 
 def pytest_addoption(parser: pytest.Parser):
@@ -85,14 +88,6 @@ def pytest_addoption(parser: pytest.Parser):
 
     with contextlib.suppress(ValueError):
         parser.addoption(
-            "--enable-mixed-precision",
-            action="store_true",
-            help="Switch unit tests from double to mixed-precision",
-            default=False,
-        )
-
-    with contextlib.suppress(ValueError):
-        parser.addoption(
             "--level",
             action="store",
             choices=_TEST_LEVELS,
@@ -107,9 +102,9 @@ def pytest_collection_modifyitems(config, items):
         return
     for item in items:
         if (marker := item.get_closest_marker("level")) is not None:
-            assert all(
-                level in _TEST_LEVELS for level in marker.args
-            ), f"Invalid test level argument on function '{item.name}' - possible values are {_TEST_LEVELS}"
+            assert all(level in _TEST_LEVELS for level in marker.args), (
+                f"Invalid test level argument on function '{item.name}' - possible values are {_TEST_LEVELS}"
+            )
             if test_level not in marker.args:
                 item.add_marker(
                     pytest.mark.skip(
--- noxfile.py
+++ noxfile.py
@@ -152,11 +152,16 @@ def test_model(
     _install_session_venv(session, extras=["fortran", "io", "testing"], groups=["test"])
 
     pytest_args = _selection_to_pytest_args(selection)
+
+    posargs_list = list(session.posargs)
+    if "--single-precision" in posargs_list:
+        session.env["FLOAT_PRECISION"] = "single"
+        posargs_list.remove("--single-precision")
     with session.chdir(f"model/{subpackage}"):
         session.run(
             *f"pytest -sv --benchmark-disable -n {os.environ.get('NUM_PROCESSES', 'auto')}".split(),
             *pytest_args,
-            *session.posargs,
+            *posargs_list,
             success_codes=[0, NO_TESTS_COLLECTED_EXIT_CODE],
         )
 
