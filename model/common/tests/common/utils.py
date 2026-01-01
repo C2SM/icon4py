0b8307b8031293ad33ae98d7a7e72bbbeedfcff9
Philip Mueller <philip.mueller@cscs.ch>
2026-01-01 14:13:33 +0100

    I am not fully like it, but for this it seems appropriate.

--- model/common/tests/common/utils.py
+++ model/common/tests/common/utils.py
@@ -6,9 +6,14 @@
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
 
+from typing import Any
 
 from icon4py.model.common.utils import data_allocation as data_alloc
 
 
-def dummy_exchange(*field: data_alloc.NDArray) -> None:
-    return None
+def dummy_exchange(*field: data_alloc.NDArray, **kwargs: Any) -> None:
+    # The real exchange function takes a `stream` argument, for the scheduled
+    #  exchange. We have to ignore it as we never do an exchange.
+    # TODO(phimuell): Is this the best way?
+    assert len(kwargs) <= 1
+    assert len(kwargs) == 1 and "stream" in kwargs
