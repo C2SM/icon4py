9642322837e8c3f686142379063bcdbbfa8c9e64
Jacopo <jacopo.canton@gmail.com>
2026-01-05 11:39:07 +0100

    one more commit

--- model/common/tests/common/metrics/unit_tests/test_metric_fields.py
+++ model/common/tests/common/metrics/unit_tests/test_metric_fields.py
@@ -18,6 +18,7 @@ from icon4py.model.common.grid import grid_refinement as refinement, horizontal
 from icon4py.model.common.metrics import metric_fields as mf
 from icon4py.model.common.utils import data_allocation as data_alloc
 from icon4py.model.testing import definitions, test_utils as testing_helpers
+from icon4py.model.testing.definitions import construct_metrics_config
 from icon4py.model.testing.fixtures.datatest import (
     backend,
     data_provider,
@@ -137,7 +138,7 @@ def test_compute_scaling_factor_for_3d_divdamp(
 @pytest.mark.datatest
 def test_compute_rayleigh_w(
     icon_grid: base_grid.Grid,
-    experiment: definitions.Experiments,
+    experiment: definitions.Experiment,
     metrics_savepoint: sb.MetricSavepoint,
     grid_savepoint: sb.IconGridSavepoint,
     backend: gtx_typing.Backend,
@@ -147,20 +148,16 @@ def test_compute_rayleigh_w(
     rayleigh_w_full = data_alloc.zero_field(
         icon_grid, dims.KDim, extend={dims.KDim: 1}, allocator=backend
     )
-    rayleigh_type = 2
-    match experiment:
-        case definitions.Experiments.EXCLAIM_APE:
-            rayleigh_coeff = 0.1
-            damping_height = 50000.0
-        case definitions.Experiments.GAUSS3D:
-            rayleigh_coeff = 0.1
-            damping_height = 45000.0
-        case definitions.Experiments.WEISMAN_KLEMP_TORUS:
-            rayleigh_coeff = 0.75
-            damping_height = 8000.0
-        case _:
-            rayleigh_coeff = 5.0
-            damping_height = 12500.0
+    (
+        _lowest_layer_thickness,
+        _model_top_height,
+        _stretch_factor,
+        damping_height,
+        rayleigh_coeff,
+        _exner_expol,
+        _vwind_offctr,
+        rayleigh_type,
+    ) = construct_metrics_config(experiment)
     mf.compute_rayleigh_w.with_backend(backend=backend)(
         rayleigh_w=rayleigh_w_full,
         vct_a=grid_savepoint.vct_a(),
