0b358d9b29f18607d64aedc20bffab7b3be1a21b
Mikael Simberg <mikael.simberg@iki.fi>
2026-01-21 13:39:08 +0100

    Revert "Use session scope for experiment fixture"
    This reverts commit b5afb76324ed63f6e489c1543c655c2e1c020e1f.

--- model/testing/src/icon4py/model/testing/fixtures/datatest.py
+++ model/testing/src/icon4py/model/testing/fixtures/datatest.py
@@ -88,7 +88,6 @@ def cpu_allocator() -> gtx_typing.FieldBufferAllocationUtil:
 
 
 @pytest.fixture(
-    scope="session",
     params=[
         definitions.Experiments.MCH_CH_R04B09,
         definitions.Experiments.EXCLAIM_APE,
