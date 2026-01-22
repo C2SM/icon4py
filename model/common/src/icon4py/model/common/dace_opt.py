3099e9f22f605273966e57cb9b8aeff98cb07c53
Edoardo Paone <edoardo.paone@cscs.ch>
2026-01-22 11:20:57 +0100

    add opt hook

---
+++ model/common/src/icon4py/model/common/dace_opt.py
@@ -0,0 +1,15 @@
+# ICON4Py - ICON inspired code in Python and GT4Py
+#
+# Copyright (c) 2022-2024, ETH Zurich and MeteoSwiss
+# All rights reserved.
+#
+# Please, refer to the LICENSE file in the root directory.
+# SPDX-License-Identifier: BSD-3-Clause
+
+import dace
+from dace.transformation import dataflow as dace_dataflow
+
+
+def graupel_run_top_level_dataflow_post(sdfg: dace.SDFG) -> None:
+    sdfg.apply_transformations_repeated(dace_dataflow.TaskletFusion, validate=False)
+    sdfg.save("graupel_run_top_level_dataflow_post.sdfg")
