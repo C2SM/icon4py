605584f0bbd0bede4f6a17734bc29ec946f7d407
Philip Müller <147368808+philip-paul-mueller@users.noreply.github.com>
2026-01-29 14:48:35 +0100

    Update To New Version Of Intre Map Optimization (#1019)
    In the previous version of the new TaskletFusion was on by default.
    However, this was changed to combat negative behaviour in the dycore,
    i.e. there is now a switch and it is disabled by default.
    This PR updates the feature to the newest version and also enables it
    for graupel by default.
    
    ---------
    
    Co-authored-by: Edoardo Paone <edoardo.paone@cscs.ch>

--- model/common/src/icon4py/model/common/model_options.py
+++ model/common/src/icon4py/model/common/model_options.py
@@ -56,6 +56,7 @@ def get_dace_options(
         backend_descriptor["use_zero_origin"] = True
         optimization_args["gpu_memory_pool"] = False
         optimization_args["make_persistent"] = True
+        optimization_args["fuse_tasklets"] = True
     if optimization_hooks:
         optimization_args["optimization_hooks"] = optimization_hooks
     if optimization_args:
