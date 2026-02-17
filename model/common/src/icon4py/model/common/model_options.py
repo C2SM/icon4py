0aa383040230fd7e34751c4056f1f66eeab3ec51
Ioannis Magkanaris <ioannis.magkanaris@cscs.ch>
2026-02-17 09:13:34 +0100

    Improvements in Graupel code (#1033)
    - Added an `if-statement` to make sure that we only enter the update
    functions only if there is some mask or previous level activated
    - Restructure `precip_qx_level_update` for better readability
    - Added `gpu_maxnreg = 128` option to the optimizer to improve the scan
    kernel performance
      - [x] Needs https://github.com/GridTools/gt4py/pull/2464
    
    ---------
    
    Co-authored-by: Edoardo Paone <edoardo.paone@cscs.ch>
    Co-authored-by: Hannes Vogt <hannes.vogt@cscs.ch>

--- model/common/src/icon4py/model/common/model_options.py
+++ model/common/src/icon4py/model/common/model_options.py
@@ -55,6 +55,7 @@ def get_dace_options(
     if program_name == "graupel_run":
         backend_descriptor["use_zero_origin"] = True
         optimization_args["fuse_tasklets"] = True
+        optimization_args["gpu_maxnreg"] = 128
         optimization_args["gpu_memory_pool"] = False
         optimization_args["make_persistent"] = True
     if optimization_hooks:
