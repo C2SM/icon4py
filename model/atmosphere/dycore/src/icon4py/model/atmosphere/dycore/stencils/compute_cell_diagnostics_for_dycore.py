494f6ac75f1604d178844225f05dcdb12731535b
OngChia <chiarui.ong@c2sm.ethz.ch>
2026-04-20 20:57:14 +0200

    remove unwanted ignore and fix bug

--- model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/stencils/compute_cell_diagnostics_for_dycore.py
+++ model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/stencils/compute_cell_diagnostics_for_dycore.py
@@ -45,9 +45,6 @@ from icon4py.model.common.math.derivative import _compute_first_vertical_derivat
 from icon4py.model.common.type_alias import vpfloat, wpfloat
 
 
-horzpres_discr_type: Final = HorizontalPressureDiscretizationType()
-
-
 @gtx.field_operator
 def _calculate_nonhydro_buoy_at_cells_on_half_levels(
     exner_w_explicit_weight_parameter: fa.CellField[ta.wpfloat],
@@ -102,7 +99,7 @@ def _compute_perturbed_quantities_and_interpolation(
             ),
             exner_at_cells_on_half_levels,
         )
-        if igradp_method == horzpres_discr_type.TAYLOR_HYDRO
+        if igradp_method == HorizontalPressureDiscretizationType.TAYLOR_HYDRO
         else exner_at_cells_on_half_levels
     )
 
@@ -178,7 +175,7 @@ def _surface_computations(
         _interpolate_to_surface(
             wgtfacq_c=wgtfacq_c, interpolant=temporal_extrapolation_of_perturbed_exner
         )
-        if igradp_method == horzpres_discr_type.TAYLOR_HYDRO
+        if igradp_method == HorizontalPressureDiscretizationType.TAYLOR_HYDRO
         else exner_at_cells_on_half_levels
     )
 
@@ -210,7 +207,7 @@ def _compute_first_and_second_vertical_derivative_of_exner(
             ),
             ddz_of_temporal_extrapolation_of_perturbed_exner_on_model_levels,
         )
-        if igradp_method == horzpres_discr_type.TAYLOR_HYDRO
+        if igradp_method == HorizontalPressureDiscretizationType.TAYLOR_HYDRO
         else ddz_of_temporal_extrapolation_of_perturbed_exner_on_model_levels
     )
 
@@ -228,7 +225,7 @@ def _compute_first_and_second_vertical_derivative_of_exner(
             ),
             d2dz2_of_temporal_extrapolation_of_perturbed_exner_on_model_levels,
         )
-        if igradp_method == horzpres_discr_type.TAYLOR_HYDRO
+        if igradp_method == HorizontalPressureDiscretizationType.TAYLOR_HYDRO
         else d2dz2_of_temporal_extrapolation_of_perturbed_exner_on_model_levels
     )
 
--- model/testing/src/icon4py/model/testing/definitions.py
+++ model/testing/src/icon4py/model/testing/definitions.py
@@ -334,13 +334,13 @@ def construct_nonhydrostatic_config(experiment: Experiment) -> solve_nh.NonHydro
 
     if experiment == Experiments.MCH_CH_R04B09:
         return solve_nh.NonHydrostaticConfig(
-            divdamp_order=dycore_states.DivergenceDampingOrder.COMBINED,  # type: ignore[arg-type] # TODO(havogt): typing in `NonHydrostaticConfig` needs to be fixed
+            divdamp_order=dycore_states.DivergenceDampingOrder.COMBINED,
             fourth_order_divdamp_factor=0.004,
             max_nudging_coefficient=0.375,
         )
     elif experiment == Experiments.EXCLAIM_APE:
         return solve_nh.NonHydrostaticConfig(
-            divdamp_order=dycore_states.DivergenceDampingOrder.COMBINED,  # type: ignore[arg-type] # TODO(havogt): typing in `NonHydrostaticConfig` needs to be fixed
+            divdamp_order=dycore_states.DivergenceDampingOrder.COMBINED,
         )
     elif experiment == Experiments.GAUSS3D:
         return solve_nh.NonHydrostaticConfig(
