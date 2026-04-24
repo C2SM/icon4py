6457e39df3d285ff84d3a2b0031de9b4238e53c7
Mikael Simberg <mikael.simberg@iki.fi>
2026-04-24 21:03:51 +0200

    Change cache dir

--- ci/scripts/gt4py-cache.sh
+++ ci/scripts/gt4py-cache.sh
@@ -10,11 +10,11 @@ set -euo pipefail
 # First clean up files and directories older than 7 days in the base cache
 # directory. The backend directories can stay, but the date-based directories
 # are removed. There may be concurrent cleanup, ignore failures.
-find "${GT4PY_BUILD_CACHE_BASE_DIR}" -mindepth 2 -maxdepth 2 -type d -mtime +7 -exec rm -rf {} + || true
+find "${GT4PY_BUILD_CACHE_BASE_DIR}/icon4py" -mindepth 2 -maxdepth 2 -type d -mtime +7 -exec rm -rf {} + || true
 
 # Then set the cache directory for this run based on the backend and current date.
 DATE=$(date +%Y-%W)
-export GT4PY_BUILD_CACHE_DIR="${GT4PY_BUILD_CACHE_BASE_DIR}/${BACKEND}/${DATE}"
+export GT4PY_BUILD_CACHE_DIR="${GT4PY_BUILD_CACHE_BASE_DIR}/icon4py/${BACKEND}/${DATE}"
 mkdir -p "${GT4PY_BUILD_CACHE_DIR}"
 
 echo "Using GT4PY_BUILD_CACHE_DIR=${GT4PY_BUILD_CACHE_DIR}"
