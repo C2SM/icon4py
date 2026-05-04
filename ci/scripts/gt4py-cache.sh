927ffb3d55c01f49f3f0a0f24367db7f98692aa0
Mikael Simberg <mikael.simberg@iki.fi>
2026-05-04 14:49:05 +0200

    Rename GT4PY_BUILD_CACHE_BASE_DIR

--- ci/scripts/gt4py-cache.sh
+++ ci/scripts/gt4py-cache.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Sets up a persistent gt4py cache directory based on backend and week to start
-# with a fresh cache every week. GT4PY_BUILD_CACHE_BASE_DIR is set as the root
-# and GT4PY_BUILD_CACHE_DIR is set to
-# ${GT4PY_BUILD_CACHE_BASE_DIR}/${BACKEND}/uv-lock-<hash of uv.lock>-${DATE}.
+# with a fresh cache every week. ICON4PY_CI_GT4PY_BUILD_CACHE_BASE_DIR is set as
+# the root and GT4PY_BUILD_CACHE_DIR is set to
+# ${ICON4PY_CI_GT4PY_BUILD_CACHE_BASE_DIR}/${BACKEND}/uv-lock-<hash of uv.lock>-${DATE}.
 
 set -euo pipefail
 
@@ -16,13 +16,13 @@ fi
 # First clean up files and directories older than 7 days in the base cache
 # directory. The backend directories can stay, but the date-based directories
 # are removed. There may be concurrent cleanup, ignore failures.
-find "${GT4PY_BUILD_CACHE_BASE_DIR}/icon4py" -mindepth 2 -maxdepth 2 -type d -mtime +7 -exec rm -rf {} + || true
+find "${ICON4PY_CI_GT4PY_BUILD_CACHE_BASE_DIR}/icon4py" -mindepth 2 -maxdepth 2 -type d -mtime +7 -exec rm -rf {} + || true
 
 uv_lock_hash=$(sha256sum "./uv.lock" | awk '{print $1}')
 
 # Then set the cache directory for this run based on the backend and current date.
 DATE=$(date +%Y-%W)
-export GT4PY_BUILD_CACHE_DIR="${GT4PY_BUILD_CACHE_BASE_DIR}/icon4py/${BACKEND}/uv-lock-${uv_lock_hash}-${DATE}"
+export GT4PY_BUILD_CACHE_DIR="${ICON4PY_CI_GT4PY_BUILD_CACHE_BASE_DIR}/icon4py/${BACKEND}/uv-lock-${uv_lock_hash}-${DATE}"
 mkdir -p "${GT4PY_BUILD_CACHE_DIR}"
 
 echo "Using GT4PY_BUILD_CACHE_DIR=${GT4PY_BUILD_CACHE_DIR}"
