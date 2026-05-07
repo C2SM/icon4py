283cd7d41b6470aeecd8d69ea2daf65162ef309d
Mikael Simberg <mikael.simberg@iki.fi>
2026-05-07 15:43:48 +0200

    Remove incorrectly restored SERIALIZED_DATA_ROOT_URLS

--- model/testing/src/icon4py/model/testing/definitions.py
+++ model/testing/src/icon4py/model/testing/definitions.py
@@ -157,14 +157,6 @@ class Grids:
     )
 
 
-# Root URLs for downloading serialized data by communicator size
-SERIALIZED_DATA_ROOT_URLS: Final = {
-    1: "https://polybox.ethz.ch/index.php/s/KBsngNwwzRcMG5J",
-    2: "https://polybox.ethz.ch/index.php/s/KfmJEEnaKCFgGSM",
-    4: "https://polybox.ethz.ch/index.php/s/eLLcDtxek858ayt",
-}
-
-
 @dataclasses.dataclass
 class Experiment:
     name: str
