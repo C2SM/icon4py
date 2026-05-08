6cd8b9f9646050c3d55ca05d53e162b3b2d262e2
Mikael Simberg <mikael.simberg@iki.fi>
2026-05-08 15:55:53 +0200

    Use readable job name slug

--- ci/scripts/gt4py-cache.sh
+++ ci/scripts/gt4py-cache.sh
@@ -3,7 +3,7 @@
 # Sets up a persistent gt4py cache directory based on backend and week to start
 # with a fresh cache every week. ICON4PY_CI_GT4PY_BUILD_CACHE_BASE_DIR is set as
 # the root and GT4PY_BUILD_CACHE_DIR is set to
-# ${ICON4PY_CI_GT4PY_BUILD_CACHE_BASE_DIR}/icon4py/${BACKEND}/uv-lock-<hash of uv.lock>-job-<hash of job name>-${DATE}.
+# ${ICON4PY_CI_GT4PY_BUILD_CACHE_BASE_DIR}/icon4py/${BACKEND}/uv-lock-<hash of uv.lock>-job-<job name>-${DATE}.
 
 set -euo pipefail
 
@@ -19,11 +19,11 @@ fi
 find "${ICON4PY_CI_GT4PY_BUILD_CACHE_BASE_DIR}/icon4py" -mindepth 2 -maxdepth 2 -type d -mtime +7 -exec rm -rf {} + || true
 
 uv_lock_hash=$(sha256sum "./uv.lock" | awk '{print $1}')
-job_name_hash=$(echo -n "${CI_JOB_NAME}" | sha256sum | awk '{print $1}')
+job_name="${CI_JOB_NAME_SLUG}"
 
 # Then set the cache directory for this run based on the backend and current date.
 DATE=$(date +%Y-%W)
-export GT4PY_BUILD_CACHE_DIR="${ICON4PY_CI_GT4PY_BUILD_CACHE_BASE_DIR}/icon4py/${BACKEND}/uv-lock-${uv_lock_hash}-job-${job_name_hash}-${DATE}"
+export GT4PY_BUILD_CACHE_DIR="${ICON4PY_CI_GT4PY_BUILD_CACHE_BASE_DIR}/icon4py/${BACKEND}/uv-lock-${uv_lock_hash}-job-${job_name}-${DATE}"
 mkdir -p "${GT4PY_BUILD_CACHE_DIR}"
 
 echo "Using GT4PY_BUILD_CACHE_DIR=${GT4PY_BUILD_CACHE_DIR}"
