C2SM/icon4py

commit 2f2a8e5e629a962105fe2fd2cdaafef824235db1

Remove FrozenNameSpace in graupel and dycore (#1201)
Remove FrozenNameSpace from icon-nwp graupel scheme and dycore after
GT4Py allows for direct use of `enum` for physics constants in a GT4Py
stencil.

model/common/src/icon4py/model/common/metrics/metric_fields.py
@@ -12,7 +12,6 @@ from __future__ import annotations
 
 from collections.abc import Callable
 from types import ModuleType
-from typing import Final
 
 import gt4py.next as gtx
 import numpy as np
@@ -31,7 +30,8 @@ from gt4py.next import (
 )
 from gt4py.next.experimental import concat_where
 
-from icon4py.model.common import constants, dimension as dims, field_type_aliases as fa
+from icon4py.model.common import dimension as dims, field_type_aliases as fa
+from icon4py.model.common.constants import RayleighType
 from icon4py.model.common.decomposition import definitions as decomposition
 from icon4py.model.common.dimension import C2E, C2E2C, C2E2CO, E2C, C2E2CODim, Koff
 from icon4py.model.common.interpolation.stencils.cell_2_edge_interpolation import (
@@ -47,9 +47,6 @@ from icon4py.model.common.math.helpers import (
 )
 from icon4py.model.common.type_alias import vpfloat, wpfloat
 from icon4py.model.common.utils import data_allocation as data_alloc
-
-
-rayleigh_damping_options: Final = constants.RayleighType()
 
 
 # TODO(nfarabullini): ddqz_z_half vertical dimension is khalf, use K2KHalf once merged for z_ifc and z_mc
@@ -221,13 +218,13 @@ def _compute_rayleigh_w(
     rayleigh_w = broadcast(0.0, (dims.KDim,))
     z_sin_diff = maximum(0.0, vct_a - damping_height)
     z_tanh_diff = vct_a_1 - vct_a  # vct_a(1) - vct_a
-    if rayleigh_type == rayleigh_damping_options.CLASSIC:
+    if rayleigh_type == RayleighType.CLASSIC:
         rayleigh_w = (
             rayleigh_coeff
             * (sin(pi_const / 2.0 * z_sin_diff / maximum(0.001, vct_a_1 - damping_height))) ** 2
         )
 
-    elif rayleigh_type == rayleigh_damping_options.KLEMP:
+    elif rayleigh_type == RayleighType.KLEMP:
         rayleigh_w = rayleigh_coeff * (
             1.0 - tanh(3.8 * z_tanh_diff / maximum(0.000001, vct_a_1 - damping_height))
         )

model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/dycore_states.py
@@ -14,7 +14,6 @@ import logging
 from typing import TYPE_CHECKING
 
 import gt4py.next as gtx
-from gt4py.eve.utils import FrozenNamespace
 
 from icon4py.model.common import (
     dimension as dims,
@@ -53,7 +52,7 @@ class DivergenceDampingType(enum.IntEnum):
     COMBINED = 32
 
 
-class DivergenceDampingOrder(FrozenNamespace[int]):
+class DivergenceDampingOrder(gtx.int32, enum.Enum):
     #: 2nd order divergence damping
     SECOND_ORDER = 2
     #: 4th order divergence damping
@@ -62,7 +61,7 @@ class DivergenceDampingOrder(FrozenNamespace[int]):
     COMBINED = 24
 
 
-class HorizontalPressureDiscretizationType(FrozenNamespace[int]):
+class HorizontalPressureDiscretizationType(gtx.int32, enum.Enum):
     """Parameter called igradp_method in ICON namelist."""
 
     #: conventional discretization with metric correction term
@@ -77,7 +76,7 @@ class HorizontalPressureDiscretizationType(FrozenNamespace[int]):
     POLYNOMIAL_HYDRO = 5
 
 
-class RhoThetaAdvectionType(FrozenNamespace[int]):
+class RhoThetaAdvectionType(gtx.int32, enum.Enum):
     """Parameter called iadv_rhotheta in ICON namelist."""
 
     #: simple 2nd order upwind-biased scheme

model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/stencils/add_temporal_tendencies_to_vn.py
@@ -5,16 +5,12 @@
 #
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
-from typing import Final
-
 import gt4py.next as gtx
 from gt4py.next import astype
 
-from icon4py.model.common import constants, dimension as dims, field_type_aliases as fa
+from icon4py.model.common import dimension as dims, field_type_aliases as fa
+from icon4py.model.common.constants import PhysicsConstants
 from icon4py.model.common.type_alias import vpfloat, wpfloat
-
-
-dycore_consts: Final = constants.PhysicsConstants()
 
 
 @gtx.field_operator
@@ -31,7 +27,7 @@ def _add_temporal_tendencies_to_vn(
 
     vn_nnew_wp = vn_nnow + dtime * (
         astype(ddt_vn_apc_ntl1, wpfloat)
-        - dycore_consts.cpd * z_theta_v_e * z_gradh_exner_wp
+        - PhysicsConstants.cpd * z_theta_v_e * z_gradh_exner_wp
         + astype(ddt_vn_phy, wpfloat)
     )
     return vn_nnew_wp

model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/stencils/compute_cell_diagnostics_for_dycore.py
@@ -19,7 +19,6 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from typing import Final
 
 import gt4py.next as gtx
 from gt4py.next import astype, broadcast, maximum
@@ -43,9 +42,6 @@ from icon4py.model.common.interpolation.stencils.interpolate_cell_field_to_half_
 )
 from icon4py.model.common.math.derivative import _compute_first_vertical_derivative_at_cells
 from icon4py.model.common.type_alias import vpfloat, wpfloat
-
-
-horzpres_discr_type: Final = HorizontalPressureDiscretizationType()
 
 
 @gtx.field_operator
@@ -102,7 +98,7 @@ def _compute_perturbed_quantities_and_interpolation(
             ),
             exner_at_cells_on_half_levels,
         )
-        if igradp_method == horzpres_discr_type.TAYLOR_HYDRO
+        if igradp_method == HorizontalPressureDiscretizationType.TAYLOR_HYDRO
         else exner_at_cells_on_half_levels
     )
 
@@ -178,7 +174,7 @@ def _surface_computations(
         _interpolate_to_surface(
             wgtfacq_c=wgtfacq_c, interpolant=temporal_extrapolation_of_perturbed_exner
         )
-        if igradp_method == horzpres_discr_type.TAYLOR_HYDRO
+        if igradp_method == HorizontalPressureDiscretizationType.TAYLOR_HYDRO
         else exner_at_cells_on_half_levels
     )
 
@@ -210,7 +206,7 @@ def _compute_first_and_second_vertical_derivative_of_exner(
             ),
             ddz_of_temporal_extrapolation_of_perturbed_exner_on_model_levels,
         )
-        if igradp_method == horzpres_discr_type.TAYLOR_HYDRO
+        if igradp_method == HorizontalPressureDiscretizationType.TAYLOR_HYDRO
         else ddz_of_temporal_extrapolation_of_perturbed_exner_on_model_levels
     )
 
@@ -228,7 +224,7 @@ def _compute_first_and_second_vertical_derivative_of_exner(
             ),
             d2dz2_of_temporal_extrapolation_of_perturbed_exner_on_model_levels,
         )
-        if igradp_method == horzpres_discr_type.TAYLOR_HYDRO
+        if igradp_method == HorizontalPressureDiscretizationType.TAYLOR_HYDRO
         else d2dz2_of_temporal_extrapolation_of_perturbed_exner_on_model_levels
     )
 

model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/stencils/compute_edge_diagnostics_for_dycore_and_update_vn.py
@@ -8,7 +8,6 @@
 
 
 import gt4py.next as gtx
-from gt4py.eve import utils as eve_utils
 from gt4py.next import broadcast, where
 from gt4py.next.experimental import concat_where
 
@@ -49,16 +48,9 @@ from icon4py.model.atmosphere.dycore.stencils.compute_horizontal_gradient_of_exn
 from icon4py.model.atmosphere.dycore.stencils.compute_vn_on_lateral_boundary import (
     _compute_vn_on_lateral_boundary,
 )
-from icon4py.model.common import (
-    constants,
-    dimension as dims,
-    field_type_aliases as fa,
-    type_alias as ta,
-)
+from icon4py.model.common import dimension as dims, field_type_aliases as fa, type_alias as ta
+from icon4py.model.common.constants import PhysicsConstants
 from icon4py.model.common.type_alias import wpfloat
-
-
-dycore_consts: eve_utils.FrozenNamespace[ta.wpfloat] = constants.PhysicsConstants()
 
 
 @gtx.field_operator
@@ -332,7 +324,7 @@ def _apply_divergence_damping_and_update_vn(
         dtime=dtime,
         wgt_nnow_vel=advection_explicit_weight_parameter,
         wgt_nnew_vel=advection_implicit_weight_parameter,
-        cpd=dycore_consts.cpd,
+        cpd=PhysicsConstants.cpd,
     )
 
     if apply_2nd_order_divergence_damping:
@@ -470,8 +462,6 @@ def compute_rho_theta_pgrad_and_update_vn(
         - is_iau_active: option for iau increment analysis
         - iau_wgt_dyn: a scaling factor for iau increment
         - limited_area: option indicating the grid is limited area or not
-        - iadv_rhotheta: advection type for air density and virtual potential temperature (see RhoThetaAdvectionType)
-        - igradp_method: option for pressure gradient computation (see HorizontalPressureDiscretizationType)
         - nflatlev: starting vertical index of flat levels
         - nflat_gradp: starting vertical index when neighboring cell centers lie within the thickness of the layer
         - start_edge_halo_level_2: start index of second halo level zone for edges

model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/stencils/compute_results_for_thermodynamic_variables.py
@@ -5,17 +5,14 @@
 #
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
-from typing import Final
 
 import gt4py.next as gtx
 from gt4py.next import astype
 
-from icon4py.model.common import constants, dimension as dims, field_type_aliases as fa
+from icon4py.model.common import dimension as dims, field_type_aliases as fa
+from icon4py.model.common.constants import PhysicsConstants
 from icon4py.model.common.dimension import Koff
 from icon4py.model.common.type_alias import vpfloat, wpfloat
-
-
-dycore_consts: Final = constants.PhysicsConstants()
 
 
 @gtx.field_operator
@@ -54,7 +51,7 @@ def _compute_results_for_thermodynamic_variables(
     theta_v_new_wp = (
         rho_now
         * theta_v_now
-        * ((exner_new_wp / exner_now - wpfloat("1.0")) * dycore_consts.cvd_o_rd + wpfloat("1.0"))
+        * ((exner_new_wp / exner_now - wpfloat("1.0")) * PhysicsConstants.cvd_o_rd + wpfloat("1.0"))
         / rho_new_wp
     )
     return rho_new_wp, exner_new_wp, theta_v_new_wp

model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/stencils/update_theta_and_exner_in_halo.py
@@ -5,16 +5,13 @@
 #
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
-from typing import Final
 
 import gt4py.next as gtx
 from gt4py.next import exp, log, where
 
-from icon4py.model.common import constants, dimension as dims, field_type_aliases as fa
+from icon4py.model.common import dimension as dims, field_type_aliases as fa
+from icon4py.model.common.constants import PhysicsConstants
 from icon4py.model.common.type_alias import wpfloat
-
-
-dycore_consts: Final = constants.PhysicsConstants()
 
 
 @gtx.field_operator
@@ -42,14 +39,14 @@ def _update_theta_and_exner_in_halo(
         mask_prog_halo_c,
         rho_now
         * theta_v_now
-        * ((exner_new / exner_now - wpfloat("1.0")) * dycore_consts.cvd_o_rd + wpfloat("1.0"))
+        * ((exner_new / exner_now - wpfloat("1.0")) * PhysicsConstants.cvd_o_rd + wpfloat("1.0"))
         / rho_new,
         exner_new,
     )
     exner_new = where(
         mask_prog_halo_c,
         exner_new,
-        exp(dycore_consts.rd_o_cvd * log(dycore_consts.rd_o_p0ref * rho_new * exner_new)),
+        exp(PhysicsConstants.rd_o_cvd * log(PhysicsConstants.rd_o_p0ref * rho_new * exner_new)),
     )
 
     return theta_v_new, exner_new

model/atmosphere/dycore/src/icon4py/model/atmosphere/dycore/stencils/vertically_implicit_dycore_solver.py
@@ -6,7 +6,6 @@
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
 
-from typing import Final
 
 import gt4py.next as gtx
 from gt4py.next import astype, broadcast
@@ -48,17 +47,9 @@ from icon4py.model.atmosphere.dycore.stencils.update_dynamical_exner_time_increm
 from icon4py.model.atmosphere.dycore.stencils.update_mass_volume_flux import (
     _update_mass_volume_flux,
 )
-from icon4py.model.common import (
-    constants,
-    dimension as dims,
-    field_type_aliases as fa,
-    type_alias as ta,
-)
+from icon4py.model.common import dimension as dims, field_type_aliases as fa, type_alias as ta
+from icon4py.model.common.constants import PhysicsConstants, RayleighType
 from icon4py.model.common.type_alias import vpfloat, wpfloat
-
-
-dycore_consts: Final = constants.PhysicsConstants()
-rayleigh_damping_options: Final = constants.RayleighType()
 
 
 @gtx.field_operator
@@ -108,7 +99,7 @@ def _compute_w_explicit_term_with_predictor_advective_tendency(
 
     w_explicit_term_wp = current_w + dtime * (
         predictor_vertical_wind_advective_tendency_wp
-        - dycore_consts.cpd * nonhydro_buoy_at_cells_on_half_levels_wp
+        - PhysicsConstants.cpd * nonhydro_buoy_at_cells_on_half_levels_wp
     )
     return w_explicit_term_wp
 
@@ -139,7 +130,7 @@ def _compute_w_explicit_term_with_interpolated_predictor_corrector_advective_ten
     w_explicit_term_wp = current_w + dtime * (
         advection_explicit_weight_parameter * predictor_vertical_wind_advective_tendency_wp
         + advection_implicit_weight_parameter * corrector_vertical_wind_advective_tendency_wp
-        - dycore_consts.cpd * nonhydro_buoy_at_cells_on_half_levels_wp
+        - PhysicsConstants.cpd * nonhydro_buoy_at_cells_on_half_levels_wp
     )
     return w_explicit_term_wp
 
@@ -159,9 +150,9 @@ def _compute_solver_coefficients_matrix(
 
     z_beta_wp = (
         dtime
-        * dycore_consts.rd
+        * PhysicsConstants.rd
         * current_exner
-        / (dycore_consts.cvd * current_rho * current_theta_v)
+        / (PhysicsConstants.cvd * current_rho * current_theta_v)
         * inv_ddqz_z_full_wp
     )
     z_alpha_wp = (
@@ -340,10 +331,10 @@ def _vertically_implicit_solver_at_predictor_step(
         z_w_expl=w_explicit_term,
         z_exner_expl=exner_explicit_term,
         dtime=dtime,
-        cpd=dycore_consts.cpd,
+        cpd=PhysicsConstants.cpd,
     )
 
-    if rayleigh_type == rayleigh_damping_options.KLEMP:
+    if rayleigh_type == RayleighType.KLEMP:
         next_w = concat_where(
             (dims.KDim > 0) & (dims.KDim < end_index_of_damping_layer + 1),
             _apply_rayleigh_damping_mechanism(
@@ -653,10 +644,10 @@ def _vertically_implicit_solver_at_corrector_step(
         z_w_expl=w_explicit_term,
         z_exner_expl=exner_explicit_term,
         dtime=dtime,
-        cpd=dycore_consts.cpd,
+        cpd=PhysicsConstants.cpd,
     )
 
-    if rayleigh_type == rayleigh_damping_options.KLEMP:
+    if rayleigh_type == RayleighType.KLEMP:
         next_w = concat_where(
             (dims.KDim > 0) & (dims.KDim < end_index_of_damping_layer + 1),
             _apply_rayleigh_damping_mechanism(

commit bbc07d9a1d82fbf3879e18af656ca133ef850570

Fix gtfn codegen for `_compute_rayleigh_w` by using int literals
The gtfn backend does not lower `IntEnum` members referenced inside a
`@gtx.field_operator` body — it emits the attribute path verbatim into
the generated C++, which fails to compile. Replace `RayleighType.CLASSIC`
and `RayleighType.KLEMP` with bare integer literals (kept in sync with
constants.RayleighType via a NOTE comment). Regression introduced by #1201.

Co-Authored-By: Claude Opus 4.7 <noreply@anthropic.com>

model/common/src/icon4py/model/common/metrics/metric_fields.py
@@ -30,7 +30,6 @@ from gt4py.next import (
 from gt4py.next.experimental import concat_where
 
 from icon4py.model.common import dimension as dims, field_type_aliases as fa
-from icon4py.model.common.constants import RayleighType
 from icon4py.model.common.decomposition import definitions as decomposition
 from icon4py.model.common.dimension import C2E, C2E2C, C2E2CO, E2C, C2E2CODim, Koff
 from icon4py.model.common.interpolation.stencils.cell_2_edge_interpolation import (
@@ -214,13 +213,17 @@ def _compute_rayleigh_w(
     rayleigh_w = broadcast(0.0, (dims.KDim,))
     z_sin_diff = maximum(0.0, vct_a - damping_height)
     z_tanh_diff = vct_a_1 - vct_a  # vct_a(1) - vct_a
-    if rayleigh_type == RayleighType.CLASSIC:
+    # NOTE: integer literals are used here instead of RayleighType.CLASSIC /
+    # RayleighType.KLEMP because gtfn codegen does not lower IntEnum members
+    # inside field_operator bodies (emits the attribute name verbatim into C++).
+    # The values must stay in sync with constants.RayleighType.
+    if rayleigh_type == 1:  # RayleighType.CLASSIC
         rayleigh_w = (
             rayleigh_coeff
             * (sin(pi_const / 2.0 * z_sin_diff / maximum(0.001, vct_a_1 - damping_height))) ** 2
         )
 
-    elif rayleigh_type == RayleighType.KLEMP:
+    elif rayleigh_type == 2:  # RayleighType.KLEMP
         rayleigh_w = rayleigh_coeff * (
             1.0 - tanh(3.8 * z_tanh_diff / maximum(0.000001, vct_a_1 - damping_height))
         )