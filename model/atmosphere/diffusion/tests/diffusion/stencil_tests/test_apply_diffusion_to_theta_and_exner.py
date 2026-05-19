8304217572ad89eec55b9a9c44196b4f80fcf8b9
Jacopo <jacopo.canton@gmail.com>
2026-05-19 21:40:14 +0200

    edit gtx.field_operator all diffusion single-operator files

--- model/atmosphere/diffusion/tests/diffusion/stencil_tests/test_apply_diffusion_to_theta_and_exner.py
+++ model/atmosphere/diffusion/tests/diffusion/stencil_tests/test_apply_diffusion_to_theta_and_exner.py
@@ -66,14 +66,14 @@ class TestApplyDiffusionToThetaAndExner(StencilTest):
             z_temp = np.where(
                 zd_diffcoef != 0.0,
                 truly_horizontal_diffusion_nabla_of_theta_over_steep_points_numpy(
-                    connectivities,
-                    zd_vertoffset,
-                    zd_diffcoef,
-                    geofac_n2s_c,
-                    geofac_n2s_nbh,
-                    vcoef,
-                    theta_v_in,
-                    z_temp,
+                    connectivities=connectivities,
+                    zd_vertoffset=zd_vertoffset,
+                    zd_diffcoef=zd_diffcoef,
+                    geofac_n2s_c=geofac_n2s_c,
+                    geofac_n2s_nbh=geofac_n2s_nbh,
+                    vcoef=vcoef,
+                    theta_v=theta_v_in,
+                    z_temp=z_temp,
                 ),
                 z_temp,
             )
