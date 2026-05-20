d85167ee3366b1f1031f9be186a16860c9933ce3
Jacopo <jacopo.canton@gmail.com>
2026-05-20 23:06:58 +0200

    unused imports

--- model/testing/src/icon4py/model/testing/definitions.py
+++ model/testing/src/icon4py/model/testing/definitions.py
@@ -13,7 +13,7 @@ import dataclasses
 import pathlib
 from typing import TYPE_CHECKING, Final
 
-from icon4py.model.common.grid import base as base_grid, icon as icon_grid, vertical as v_grid
+from icon4py.model.common.grid import icon as icon_grid, vertical as v_grid
 from icon4py.model.common.interpolation import interpolation_factory
 from icon4py.model.common.metrics import metrics_factory
 from icon4py.model.standalone_driver import config as driver_config
