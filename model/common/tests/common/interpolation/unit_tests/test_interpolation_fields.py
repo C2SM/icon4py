c14f0ebd41ada079e6e9edd1f53f561cc4a87fec
Jacopo <jacopo.canton@gmail.com>
2026-05-20 23:10:19 +0200

    something happened here

--- model/common/tests/common/interpolation/unit_tests/test_interpolation_fields.py
+++ model/common/tests/common/interpolation/unit_tests/test_interpolation_fields.py
@@ -379,7 +379,7 @@ def test_compute_e_bln_c_s(
 
     match icon_grid.grid_params.geometry_type:
         case icon.GeometryType.ICOSAHEDRON:
-            e_bln_c_s = compute_e_bln_c_s(c2e, cells_lat, cells_lon, edges_lat, edges_lon, 0.0)
+            e_bln_c_s = compute_e_bln_c_s(c2e, cells_lat, cells_lon, edges_lat, edges_lon)
         case icon.GeometryType.TORUS:
             e_bln_c_s = compute_e_bln_c_s_torus(c2e)
     assert test_helpers.dallclose(
