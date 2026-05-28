37db601e94a17103a51d74dfb2bd9ff333a8b391
Jacopo <jacopo.canton@gmail.com>
2026-05-28 10:38:18 +0200

    unused import

--- model/atmosphere/diffusion/tests/diffusion/stencil_tests/test_calculate_diagnostics_for_turbulence.py
+++ model/atmosphere/diffusion/tests/diffusion/stencil_tests/test_calculate_diagnostics_for_turbulence.py
@@ -6,8 +6,6 @@
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
 
-from typing import Any
-
 import gt4py.next as gtx
 import numpy as np
 import pytest
