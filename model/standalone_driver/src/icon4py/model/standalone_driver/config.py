80487bfcf061580acc12b13940310bbfd5c18fcb
Jacopo <jacopo.canton@gmail.com>
2026-06-16 17:58:07 +0200

    missing names

--- model/standalone_driver/src/icon4py/model/standalone_driver/config.py
+++ model/standalone_driver/src/icon4py/model/standalone_driver/config.py
@@ -83,10 +83,10 @@ class DriverConfig:
             .removeprefix("NAMELIST_")
             .removesuffix("_sb_atm"),
             dtime=datetime.timedelta(seconds=dtime),
-            start_datetime=datetime.datetime.fromisoformat(
+            start_of_simulation=datetime.datetime.fromisoformat(
                 start_datetime_str.replace("Z", "+00:00")
             ),
-            end_simulation=datetime.datetime.fromisoformat(end_datetime_str.replace("Z", "+00:00")),
+            end_of_simulation=datetime.datetime.fromisoformat(end_datetime_str.replace("Z", "+00:00")),
             apply_extra_second_order_divdamp=nonhydrostatic_nml["lextra_diffu"],
             vertical_cfl_threshold=ta.wpfloat(str(nonhydrostatic_nml["vcfl_threshold"])),
             ndyn_substeps=nonhydrostatic_nml["ndyn_substeps"],
