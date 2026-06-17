b8e22b4722dcd9d1acf6e1b124ada7b836f08d71
Mikael Simberg <mikael.simberg@iki.fi>
2026-06-17 17:05:31 +0200

    Quote all args in ci-mpi-wrapper.sh

--- ci/scripts/ci-mpi-wrapper.sh
+++ ci/scripts/ci-mpi-wrapper.sh
@@ -30,7 +30,7 @@ fi
 
 if [[ "${subcomm_rank}" -eq 0 ]]; then
     echo "Starting pytest on rank ${rank}, logging to stdout and ${log_file}"
-    $@ |& tee "${log_file}"
+    "$@" |& tee "${log_file}"
 else
     echo "Starting job on rank ${rank}, logging to ${log_file}"
     "$@" >& "${log_file}"
