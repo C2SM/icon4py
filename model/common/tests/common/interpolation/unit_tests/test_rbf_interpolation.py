fb34d2720157a8aff29ad869db121d84bf74299f
Mikael Simberg <mikael.simberg@iki.fi>
2026-07-11 19:38:11 +0200

    Enable mypy for interpolation/rbf_interpolation.py

--- model/common/tests/common/interpolation/unit_tests/test_rbf_interpolation.py
+++ model/common/tests/common/interpolation/unit_tests/test_rbf_interpolation.py
@@ -180,7 +180,7 @@ def test_rbf_interpolation_coeffs_cell(
         else pytest.fail("geometry_type cannot be None")
     )
 
-    rbf_vec_coeff_c1, rbf_vec_coeff_c2 = rbf.compute_rbf_interpolation_coeffs_cell(  # type: ignore[misc] # function returns two vars
+    rbf_vec_coeff_c1, rbf_vec_coeff_c2 = rbf.compute_rbf_interpolation_coeffs_cell(
         cell_center_lat=geometry.get(geometry_attrs.CELL_LAT).ndarray,
         cell_center_lon=geometry.get(geometry_attrs.CELL_LON).ndarray,
         cell_center_x=geometry.get(geometry_attrs.CELL_CENTER_X).ndarray,
