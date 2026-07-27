d763a337c5d30078fa0612b54faa2dabb817c829
yiluchen1066 <yilu.chen@c2sm.ethz.ch>
2026-07-27 15:10:44 +0200

    small clean up and added physics_driver to the workflow matrix

--- model/atmosphere/subgrid_scale_physics/muphys/tests/muphys/integration_tests/test_component_datatest.py
+++ model/atmosphere/subgrid_scale_physics/muphys/tests/muphys/integration_tests/test_component_datatest.py
@@ -51,14 +51,13 @@ def test_granule_matches_direct_muphys(
     experiment: utils.MuphysExperiment,
 ) -> None:
     allocator = model_backends.get_allocator(backend_like)
-    inp = common.GraupelInput.load(filename=experiment.input_file, allocator=allocator)
-    # TODO (Yilu): rename inp
+    graupel_input = common.GraupelInput.load(filename=experiment.input_file, allocator=allocator)
 
-    te0 = inp.t.asnumpy().copy()
-    q0 = {s: getattr(inp, f"q{s}").asnumpy().copy() for s in SPECIES}
+    te0 = graupel_input.t.asnumpy().copy()
+    q0 = {s: getattr(graupel_input, f"q{s}").asnumpy().copy() for s in SPECIES}
 
     muphys_program = run_full_muphys.setup_muphys(
-        inp=inp,
+        inp=graupel_input,
         dt=experiment.dt,
         qnc=experiment.qnc,
         backend=backend_like,
@@ -66,36 +65,36 @@ def test_granule_matches_direct_muphys(
     )
 
     granule = MuphysComponent(
-        grid=_FullDomainGrid(inp.ncells, inp.nlev),  # type: ignore[arg-type]  # mini data has no icon grid
+        grid=_FullDomainGrid(graupel_input.ncells, graupel_input.nlev),  # type: ignore[arg-type]  # mini data has no icon grid
         dtime=datetime.timedelta(seconds=experiment.dt),
         qnc=experiment.qnc,
         backend=backend_like,
         step=muphys_program,
     )
     state = {
-        "dz": inp.dz,
-        "te": inp.t,
-        "p": inp.p,
-        "rho": inp.rho,
-        "qv": inp.qv,
-        "qc": inp.qc,
-        "qr": inp.qr,
-        "qs": inp.qs,
-        "qi": inp.qi,
-        "qg": inp.qg,
+        "dz": graupel_input.dz,
+        "te": graupel_input.t,
+        "p": graupel_input.p,
+        "rho": graupel_input.rho,
+        "qv": graupel_input.qv,
+        "qc": graupel_input.qc,
+        "qr": graupel_input.qr,
+        "qs": graupel_input.qs,
+        "qi": graupel_input.qi,
+        "qg": graupel_input.qg,
     }
     out = granule(state, _T0)
 
     direct = common.GraupelOutput.allocate(
         allocator=allocator,
-        domain=gtx.domain({dims.CellDim: inp.ncells, dims.KDim: inp.nlev}),
+        domain=gtx.domain({dims.CellDim: graupel_input.ncells, dims.KDim: graupel_input.nlev}),
     )
     muphys_program(
-        dz=inp.dz,
-        te=inp.t,
-        p=inp.p,
-        rho=inp.rho,
-        q_in=inp.q,
+        dz=graupel_input.dz,
+        te=graupel_input.t,
+        p=graupel_input.p,
+        rho=graupel_input.rho,
+        q_in=graupel_input.q,
         t_out=direct.t,
         q_out=direct.q,
         pflx=direct.pflx,
