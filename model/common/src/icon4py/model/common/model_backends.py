66f190dc03df867d27897c786948cb6b80afd66b
Edoardo Paone <edoardo.paone@cscs.ch>
2026-08-03 15:39:41 +0200

    edit

--- model/common/src/icon4py/model/common/model_backends.py
+++ model/common/src/icon4py/model/common/model_backends.py
@@ -113,7 +113,7 @@ def make_custom_dace_backend(
     Returns:
         A dace backend with custom configuration for the target device.
     """
-    if use_external_workspace and device is not None:
+    if use_external_workspace and device in (gtx.DeviceType.CUDA, gtx.DeviceType.ROCM):
         external_workspace = dace_workspace.ICON_WORKSPACE_ALLOCATOR.allocate(device)
         if optimization_args is None:
             optimization_args = {
