76f32f8a0b0032a2cb3743ce65c4bb0c62fac7a4
Mikael Simberg <mikael.simberg@iki.fi>
2026-09-10 10:30:45 +0200

    Bust gt4py cache

--- .cscs-ci/scripts/gt4py-cache.sh
+++ .cscs-ci/scripts/gt4py-cache.sh
@@ -23,7 +23,9 @@ flags_hash=$(echo -n "CXXFLAGS=${CXXFLAGS:-} NVCC_APPEND_FLAGS=${NVCC_APPEND_FLA
 
 # Then set the cache directory for this run based on the backend and current date.
 DATE=$(date +%Y-%W)
-export GT4PY_BUILD_CACHE_DIR="${ICON4PY_CI_GT4PY_BUILD_CACHE_BASE_DIR}/icon4py/gt4py-cache/base-${base_image_hash}-uv-lock-${uv_lock_hash}-flags-${flags_hash}-job-${job_name_hash}-${DATE}"
+# TODO: remove before merging
+CACHE_BUST=$(date +%s)
+export GT4PY_BUILD_CACHE_DIR="${ICON4PY_CI_GT4PY_BUILD_CACHE_BASE_DIR}/icon4py/gt4py-cache/base-${base_image_hash}-uv-lock-${uv_lock_hash}-flags-${flags_hash}-job-${job_name_hash}-${DATE}-${CACHE_BUST}"
 mkdir -p "${GT4PY_BUILD_CACHE_DIR}"
 
 echo "Using GT4PY_BUILD_CACHE_DIR=${GT4PY_BUILD_CACHE_DIR}"
