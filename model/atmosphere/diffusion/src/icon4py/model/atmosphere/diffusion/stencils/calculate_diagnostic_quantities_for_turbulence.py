9bb153c06db7ee8ed1389cd536b68ea4fa02a09c
Edoardo Paone <edoardo.paone@cscs.ch>
2026-09-17 14:34:56 +0200

    inline _temporary_fields_for_turbulence_diagnostics and _calculate_diagnostics_for_turbulence

--- model/atmosphere/diffusion/src/icon4py/model/atmosphere/diffusion/stencils/calculate_diagnostic_quantities_for_turbulence.py
+++ model/atmosphere/diffusion/src/icon4py/model/atmosphere/diffusion/stencils/calculate_diagnostic_quantities_for_turbulence.py
@@ -14,28 +14,20 @@ from icon4py.model.common.type_alias import vpfloat, wpfloat
 
 
 @gtx.field_operator
-def _temporary_fields_for_turbulence_diagnostics(
+def _calculate_diagnostic_quantities_for_turbulence(
     kh_smag_ec: fa.EdgeKField[vpfloat],
     vn: fa.EdgeKField[wpfloat],
     e_bln_c_s: gtx.Field[gtx.Dims[dims.CellDim, dims.C2EDim], wpfloat],
     geofac_div: gtx.Field[gtx.Dims[dims.CellDim, dims.C2EDim], wpfloat],
     diff_multfac_smag: gtx.Field[gtx.Dims[dims.KDim], vpfloat],
-) -> tuple[fa.CellKField[vpfloat], fa.CellKField[vpfloat]]:
-    kh_smag_ec_wp, diff_multfac_smag_wp = astype((kh_smag_ec, diff_multfac_smag), wpfloat)
-
-    kh_c_wp = neighbor_sum(kh_smag_ec_wp(C2E) * e_bln_c_s, axis=dims.C2EDim) / diff_multfac_smag_wp
-    div_wp = neighbor_sum(vn(C2E) * geofac_div, axis=dims.C2EDim)
-    return astype((kh_c_wp, div_wp), vpfloat)
-
-
-@gtx.field_operator
-def _calculate_diagnostics_for_turbulence(
-    div: fa.CellKField[vpfloat],
-    kh_c: fa.CellKField[vpfloat],
     wgtfac_c: fa.CellKHalfField[vpfloat],
 ) -> tuple[fa.CellKHalfField[vpfloat], fa.CellKHalfField[vpfloat]]:
+    kh_smag_ec_wp, diff_multfac_smag_wp = astype((kh_smag_ec, diff_multfac_smag), wpfloat)
+    kh_c_wp = neighbor_sum(kh_smag_ec_wp(C2E) * e_bln_c_s, axis=dims.C2EDim) / diff_multfac_smag_wp
+    div_wp = neighbor_sum(vn(C2E) * geofac_div, axis=dims.C2EDim)
+    kh_c, div = astype((kh_c_wp, div_wp), vpfloat)
+
     wgtfac_c_wp = astype(wgtfac_c, wpfloat)
-    div_wp, kh_c_wp = astype((div, kh_c), wpfloat)
 
     div_ic_wp = astype(wgtfac_c * div(dims.KHalfDim + 0.5), wpfloat) + (
         wpfloat("1.0") - wgtfac_c_wp
@@ -46,19 +38,3 @@ def _calculate_diagnostics_for_turbulence(
     hdef_ic_wp = hdef_ic_wp * hdef_ic_wp
 
     return astype((div_ic_wp, hdef_ic_wp), vpfloat)
-
-
-@gtx.field_operator
-def _calculate_diagnostic_quantities_for_turbulence(
-    kh_smag_ec: fa.EdgeKField[vpfloat],
-    vn: fa.EdgeKField[wpfloat],
-    e_bln_c_s: gtx.Field[gtx.Dims[dims.CellDim, dims.C2EDim], wpfloat],
-    geofac_div: gtx.Field[gtx.Dims[dims.CellDim, dims.C2EDim], wpfloat],
-    diff_multfac_smag: gtx.Field[gtx.Dims[dims.KDim], vpfloat],
-    wgtfac_c: fa.CellKHalfField[vpfloat],
-) -> tuple[fa.CellKHalfField[vpfloat], fa.CellKHalfField[vpfloat]]:
-    kh_c, div = _temporary_fields_for_turbulence_diagnostics(
-        kh_smag_ec, vn, e_bln_c_s, geofac_div, diff_multfac_smag
-    )
-    div_ic_vp, hdef_ic_vp = _calculate_diagnostics_for_turbulence(div, kh_c, wgtfac_c)
-    return div_ic_vp, hdef_ic_vp
