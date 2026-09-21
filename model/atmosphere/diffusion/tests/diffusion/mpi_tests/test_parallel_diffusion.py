0e8951699ace3cd29419745d4471f33ff6774d37
Hannes Vogt <vogt@hey.com>
2026-09-21 15:29:56 +0200

    Restore the embedded marker on the parallel diffusion test, which cannot be run without multi-rank data

--- model/atmosphere/diffusion/tests/diffusion/mpi_tests/test_parallel_diffusion.py
+++ model/atmosphere/diffusion/tests/diffusion/mpi_tests/test_parallel_diffusion.py
@@ -27,6 +27,7 @@ _log = logging.getLogger(__file__)
 
 @pytest.mark.datatest
 @pytest.mark.mpi
+@pytest.mark.uses_concat_where
 @pytest.mark.parametrize(
     "experiment_description, step_date_init, step_date_exit",
     [
